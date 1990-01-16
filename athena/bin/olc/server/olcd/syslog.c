d5001716332bf4aed112a3ffb31f31b9259753a4
Kenneth G Raeburn <raeburn@mit.edu>
1990-01-16 04:41:01 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2586 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/bin/olc/server/olcd/syslog.c
@@ -0,0 +1,210 @@
+/*
+ * This file is part of the OLC On-Line Consulting system.
+ * It contains the routines used for recording OLC daemon log
+ * messages.
+ *
+ * Copyright (C) 1990 by the Massachusetts Institute of Technology.
+ * $Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/syslog.c,v $
+ * $Id: syslog.c,v 1.1 1990-01-16 04:41:01 raeburn Exp $
+ */
+
+/*
+ * If we're compiling with TEST, we want to use the stderr log, so
+ * punt syslog for now.
+ */
+#ifdef TEST
+#undef SYSLOG
+#endif
+
+#include <stdio.h>
+#ifdef SYSLOG
+#include <syslog.h>
+#endif
+#include <errno.h>
+#include <com_err.h>
+#include <olc/olc.h>
+#include "olcd.h"
+
+static const char rcsid[] =
+    "$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/syslog.c,v 1.1 1990-01-16 04:41:01 raeburn Exp $";
+
+#ifndef SYSLOG
+
+static FILE *status_log = (FILE *)NULL;
+static FILE *error_log = (FILE *)NULL;
+static FILE *admin_log = (FILE *) NULL;
+
+/*
+ * Function:	open_log
+ * Purpose:	Opens a log file.  If the file cannot be opened, an
+ *		attempt to send off a message to this effect is made,
+ *		and exit is called.
+ * Arguments:	const char *filename: Name of the file to be opened.
+ * Returns:	FILE *: The opened file.
+ * Visibility:	static
+ * Notes:
+ */
+
+static FILE *
+#if __STDC__
+open_log (const char *filename)
+#else
+open_log (filename) char *filename;
+#endif
+{
+    char msgbuf[BUFSIZ];
+    FILE *file;
+
+#ifdef TEST
+
+    return stderr;
+
+#else
+    
+    file = fopen (filename, "a");
+    if (file)
+	return file;
+    /* We lose.  */
+    sprintf (msgbuf, "OLCD fatal error: unable to open log file %s: %s",
+	     filename, error_message (errno));
+    fprintf (stderr,
+#if __STDC__
+	     "\a%s\a\n",
+#else
+	     "%s\n",
+#endif
+	     msgbuf);
+    fflush (stderr);
+    olc_broadcast_message ("syserror", msgbuf, "system");
+    exit (1);
+
+#endif /* TEST */
+}
+
+static void
+#if __STDC__
+log_to_file (FILE *file, const char *text)
+#else
+log_to_file (file, text) FILE *file; char *text;
+#endif
+{
+    char time_buf[32];
+    time_now (time_buf);
+    fprintf (file, "%s ", time_buf);
+    write_line_to_log (file, text);
+    (void) fflush (file);
+}
+
+#else /* use syslog */
+
+static void
+#if __STDC__
+log_to_syslogd (int level, const char *text)
+#else
+log_to_syslogd (level, text) char *text;
+#endif
+{
+    static int initialized = 0;
+    if (!initialized)
+	openlog ("olc", LOG_CONS, SYSLOG_LEVEL);
+    syslog (level, text);
+}
+
+#endif /* ! SYSLOG */
+    
+/*
+ * Function:	log_error() writes an error message into the daemon's error
+ *			log file.
+ * Arguments:	message:	Error message to be written.
+ * Returns:	nothing
+ * Notes:
+ *	First, open the error log file, printing an error message and dying 
+ *	if an error occurs here.  Then, get the system time and print a
+ *	formatted version in the error log, followed by the error message.  
+ *	Finally, close the file and return.
+ */
+
+void
+#if __STDC__
+log_error(const char *message)
+#else
+log_error (message) char *message;
+#endif
+{
+  char time_buf[32];
+  char *time_string = &time_buf[0];
+
+
+#ifdef SYSLOG
+
+  log_to_syslogd (LOG_ERR, message);
+
+#else
+
+  if (error_log == (FILE *)NULL) 
+      error_log = open_log (ERROR_LOG);
+
+  log_to_file (error_log, message);
+  olc_broadcast_message("syserror",message, "system");
+#endif
+
+}
+
+/*
+ * Function:	log_status() writes a message to the olcd status log.
+ * Arguments:	message:	Message to be written.
+ * Returns:	nothing
+ * Notes:
+ *	First, open the status log file, recording an error and exiting 
+ *	if one occurs.  Then, write the formatted current time and the
+ *	status message into the status log.  Finally, close the log file
+ *	and return.
+ */
+
+void
+#if __STDC__
+log_status(const char *message)
+#else
+log_status(message) char *message;
+#endif
+{
+  char time_buf[32];
+
+#ifdef SYSLOG
+
+  syslog(LOG_INFO,message);
+
+#else
+
+  if (!status_log)
+      status_log = open_log (STATUS_LOG);
+
+  log_to_file (status_log, message);
+
+#endif
+}
+
+
+
+void
+#if __STDC__
+log_admin(char *message)
+#else
+log_admin(message) char *message;
+#endif
+{
+  char time_buf[32];
+
+#ifdef SYSLOG
+
+  syslog(LOG_NOTICE, message);
+
+#else
+
+  if (admin_log == (FILE *) NULL)
+      admin_log = open_log (ADMIN_LOG);
+
+  log_to_file (admin_log, message);
+
+#endif
+}
