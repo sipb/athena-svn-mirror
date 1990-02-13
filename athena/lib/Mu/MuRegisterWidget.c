82208b1763b967c68b5bcaae6d7143b4b2909022
David J Flanagan <djf@mit.edu>
1990-02-13 14:21:08 +0000

    cd ../vax
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2683 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/Mu/MuRegisterWidget.c
+++ athena/lib/Mu/MuRegisterWidget.c
@@ -18,6 +18,9 @@
  * $Source: /afs/dev.mit.edu/source/repository/athena/lib/Mu/MuRegisterWidget.c,v $
  * $Author: djf $
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  89/12/09  15:14:44  djf
+ * Initial revision
+ * 
  *
  * This file contains the functions MuRegisterWidgetCallback (which is
  * called from uil as MuRegisterWidget -- see register.c) and MuGetWidget.
@@ -243,7 +246,7 @@ char *name;
     i = hash_store(h, quark, w);
   
     if (i == 1) {/* if a widget was already stored by that name, warn */
-	sprintf(err_buf,
+	(void)sprintf(err_buf,
 "MuRegisterName: reuse of name %s.  Previously registered value overwritten",
 		name);
 	XtWarning(err_buf);
