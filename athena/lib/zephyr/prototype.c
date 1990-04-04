3622d7d11de65c4c3ee0a944f521b95d9e2dc23c
Craig Fields <cfields@mit.edu>
1990-04-04 19:14:22 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2790 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/lib/zephyr/prototype.c
@@ -0,0 +1,20 @@
+You should replace all of the <> bracketed text before you can compile
+this file.
+/* This file is part of the Project Athena Zephyr Notification System.
+ * It contains functions for <frobbing nakrabs>.
+ *
+ *	Created by:	<Joe Random Hacker>
+ *
+ *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/prototype.c,v $
+ *	$Author: cfields $
+ *
+ *	Copyright (c) 1990 by the Massachusetts Institute of Technology.
+ *	For copying and distribution information, see the file
+ *	"mit-copyright.h". 
+ */
+
+#include <zephyr/mit-copyright.h>
+
+#ifndef lint
+static char rcsid_<foo_c>[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/prototype.c,v 1.1 1990-04-04 19:14:22 cfields Exp $";
+#endif lint
