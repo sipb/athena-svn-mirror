d4ebbef2f1a82ccfd25f9f4b309ff275972e5438
Chris `vertigo' VanHaren <vanharen@mit.edu>
1990-04-25 16:41:32 +0000

    fixed parser so that it won't core dump on "i -help"
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2903 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/parser/p_instance.c
+++ athena/bin/olc/clients/parser/p_instance.c
@@ -16,11 +16,11 @@
  *      Copyright (c) 1989 by the Massachusetts Institute of Technology
  *
  *      $Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_instance.c,v $
- *      $Author: tjcoppet $
+ *      $Author: vanharen $
  */
 
 #ifndef lint
-static char rcsid[]= "$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_instance.c,v 1.5 1989-11-17 14:07:28 tjcoppet Exp $";
+static char rcsid[]= "$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_instance.c,v 1.6 1990-04-25 16:41:32 vanharen Exp $";
 #endif
 
 #include <olc/olc.h>
@@ -36,8 +36,10 @@ do_olc_instance(arguments)
   if(fill_request(&Request) != SUCCESS)
     return(ERROR);
 
-  if(*++arguments != NULL)
+  while (*arguments != (char *) NULL)
     {
+      arguments++;
+
       if(string_equiv(*arguments,"-instance", max(strlen(*arguments),2)) ||
 	 string_equiv(*arguments,"-change", max(strlen(*arguments),2)))
 	{
@@ -46,24 +48,20 @@ do_olc_instance(arguments)
 	    instance = atoi(*arguments);
 	  else
 	    instance = -2;
+	  continue;
 	}
-      else
-	{
-	  arguments = handle_argument(arguments, &Request, &status);
-	  if(status)
-	    return(ERROR);
 
-	  if(arguments == (char **) NULL)
-	    {
-	      printf("The argument \"%s\" is invalid.\n",*arguments);
-	      printf("Usage is: \tinstance [-instance <n>] [-change]\n");
-	      printf("\t\t[<username> <instance id>]\n");
-	    }
+      arguments = handle_argument(arguments, &Request, &status);
+      if(status)
+	return(ERROR);
+
+      if(arguments == (char **) NULL)
+	{
+	  printf("Usage is: \tinstance [-instance <n>] [-change]\n");
+	  printf("\t\t[<username> <instance id>]\n");
 	  return(ERROR);
 	}
     }
-  else
-    instance = -1;
 
   return(t_instance(&Request,instance));
 }
