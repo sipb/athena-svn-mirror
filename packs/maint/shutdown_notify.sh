d69e7e7477b79cfd06d71e1f149175416547b9f1
Richard Basch <probe@mit.edu>
1990-05-01 18:09:21 +0000

    Run faster... use "exec" and use /bin/sh, not /bin/csh
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2971 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/shutdown_notify.sh
+++ packs/maint/shutdown_notify.sh
@@ -1,9 +1,9 @@
-#!/bin/csh -f
+#!/bin/sh
 # Script to notify people that a server is going down.
 # This is run by shutdown
 #
 #       $Source: /afs/dev.mit.edu/source/repository/packs/maint/shutdown_notify.sh,v $
 #       $Author: probe $
-#       $Header: /afs/dev.mit.edu/source/repository/packs/maint/shutdown_notify.sh,v 1.1 1990-05-01 18:08:13 probe Exp $
+#       $Header: /afs/dev.mit.edu/source/repository/packs/maint/shutdown_notify.sh,v 1.2 1990-05-01 18:09:21 probe Exp $
 
-/etc/athena/zshutdown_notify
+exec /etc/athena/zshutdown_notify
