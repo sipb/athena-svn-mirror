3af7157492d508edbd3ab9c410972e06898ab63a
Richard Basch <probe@mit.edu>
1990-05-01 18:08:13 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@2970 728af825-273c-0410-89f9-f7d3b574a069

---
+++ packs/maint/shutdown_notify.sh
@@ -0,0 +1,9 @@
+#!/bin/csh -f
+# Script to notify people that a server is going down.
+# This is run by shutdown
+#
+#       $Source: /afs/dev.mit.edu/source/repository/packs/maint/shutdown_notify.sh,v $
+#       $Author: probe $
+#       $Header: /afs/dev.mit.edu/source/repository/packs/maint/shutdown_notify.sh,v 1.1 1990-05-01 18:08:13 probe Exp $
+
+/etc/athena/zshutdown_notify
