b12273e9f5cb141a9cedb694fb8887c546f80f4d
Tom Coppeto <tom@mit.edu>
1990-05-26 13:36:31 +0000

    instances are now set to device name in ascii and a variable to query mount point has been added
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3028 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/snmp/server/src/disk_grp.c
+++ athena/etc/snmp/server/src/disk_grp.c
@@ -15,6 +15,9 @@
  *    $Author: tom $
  *    $Locker:  $
  *    $Log: not supported by cvs2svn $
+ * Revision 1.3  90/04/26  17:06:14  tom
+ * added rcsid
+ * 
  * Revision 1.2  90/04/26  16:22:18  tom
  * cleaned up some code
  * 
@@ -22,7 +25,7 @@
  */
 
 #ifndef lint
-static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/disk_grp.c,v 1.3 1990-04-26 17:06:14 tom Exp $";
+static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/disk_grp.c,v 1.4 1990-05-26 13:36:31 tom Exp $";
 #endif
 
 #include "include.h"
@@ -133,6 +136,8 @@ lu_ndparts(varnode, repl, instptr, reqflg)
 /*
  * Function:    lu_disk()
  * Description: Top level callback. Suports the following:
+ *                 DKPATH:  (STR) mounted on pathname
+ *                 DKDNAME: (STR) block device pathname
  *                 PTUSED:  (INT) used disk in bytes
  *                 PTAVAIL: (INT) available disk in bytes
  *                 PTFREE:  (INT) free disk in bytes
@@ -156,23 +161,29 @@ lu_disk(varnode, repl, instptr, reqflg)
   struct stat statbuf;
   struct mntent *mnt;
   FILE *mtabp;
-  int pnum = 0; 
   int cnt = 0;
+  int len;
+  char *ch;
   struct thing df;     
   struct thing di;
+  char disk[SNMPMXSID];
+  int nflag = 0;
 
-  if (varnode->flags & NOT_AVAIL ||
-      varnode->offset <= 0 ||     /* not expecting offset here */
-      ((varnode->flags & NULL_OBJINST) && (reqflg == NXT)))
+  if ((varnode->flags & NOT_AVAIL) || (varnode->offset <= 0))
     return (BUILD_ERR);
 
   if((instptr == (objident *) NULL) || (instptr->ncmp == 0))
-    pnum = 1;
+    disk[0] = '\0';
   else
-    pnum = instptr->cmp[0];
-
-  if((reqflg == NXT) && (instptr != (objident *) NULL) && (instptr->ncmp !=0))
-     pnum++;
+    {
+      cnt = 0;
+      while((cnt < instptr->ncmp) && (cnt < SNMPMXSID))
+	{
+	  disk[cnt] = instptr->cmp[cnt];
+	  ++cnt;
+	}
+      disk[cnt] = '\0';
+    }
 
   /*
    * open mtab and get the partition associated with given instance 
@@ -188,9 +199,30 @@ lu_disk(varnode, repl, instptr, reqflg)
     {
       if(strcmp(mnt->mnt_type, MNTTYPE_42) != 0)
 	continue;
-      cnt++;
-      if(cnt == pnum)
+      /*
+       * the first one
+       */
+      if(*disk == '\0')
 	break;
+      /*
+       * the next one
+       */
+      if(nflag)
+	break;
+      /*
+       * the one we want
+       */
+      if(strcmp(mnt->mnt_fsname, disk) == 0)
+	{
+	  if((reqflg == NXT) && (instptr != (objident *) NULL) &&
+	     (instptr->ncmp !=0))
+	    {
+	      nflag = 1;
+	      continue;
+	    }
+	  else
+	    break;
+	}
     }
 
   endmntent(mtabp);
@@ -199,17 +231,18 @@ lu_disk(varnode, repl, instptr, reqflg)
    * instance too high?
    */
 
-  if(cnt != pnum)
-    return(BUILD_ERR);
-
+  
+  if(mnt == (struct mntent *) NULL)
+    {
+      repl->name.ncmp++;
+      return(BUILD_ERR);
+    }
   
   /*
    * Build reply
    */
 
   bcopy ((char *)varnode->var_code, (char *) &repl->name, sizeof(repl->name));
-  repl->name.cmp[repl->name.ncmp] = pnum;
-  repl->name.ncmp++;                
   repl->val.type = INT; 
 
 
@@ -223,13 +256,45 @@ lu_disk(varnode, repl, instptr, reqflg)
     dfreedev(mnt->mnt_fsname, &df, &di);
   else
     dfreemnt(mnt->mnt_dir, &df, &di);
-	
+
+   /*
+    *  fill in object instance with block device path
+    */
+  
+  cnt = 0;
+  ch = &(mnt->mnt_fsname[0]);
+  len = strlen(mnt->mnt_fsname);
+  if(len > SNMPMXSID)
+    len = SNMPMXSID;
+
+  while (cnt < len)
+    {
+      repl->name.cmp[repl->name.ncmp] = *ch & 0xff;
+      repl->name.ncmp++;
+      cnt++;
+      ch++;
+    }
+
   /*
    * return values
    */
 
   switch(varnode->offset)
     {
+    case N_DKPATH:
+      repl->val.type = STR;
+      repl->val.value.str.len = strlen(mnt->mnt_dir);
+      repl->val.value.str.str = (char *) malloc(strlen(mnt->mnt_dir)+1);
+      strcpy(repl->val.value.str.str, mnt->mnt_dir);
+      return(BUILD_SUCCESS);
+
+    case N_DKDNAME:
+      repl->val.type = STR;
+      repl->val.value.str.len = strlen(mnt->mnt_fsname);
+      repl->val.value.str.str = (char *) malloc(strlen(mnt->mnt_fsname)+1);
+      strcpy(repl->val.value.str.str, mnt->mnt_fsname);
+      return(BUILD_SUCCESS);
+
     case N_PTUSED:
       repl->val.value.intgr = df.used;
       return(BUILD_SUCCESS);
