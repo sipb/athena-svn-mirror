6f042901fc1932bbeb182c6be1b2ba29a4b04f5c
Ezra Peisach <epeisach@mit.edu>
1990-07-10 20:44:08 +0000

    Group quota returns More info in config.h
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3192 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/lpr/quota/quota.h
+++ athena/bin/lpr/quota/quota.h
@@ -1,7 +1,7 @@
 /*
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/lpr/quota/quota.h,v $
  *	$Author: epeisach $
- *	$Header: /afs/dev.mit.edu/source/repository/athena/bin/lpr/quota/quota.h,v 1.2 1990-04-25 11:48:27 epeisach Exp $
+ *	$Header: /afs/dev.mit.edu/source/repository/athena/bin/lpr/quota/quota.h,v 1.3 1990-07-10 20:44:08 epeisach Exp $
  */
 
 /*
@@ -10,24 +10,12 @@
  */
 
 #include "mit-copyright.h"
+#include "config.h"
 
 #include <stdio.h>
 #include <errno.h>
 #include <syslog.h>
 #include <strings.h>
-
-#define DEFACLFILE	"/usr/spool/quota/acl"
-#define DEFSACLFILE	"/usr/spool/quota/sacl"
-#define DBM_DEF_FILE	"/usr/spool/quota/quota_db"
-#define DEFREPORTFILE	"/usr/spool/quota/report"
-#define DEFCAPFILE      "/usr/spool/quota/quotacap"
-#define LOGTRANSFILE		"/usr/spool/quota/logtrans"
-#define DEFQUOTA        500
-#define DEFCURRENCY     "cents"
-#define DEFSERVICE	"athena"
-
-#define QUOTASERVENT	3701	
-#define QUOTASERVENTNAME "lpallow"
 
 #define UDPPROTOCOL	1
 #define RETRY_COUNT	2
@@ -40,6 +28,7 @@ extern short MA;    /* Mutual Authentication   */
 extern int   DQ;    /* Default quota */
 extern char *AF;    /* Acl File */
 extern char *QF;    /* Master quota file */
+extern char *GF;    /* Group quota file */
 extern char *RF;    /* Report file for logger to grok thru */
 extern char *QC;    /* Quota currency */
 
@@ -48,13 +37,14 @@ extern char *bp;
 
 char *malloc();
 
-#define KLPQUOTA_SERVICE "rcmd"
-
 #define TRUE 1
 #define FALSE 0
 
-#define ALLOWEDTOPRINT 0
-#define NOALLOWEDTOPRINT 1
-#define UNKNOWNUSER 2
+#define ALLOWEDTOPRINT     0
+#define NOALLOWEDTOPRINT   1
+#define UNKNOWNUSER        2
+#define UNKNOWNGROUP       3
+#define USERNOTINGROUP     4
+#define USERDELETED        5
+#define GROUPDELETED       6
 
-void PROTECT(), UNPROTECT(), CHECK_PROTECT();
