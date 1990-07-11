4494039efa03ac46d689938de6138e1321cf5bee
Richard Basch <probe@mit.edu>
1990-07-11 14:50:12 +0000

    changes.71#248 PMAX port
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3213 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/gms/get_servername.c
+++ athena/bin/gms/get_servername.c
@@ -1,7 +1,7 @@
 /* This file is part of the Project Athena Global Message System.
  * Created by: Mark W. Eichin <eichin@athena.mit.edu>
  * $Source: /afs/dev.mit.edu/source/repository/athena/bin/gms/get_servername.c,v $
- * $Author: eichin $
+ * $Author: probe $
  *
  *	Copyright (c) 1988 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
@@ -9,16 +9,17 @@
  */
 #include <mit-copyright.h>
 #ifndef lint
-static char rcsid_get_servername_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/gms/get_servername.c,v 1.1 1988-09-26 15:39:47 eichin Exp $";
+static char rcsid_get_servername_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/gms/get_servername.c,v 1.2 1990-07-11 14:50:12 probe Exp $";
 #endif lint
 
 #include "globalmessage.h"
 #include <hesiod.h>
+#include "hesiod_err.h"
 
 Code_t get_servername(ret_name)
      char ***ret_name;		/* pointer to array of string... */
 {
-  char **retval, **data;	/* for copying out the hesiod data */
+  char **retval, **_data;	/* for copying out the hesiod data */
   int datacnt, i;		/* number of hesiod records returned */
 
   /* Guard against NULL arguments */
@@ -27,25 +28,29 @@ Code_t get_servername(ret_name)
   }
   
   /* Fetch the list of servers from Hesiod. */
-  data = hes_resolve(GMS_NAME_CLASS, GMS_NAME_TYPE);
+  _data = hes_resolve(GMS_NAME_CLASS, GMS_NAME_TYPE);
   
-  if(!data) {
+  if(!_data) {
     /* deal with hesiod error */
     return(hesiod_error());
   }
 
   /* Copy the Hesiod data into stable space. */
-  for(datacnt=0; data[datacnt++]; ); /* count the data */
+  for(datacnt=0; _data[datacnt]; datacnt++); /* count the data */
 
+  if (!datacnt) {
+      /* an answer, but no contents! */
+      return(HESIOD_ER_INVALID);	/* XXX */
+  }
   retval = (char **)malloc(datacnt * sizeof(char *));
   for(i=0; i<datacnt; i++) {
-    retval[i] = malloc(strlen(data[i])+1);
+    retval[i] = malloc(strlen(_data[i])+1);
     if(!retval[i]) {
       /* malloc failed... */
       for(;--i;free(retval[i]));
       return(GMS_MALLOC_ERR);
     }
-    strcpy(retval[i], data[i]);
+    strcpy(retval[i], _data[i]);
   }
   
   /* Clean up and return normally. */
