218903fab325ed7cba2c4bcdb9835d0403affa3a
Emanuel 'Jay' Berkenbilt <qjb@mit.edu>
1990-07-24 14:46:32 +0000

    Added get_user_realm
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3359 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/aklog/aklog.h
+++ athena/bin/aklog/aklog.h
@@ -1,5 +1,5 @@
 /* 
- * $Id: aklog.h,v 1.3 1990-06-27 13:05:02 qjb Exp $
+ * $Id: aklog.h,v 1.4 1990-07-24 14:46:32 qjb Exp $
  * $Source: /afs/dev.mit.edu/source/repository/athena/bin/aklog/aklog.h,v $
  * $Author: qjb $
  *
@@ -9,7 +9,7 @@
 #define __AKLOG_H__
 
 #if !defined(lint) && !defined(SABER)
-static char *rcsid_aklog_h = "$Id: aklog.h,v 1.3 1990-06-27 13:05:02 qjb Exp $";
+static char *rcsid_aklog_h = "$Id: aklog.h,v 1.4 1990-07-24 14:46:32 qjb Exp $";
 #endif /* lint || SABER */
 
 #include <krb.h>
@@ -26,6 +26,7 @@ typedef struct {
     int (*isdir)ARGS((char *, unsigned char *));
     char *(*getwd)ARGS((char *));
     int (*get_cred)ARGS((char *, char *, char *, CREDENTIALS *));
+    int (*get_user_realm)ARGS((char *));
     void (*pstderr)ARGS((char *));
     void (*pstdout)ARGS((char *));
     void (*exitprog)ARGS((char));
