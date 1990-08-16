c9d2388425a3005796d9c03efae2814f406321de
Theodore Y. Ts'o <tytso@mit.edu>
1990-08-16 21:29:26 +0000

    Checked in changes made by Ken (Audited by Ted) which fix brokenness in how error codes are returned by SendKerberosData() and used by bdump_send() and bdump_get().  Kerberos codes are now returned as Kerberos error plus kerberos error base, instead of just the Kerberos error number.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3375 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/server/kstuff.c
+++ athena/lib/zephyr/server/kstuff.c
@@ -10,14 +10,15 @@
  */
 /*
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/kstuff.c,v $
- *	$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/kstuff.c,v 1.8 1989-03-23 09:29:08 jtkohl Exp $
+ *	$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/kstuff.c,v 1.9 1990-08-16 21:29:26 tytso Exp $
  */
 
 #ifndef lint
-static char rcsid_kstuff_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/kstuff.c,v 1.8 1989-03-23 09:29:08 jtkohl Exp $";
+static char rcsid_kstuff_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/kstuff.c,v 1.9 1990-08-16 21:29:26 tytso Exp $";
 #endif lint
 
 #include "zserver.h"
+#include <zephyr/krb_err.h>
 
 #include <ctype.h>
 #include <netdb.h>
@@ -85,7 +86,7 @@ GetKerberosData(fd, haddr, kdata, service, srvtab)
  * SendKerberosData
  * 
  * create and transmit a ticket over the file descriptor for service.host
- * return Kerberos failure codes if appropriate, or KSUCCESS if we
+ * return failure codes if appropriate, or 0 if we
  * get the ticket and write it to the file descriptor
  */
 
@@ -103,25 +104,25 @@ char *service, *host;			/* service name, foreign host */
 
     rem = krb_get_lrealm(krb_realm,1);
     if (rem != KSUCCESS)
-      return(rem);
+	return rem + ERROR_TABLE_BASE_krb;
 
     rem = krb_mk_req( ticket, service, host, krb_realm, (u_long)0 );
     if (rem != KSUCCESS)
-      return(rem);
+	return rem + ERROR_TABLE_BASE_krb;
 
     (void) sprintf(p,"%d ",ticket->length);
     if ((written = write(fd, p, strlen(p))) != strlen(p))
 	    if (written < 0)
-		    return(written);
+		    return errno;
 	    else
 		    return(ZSRV_PKSHORT);
     if ((written = write(fd, (caddr_t) (ticket->dat), ticket->length)) != ticket->length)
 	    if (written < 0)
-		    return(written);
+		    return errno;
 	    else
 		    return(ZSRV_PKSHORT);
-	    
-    return(rem);
+
+    return 0;
 }
 
 static char tkt_file[] = ZEPHYR_TKFILE;
