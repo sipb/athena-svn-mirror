efa6b48ec89b167891223787f8cb9d20528f964d
Ezra Peisach <epeisach@mit.edu>
1990-11-08 15:36:28 +0000

    Working version - still doesn't check against the userdb
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3579 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/lpr/quota/consist.c
+++ athena/bin/lpr/quota/consist.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "logger.h"
+#include <sys/types.h>
 
-log_header head;
+#define ISSET(n) FD_ISSET(n, bitmap)
+#define SET(n)	FD_SET(n, bitmap)
+#define CLR(n)	FD_CLR(n, bitmap)
+fd_set *bitmap; 
+int bitmap_size;
 
 main(argc, argv) 
 int argc;
@@ -10,19 +15,19 @@ char *argv[];
     int i;
     log_entity *l;
     char buf[1024];
-    char *name, *string;
-
+    char *name;
+    log_header head;
+    int error=0;
+    
     if (argc < 2) {
 	name = "/usr/spool/quota/journal.db";
-	string = "/usr/spool/quota/string.db";
     }
     else {
-	if (argc != 3) {
-	    fprintf(stderr, "Usage: consist [journal_db  string_db]\n");
+	if (argc != 2) {
+	    fprintf(stderr, "Usage: consist [journal_db]\n");
 	    exit(1);
 	}
 	name = argv[1];
-	string = argv[2];
     }
 
     if(logger_journal_set_name(name)) {
@@ -35,26 +40,93 @@ char *argv[];
 	exit(1);
     }
 
+#ifdef DEBUG
     printf("ver %d, num_ent %d, last_q_time %d, pos %d\n", 
 	   head.version, head.num_ent, head.last_q_time, head.quota_pos);
-
-    if(logger_string_set_name(string) ||
-       logger_read_strings()==0){
-	printf("Could not read string database\n");
+#endif
+    if(head.version != LOGGER_VERSION) {
+	printf("Version mismatch on logs database: %d != %d\n", 
+	       head.version, LOGGER_VERSION);
 	exit(1);
     }
 
+
+    /* Cheat and create the bitmap that we want */
+    bitmap_size = sizeof(fd_set) + howmany(head.num_ent, NFDBITS) * sizeof(long);
+    bitmap = (fd_set *) malloc(bitmap_size);
+    bzero(bitmap, bitmap_size);
+
+    
     for(i=1; i < (int)head.num_ent; i++) {
-	l = logger_journal_get_line(i);
-	if(logger_cvt_line(l, buf)) {
-	    fprintf(stderr, "line %d: printing bad %d\n", i,l->func);
-	    /* exit(1); */
+	if(ISSET(i)) continue; /* Skip entries already found */
+#ifdef DEBUG
+	printf("Checking %d\n", i);
+#endif
+	if(follow_chain(i)) {
+	    fprintf(stderr, "Chain starting at %d failed\n", i);
+	    exit(1);
 	}
-	printf("%s\n",buf);
     }
+
+    for(i=1; i < (int)head.num_ent; i++) {
+	if(ISSET(i)) continue;
+	fprintf(stderr, "Entry %d not in chains\n");
+	error = -1;
+    }
+
+    /* All good */
+    exit(error);
+
 }
 
 
 void PROTECT(){}   
 void UNPROTECT(){}   
 	
+
+
+int follow_chain(start)
+Pointer start;
+{
+
+    log_entity *lent;
+    Pointer n = start, prev=0;
+
+    /* Start off the progression */
+    if(!(lent = logger_journal_get_line(n))) {
+	fprintf(stderr, "Could not read line %d\n", n);
+	return -1;
+    }
+
+    if(lent->prev != 0) {
+	fprintf(stderr,"Not starting at head of chain %d\n", start); 
+    }
+
+    prev = n;
+    SET(n);
+    while(lent->next != 0) {
+	prev = n;
+	n = lent->next;
+
+	if(ISSET(n)) {
+	    fprintf(stderr, "Already referenced - failure %d\n", n);
+	    return -1;
+	}
+
+	if(!(lent = logger_journal_get_line(n))) {
+	    fprintf(stderr, "Could not read line %d\n", n);
+	    return -1;
+	}
+	if(lent->prev != prev) {
+	    fprintf(stderr, "prev not match in line %d %d !=% d\n", n,
+		    prev, lent->prev);
+	    return -1;
+	}
+	
+	SET(n);
+    }
+
+    return 0;
+}
+
+
