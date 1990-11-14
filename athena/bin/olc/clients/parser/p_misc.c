307366062b560830b23e208f1c197cfa794c2238
Lucien Van Elsen <lwvanels@mit.edu>
1990-11-14 12:32:48 +0000

    added saber ifdef got rid of do olc_happy, do_olc_home, and do_olc_cheese
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3628 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/parser/p_misc.c
+++ athena/bin/olc/clients/parser/p_misc.c
@@ -12,18 +12,21 @@
  *
  *      Tom Coppeto
  *	Chris VanHaren
+ *	Lucien Van Elsen
  *      MIT Project Athena
  *
  * Copyright (C) 1989,1990 by the Massachusetts Institute of Technology.
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v $
- *	$Id: p_misc.c,v 1.5 1990-07-16 08:21:27 lwvanels Exp $
+ *	$Id: p_misc.c,v 1.6 1990-11-14 12:32:48 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v 1.5 1990-07-16 08:21:27 lwvanels Exp $";
+#ifndef SABER
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v 1.6 1990-11-14 12:32:48 lwvanels Exp $";
+#endif
 #endif
 
 #include <mit-copyright.h>
@@ -56,7 +59,6 @@ do_olc_load_user(arguments)
 
   return(t_load_user(&Request));
 }
-
 
 do_olc_dump(arguments)
      char **arguments;
@@ -125,10 +127,6 @@ do_olc_dump(arguments)
   return(status);
 }
 
-
-
-
-
 do_olc_dbinfo(arguments)
      char **arguments;
 {
@@ -190,115 +188,4 @@ do_olc_dbinfo(arguments)
   if(!save_file)
     (void) unlink(file);
   return(status);
-}
-
-
-
-
-do_olc_happy(arguments)
-     char **arguments;
-{
-  REQUEST Request;
-  char *argv0;
-  int status;
-
-  argv0 = *arguments;
-
-  if(fill_request(&Request) != SUCCESS)
-    return(ERROR);
-  
-  for (arguments++; *arguments != (char *) NULL; arguments++)
-    {
-      arguments = handle_argument(arguments, &Request, &status);
-      if(status)
-	return(ERROR);
-       
-      if(arguments == (char **) NULL)   /* error */
-        {
-          printf("Usage is: \t%s\n",argv0);
-          return(ERROR);
-        }
-
-      if(*arguments == (char *) NULL)   /* end of list */
-        break;
-    }
-
-  printf("%s.\n",happy_message());
-  return(SUCCESS);
-}
-
-do_olc_home(arguments)
-     char **arguments;
-{
-  REQUEST Request;
-  int status;
-
-  if(fill_request(&Request) != SUCCESS)
-    return(ERROR);
-  
-  for (arguments++; *arguments != (char *) NULL; arguments++)
-    {
-      arguments = handle_argument(arguments, &Request, &status);
-      if(status)
-	return(ERROR);
-       
-      if(arguments == (char **) NULL)   /* error */
-        {
-          printf("Usage is: \thome [<username>]\n");
-          return(ERROR);
-        }
-
-      if(*arguments == (char *) NULL)   /* end of list */
-        break;
-    }
-
-
-  if(string_eq(Request.target.username,"carla"))
-    printf("227-9517\n");
-  else
-    if(string_eq(Request.target.username,"vanharen"))
-      printf("248-0946\n");
-    else
-      if(string_eq(Request.target.username,"tjcoppet"))
-	printf("248-0946\n");
-      else
-	if(string_eq(Request.target.username,"srz"))
-	  printf("577-1685\n");
-	else
-	  if(string_eq(Request.target.username,"jon"))
-	    printf("577-1685\n");
-	  else
-	    if(string_eq(Request.target.username,"hoffmann"))
-	      printf("484-2098\n");
-	    else
-	      printf("No home information for %s.\n",Request.target.username);
-
-  return(SUCCESS);
-}
-
-static int swiss = 0;
-
-do_olc_cheese(arguments)
-     char **arguments;
-{
-  swiss ++;
-  switch(random() % swiss)
-    {
-    case 0:
-      printf("You are standing alone.\n");
-      break;
-    case 1: 
-      printf("The window is slightly ajar. You are standing alone.\n");
-      break;
-    case 2:
-      printf("The door slammed shut. There is a lantern on the wall and a magazine\n");
-      printf("on the table. Suddenly you hear laughter from upstairs and the lights\n");
-      printf("go out. The wind is making eerie noises through the slightly ajar window\n");
-      printf("You are standing alone.\n");
-      break;
-    case 3:
-      break;
-    }
-  if(swiss > 10)
-    swiss = 0;
 }
