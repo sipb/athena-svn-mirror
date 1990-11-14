0c644718263e8fd5b725df9d202d9c8b9572cfef
Lucien Van Elsen <lwvanels@mit.edu>
1990-11-14 15:00:07 +0000

    removed extra argument to display_file
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3644 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/tty/t_topic.c
+++ athena/bin/olc/clients/tty/t_topic.c
@@ -12,18 +12,21 @@
  *
  *      Tom Coppeto
  *	Chris VanHaren
+ *	Lucien Van Elsen
  *      MIT Project Athena
  *
  * Copyright (C) 1989,1990 by the Massachusetts Institute of Technology.
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_topic.c,v $
- *	$Id: t_topic.c,v 1.8 1990-07-16 08:10:15 lwvanels Exp $
+ *	$Id: t_topic.c,v 1.9 1990-11-14 15:00:07 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_topic.c,v 1.8 1990-07-16 08:10:15 lwvanels Exp $";
+#ifndef SABER
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_topic.c,v 1.9 1990-11-14 15:00:07 lwvanels Exp $";
+#endif
 #endif
 
 #include <mit-copyright.h>
@@ -128,7 +131,7 @@ t_list_topics(Request, file, display)
     {
     case SUCCESS:
       if(display)
-	display_file(file,TRUE);
+	display_file(file);
       break;
 
     case ERROR:
