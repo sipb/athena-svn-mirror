a8ab5318aaa45e981c4fbe599f45dc96b3ef49cc
Lucien Van Elsen <lwvanels@mit.edu>
1990-11-14 14:59:05 +0000

    removed extra argument from display_file
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3642 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/tty/t_misc.c
+++ athena/bin/olc/clients/tty/t_misc.c
@@ -12,18 +12,21 @@
  *
  *      Tom Coppeto
  *	Chris VanHaren
+ *	Lucien Van Elsen
  *      MIT Project Athena
  *
  * Copyright (C) 1990 by the Massachusetts Institute of Technology.
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_misc.c,v $
- *	$Id: t_misc.c,v 1.4 1990-07-16 08:09:13 lwvanels Exp $
+ *	$Id: t_misc.c,v 1.5 1990-11-14 14:59:05 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_misc.c,v 1.4 1990-07-16 08:09:13 lwvanels Exp $";
+#ifndef SABER
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_misc.c,v 1.5 1990-11-14 14:59:05 lwvanels Exp $";
+#endif
 #endif
 
 #include <mit-copyright.h>
@@ -40,7 +43,7 @@ t_dump(Request,type,file)
 
   status = ODump(Request,type,file);
   if(status == SUCCESS)
-    display_file(file,TRUE);
+    display_file(file);
   else
     status = handle_response(status,Request);
 
