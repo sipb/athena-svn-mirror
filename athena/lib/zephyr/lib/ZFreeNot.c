6dfc5634383f341bebb1877828e467339e89431d
Kenneth G Raeburn <raeburn@mit.edu>
1990-11-30 08:44:07 +0000

    Always return 0.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3879 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZFreeNot.c
+++ athena/lib/zephyr/lib/ZFreeNot.c
@@ -4,16 +4,16 @@
  *	Created by:	Robert French
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFreeNot.c,v $
- *	$Author: jtkohl $
+ *	$Author: raeburn $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFreeNot.c,v 1.2 1988-06-15 20:46:13 jtkohl Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFreeNot.c,v 1.3 1990-11-30 08:44:07 raeburn Exp $ */
 
 #ifndef lint
-static char rcsid_ZFreeNotice_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFreeNot.c,v 1.2 1988-06-15 20:46:13 jtkohl Exp $";
+static char rcsid_ZFreeNotice_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFreeNot.c,v 1.3 1990-11-30 08:44:07 raeburn Exp $";
 #endif lint
 
 #include <zephyr/mit-copyright.h>
@@ -24,4 +24,5 @@ Code_t ZFreeNotice(notice)
     ZNotice_t *notice;
 {
     free(notice->z_packet);
+    return 0;
 }
