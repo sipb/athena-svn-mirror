14edbc9007f2182d30bba197916201d6f32d71b5
Lucien Van Elsen <lwvanels@mit.edu>
1990-12-02 23:09:33 +0000

    Changed to use syslog Forks & disassociates itself from controlling terminal
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3910 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/server/rpd/rpd.c
+++ athena/bin/olc/server/rpd/rpd.c
@@ -6,7 +6,7 @@
 
 #ifndef lint
 #ifndef SABER
-static char *RCSid = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/rpd/rpd.c,v 1.4 1990-11-27 14:09:44 lwvanels Exp $";
+static char *RCSid = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/rpd/rpd.c,v 1.5 1990-12-02 23:09:33 lwvanels Exp $";
 #endif
 #endif
 
@@ -17,7 +17,7 @@ int fd;		/* the accepting socket */
 
 main()
 {
-  int len;
+  int len,max_fd;
   struct sockaddr_in sin,from;
   struct servent *sent;
   int onoff;
@@ -27,16 +27,52 @@ main()
   signal(SIGTERM,clean_up);
   signal(SIGPIPE,SIG_IGN);
 
+  openlog("rpd",LOG_CONS | LOG_PID,SYSLOG_FACILITY);
+
+  /* Fork off */
+  switch (fork()) {
+  case 0:		/* Child */
+    break;
+  case 1:
+    syslog(LOG_ERR,"Can't fork: %m");
+    exit(1);
+  default:
+    exit(0);		/* Parent */
+  }
+
+  max_fd = getdtablesize();
+
+  for(fd = 0;fd<max_fd;fd++)
+    close(fd);
+  fd = open("/",O_RDONLY,S_IREAD);
+  if (fd < 0) {
+    syslog(LOG_ERR,"Can't open /: %m");
+    exit(1);
+  }
+  if (fd != 0)
+    dup2 (fd, 0);
+  if (fd != 1)
+    dup2 (fd, 1);
+  if (fd != 2)
+    dup2 (fd, 2);
+  if (fd > 2)
+    close (fd);
+
+  fd = open("/dev/tty",O_RDWR,0);
+  if (fd>=0) {
+    ioctl(fd, TIOCNOTTY, (char *) NULL);
+    close(fd);
+  }
 
   init_cache();
 
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    perror("replayerd: socket");
+    syslog(LOG_ERR,"socket: %m");
     exit(1);
   }
 
   if ((sent = getservbyname("ols","tcp")) == NULL) {
-    fprintf(stderr,"replayerd: ols/tcp unknown service\n");
+    syslog(LOG_ERR,"ols/tcp unknown service");
     exit(1);
   }
 
@@ -46,19 +82,21 @@ main()
   sin.sin_port = sent->s_port;
 
   if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &onoff, sizeof(int)) < 0) {
-    perror("replayerd: setsockopt");
+    syslog(LOG_ERR,"setsockopt: %m");
     exit(1);
   }
 
   if (bind(sock, (struct sockaddr *)&sin, sizeof(sin)) == -1) {
-    perror("replayerd: bind");
+    syslog(LOG_ERR,"bind: %m");
     exit(1);
   }
 
   if (listen(sock,SOMAXCONN) == -1) {
-    perror("replayerd: listen");
+    syslog(LOG_ERR,"bind: %m");
     exit(1);
   }
+
+  syslog(LOG_INFO,"Ready to accept connections..");
 
   /* Main loop... */
 
@@ -66,10 +104,10 @@ main()
     len = sizeof(from);
     if ((fd = accept(sock,(struct sockaddr *) &from, &len)) == -1) {
       if (errno == EINTR) {
-	fprintf(stderr,"Interrupted by signal in accept; continuing\n");
+	syslog(LOG_WARNING,"Interrupted by signal in accept; continuing");
 	continue;
       }
-      perror("replayerd: accept");
+      syslog(LOG_ERR,"accept: %m");
       exit(1);
     }
     handle_request(fd,from);
@@ -84,6 +122,6 @@ clean_up(signal)
   close(fd);
   shutdown(sock,2);
   close(sock);
-  psignal(signal,"rpd shutting down: ");
+  syslog(LOG_NOTICE,"rpd shutting down on signal %d",signal);
   exit(0);
 }
