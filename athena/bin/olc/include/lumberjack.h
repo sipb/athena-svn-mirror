f1a4b70f8beca20a9d699793605eae4ff2db850f
Lucien Van Elsen <lwvanels@mit.edu>
1990-12-07 09:16:41 +0000

    Added additional defines & ifdefs
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@3946 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/include/lumberjack.h
+++ athena/bin/olc/include/lumberjack.h
@@ -9,8 +9,8 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/include/lumberjack.h,v $
- *	$Id: lumberjack.h,v 1.1 1990-07-16 10:09:13 vanharen Exp $
- *	$Author: vanharen $
+ *	$Id: lumberjack.h,v 1.2 1990-12-07 09:16:41 lwvanels Exp $
+ *	$Author: lwvanels $
  */
 
 #include <mit-copyright.h>
@@ -19,5 +19,20 @@
 #define __lumberjack_h __FILE__
 
 char *DONE_DIR		 = "/usr/spool/olc/donelogs";
+
+#define DSPIPE	"/usr/athena/dspipe" /* name of program to send off logs */
+
+#ifdef OLX
+#define PREFIX	"FIONAVAR.MIT.EDU:/usr/spool/discuss/ot" /* meeting prefix */
+#else
+#define PREFIX	"MATISSE.MIT.EDU:/usr/spool/discuss/o"
+#endif
+
+#ifdef LAVIN
+#define PREFIX  "NEMESIS.MIT.EDU:/usr/spool/discuss/o"
+#endif
+
+#define LOCKFILE  "lockfile"	/* name of lockfile */
+#define SIZE	1024
 
 #endif /* __lumberjack_h */
