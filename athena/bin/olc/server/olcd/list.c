9120c09e119fab745e2be59918d33e902899bbea
Lucien Van Elsen <lwvanels@mit.edu>
1990-12-12 15:14:32 +0000

    removed unused vars from list_queue dump_list: (new)  Puts list on disk for caching.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4003 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/server/olcd/list.c
+++ athena/bin/olc/server/olcd/list.c
@@ -6,19 +6,30 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/list.c,v $
- *	$Id: list.c,v 1.10 1990-12-05 21:22:02 lwvanels Exp $
+ *	$Id: list.c,v 1.11 1990-12-12 15:14:32 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/list.c,v 1.10 1990-12-05 21:22:02 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/list.c,v 1.11 1990-12-12 15:14:32 lwvanels Exp $";
 #endif
 #endif
 
 #include <mit-copyright.h>
 
 #include <olcd.h>
+
+#ifdef __STDC__
+# define        P(s) s
+#else
+# define P(s) ()
+#endif
+
+static void get_dlist_info P((D_LIST *item , KNUCKLE *k ));
+static void put_queue P((FILE *f , D_LIST *q , int n , char *name ));
+
+#undef P
 
 #define NBLOCKS 100
 
@@ -85,10 +96,8 @@ list_redundant(knuckle)
 
 
 int
-list_queue(queue,data,queues,topics,stati,name,size)
-     int queue;
+list_queue(data,topics,stati,name,size)
      LIST **data;
-     int queues;
      int *topics;
      int stati;
      char *name;
@@ -165,6 +174,156 @@ list_queue(queue,data,queues,topics,stati,name,size)
 }
 
 
+void
+dump_list()
+{
+  KNUCKLE **k_ptr;
+  static D_LIST *pending_q, *pickup_q, *refer_q;
+  static int mx_pending, mx_pickup, mx_refer;
+  int n_pending, n_pickup, n_refer;
+  FILE *f;
 
+  /* Allocate initial space for queues */
+  /* Make some good guesses about sizes- can't hurt to overguess a little */
 
+  if (mx_pending == 0) {
+    mx_pending = 50;
+    mx_pickup = 50;
+    mx_refer = 10;
 
+    pending_q = calloc(mx_pending,sizeof(D_LIST));
+    pickup_q = calloc(mx_pickup,sizeof(D_LIST));
+    refer_q = calloc(mx_refer,sizeof(D_LIST));
+  }
+
+  n_pending = n_pickup = n_refer = 0;
+
+  if ((pending_q == NULL) || (pickup_q == NULL) || (refer_q == NULL)) {
+    log_error("dump_list: calloc failed");
+    return;
+  }
+
+  for (k_ptr = Knuckle_List; *k_ptr != (KNUCKLE *) NULL; k_ptr++)
+    if(!list_redundant((*k_ptr)) && is_active((*k_ptr)))
+      {
+	if ((*k_ptr)->status & (PENDING | NOT_SEEN)) {
+	  get_dlist_info(&pending_q[n_pending],*k_ptr);
+	  n_pending++;
+	  if (n_pending == mx_pending) {
+	    mx_pending *= 2;
+	    pending_q = realloc(pending_q,mx_pending);
+	    if (pending_q == NULL) {
+	      log_error("dump_list: realloc failed");
+	      return;
+	    }
+	  }
+	}
+	else if ((*k_ptr)->status & PICKUP) {
+	  get_dlist_info(&pickup_q[n_pickup],*k_ptr);
+	  n_pickup++;
+	  if (n_pickup == mx_pickup) {
+	    mx_pickup *= 2;
+	    pickup_q = realloc(pickup_q,mx_pickup);
+	    if (pickup_q == NULL) {
+	      log_error("dump_list: realloc failed");
+	      return;
+	    }
+	  }
+	}
+	else {
+	  get_dlist_info(&refer_q[n_refer],*k_ptr);
+	  n_refer++;
+	  if (n_refer == mx_refer) {
+	    mx_refer *= 2;
+	    refer_q = realloc(refer_q,mx_refer);
+	    if (refer_q == NULL) {
+	      log_error("dump_list: realloc failed");
+	      return;
+	    }
+	  }
+	}
+      }
+
+  /* Got lists, now output them... */
+  f = fopen(LIST_TMP_NAME,"w+");
+  if (f == NULL) {
+    log_error("dump_list: unable to open list file");
+    return;
+  }
+  put_queue(f,pending_q,n_pending,"pending");
+  put_queue(f,pickup_q,n_pickup,"pickup");
+  put_queue(f,refer_q,n_refer,"refer");
+  fclose(f);
+  rename(LIST_TMP_NAME,LIST_FILE_NAME);
+  return;
+}
+
+static void
+  get_dlist_info(item,k)
+D_LIST *item;
+KNUCKLE *k;
+{
+  item->username = k->user->username;
+  item->machine = k->user->machine;
+  item->instance = k->instance;
+  item->kstatus = k->status;
+  item->ustatus = k->user->status;
+  if (is_connected(k)) {
+    item->cusername = k->connected->user->username;
+    item->cinstance = k->connected->instance;
+    item->cstatus = k->connected->status;
+  }
+  else {
+    item->cusername = "";
+    item->cinstance = -1;
+    item->cstatus = -1;
+  }
+  item->n_consult = k->question->nseen;
+  item->topic = k->question->topic;
+  item->timestamp = k->timestamp;
+  item->note = k->question->note;
+}
+
+static void
+  put_queue(f,q,n,name)
+FILE *f;
+D_LIST *q;
+int n;
+char *name;
+{
+  int i;
+  struct tm *t;
+  char st[LABEL_SIZE];
+
+  fprintf(f,"%s\n%d\n",name,n);
+
+  for(i=0;i<n;i++) {
+    fprintf(f,"%s\n%s\n%d\n", q[i].username, q[i].machine, q[i].instance);
+    if (q[i].ustatus & LOGGED_OUT)
+      fprintf(f,"logout\n");
+    else if (q[i].ustatus & SERVICED) {
+      if (q[i].ustatus & PICKUP)
+	fprintf(f,"pickup\n");
+      else if (q[i].ustatus & REFERRED)
+	fprintf(f,"refer\n");
+    }
+    OGetStatusString(q[i].kstatus,st);
+    if (q[i].ustatus & LOGGED_OUT)
+      fprintf(f,"logout\n");
+    else
+      fprintf(f,"%s\n",st);
+    
+    fprintf(f,"%s\n%d\n",q[i].cusername, q[i].cinstance);
+    OGetStatusString(q[i].cstatus,st);
+    if (q[i].cstatus = OFF)
+      fprintf(f,"\n");
+    else
+      fprintf(f,"%s\n",st);
+    
+    t = localtime(&q[i].timestamp);
+
+    fprintf(f,"%d\n%s\n%02d/%02d\n%02d%02d\n", q[i].n_consult, q[i].topic,
+	    t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min);
+    fprintf(f,"%s\n",q[i].note);
+  }
+}
