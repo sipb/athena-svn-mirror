7f99b4204f3853fefc995c3b20a985b14a869834
John F. Carr <jfc@mit.edu>
1990-12-13 11:34:57 +0000

    Declare cert_routine argument correctly.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4018 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZFmtList.c
+++ athena/lib/zephyr/lib/ZFmtList.c
@@ -4,16 +4,16 @@
  *	Created by:	Robert French
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFmtList.c,v $
- *	$Author: jtkohl $
+ *	$Author: jfc $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFmtList.c,v 1.8 1988-06-23 10:29:11 jtkohl Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFmtList.c,v 1.9 1990-12-13 11:34:57 jfc Exp $ */
 
 #ifndef lint
-static char rcsid_ZFormatNoticeList_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFmtList.c,v 1.8 1988-06-23 10:29:11 jtkohl Exp $";
+static char rcsid_ZFormatNoticeList_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFmtList.c,v 1.9 1990-12-13 11:34:57 jfc Exp $";
 #endif lint
 
 #include <zephyr/mit-copyright.h>
@@ -27,7 +27,7 @@ Code_t ZFormatNoticeList(notice, list, nitems, buffer, ret_len,
     int nitems;
     char **buffer;
     int *ret_len;
-    int (*cert_routine)();
+    Z_AuthProc cert_routine;
 {
     char header[Z_MAXHEADERLEN];
     int hdrlen, i, size;
