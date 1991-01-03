3ef0b70ab333b8778aa155cf0716c1eaaf7cc239
Lucien Van Elsen <lwvanels@mit.edu>
1991-01-03 15:42:25 +0000

    (do_olc_motd) changed to reflect new get/change file commands (do_olc_hours) (new) -very like mod
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4120 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/parser/p_motd.c
+++ athena/bin/olc/clients/parser/p_motd.c
@@ -19,13 +19,13 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_motd.c,v $
- *	$Id: p_motd.c,v 1.13 1990-11-14 12:26:11 lwvanels Exp $
+ *	$Id: p_motd.c,v 1.14 1991-01-03 15:42:25 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_motd.c,v 1.13 1990-11-14 12:26:11 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_motd.c,v 1.14 1991-01-03 15:42:25 lwvanels Exp $";
 #endif
 #endif
 
@@ -110,13 +110,105 @@ do_olc_motd(arguments)
 	}
     }
 
-  if(!change_flag)
-    status = t_get_motd(&Request,type,file,!save_file);
-  else
-    status = t_change_motd(&Request,type,file,editor, !save_file);
-
+  if(!change_flag) {
+    Request.request_type = OLC_MOTD;
+    status = t_get_file(&Request,type,file,!save_file);
+  }
+  else {
+    Request.request_type = OLC_CHANGE_MOTD;
+    status = t_change_file(&Request,type,file,editor,
+			   (!save_file ? OLC_MOTD : 0)); 
+  }
   if(!save_file)
     (void) unlink(file);
   return(status);
 }
+
+do_olc_hours(arguments)
+     char **arguments;
+{
+  REQUEST Request;
+  char file[NAME_SIZE];
+  int status;
+  int save_file = 0;
+  int type=0;
+  int change_flag = 0;
+  char editor[NAME_SIZE];
+
+  strcpy(file, "");
+  strcpy(editor, "");
+
+  if(fill_request(&Request) != SUCCESS)
+    return(ERROR);
   
+  make_temp_name(file);
+
+  arguments++;
+  while(*arguments != (char *) NULL)
+    {
+      if(string_eq(*arguments, ">") || string_equiv(*arguments,"-file",
+						    max(strlen(*arguments),2)))
+	{
+          ++arguments;
+	  unlink(file);
+	  if(*arguments == NULL)
+            {
+	      file[0] = '\0';
+              get_prompted_input("Enter file name: ",file);
+	      if(file[0] == '\0')
+		return(ERROR);
+            }
+	  else {
+	    (void) strcpy(file,*arguments);
+	    arguments++;
+	  }
+	  
+	  save_file = TRUE;
+	  continue;
+	}
+
+      if (string_equiv(*arguments, "-editor",max(strlen(*arguments),2)))
+        {
+          ++arguments;
+          if(*arguments != (char *) NULL) {
+            (void) strcpy(editor, *arguments);
+	    arguments++;
+	  }
+          else
+            (void) strcpy(editor, NO_EDITOR);
+        }
+
+      if(string_equiv(*arguments,"-change", max(strlen(*arguments),2)))
+	{
+          ++arguments;
+	  change_flag = TRUE;
+	  continue;
+	}
+
+      arguments = handle_argument(arguments, &Request, &status);
+      if(status)
+	return(ERROR);
+	
+      arguments += num_of_args;		/* HACKHACKHACK */
+	
+      if(arguments == (char **) NULL)   /* error */
+	{
+	  printf("Usage is: \thours  [-file <filename>] [-change] ");
+	  printf("[-editor <editor>]\n");
+	  return(ERROR);
+	}
+    }
+
+  if(!change_flag) {
+    Request.request_type = OLC_GET_HOURS;
+    status = t_get_file(&Request,type,file,!save_file);
+  }
+  else {
+    Request.request_type = OLC_CHANGE_HOURS;
+    status = t_change_file(&Request,type,file,editor,
+			   (!save_file ? OLC_GET_HOURS : 0));
+  }
+  if(!save_file)
+    (void) unlink(file);
+  return(status);
+}
