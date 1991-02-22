10fb8bb53c44c058d7d141bf6d37cc6137ce9840
Jonathan I. Kamens <jik@mit.edu>
1991-02-22 06:33:34 +0000

    Define the mystat structure.  Refine the size_to_k macro a little bit to round up if there is a fraction of a k left over.  Put in a long comment explaining why st_blocks and statfs are both confusing and aren't being used to calculate file sizes.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4423 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/delete/directories.h
+++ athena/bin/delete/directories.h
@@ -1,7 +1,7 @@
 /*
  * $Source: /afs/dev.mit.edu/source/repository/athena/bin/delete/directories.h,v $
  * $Author: jik $
- * $Header: /afs/dev.mit.edu/source/repository/athena/bin/delete/directories.h,v 1.9 1991-02-20 17:34:39 jik Exp $
+ * $Header: /afs/dev.mit.edu/source/repository/athena/bin/delete/directories.h,v 1.10 1991-02-22 06:33:34 jik Exp $
  * 
  * This file is part of a package including delete, undelete,
  * lsdel, expunge and purge.  The software suite is meant as a
@@ -18,13 +18,54 @@ typedef short Boolean;
 #define False			(Boolean) 0
 
 
-#define size_to_k(x)		(x / 1024)
+#define size_to_k(x)		((x) / 1024 + (((x) % 1024) ? 1 : 0))
 
 #define FOLLOW_LINKS		1
 #define DONT_FOLLOW_LINKS	0
 
 #define DIR_MATCH		1
 #define DIR_NO_MATCH		0
+
+typedef struct mystat {
+     dev_t st_dev;
+     ino_t st_ino;
+     unsigned short st_mode;
+     off_t st_size;
+     time_t st_ctime;
+#ifdef notdef
+     /*
+      * I've tried, unsuccessfully, to figure out exactly what this
+      * field means and how I can use it.  Supposedly, it indicates
+      * the number of blocks the file actually occupies, i.e. the size
+      * of the file minus any holes in it there may be.  The question,
+      * however, is this: what's a "block?"
+      *
+      * At first, I thought that a block is as big as f_bsize returned
+      * by a statfs on the file.  But that doesn't prove to be the
+      * case, because my home directory in AFS has f_bsize of 8192,
+      * st_size of 8192, and st_blocks of 16 (!!), indicating that a
+      * block size of 512 bytes is being used.  Where does that size
+      * come from, and why isn't it consistent with the f_bsize
+      * retrieved from statfs?
+      *
+      * Until someone can answer these questions for me enough that
+      * I'm willing to trust the value in this field, I can't use it.
+      * Besides that, it doesn't even exist in the POSIX stat
+      * structure, so I'm not even sure it's worth trying to use it.
+      *
+      * Here's another dilemma: When I do a statfs on my home
+      * directory in AFS, it tells me that the f_bsize is 8192.  If
+      * that's the case, then when I create a one-character file in my
+      * home directory, my quota usage should go up by 8k.  But it
+      * doesn't, it goes up by just 1k.  Which means that the f_bsize
+      * I'm getting from statfs has nothing to do with the minimum
+      * block size of the filesystem.  So what *does* it have to do
+      * with?
+      */
+     long st_blocks;
+#endif
+} mystat;
+
      
 typedef struct filrec {
      char name[MAXNAMLEN];
@@ -35,7 +76,7 @@ typedef struct filrec {
      struct filrec *next;
      Boolean specified;
      Boolean freed;
-     struct stat specs;
+     struct mystat specs;
 } filerec;
 
 
