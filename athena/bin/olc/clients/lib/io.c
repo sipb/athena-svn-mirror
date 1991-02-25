d11af89ec2b6d11a99446b6ba5ad094c2e3b5ecc
Lucien Van Elsen <lwvanels@mit.edu>
1991-02-25 16:31:09 +0000

    don't rely on structs being aligned the same way on all platforms to pass over the network
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4472 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/lib/io.c
+++ athena/bin/olc/clients/lib/io.c
@@ -20,13 +20,13 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/lib/io.c,v $
- *	$Id: io.c,v 1.10 1990-11-13 18:35:20 lwvanels Exp $
+ *	$Id: io.c,v 1.11 1991-02-25 16:31:09 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/lib/io.c,v 1.10 1990-11-13 18:35:20 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/lib/io.c,v 1.11 1991-02-25 16:31:09 lwvanels Exp $";
 #endif
 #endif
 
@@ -77,7 +77,8 @@ send_request(fd, request)
      int fd;
      REQUEST *request;
 {
-  IO_REQUEST net_rq;
+  IO_REQUEST net_req;
+  long i;
 
 #ifdef KERBEROS
   int klength;
@@ -87,17 +88,50 @@ send_request(fd, request)
   printf("%d %d\n",request->requester.uid,CURRENT_VERSION);
 #endif /* TEST */
 
-  net_rq.version            = (int) htonl((u_long) CURRENT_VERSION);
-  net_rq.requester          = request->requester;
-  net_rq.target             = request->target;
-  net_rq.request_type       = (int) htonl((u_long) request->request_type);
-  net_rq.options            = (int) htonl((u_long) request->options);
-  net_rq.target.uid         = (int) htonl((u_long) request->target.uid);
-  net_rq.requester.uid      = (int) htonl((u_long) request->requester.uid);
-  net_rq.target.instance    = (int) htonl((u_long) request->target.instance);
-  net_rq.requester.instance = (int) htonl((u_long) request->requester.instance);
+  bzero((char *) &net_req, sizeof(net_req));
 
-  if (swrite(fd, (char *) &net_rq, sizeof(IO_REQUEST)) != sizeof(IO_REQUEST))
+/* build up struct to send over */
+
+  i = htonl((u_long) CURRENT_VERSION);
+  bcopy((char *) &i, (char *) net_req.data, sizeof(i));
+  
+  i = htonl((u_long) request->request_type);
+  bcopy((char *) &i, (char *) (net_req.data+4), sizeof(i));
+
+  i = htonl((u_long) request->options);
+  bcopy((char *) &i, (char *) (net_req.data+8), sizeof(i));
+
+/* options unset */
+
+  i = htonl((u_long) request->requester.uid);
+  bcopy((char *) &i, (char *) (net_req.data+16), sizeof(i));
+
+  i = htonl((u_long) request->requester.instance);
+  bcopy((char *) &i, (char *) (net_req.data+20), sizeof(i));
+
+  strncpy((char *) (net_req.data+24), request->requester.username,10);
+  strncpy((char *) (net_req.data+34), request->requester.realname,32);
+  strncpy((char *) (net_req.data+66), request->requester.realm,40);
+  strncpy((char *) (net_req.data+106), request->requester.inst,40);
+  strncpy((char *) (net_req.data+146), request->requester.nickname,16);
+  strncpy((char *) (net_req.data+162), request->requester.title,32);
+  strncpy((char *) (net_req.data+194), request->requester.machine,32);
+
+  i = htonl((u_long) request->target.uid);
+  bcopy((char *) &i, (char *) (net_req.data+227), sizeof(i));
+
+  i = htonl((u_long) request->target.instance);
+  bcopy((char *) &i, (char *) (net_req.data+232), sizeof(i));
+
+  strncpy((char *) (net_req.data+236), request->target.username,10);
+  strncpy((char *) (net_req.data+246), request->target.realname,32);
+  strncpy((char *) (net_req.data+278), request->target.realm,40);
+  strncpy((char *) (net_req.data+318), request->target.inst,40);
+  strncpy((char *) (net_req.data+358), request->target.nickname,16);
+  strncpy((char *) (net_req.data+374), request->target.title,32);
+  strncpy((char *) (net_req.data+406), request->target.machine,32);
+
+  if (swrite(fd, (char *) &net_req, sizeof(IO_REQUEST)) != sizeof(IO_REQUEST))
     return(ERROR);
 
 #ifdef KERBEROS
@@ -125,66 +159,75 @@ send_request(fd, request)
   return(SUCCESS);
 }
 
-
-
+ERRCODE
 read_list(fd, list)
      int fd;
      LIST *list;
 {
   int size;
+  int len;
+  IO_LIST net_req;
+  long *i;
 
-  bzero(list, sizeof(LIST));
-  size = sread(fd, (char *) list, sizeof(LIST));
+  bzero((char *)list, sizeof(LIST));
 
-  if(size == -1)        /* let's not hold up the server if we can't  */
-    {                   /* handle it. */
-      send_response(fd,SUCCESS);
-      return(FATAL);
-    }
+  bzero((char *) &net_req, sizeof(IO_LIST));
 
-  if(size != sizeof(LIST))
-    {
-      send_response(fd,ERROR);
-      return(ERROR);
-    }
+  len = 0;
+  while (len < sizeof(IO_LIST)) {
+    size = sread(fd, (char *) (&net_req+len), sizeof(IO_LIST));
+
+    if(size == -1)        /* let's not hold up the server if we can't  */
+      {                   /* handle it. */
+	send_response(fd,SUCCESS);
+	return(FATAL);
+      }
+    len += size;
+  }
     
   send_response(fd,SUCCESS);
 
-  list->nseen              = ntohl((u_long) list->nseen);
-  list->ustatus            = ntohl((u_long) list->ustatus);
-  list->cstatus            = ntohl((u_long) list->cstatus);
-  list->ukstatus           = ntohl((u_long) list->ukstatus);
-  list->ckstatus           = ntohl((u_long) list->ckstatus);
-  list->utime              = ntohl((u_long) list->utime);
-  list->ctime              = ntohl((u_long) list->ctime);
-  list->umessage           = ntohl((u_long) list->umessage);
-  list->cmessage           = ntohl((u_long) list->cmessage);
-  list->user.instance      = ntohl((u_long) list->user.instance);
-  list->user.uid           = ntohl((u_long) list->user.uid);
-  list->connected.instance = ntohl((u_long) list->connected.instance);
-  list->connected.uid      = ntohl((u_long) list->connected.uid);
 
+  list->ustatus	 = (ntohl(*((u_long *) net_req.data)));
+  list->cstatus  = (ntohl(*((u_long *) (net_req.data+4))));
+  list->ukstatus = (ntohl(*((u_long *) (net_req.data+8))));
+  list->ckstatus = (ntohl(*((u_long *) (net_req.data+12))));
+  list->utime    = (ntohl(*((u_long *) (net_req.data+16))));
+  list->ctime    = (ntohl(*((u_long *) (net_req.data+20))));
+  list->umessage = (ntohl(*((u_long *) (net_req.data+24))));
+  list->cmessage = (ntohl(*((u_long *) (net_req.data+28))));
+  list->nseen	 = (ntohl(*((u_long *) (net_req.data+32))));
 
+  strncpy(list->topic,(char *)net_req.data+36, TOPIC_SIZE);
+  strncpy(list->note, (char *)net_req.data+60, NOTE_SIZE);
+
+  list->user.uid = (ntohl(*((u_long *) (net_req.data+124))));
+  list->user.instance = (ntohl(*((u_long *) (net_req.data+128))));
+  
+  strncpy(list->user.username, (char *)net_req.data+132, LOGIN_SIZE+1);
+  strncpy(list->user.realname, (char *)net_req.data+142, TITLE_SIZE);
+  strncpy(list->user.realm,    (char *)net_req.data+174, REALM_SZ);
+  strncpy(list->user.inst,     (char *)net_req.data+214, INST_SZ);
+  strncpy(list->user.nickname, (char *)net_req.data+254, STRING_SIZE);
+  strncpy(list->user.title,    (char *)net_req.data+270, TITLE_SIZE);
+  strncpy(list->user.machine,  (char *)net_req.data+302, TITLE_SIZE);
+
+  list->connected.uid = (ntohl(*((u_long *) (net_req.data+336))));
+  list->connected.instance = (ntohl(*((u_long *) (net_req.data+340))));
+  
+  strncpy(list->connected.username, (char *)net_req.data+344, LOGIN_SIZE+1);
+  strncpy(list->connected.realname, (char *)net_req.data+354, TITLE_SIZE);
+  strncpy(list->connected.realm,    (char *)net_req.data+386, REALM_SZ);
+  strncpy(list->connected.inst,     (char *)net_req.data+426, INST_SZ);
+  strncpy(list->connected.nickname, (char *)net_req.data+466, STRING_SIZE);
+  strncpy(list->connected.title,    (char *)net_req.data+482, TITLE_SIZE);
+  strncpy(list->connected.machine,  (char *)net_req.data+514, TITLE_SIZE);
 
 #ifdef TEST
   printf("%s %s %s\n",list->user.username,list->user.realname,list->user.machine);
   printf("%s %s %s\n",list->connected.username,list->connected.realname,list->connected.machine);
   printf("%d %d %d %d",list->nseen,list->ukstatus,list->user.instance,list->user.uid);
 #endif /* TEST */
-  return(SUCCESS);
-}
-
-
-
-read_person(fd, person)
-     int fd;
-     PERSON *person;
-{
-  if (read(fd, person, sizeof(PERSON)) != sizeof(PERSON))
-    return(ERROR);
-
-  person->instance =  ntohl((u_long) person->instance);
-  person->uid      =  ntohl((u_long) person->uid);
   return(SUCCESS);
 }
 
@@ -202,7 +245,7 @@ read_person(fd, person)
  *	the file descriptor attached to the socket.
  */
 
-int
+ERRCODE
 open_connection_to_daemon(request, fd)
      REQUEST *request;
      int *fd;
