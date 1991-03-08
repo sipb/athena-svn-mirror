2b2ee372511389b2a444413b025d01873fbb2fdb
John F. Carr <jfc@mit.edu>
1991-03-08 13:49:51 +0000

    AIX 1.2 defines ulong in <sys/types.h>
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4612 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/zwgc/ulong.h
+++ athena/lib/zephyr/zwgc/ulong.h
@@ -5,20 +5,21 @@
  *      Created by:     Marc Horowitz <marc@athena.mit.edu>
  *
  *      $Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/zwgc/ulong.h,v $
- *      $Author: marc $
- *	$Id: ulong.h,v 1.1 1989-11-15 23:52:18 marc Exp $
+ *      $Author: jfc $
+ *	$Id: ulong.h,v 1.2 1991-03-08 13:49:51 jfc Exp $
  *
  *      Copyright (c) 1989 by the Massachusetts Institute of Technology.
  *      For copying and distribution information, see the file
  *      "mit-copyright.h".
  */
-
-
-#include <zephyr/mit-copyright.h>
-
 #ifndef ulong_MODULE
 #define ulong_MODULE
 
+#if defined(_AIX) && defined(i386)
+/* AIX 1.2 defines ulong in <sys/types.h>.  */
+#include <sys/types.h>
+#else
 typedef unsigned long ulong;
+#endif
 
 #endif
