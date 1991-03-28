8a5e6743c3edaf46a102ac0f9043f32418d1ec73
Lucien Van Elsen <lwvanels@mit.edu>
1991-03-28 16:31:09 +0000

    fixed sys_errlist for the suns (really should be a general define)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4792 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/tty/t_send.c
+++ athena/bin/olc/clients/tty/t_send.c
@@ -19,13 +19,13 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_send.c,v $
- *	$Id: t_send.c,v 1.20 1991-02-24 15:47:27 lwvanels Exp $
+ *	$Id: t_send.c,v 1.21 1991-03-28 16:31:09 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_send.c,v 1.20 1991-02-24 15:47:27 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/tty/t_send.c,v 1.21 1991-03-28 16:31:09 lwvanels Exp $";
 #endif
 #endif
 
@@ -37,7 +37,7 @@ static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/errno.h>
-#if defined(mips) || defined(m68k) || defined(i386)
+#if defined(mips) || defined(m68k) || defined(i386) || defined(sun)
   extern char *sys_errlist[];
 #endif
 
