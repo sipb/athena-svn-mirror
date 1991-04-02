e36dff13df4f69f2d4bc9ccbc1365d23107d0560
Lucien Van Elsen <lwvanels@mit.edu>
1991-04-02 14:01:30 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@4820 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/bin/olc/include/olcm.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the OLC On-Line Consulting System.
+ * It contains the defintions used in the program to receive questions via
+ * mail
+ *
+ *      Lucien Van Elsen
+ *      MIT Project Athena
+ *
+ * Copyright (C) 1991 by the Massachusetts Institute of Technology.
+ * For copying and distribution information, see the file "mit-copyright.h".
+ *
+ *      $Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/include/olcm.h,v $
+ *      $Id: olcm.h,v 1.1 1991-04-02 13:59:12 lwvanels Exp $
+ *      $Author: lwvanels $
+ */
+
+#include <mit-copyright.h>
+
+#define DFLT_TOPIC	"test"
+#define DFLT_SERVER	"fionavar.mit.edu"
+#define DFLT_USERNAME	"nobody"
+#define OLCR_PATH	"/usr/local/olcr"
+#define SYSLOG_FACILITY LOG_LOCAL6
+
+#ifdef KERBEROS
+#ifdef ATHENA
+#define DFLT_REALM	"ATHENA.MIT.EDU"
+#else
+/* Insert your realm here */
+#define DFLT_REALM	"FOO.BAR.EDU"
+#endif
+
+#define SRVTAB_FILE	"/etc/srvtab"
+#endif
