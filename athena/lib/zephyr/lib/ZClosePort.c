9b31220b9089fc6bd9155b6ba915950ff2465519
John F. Carr <jfc@mit.edu>
1991-06-18 13:18:04 +0000

    #endif lint -> #endif
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5082 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZClosePort.c
+++ athena/lib/zephyr/lib/ZClosePort.c
@@ -4,17 +4,17 @@
  *	Created by:	Robert French
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZClosePort.c,v $
- *	$Author: rfrench $
+ *	$Author: jfc $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZClosePort.c,v 1.5 1988-05-13 14:46:05 rfrench Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZClosePort.c,v 1.6 1991-06-18 13:18:04 jfc Exp $ */
 
 #ifndef lint
-static char rcsid_ZClosePort_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZClosePort.c,v 1.5 1988-05-13 14:46:05 rfrench Exp $";
-#endif lint
+static char rcsid_ZClosePort_c[] = "$Id: ZClosePort.c,v 1.6 1991-06-18 13:18:04 jfc Exp $";
+#endif
 
 #include <zephyr/mit-copyright.h>
 
--- athena/lib/zephyr/lib/ZCmpUID.c
+++ athena/lib/zephyr/lib/ZCmpUID.c
@@ -4,17 +4,17 @@
  *	Created by:	Robert French
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v $
- *	$Author: rfrench $
+ *	$Author: jfc $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v 1.5 1988-05-17 21:21:16 rfrench Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v 1.6 1991-06-18 13:17:22 jfc Exp $ */
 
 #ifndef lint
-static char rcsid_ZCompareUID_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v 1.5 1988-05-17 21:21:16 rfrench Exp $";
-#endif lint
+static char rcsid_ZCompareUID_c[] = "$Id: ZCmpUID.c,v 1.6 1991-06-18 13:17:22 jfc Exp $";
+#endif
 
 #include <zephyr/mit-copyright.h>
 
