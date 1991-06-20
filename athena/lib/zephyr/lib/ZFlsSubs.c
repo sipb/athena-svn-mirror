91b36aed022cfa1ac78d335646a1d8325c55184a
John F. Carr <jfc@mit.edu>
1991-06-20 08:49:17 +0000

    Add zsub_ prefix to subscription structure field names.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5104 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZFlsSubs.c
+++ athena/lib/zephyr/lib/ZFlsSubs.c
@@ -4,33 +4,31 @@
  *	Created by:	Robert French
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFlsSubs.c,v $
- *	$Author: rfrench $
+ *	$Author: jfc $
  *
  *	Copyright (c) 1987 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFlsSubs.c,v 1.2 1987-07-29 15:15:37 rfrench Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFlsSubs.c,v 1.3 1991-06-20 08:49:17 jfc Exp $ */
 
 #ifndef lint
-static char rcsid_ZFlushSubscriptions_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZFlsSubs.c,v 1.2 1987-07-29 15:15:37 rfrench Exp $";
-#endif lint
-
-#include <zephyr/mit-copyright.h>
+static char rcsid_ZFlushSubscriptions_c[] = "$Id: ZFlsSubs.c,v 1.3 1991-06-20 08:49:17 jfc Exp $";
+#endif
 
 #include <zephyr/zephyr_internal.h>
 
 Code_t ZFlushSubscriptions()
 {
-	int i;
+	register int i;
 	
 	if (!__subscriptions_list)
 		return (ZERR_NONE);
 
 	for (i=0;i<__subscriptions_num;i++) {
-		free(__subscriptions_list[i].class);
-		free(__subscriptions_list[i].classinst);
-		free(__subscriptions_list[i].recipient);
+		free(__subscriptions_list[i].zsub_class);
+		free(__subscriptions_list[i].zsub_classinst);
+		free(__subscriptions_list[i].zsub_recipient);
 	}
 	
 	free((char *)__subscriptions_list);
