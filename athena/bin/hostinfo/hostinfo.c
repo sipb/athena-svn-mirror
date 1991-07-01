f69d4c60cb917a97b770a1e98f2ba6da3ec30672
Ezra Peisach <epeisach@mit.edu>
1991-07-01 14:59:14 +0000

    Now prints all host records. [tom]
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5235 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/hostinfo/hostinfo.c
+++ athena/bin/hostinfo/hostinfo.c
@@ -13,11 +13,14 @@
  *
  * Modified:	02/04/87	Doug Alan
  *		Take flags to control output.
+ *
+ * Modified:    08/20/90	Tom Coppet
+ *		Added HINFO queries and list all addrs.
  *			
  */
 
 #ifndef lint
-static char rcsid[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/hostinfo/hostinfo.c,v 1.4 1990-10-25 12:05:25 epeisach Exp $";
+static char rcsid[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/hostinfo/hostinfo.c,v 1.5 1991-07-01 14:59:14 epeisach Exp $";
 #endif
 
 #include <stdio.h>			/* Standard IO */
@@ -27,20 +30,21 @@ static char rcsid[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/ho
 #include <strings.h>			/* String fct. declarations. */
 #include <ctype.h>			/* Character type macros. */
 #include <netinet/in.h>			/* Internet defs. */
-#include <sys/param.h>			/* for MAXHOSTNAMELEN */
 
 typedef int bool;
 
 #define TRUE		1
 #define FALSE		0
-#define ERROR		1
+#define LINE_LENGTH	128
+#define ERROR		-1
 #define CPNULL		((char *) NULL)
 
 static char *usage[] = {
-  "Usage: %s [-h | -a] hostname|address ...",
+  "Usage: %s <host-name-or-address>",
   "   -h: output only hostname",
-  "   -a: output only Internet address",
-  "   Both flags must not be set",
+  "   -a: output only internet address",
+  "   -n: punt hinfo query",
+  "   Both -h and -a flags must not be set",
   CPNULL,
 };
 
@@ -49,118 +53,125 @@ char *myname;				/* How program was invoked. */
 static void usage_error();
 
 main(argc, argv)
-int argc;
-char *argv[];
-{
-     extern int getopt();
-     extern int optind;
-     extern int h_errno;
-
-     struct hostent *host_entry;	/* Host entry of it. */
-     bool h_flag = FALSE;
-     bool a_flag = FALSE;
-     int flagc;
-     int status = 0;
-  
-     myname = (myname = rindex(argv[0], '/')) ? myname + 1 : argv[0];
-
-     while ((flagc = getopt(argc, argv, "ha")) != EOF)
-	  switch ((char) flagc) {
-	  case 'h':
-	       if (a_flag) usage_error();
-	       else h_flag = TRUE;
-	       break;
-	  case 'a':
-	       if (h_flag) usage_error();
-	       else a_flag = TRUE;
-	       break;
-	  case '?':
-	  default:
-	       usage_error();
-	       break;
-	  }
-     if (argc == optind)
-	  usage_error();
+     int argc;
+     char *argv[];
      
-     while (optind < argc) {
-	  char *hostname = argv[optind++];
-	  host_entry = NULL;
-	  
-	  if (*hostname >= '0' && *hostname <= '9') {
-	       unsigned long host_address = inet_addr(hostname);
-	       if (host_address != -1)
-		    host_entry = gethostbyaddr((char *) &host_address,
-					       4, AF_INET);
-	  }
-	  if (! host_entry)
-	       host_entry = gethostbyname(hostname);
-
-	  status |= print_it(hostname, host_entry, h_flag, a_flag);
-	  /* Print a blank line between blocks of verbose output */
-	  if (! (h_flag || a_flag || (optind == argc)))
-	       printf("\n");
-     }
-     exit(status);
-}
-
-
-print_it(hostname, entry, h_flag, a_flag)
-char *hostname;
-struct hostent *entry;
-bool h_flag, a_flag;
 {
-     if (! entry) {
-	  switch (h_errno) {
+  extern int getopt();
+  extern int optind;
+  extern int h_errno;
+
+  char hostname[LINE_LENGTH];	           /* Name of desired host. */
+  struct hostent *host_entry = NULL;	   /* Host entry of it. */
+  struct hostent hostbuf;
+  struct in_addr internet_address;
+  char **alias;				   /* Ptr. to aliases. */
+  char **addr;
+  char *hinfo = NULL;
+  int  i;
+  bool h_flag = FALSE;
+  bool a_flag = FALSE;
+  bool n_flag = FALSE;
+  int flagc;
+
+  myname = (myname = rindex(argv[0], '/')) ? myname + 1 : argv[0];
+
+  while ((flagc = getopt(argc, argv, "nha")) != EOF)
+    switch ((char) flagc) {
+    case 'h':
+      if (a_flag) usage_error();
+      else h_flag = TRUE;
+      break;
+    case 'a':
+      if (h_flag) usage_error();
+      else a_flag = TRUE;
+      break;
+    case 'n':
+      n_flag = TRUE;
+      break;
+    case '?':
+    default:
+      usage_error();
+      break;
+    }
+  if (argc -1 != optind) usage_error();
+  (void) strcpy(hostname, argv[optind]);
+  if (*hostname >= '0' && *hostname <= '9')
+    {
+      unsigned long host_address = inet_addr(hostname);
+      if (host_address == -1) goto oops;
+      host_entry = gethostbyaddr((char *) &host_address, 4, AF_INET);
+      if(host_entry != (struct hostent *) NULL) {
+	  bcopy(host_entry, &hostbuf, sizeof(hostbuf));
+	  host_entry = &hostbuf;	    
+	  if(!n_flag)
+	    hinfo = (char *) gethinfobyaddr((char *) &host_address, 4, AF_INET);
+      }
+    }
+  else
+    {
+    oops:
+      host_entry = gethostbyname(hostname);
+      if(host_entry != (struct hostent *) NULL) {
+	  bcopy(host_entry, &hostbuf, sizeof(hostbuf));
+	  host_entry = &hostbuf;	      
+ 	  if(!n_flag)
+	    hinfo = (char *) gethinfobyname(hostname);
+      }
+    }
+  if (host_entry == NULL)
+    {
+	switch (h_errno) {
 #ifdef ultrix
-	       /*
-		*  it can return NULL, h_errno == 0 in some cases when
-		*  there is no name
-		*/
-	  case 0:
+	/* it can return NULL, h_errno == 0 in some cases when
+	   there is no name */
+	case 0:
 #endif
-	  case HOST_NOT_FOUND:
-	       printf("No such host '%s'.\n",hostname);
-	       return(ERROR);
-	  default:
-	  case TRY_AGAIN:
-	  case NO_RECOVERY:
-	       printf("Cannot resolve name '%s' due to network difficulties.\n",
-		      hostname);
-	       return(ERROR);
-	  case NO_ADDRESS:
-	       /* should look up MX record? */
-	       /*
-		*  this error return appears if there is some entry for the
-		*   requested name, but no A record (e.g. only an NS record)
-		*/
-	       printf("No address for '%s'.\n", hostname);
-	       return(ERROR);
-	  }
-     }
-     if (h_flag)
-	  printf("%s\n", entry->h_name);
-     else if (a_flag)
-	  printf("%s\n", inet_ntoa(*((struct in_addr *) entry->h_addr)));
-     else {
-	  char **alias;
-	  char **addr;
-       
-	  printf("Desired host:\t%s\n", hostname);
-	  printf("Official name:\t%s\n", entry->h_name);
-	  for (alias = entry->h_aliases; *alias; alias++)
-	       printf("Alias:\t\t%s\n", *alias);
-	  for (addr = entry->h_addr_list; *addr; addr++)
-	       printf("Host address:\t%s\n",
-		      inet_ntoa(*((struct in_addr *) *addr)));
-     }
-     return(0);
-}
+	case HOST_NOT_FOUND:
+	    printf("No such host '%s'.\n",hostname);
+	    exit(ERROR);
+	    break;
+	default:
+	case TRY_AGAIN:
+	case NO_RECOVERY:
+	    printf("Cannot resolve name '%s' due to network difficulties.\n",
+		   hostname);
+	    exit(ERROR);
+	    break;
+	case NO_ADDRESS:
+	    /* should look up MX record? */
+	    /* this error return appears if there is some entry for the
+	       requested name, but no A record (e.g. only an NS record) */
+	    printf("No address for '%s'.\n", hostname);
+	    exit(ERROR);
+	    break;
+	}
+    }
 
+  if (h_flag) printf("%s\n", host_entry->h_name);
+  else  {
+      if(!a_flag) {
+      	printf("Desired host:\t%s\n", hostname);
+      	printf("Official name:\t%s\n", host_entry->h_name);
+      	for (alias = host_entry->h_aliases; *alias; alias++)
+		printf("Alias:\t\t%s\n", *alias);
+      }
+      for (addr = host_entry->h_addr_list; *addr; addr++) {
+	  bcopy(*addr, &internet_address, host_entry->h_length);
+          if(a_flag)
+		printf("%s\n", inet_ntoa(internet_address));
+	  else
+	  	printf("Host address:\t%s\n", inet_ntoa(internet_address));      
+      }
+      
+      if(hinfo && !a_flag) {
+	  i = (int) hinfo[0];
+	  hinfo[i+1] = '/';
+	  printf("Host info:\t%s\n", hinfo+1);
+      }
+   }
+} /* main */
 
-
-
-
-     
 static void
 usage_error()
 {
