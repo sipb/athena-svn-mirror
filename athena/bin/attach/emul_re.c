4208d99a66627607e9f4cb9b0177fdde9779e242
Richard Basch <probe@mit.edu>
1991-07-06 14:56:34 +0000

    Added "int Error;" (libc is broken on A/UX)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5261 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/attach/emul_re.c
+++ athena/bin/attach/emul_re.c
@@ -1,32 +1,32 @@
-/* This is for AUX.  It is a wrapper around the C library regex functions. */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/bin/attach/emul_re.c,v 1.1 1990-07-09 01:06:43 jfc Exp $ */
+/*
+ * This is for A/UX.  It is a wrapper around the C library regex functions.
+ *
+ * $Id: emul_re.c,v 1.2 1991-07-06 14:56:34 probe Exp $
+ */
 
 #ifdef _AUX_SOURCE
 
 static char *re;
+int Error;
 
 char *re_comp(s)
-
-char *s;
-
+    char *s;
 {
-  if(!s)
+    if (!s)
+	return 0;
+    if (re)
+	free(re);
+
+    if (!(re = regcmp(s, (char *)0)))
+	return "Bad argument to re_comp";
+
     return 0;
-  if(re)
-    free(re);
-
-  if(!(re = regcmp(s, (char *)0)))
-    return "Bad argument to re_comp";
-
-  return 0;
 }
 
 int re_exec(s)
-
-char *s;
-
+    char *s;
 {
-  return regex(re, s) != 0;
+    return regex(re, s) != 0;
 }
 
 #endif
