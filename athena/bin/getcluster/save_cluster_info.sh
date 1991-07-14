8c6b3a8499d2d73f6975f545dc2f94b3d8316e8f
Lucien Van Elsen <lwvanels@mit.edu>
1991-07-14 15:29:30 +0000

    moved clusterinfo to /etc/athena from /etc
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5318 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/getcluster/save_cluster_info.sh
+++ athena/bin/getcluster/save_cluster_info.sh
@@ -4,15 +4,15 @@
 # activated.
 #
 #	$Source: /afs/dev.mit.edu/source/repository/athena/bin/getcluster/save_cluster_info.sh,v $
-#	$Author: treese $
-#	$Header: /afs/dev.mit.edu/source/repository/athena/bin/getcluster/save_cluster_info.sh,v 1.4 1987-08-22 17:42:07 treese Exp $
+#	$Author: lwvanels $
+#	$Header: /afs/dev.mit.edu/source/repository/athena/bin/getcluster/save_cluster_info.sh,v 1.5 1991-07-14 15:29:30 lwvanels Exp $
 #
 # Errors from getcluster guarantee that stdout will be size 0,
 # therefore a size # 0 is a sufficient test.
 # Use old data from last session if getcluster fails.
 
 HOSTNAME=`/bin/hostname`
-VERSION=`/bin/awk '{vers = $5} END {print vers}' /etc/version`
+VERSION=`/bin/awk '{vers = $5} END {print vers}' /etc/athena/version`
 
 if [ ${VERSION}x = Updatex ]; then
 	echo "This system is in the middle of an update."
@@ -23,12 +23,12 @@ fi
 /bin/athena/getcluster -b $HOSTNAME $VERSION > /tmp/clusterinfo.bsh
 if [ -s /tmp/clusterinfo.bsh ]
 then
-	/bin/cp /tmp/clusterinfo.bsh /etc/clusterinfo.bsh
-	chmod 666 /etc/clusterinfo.bsh 2>/dev/null
+	/bin/cp /tmp/clusterinfo.bsh /etc/athena/clusterinfo.bsh
+	chmod 666 /etc/athena/clusterinfo.bsh 2>/dev/null
 fi
 /bin/athena/getcluster $HOSTNAME $VERSION > /tmp/clusterinfo
 if [ -s /tmp/clusterinfo ]
 then
-	/bin/cp /tmp/clusterinfo /etc/clusterinfo
-	chmod 666 /etc/clusterinfo 2>/dev/null
+	/bin/cp /tmp/clusterinfo /etc/athena/clusterinfo
+	chmod 666 /etc/athena/clusterinfo 2>/dev/null
 fi
