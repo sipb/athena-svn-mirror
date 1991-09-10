c16c7881eecc104785279d040059cdd5106c2fbc
Lucien Van Elsen <lwvanels@mit.edu>
1991-09-10 11:28:26 +0000

    do_olc_describe should return ERRCODE (can't assume it's always an int)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5573 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/parser/p_describe.c
+++ athena/bin/olc/clients/parser/p_describe.c
@@ -19,13 +19,13 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_describe.c,v $
- *	$Id: p_describe.c,v 1.6 1990-11-14 12:24:05 lwvanels Exp $
+ *	$Id: p_describe.c,v 1.7 1991-09-10 11:28:26 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_describe.c,v 1.6 1990-11-14 12:24:05 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_describe.c,v 1.7 1991-09-10 11:28:26 lwvanels Exp $";
 #endif
 #endif
 
@@ -33,6 +33,7 @@ static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc
 #include <olc/olc.h>
 #include <olc/olc_parser.h>
 
+ERRCODE
 do_olc_describe(arguments)
      char **arguments;
 {
