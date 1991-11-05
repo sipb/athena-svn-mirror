6b767f288a65c3cc0e56f7dabfcba18e92efca32
Lucien Van Elsen <lwvanels@mit.edu>
1991-11-05 14:39:34 +0000

    Add routines to write statistics out           when a question is asked or resolved.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5688 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/server/olcd/statistics.c
+++ athena/bin/olc/server/olcd/statistics.c
@@ -10,19 +10,21 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/statistics.c,v $
- *	$Id: statistics.c,v 1.9 1990-12-05 21:29:43 lwvanels Exp $
+ *	$Id: statistics.c,v 1.10 1991-11-05 14:39:34 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/statistics.c,v 1.9 1990-12-05 21:29:43 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/server/olcd/statistics.c,v 1.10 1991-11-05 14:39:34 lwvanels Exp $";
 #endif
 #endif
 
 #include <mit-copyright.h>
 
 #include <olcd.h>
+#include <fcntl.h>
+#include <time.h>
 
 /*
  * Function:	dump_request_stats()  dumps info about the number of
@@ -93,4 +95,77 @@ dump_question_stats(file)
     fprintf(fp, "No other statistics generated yet.\n");
     fprintf(fp, "-------------------------\n");
     fclose(fp);
+}
+
+void
+write_ask_stats(username,topic,machine,ask_by)
+     char *username;
+     char *topic;
+     char *machine;
+     char *ask_by;
+{
+  int fd;
+  char buf[BUFSIZ];
+  char ask_time[26];
+  time_t now;
+
+  if ((fd = open(ASK_STATS_FILE,O_APPEND|O_WRONLY,0600)) < 0) {
+    if (errno != ENOENT) {
+      sprintf(buf,"Error opening ask stats file %s: %%m", ASK_STATS_FILE);
+      log_error(buf);
+    }
+    return;
+  }
+
+  now = time(0);
+  strcpy(ask_time,ctime(&now));
+  ask_time[24] = '\0';
+  sprintf(buf,"%s %s %s %s %s\n",ctime(&now), username, topic, machine,
+	  ask_by);
+  write(fd,buf,strlen(buf));
+  close(fd);
+}
+
+void
+write_res_stats(q)
+     QUESTION *q;
+{
+  int fd,i,res_uid;
+  char buf[BUFSIZ];
+  char ask_time[26],res_time[26];
+  time_t now;
+
+  if ((fd = open(RES_STATS_FILE,O_APPEND|O_WRONLY,0600)) < 0) {
+    if (errno != ENOENT) {
+      sprintf(buf,"Error opening res stats file %s: %%m", RES_STATS_FILE);
+      log_error(buf);
+    }
+    return;
+  }
+
+  now = time(0);
+  strcpy(res_time,ctime(&now));
+  res_time[24] = '\0';
+  strcpy(ask_time,ctime(&q->owner->timestamp));
+  ask_time[24] = '\0';
+  sprintf(buf,"%s %s %s %s %d %d %d %d %d\n", ask_time, res_time,
+	  q->owner->user->username, q->owner->user->machine,
+	  q->stats.n_crepl, q->stats.n_cmail, q->stats.n_urepl,
+	  q->stats.time_to_fr, q->nseen);
+
+  write(fd,buf,strlen(buf));
+
+  for(i=0;i<q->nseen;i++) {
+    sprintf(buf,"%d ",q->seen[i]);
+    write(fd,buf,strlen(buf));
+  }
+  write(fd,"\n",1);
+  if (q->owner->connected == NULL)
+    res_uid = -1;
+  else
+    res_uid = q->owner->connected->user->uid;
+
+  sprintf(buf,"%d\n",res_uid);
+  write(fd,buf,strlen(buf));
+  close(fd);
 }
