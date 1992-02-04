ad9a8cc149c7607f9eeeefc96c30398dd4dd37e8
Lucien Van Elsen <lwvanels@mit.edu>
1992-02-04 21:46:59 +0000

    Don't hardcode in max filename size, use 	  MAXPATHLEN instead 	  ifdef location of BASE_DIR on ATHENA
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5858 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/browser/motif/cref.h
+++ athena/bin/olc/browser/motif/cref.h
@@ -4,12 +4,12 @@
  */
 
 #include <mit-copyright.h>
+#include <sys/param.h>
 
 /* Structure describing a CREF entry. */
 
 #define  TITLE_SIZE	128		/* Size of a section title. */
 #define  MAX_TITLES	25		/* Maximum number of titles. */
-#define  FILENAME_SIZE	120		/* Size of a filename. */
 #define  LINE_LENGTH	200		/* Length of a line of text. */
 #define  SUBDIR		1		/* Subdirectory in menu. */
 #define  PLAINFILE	0		/* Entry in menu. */
@@ -18,9 +18,9 @@
 typedef struct tENTRY {
         int type;			/* Type of entry. */
         char title[TITLE_SIZE];		/* Title of entry. */
-        char filename[FILENAME_SIZE];	/* Filename for entry. */
+        char filename[MAXPATHLEN];	/* Filename for entry. */
         char formatter[LINE_LENGTH];	/* Text formatter to use. */
-        char maintainer[FILENAME_SIZE];	/* Maintainer of file. */
+        char maintainer[MAXPATHLEN];	/* Maintainer of file. */
         } ENTRY;
 
 /* Other CREF stuff. */
@@ -30,3 +30,10 @@ typedef struct tENTRY {
 #define  B_SUBDIR	"directory"	/* String for a directory entry. */
 #define  COMMENT_CHAR	'#'		/* Comment char in contents. */
 #define  CONTENTS_DELIM	':'		/* Delimiter in contents. */
+
+#ifdef ATHENA
+/* Root of the stock answer tree */
+#define	BASE_DIR	"/mit/olc-stock"
+#else
+#define BASE_DIR	"/your/local/tree"
+#endif
