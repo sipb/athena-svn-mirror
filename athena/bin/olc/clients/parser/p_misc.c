4477d9481b6deef74c40d78fc7fee5ba6e229e6d
Lucien Van Elsen <lwvanels@mit.edu>
1992-02-05 02:56:47 +0000

    removed unused do_olc_dump.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5872 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/olc/clients/parser/p_misc.c
+++ athena/bin/olc/clients/parser/p_misc.c
@@ -19,13 +19,13 @@
  * For copying and distribution information, see the file "mit-copyright.h".
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v $
- *	$Id: p_misc.c,v 1.8 1992-01-10 20:00:23 lwvanels Exp $
+ *	$Id: p_misc.c,v 1.9 1992-02-05 02:56:47 lwvanels Exp $
  *	$Author: lwvanels $
  */
 
 #ifndef lint
 #ifndef SABER
-static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v 1.8 1992-01-10 20:00:23 lwvanels Exp $";
+static char rcsid[] ="$Header: /afs/dev.mit.edu/source/repository/athena/bin/olc/clients/parser/p_misc.c,v 1.9 1992-02-05 02:56:47 lwvanels Exp $";
 #endif
 #endif
 
@@ -59,74 +59,6 @@ do_olc_load_user(arguments)
     }
 
   return(t_load_user(&Request));
-}
-
-ERRCODE
-do_olc_dump(arguments)
-     char **arguments;
-{
-  REQUEST Request;
-  int status;            
-  char file[NAME_SIZE];
-  int save_file = 0;
-  int type = OLC_DUMP;
-
-  if(fill_request(&Request) != SUCCESS)
-    return(ERROR);
-  
-  make_temp_name(file);
-  for (arguments++; *arguments != (char *) NULL; arguments++)
-    {
-      if(string_eq(*arguments, ">") || string_equiv(*arguments,"-file",
-                                                    max(strlen(*arguments),2)))
-        {
-          ++arguments;
-          unlink(file);
-          if(*arguments == NULL)
-            {
-              file[0] = '\0';
-              get_prompted_input("Enter file name: ",file,NAME_SIZE,0);
-              if(file[0] == '\0')
-                return(ERROR);
-            }
-	  else
-	    (void) strcpy(file,*arguments);
-
-          save_file = TRUE;
-	  continue;
-        }
-
-      if(string_equiv(*arguments,"-requests",max(strlen(*arguments),2)))
-	{
-	  type = OLC_DUMP_REQ_STATS;
-	  continue;
-	}
-
-      if(string_equiv(*arguments,"-questions",max(strlen(*arguments),2)))
-	{
-	  type = OLC_DUMP_QUES_STATS;
-	  continue;
-	}
-
-      arguments = handle_argument(arguments, &Request, &status);
-      if(status)
-	return(ERROR);
-
-      if(arguments == (char **) NULL)   /* error */
-        {
-          printf("Usage is: \tdump  [-requests] [-questions]");
-	  printf(" [-file <filename>]\n");
-          return(ERROR);
-        }
-
-      if(*arguments == (char *) NULL)   /* end of list */
-        break;
-    }
-
-  status = t_dump(&Request,type,file);
-    if(!save_file)
-    (void) unlink(file);
-  return(status);
 }
 
 ERRCODE
