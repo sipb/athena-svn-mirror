9dcd9131a1d6dbcb09748b1206d83d7e5a6f8a43
Tom Coppeto <tom@mit.edu>
1992-04-22 02:02:12 +0000

    release 7.4 	fixed name of queued mail files
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@5996 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/snmp/server/src/mail_grp.c
+++ athena/etc/snmp/server/src/mail_grp.c
@@ -15,6 +15,9 @@
  *    $Author: tom $
  *    $Locker:  $
  *    $Log: not supported by cvs2svn $
+ * Revision 1.4  90/05/26  13:38:30  tom
+ * athena release 7.0e
+ * 
  * Revision 1.3  90/04/26  17:05:15  tom
  * added rcsid
  * 
@@ -25,7 +28,7 @@
  */
 
 #ifndef lint
-static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/mail_grp.c,v 1.4 1990-05-26 13:38:30 tom Exp $";
+static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/mail_grp.c,v 2.0 1992-04-22 02:02:12 tom Exp $";
 #endif
 
 #include "include.h"
@@ -39,7 +42,6 @@ static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snm
  * Mail queue directory
  */
 
-char *mqdir = "/usr/spool/mqueue";
 
 static int crock_mailq();
 
@@ -70,7 +72,8 @@ lu_mail(varnode, repl, instptr, reqflg)
   bcopy ((char *)varnode->var_code, (char *) &repl->name, sizeof(repl->name));
   repl->name.ncmp++;                    /* include the "0" instance */
   repl->val.type = INT;
-  repl->val.value.intgr = crock_mailq();
+  if((repl->val.value.intgr = crock_mailq()) != BUILD_SUCCESS)
+    return(BUILD_ERR);
   return(BUILD_SUCCESS);
 }
 
@@ -87,18 +90,22 @@ static int
 crock_mailq()
 {
   DIR *d;
+#ifdef POSIX
+  struct dirent *dp;
+#else
   struct direct *dp;
+#endif
   int count = 0;
 
-  d = opendir(mqdir);
+  d = opendir(mail_q);
   if(d == (DIR *) NULL)
     {
-      syslog(LOG_ERR, "lu_mail: cannot open %d", mqdir);
+      syslog(LOG_WARNING, "lu_mail: cannot open %s", mail_q);
       return(BUILD_ERR);
     }
 
   while(dp = readdir(d))
-    if(strncmp(dp->d_name, "qfa", 3) == 0)
+    if(strncmp(dp->d_name, "qf", 2) == 0)
       ++count;
 
   closedir(d);
