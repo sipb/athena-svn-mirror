8d0f082144aa850e198d3af5869c2cfc93c2d749
Tom Coppeto <tom@mit.edu>
1992-04-22 02:05:03 +0000

    release 7.4 	new rc variables
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@6001 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/snmp/server/src/rc_grp.c
+++ athena/etc/snmp/server/src/rc_grp.c
@@ -15,6 +15,10 @@
  *    $Author: tom $
  *    $Locker:  $
  *    $Log: not supported by cvs2svn $
+ * Revision 1.5  90/07/17  14:21:28  tom
+ * do not return error if variable does not exist... get-next will discontinue
+ * this branch if an error occurrs... instead set string to null.
+ * 
  * Revision 1.4  90/07/15  18:11:49  tom
  * malloc only what we need for variable size
  * 
@@ -28,7 +32,7 @@
  */
 
 #ifndef lint
-static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/rc_grp.c,v 1.5 1990-07-17 14:21:28 tom Exp $";
+static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/rc_grp.c,v 2.0 1992-04-22 02:05:03 tom Exp $";
 #endif
 
 
@@ -38,7 +42,9 @@ static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snm
 #ifdef MIT
 #ifdef ATHENA
 
-static int get_variable();
+static int get_rc_variable();
+static int get_srv_number();
+static char *get_service();
 
 /*
  * Function:    lu_rcvar()
@@ -71,6 +77,16 @@ lu_rcvar(varnode, repl, instptr, reqflg)
 
   switch (varnode->offset) 
     {
+    case N_RCFILE:
+      repl->val.value.str.str = malloc(strlen(rc_file) + 1);
+      if(repl->val.value.str.str)
+	{
+	  strcpy(repl->val.value.str.str, rc_file);
+	  repl->val.value.str.len = strlen(repl->val.value.str.str);
+	  return(BUILD_SUCCESS);
+	}
+      else
+	return(BUILD_ERR);
     case N_RCHOST:
       return(get_variable("HOST",      &(repl->val.value.str)));
     case N_RCADDR:
@@ -137,11 +153,87 @@ lu_rcvar(varnode, repl, instptr, reqflg)
       return(get_variable("NOCREATE",  &(repl->val.value.str)));
     case N_RCNOATTACH:
       return(get_variable("NOATTACH",  &(repl->val.value.str)));
+    case N_AFSADJUST:
+      return(get_variable("AFSADJUST", &(repl->val.value.str)));
+    case N_SNMP:
+      return(get_variable("SNMP",      &(repl->val.value.str)));
+    case N_AUTOUPDATE:
+      return(get_variable("AUTOUPDATE",&(repl->val.value.str)));
     default:
       syslog (LOG_ERR, "lu_rc: bad rc offset: %d", varnode->offset);
       return(BUILD_ERR);
     }
 }
+
+
+
+
+/*
+ * Function:    lu_service()
+ * Description: Top level callback. Suports the following:
+ * Returns:     BUILD_ERR/BUILD_SUCCESS
+ */
+
+int
+lu_service(varnode, repl, instptr, reqflg)
+     struct snmp_tree_node *varnode;
+     varbind *repl;
+     objident *instptr;
+     int reqflg;
+{
+  char s[SNMPMXID];
+  char *ch;
+  int  cnt;
+
+  if((varnode->flags & NOT_AVAIL) || (varnode->offset <= 0))
+    return (BUILD_ERR);
+  
+  if((instptr == (objident *) NULL) || (instptr->ncmp == 0))
+    s[0] = '\0';
+  else
+    {
+      cnt = 0;
+      while((cnt < instptr->ncmp) && (cnt < SNMPMXID))
+        {
+          s[cnt] = instptr->cmp[cnt];
+          ++cnt;
+        }
+      s[cnt] = '\0';
+    }
+  
+  if((ch = get_service(s, reqflg)) == (char *) NULL)
+    return(BUILD_ERR);
+  
+  bcopy ((char *)varnode->var_code, (char *) &repl->name, sizeof(repl->name));
+  repl->val.type = STR;
+  if((repl->val.value.str.str = malloc(strlen(ch) + 1)) == (char *) NULL)
+    {
+      syslog(LOG_ERR, "malloc failed");
+      return(BUILD_ERR);
+    }
+  strcpy(repl->val.value.str.str, ch);
+  repl->val.value.str.len = strlen(ch);
+  
+
+  /*
+   *  fill in object instance name
+   */
+
+  cnt = 0;
+  len = strlen(ch);
+  if((len + repl->name.ncmp) > SNMPMXID)
+    len = SNMPMXID - repl->name.ncmp;
+
+  while (cnt < len)
+    {
+      repl->name.cmp[repl->name.ncmp] = *ch & 0xff;
+      repl->name.ncmp++;
+      cnt++;
+      ch++;
+    }
+  return(BUILD_SUCCESS);
+}
+
 
 
 
@@ -152,25 +244,116 @@ lu_rcvar(varnode, repl, instptr, reqflg)
  */
 
 static int 
-get_variable(var, s)
+get_rc_variable(var, s)
   char  *var;
   strng *s;    
 {
-  char *ptr;
-
-  if((ptr = (char *) getenv(var)) == (char *) NULL)
+  FILE *fp;
+  char *a;
+  char *b;
+  
+  if((fp = fopen(rc_file, "r")) == (FILE *) NULL)
     {
-      s->str = (char *) NULL;
-      s->len = 0;
+      syslog(LOG_ERR, "unable to open rc config file \"%s\".", rc_file);
+      return(BUILD_ERR);
+    }
+
+  while(fgets(lbuf, sizeof(lbuf)-1, fp) != (char *) NULL)
+    {
+      a = var;
+      b = lbuf;
+      while(*a++ == *b++);
+      while(((*b == ' ') || (*b == '\t')) && (*b != '\0'))
+	++b;
+      if(*b++ != '=')
+	continue;
+      c = b;
+      while((*c != '\0') && (*c != ';'))
+	++c;
+      if((*c == '\0') || (*c == '\n'))
+	continue;
+      *c = '\0';
+      fclose(fp);
+
+      if((s->str = (char *) malloc(strlen(b) * sizeof(char))) == 
+	 (char *) NULL)
+	return(BUILD_ERR);
+	
+      strcpy(s->str, b);      
+      s->len = strlen(b);
       return(BUILD_SUCCESS);
     }
-  if((s->str = (char *) malloc(strlen(ptr) * sizeof(char))) == (char *) NULL)
-    return(BUILD_ERR);
-  
-  strcpy(s->str, ptr);      
-  s->len = strlen(ptr);
+
+  fclose(fp);
+  s->str = (char *) NULL;
+  s->len = 0;
   return(BUILD_SUCCESS);
 }
+
+
+static int
+get_srv_number(n)
+     int *n;
+{
+  FILE *fp;
+  char *c;
+  int i = 0;
+
+  if((fp = fopen(srv_file, "r")) == (FILE *) NULL)
+    return(BUILD_ERR);
+    
+  while(fgets(lbuf, sizeof(lbuf)-1, fp) != (char *) NULL)
+    {
+      c = lbuf;
+      while((*c != '\0') && ((*c == ' ') || (*c == '\t')))
+	*c++;
+      if((*c == '\n') || (*c == '\0'))
+	continue;
+      ++i;
+    }
+  fclose(fp);
+  *n = i;
+  return(BUILD_SUCCESS);
+}
+
+
+
+static char *
+get_service(name, req)
+     char *name;
+     int  req;
+{
+  FILE *fp;
+  char *c;
+  int onemore = 0;
+
+  if((fp = fopen(srv_file, "r")) == (FILE *) NULL)
+    return(BUILD_ERR);
+    
+  while(fgets(lbuf, sizeof(lbuf)-1, fp) != (char *) NULL)
+    {
+      c = lbuf;
+      while((*c != '\0') && ((*c == ' ') || (*c == '\t')))
+	*c++;
+      if((*c == '\n') || (*c == '\0'))
+	continue;
+      if(*name && (strcmp(name, c) == 0))
+	{
+	  if((req == NXT) && !onemore)
+	    {
+	      onemore = 1;
+	      continue;
+	    }
+	  break;
+	}
+      if((*name == '\0') && (req == NXT))
+	break;
+    }
+
+  fclose(fp);
+  return(c);
+}
+
 
 
 #endif ATHENA
