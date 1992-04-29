071f86c350d47ce16a8760d3226767500e4994e5
Craig Fields <cfields@mit.edu>
1992-04-29 14:12:25 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@6058 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/bin/dash/src/xcluster/Icon.h
@@ -0,0 +1,45 @@
+/*
+ * $Source: /afs/dev.mit.edu/source/repository/athena/bin/dash/src/xcluster/Icon.h,v $
+ * $Author: cfields $ 
+ *
+ * Copyright 1990, 1991 by the Massachusetts Institute of Technology. 
+ *
+ * For copying and distribution information, please see the file
+ * <mit-copyright.h>. 
+ *
+ */
+
+#include "Jets.h"
+
+extern JetClass iconJetClass;
+extern void SetIcon();
+
+typedef struct {int littlefoo;} IconClassPart;
+
+typedef struct _IconClassRec {
+  CoreClassPart		core_class;
+  IconClassPart	icon_class;
+} IconClassRec;
+
+extern IconClassRec iconClassRec;
+
+typedef struct {
+  XjPixmap *pixmap;
+  GC gc, reversegc;
+  int inverted;
+  int foreground, background;
+  Boolean reverseVideo;
+  Boolean centerX, centerY;
+  int x, y;
+} IconPart;
+
+typedef struct _IconRec {
+  CorePart	core;
+  IconPart	icon;
+} IconRec;
+
+typedef struct _IconRec *IconJet;
+typedef struct _IconClassRec *IconJetClass;
+
+#define XjCIcon "Icon"
+#define XjNicon "icon"
