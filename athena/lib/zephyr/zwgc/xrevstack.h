db372f459396704134399c833eabc242b5b78d6a
Lucien Van Elsen <lwvanels@mit.edu>
1992-08-08 23:57:49 +0000

    add defs of unlinked and unlink_gram
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@6279 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/zwgc/xrevstack.h
+++ athena/lib/zephyr/zwgc/xrevstack.h
@@ -5,7 +5,7 @@
  *      Created by:     Marc Horowitz <marc@athena.mit.edu>
  *
  *      $Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/zwgc/xrevstack.h,v $
- *      $Author: marc $
+ *      $Author: lwvanels $
  *
  *      Copyright (c) 1989 by the Massachusetts Institute of Technology.
  *      For copying and distribution information, see the file
@@ -16,16 +16,18 @@
 #define _XREVSTACK_H_
 
 #if (!defined(lint) && !defined(SABER))
-static char rcsid_xrevstack_h[] = "$Id: xrevstack.h,v 1.2 1989-11-15 22:46:51 marc Exp $";
+static char rcsid_xrevstack_h[] = "$Id: xrevstack.h,v 1.3 1992-08-08 23:57:49 lwvanels Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
 
 extern x_gram *bottom_gram; /* for testing against NULL */
+extern x_gram *unlinked;
 extern int reverse_stack; /* is reverse stack on? */
 
 extern void add_to_bottom(/* x_gram */);
 extern void delete_gram(/* x_gram */);
+extern void unlink_gram(/* x_gram */);
 extern void pull_to_top(/* x_gram */);
 extern void push_to_bottom(/* x_gram */);
 
