fa62ad59e5a45b84d5ab01c1ab4a66bf48a06fcd
Michal N Lusztig <miki@mit.edu>
1993-04-29 19:06:13 +0000

    made more portable
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@6590 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/getcluster/save_cluster_info.sh
+++ athena/bin/getcluster/save_cluster_info.sh
@@ -3,14 +3,17 @@
 # This is normally exected by /etc/rc and each time a workstation is
 # activated.
 #
-# $Id: save_cluster_info.sh,v 1.6 1991-07-25 11:52:39 probe Exp $
+# $Id: save_cluster_info.sh,v 1.7 1993-04-29 19:06:13 miki Exp $
 #
 # Errors from getcluster guarantee that stdout will be size 0,
 # therefore a size # 0 is a sufficient test.
 # Use old data from last session if getcluster fails.
 
-HOSTNAME=`/bin/hostname`
-VERSION=`/bin/awk '{vers = $5}; END {print vers}' /etc/athena/version`
+PATH=/bin:/usr/ucb; export PATH
+
+. /etc/athena/rc.conf
+HOSTNAME=`hostname`
+VERSION=`awk '{vers = $5}; END {print vers}' /etc/athena/version`
 
 if [ "${VERSION}" = "Update" ]; then
 	echo "This system is in the middle of an update."
@@ -21,12 +24,12 @@ fi
 /bin/athena/getcluster -b ${HOSTNAME} ${VERSION} > /tmp/clusterinfo.bsh
 if [ -s /tmp/clusterinfo.bsh ]
 then
-	/bin/cp /tmp/clusterinfo.bsh /etc/athena/clusterinfo.bsh
+	cp /tmp/clusterinfo.bsh /etc/athena/clusterinfo.bsh
 	chmod 666 /etc/athena/clusterinfo.bsh 2>/dev/null
 fi
 /bin/athena/getcluster ${HOSTNAME} ${VERSION} > /tmp/clusterinfo
 if [ -s /tmp/clusterinfo ]
 then
-	/bin/cp /tmp/clusterinfo /etc/athena/clusterinfo
+	cp /tmp/clusterinfo /etc/athena/clusterinfo
 	chmod 666 /etc/athena/clusterinfo 2>/dev/null
 fi
