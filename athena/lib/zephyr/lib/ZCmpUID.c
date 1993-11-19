7b563d71ef20c232bdcec31c41689a202779a8c4
Richard Basch <probe@mit.edu>
1993-11-19 15:26:04 +0000

    Changed bcmp to memcmp [ANSI]
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7011 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZCmpUID.c
+++ athena/lib/zephyr/lib/ZCmpUID.c
@@ -10,10 +10,10 @@
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v 1.7 1993-09-24 16:19:19 probe Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZCmpUID.c,v 1.8 1993-11-19 15:26:04 probe Exp $ */
 
 #ifndef lint
-static char rcsid_ZCompareUID_c[] = "$Id: ZCmpUID.c,v 1.7 1993-09-24 16:19:19 probe Exp $";
+static char rcsid_ZCompareUID_c[] = "$Id: ZCmpUID.c,v 1.8 1993-11-19 15:26:04 probe Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
@@ -23,5 +23,5 @@ static char rcsid_ZCompareUID_c[] = "$Id: ZCmpUID.c,v 1.7 1993-09-24 16:19:19 pr
 int ZCompareUID(uid1, uid2)
     ZUnique_Id_t *uid1, *uid2;
 {
-    return (!_BCMP((char *)uid1, (char *)uid2, sizeof (*uid1)));
+    return (!memcmp((char *)uid1, (char *)uid2, sizeof (*uid1)));
 }
