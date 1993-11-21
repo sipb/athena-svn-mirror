f8f3229738456283d2c8687e9d23e4397504c681
Richard Basch <probe@mit.edu>
1993-11-21 03:19:19 +0000

    Changed RCS id string...
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7063 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/lib/ZGetSender.c
+++ athena/lib/zephyr/lib/ZGetSender.c
@@ -10,11 +10,11 @@
  *	For copying and distribution information, see the file
  *	"mit-copyright.h". 
  */
-/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZGetSender.c,v 1.10 1993-11-21 03:18:13 probe Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/lib/ZGetSender.c,v 1.11 1993-11-21 03:19:19 probe Exp $ */
 
 #ifndef lint
 static char rcsid_ZGetSender_c[] =
-    "$Zephyr: ZGetSender.c,v 1.7 88/05/13 15:05:07 rfrench Exp $";
+    "$Id: ZGetSender.c,v 1.11 1993-11-21 03:19:19 probe Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
