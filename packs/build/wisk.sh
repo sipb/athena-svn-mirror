425dcc23cb4d0c1b7a67245b24910710330dccdf
Mark D. Virtue <vrt@mit.edu>
1994-05-26 13:15:24 +0000

    added a $end to do the dotfiles and the configuration /
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7377 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/wisk.sh
+++ packs/build/wisk.sh
@@ -12,6 +12,7 @@ else if ($machine == "decmips") then
 else if ($machine == "rsaix" ) then
 	setenv  GCC_EXEC_PREFIX /mit/cygnus-930630/rsaix/lib/gcc-lib/
 	set AFS = "rs_aix32"
+	attach -n cygnus
 endif
 if ($machine == "sun4") then
 setenv LD_LIBRARY_PATH /usr/openwin/lib
@@ -37,6 +38,8 @@ set libs2=" athena/lib/kerberos2 athena/lib/acl athena/lib/gdb athena/lib/gdss a
 set etcs="athena/etc/track athena/etc/rvd athena/etc/nfsc athena/etc/newsyslog athena/etc/cleanup athena/etc/synctree athena/etc/ftpd athena/etc/inetd athena/etc/netconfig athena/etc/gettime athena/etc/traceroute athena/etc/xdm athena/etc/scripts athena/etc/timed athena/etc/snmpd"
 
 set bins=" athena/bin/session athena/bin/olc.dev athena/bin/finger athena/bin/ispell athena/bin/Ansi athena/bin/sendbug athena/bin/just athena/bin/rep athena/bin/cxref athena/bin/tarmail athena/bin/access athena/bin/mon athena/bin/olh athena/bin/dent athena/bin/xquota athena/bin/attach athena/bin/dash athena/bin/xmore athena/bin/mkserv athena/bin/cal athena/bin/xps athena/bin/scripts athena/bin/afs-nfs athena/bin/xdsc athena/bin/rkinit.76 athena/bin/xprint athena/bin/xversion athena/bin/viewscribe athena/bin/kerberometer athena/bin/discuss athena/bin/from athena/bin/delete athena/bin/getcluster athena/bin/gms athena/bin/hostinfo athena/bin/machtype athena/bin/login athena/bin/ls athena/bin/tcsh athena/bin/write athena/bin/tar athena/bin/tinkerbell"
+
+set end="config/dotfiles config/config"
 
 #I removed athena/bin/inittty as there was no Imakefile there
  
