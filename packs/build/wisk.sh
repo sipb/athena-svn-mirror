96a30409eedf99a1d3040834063a9d9b52023504
Craig Fields <cfields@mit.edu>
1994-06-07 06:47:42 +0000

    Build & install perl before TeX. (Bert wrote a perl script for TeX to check trip/trap output.) Install afs.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7413 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/wisk.sh
+++ packs/build/wisk.sh
@@ -33,7 +33,7 @@ set libs1=" athena/lib/et athena/lib/ss athena/lib/hesiod athena/lib/kerberos1 "
 
 set tools="athena/etc/synctree"
 
-set third="third/supported/afs third/supported/X11R5 third/supported/X11R4 third/supported/motif third/supported/tcsh6 third/supported/emacs-18.59 third/supported/tex third/unsupported/top third/unsupported/sysinfo  third/unsupported/perl"
+set third="third/supported/afs third/supported/X11R5 third/supported/X11R4 third/supported/motif third/supported/tcsh6 third/supported/emacs-18.59 third/unsupported/perl third/supported/tex third/unsupported/top third/unsupported/sysinfo"
 
 set libs2=" athena/lib/kerberos2 athena/lib/acl athena/lib/gdb athena/lib/gdss athena/lib/zephyr.p4 athena/lib/moira.dev athena/lib/neos"
 
@@ -438,6 +438,7 @@ if ( $installonly == "0" ) then
 	cp -rp /build/$package/$AFS/dest/lib /usr/transarc
 	cp -rp /build/$package/$AFS/dest/include /usr/transarc
 endif # installonly
+	(cd /build/$package; make install DESTDIR=$SRVD >>& $outfile)
 	breaksw	
 
 	case third/supported/X11R5
