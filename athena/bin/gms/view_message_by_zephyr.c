968d96e48b81fe65f0e2505993a3ca5bc04c9a0a
Mark D. Virtue <vrt@mit.edu>
1994-06-28 16:33:22 +0000

    fixed so that zwrite is execed as /usr/athena/bin/zwrite per bug report 909 in netporb
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7486 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/gms/view_message_by_zephyr.c
+++ athena/bin/gms/view_message_by_zephyr.c
@@ -9,7 +9,7 @@
  */
 #include <mit-copyright.h>
 #ifndef lint
-static char rcsid_view_message_by_zephyr_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/gms/view_message_by_zephyr.c,v 1.2 1994-04-30 13:17:44 vrt Exp $";
+static char rcsid_view_message_by_zephyr_c[] = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/gms/view_message_by_zephyr.c,v 1.3 1994-06-28 16:33:22 vrt Exp $";
 #endif lint
 
 #include "globalmessage.h"
@@ -47,7 +47,7 @@ void view_message_by_zephyr(message)
   /* check that there is *something* after the headers */
   if(*ptr) {
     /* don't even fork... this just exits anyway... */
-    execl("/usr/athena/zwrite",
+    execl("/usr/athena/bin/zwrite",
 	  "zwrite", "-d", "-q", "-n",  whoami, "-m", ptr, 0);
     /* put logging here in case the exec fails. */
     syslog(LOG_INFO, "GMS client execl of zwrite failed [%s]",
