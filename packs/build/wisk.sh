0d062359318bfae86461c2a91c6b6706d845b9f5
Craig Fields <cfields@mit.edu>
1994-07-25 02:43:39 +0000

    Added tn3270 and gcore for the DECstation.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7511 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/wisk.sh
+++ packs/build/wisk.sh
@@ -1,6 +1,6 @@
 #!/bin/athena/tcsh
 
-# $Revision: 1.30 $
+# $Revision: 1.31 $
 
 umask 2
 
@@ -39,7 +39,7 @@ set third="third/supported/afs third/supported/X11R5 third/supported/X11R4 third
 
 switch ( $machine )
   case decmips
-    set machthird="third/unsupported/ditroff third/unsupported/transcript-v2.1 third/supported/saber-3.0.1"
+    set machthird="third/unsupported/ditroff third/unsupported/transcript-v2.1 third/supported/saber-3.0.1 athena/ucb/tn3270 third/unsupported/gcore"
     breaksw
 
   case sun4
