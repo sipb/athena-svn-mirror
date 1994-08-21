a9cdfa54e76c5d33f4ad70b68605381e77631ff4
Craig Fields <cfields@mit.edu>
1994-08-21 19:30:27 +0000

    Don't declare index... Include string.h Add reverse resolution of addresses. 7.7 checkin; changes by brlewis
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7594 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/AL/get_phost.c
+++ athena/lib/AL/get_phost.c
@@ -1,6 +1,6 @@
 /*
  * $Source: /afs/dev.mit.edu/source/repository/athena/lib/AL/get_phost.c,v $
- * $Author: vrt $
+ * $Author: cfields $
  *
  * Copyright 1988 by the Massachusetts Institute of Technology.
  *
@@ -10,15 +10,14 @@
 
 #ifndef lint
 static char *rcsid_phost_c =
-"$Header: /afs/dev.mit.edu/source/repository/athena/lib/AL/get_phost.c,v 4.9 1993-06-01 12:55:07 vrt Exp $";
+"$Header: /afs/dev.mit.edu/source/repository/athena/lib/AL/get_phost.c,v 4.10 1994-08-21 19:30:27 cfields Exp $";
 #endif /* lint */
 
 #include <mit-copyright.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <netdb.h>
-
-char *index();
+#include <string.h>
 
 /*
  * This routine takes an alias for a host name and returns the first
@@ -33,6 +32,10 @@ char *index();
  * This should go away: the instance should be the domain name
  * (MENELAUS.MIT.EDU).  But for now we need this routine...
  *
+ * Then you have names like test.dialup.mit.edu, where the key you want
+ * is really "rcmd.pesto@ATHENA.MIT.EDU".  It's necessary to reverse-
+ * resolve the IP address.
+ *
  * A pointer to the name is returned, if found, otherwise a pointer
  * to the original "alias" argument is returned.
  */
@@ -41,32 +44,31 @@ char * krb_get_phost(alias)
     char *alias;
 {
     struct hostent *h;
-#ifdef SOLARIS
-    char phost[100];
+    static char phost[100];
     char *p;
+    extern int krb_ReverseResolve;
+
     strcpy(phost, alias);
-    if ((h=gethostbyname(&phost[0])) != (struct hostent *)NULL ) {
-        p = index( h->h_name, '.' );
-#else
-    char *phost = alias;
-    if ((h=gethostbyname(alias)) != (struct hostent *)NULL ) {
-        char *p = index( h->h_name, '.' );
-#endif
-        if (p)
-            *p = NULL;
-#ifdef SOLARIS
-	p = h->h_name;
+    if ((h=gethostbyname(phost)) != (struct hostent *)0 ) {
+        /* copy the new name */
+        strcpy(phost, h->h_name);
+
+	/* reverse-resolve the address */
+	if (krb_ReverseResolve) {
+	  if ((h=gethostbyaddr(h->h_addr, h->h_length, h->h_addrtype))
+	      != (struct hostent *)0)
+	    strcpy(phost, h->h_name);
+	}
+
+        /* cut off the domain name */
+        p = strchr( phost, '.' );
+        if (p) *p = '\0';
+
+	/* convert to lowercase */
+	p = phost;
 	do {
 		if (isupper(*p)) *p=tolower(*p);
 	} while  (*p++);
-        p = h->h_name;
-	strcpy(phost,p);
-#else
-        p = phost = h->h_name;
-#endif
-        do {
-            if (isupper(*p)) *p=tolower(*p);
-        } while (*p++);
     }
     return(phost);
 }
