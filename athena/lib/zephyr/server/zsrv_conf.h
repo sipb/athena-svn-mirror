ae48352b8d716ba36d7062dc5dbc669c3c145673
Greg Hudson <ghudson@mit.edu>
1994-10-31 05:53:01 +0000

    Moved config files to /etc/athena/zephyr.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7638 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/server/zsrv_conf.h
+++ athena/lib/zephyr/server/zsrv_conf.h
@@ -4,8 +4,8 @@
  *	Created by:	John T. Kohl
  *
  *	$Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/zsrv_conf.h,v $
- *	$Author: probe $
- *	$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/zsrv_conf.h,v 1.8 1993-11-19 15:57:35 probe Exp $
+ *	$Author: ghudson $
+ *	$Header: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/server/zsrv_conf.h,v 1.9 1994-10-31 05:53:01 ghudson Exp $
  *
  *	Copyright (c) 1988 by the Massachusetts Institute of Technology.
  *	For copying and distribution information, see the file
@@ -18,28 +18,26 @@
 
 /* Magic path names */
 #ifndef HESIOD
-#define SERVER_LIST_FILE	"/usr/athena/lib/zephyr/server.list"
+#define SERVER_LIST_FILE	"/etc/athena/zephyr/server.list"
 #endif
 
 /* ACL's for pre-registered classes */
 /* Directory containing acls and other info */
 #ifndef ZEPHYR_ACL_DIR
-#define	ZEPHYR_ACL_DIR		"/usr/athena/lib/zephyr/acl/"
+#define	ZEPHYR_ACL_DIR		"/etc/athena/zephyr/acl/"
 #endif
 /* name of the class registry */
 #define	ZEPHYR_CLASS_REGISTRY	"class-registry.acl"
 
 #ifdef KERBEROS
 /* name of file to hold the tickets for keys to exchange with other servers */
-#define	ZEPHYR_TKFILE		"/usr/athena/lib/zephyr/ztkts"
+#define	ZEPHYR_TKFILE		"/etc/athena/zephyr/ztkts"
 
-/* Pathname of Kerberos srvtab file.
- * WARNING: lib/ZCkAuth.c needs to have a corresponding definition! */
-#define SERVER_SRVTAB		"/usr/athena/lib/zephyr/srvtab"
+/* The pathname of the Kerberos srvtab file is defined in zephyr_conf.h. */
 #endif /* KERBEROS */
 
 /* default subscription file */
-#define	DEFAULT_SUBS_FILE	"/usr/athena/lib/zephyr/default.subscriptions"
+#define	DEFAULT_SUBS_FILE	"/etc/athena/zephyr/default.subscriptions"
 
 /* client defines */
 #define	REXMIT_SECS	((long) 20)	/* rexmit delay on normal notices */
