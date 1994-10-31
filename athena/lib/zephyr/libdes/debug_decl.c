3039be7c0b8cec0de94829a2ac791c23ae18dbc8
Greg Hudson <ghudson@mit.edu>
1994-10-31 05:57:11 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7640 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/lib/zephyr/libdes/debug_decl.c
@@ -0,0 +1,19 @@
+/*
+ * $Source: /afs/dev.mit.edu/source/repository/athena/lib/zephyr/libdes/debug_decl.c,v $
+ * $Author: ghudson $
+ *
+ * Copyright 1988 by the Massachusetts Institute of Technology.
+ *
+ * For copying and distribution information, please see the file
+ * <mit-copyright.h>.
+ *
+ * Declaration of debugging flag.
+ */
+
+#ifndef	lint
+static char rcsid_debug_decl_c[] =
+    "$Id: debug_decl.c,v 1.1 1994-10-31 05:54:16 ghudson Exp $";
+#endif
+
+#include <mit-copyright.h>
+int	des_debug	= 0;
