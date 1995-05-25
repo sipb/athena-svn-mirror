04a8ef72eb30a07fc59eff16201c49060f213499
Craig Fields <cfields@mit.edu>
1995-05-25 22:13:13 +0000

    As-is, eh? Sigh.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7835 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/config_afs.sh
+++ packs/maint/config_afs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -
 #
-# $Id: config_afs.sh,v 1.8 1993-09-15 17:38:35 cfields Exp $
+# $Id: config_afs.sh,v 1.9 1995-05-25 22:13:13 cfields Exp $
 #
 # This script configures the workstation's notion of AFS.
 # 1. It updates the cell location information from /usr/vice/etc/CellServDB
 # 2. It updates the cell setuid information from /usr/vice/etc/SuidCells
-PATH=/bin:/bin/athena; export PATH
+PATH=/bin:/bin/athena:/usr/bin; export PATH
 
 VICEDIR=/usr/vice/etc
 CELLDB=${VICEDIR}/CellServDB
@@ -17,13 +17,16 @@ cp /afs/athena.mit.edu/service/CellServDB ${VICEDIR}/Ctmp && \
 	[ -s ${VICEDIR}/Ctmp ] && \
 	mv -f ${VICEDIR}/Ctmp ${CELLDB}.public && \
 	cat ${CELLDB}.public ${CELLDB}.local >${VICEDIR}/Ctmp 2>/dev/null
+rm -f ${CELLDB}.last
+ln ${CELLDB} ${CELLDB}.last
 mv -f ${VICEDIR}/Ctmp ${CELLDB}
+chmod 644 ${CELLDB}
 
-/bin/awk ' \
+cmp -s ${CELLDB}.last ${CELLDB} || \
+awk ' \
 	  /^>/ {printf("\nfs newcell %s", substr($1,2,length($1)-1))}; \
 	  /^[0-9]/ {printf(" %s",$1)}; \
-	  END {printf("\n")}' ${CELLDB} | \
-	/bin/sh
+	  END {printf("\n")}' ${CELLDB} | sh
 
 echo "Updating setuid cell information"
 rm -f ${VICEDIR}/Ctmp
@@ -32,6 +35,7 @@ cp /afs/athena.mit.edu/service/SuidCells ${VICEDIR}/Ctmp && \
 	mv -f ${VICEDIR}/Ctmp ${SUIDDB}.public && \
 	cat ${SUIDDB}.public ${SUIDDB}.local >${VICEDIR}/Ctmp 2>/dev/null
 mv -f ${VICEDIR}/Ctmp ${SUIDDB}
+chmod 644 ${SUIDDB}
 
 echo "Only allowing setuid/setgid programs from the following cells:"
 
@@ -44,9 +48,16 @@ echo "Only allowing setuid/setgid programs from the following cells:"
 		for (j=1;j<=i;j++) { \
 		  if (suid[j]){ns++;scmd=scmd" "cells[j];\
 				print "echo",cells[j];}\
-		  else {nn++;ncmd=ncmd" "cells[j];}; } \
+		  else {nn++;ncmd=ncmd" "cells[j];};\
+		  if (ns>20) \
+		    {printf("fs setcell %s -suid\n", scmd);scmd="";ns=0;};\
+		  if (nn>20) \
+		    {printf("fs setcell %s -nosuid\n",ncmd);ncmd="";nn=0;};\
+		} \
 		if (ns) {printf("fs setcell %s -suid\n", scmd)};\
 		if (nn) {printf("fs setcell %s -nosuid\n",ncmd)};\
 	}' | sh
 
 exit 0
+--[3356]--
+
