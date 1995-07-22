d29e3befa0fdaffb90838337c1d4e5ae71709aeb
Craig Fields <cfields@mit.edu>
1995-07-22 20:48:50 +0000

    Defining flock constants here was incorrect; correct fix is to add -I/usr/ucbinclude to the compile line. See Imakefile.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@7961 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/lpr/filters/ln01raw.c
+++ athena/bin/lpr/filters/ln01raw.c
@@ -2,15 +2,6 @@
 #include <sys/types.h>
 #endif
 #include <sys/file.h>
-#ifdef SOLARIS
-/*
- * flock operations.
- */
-#define LOCK_SH               1       /* shared lock */
-#define LOCK_EX               2       /* exclusive lock */
-#define LOCK_NB               4       /* don't block when locking */
-#define LOCK_UN               8       /* unlock */
-#endif
 
 main()
 {
