afef92b3f3528ab15256cb55c332a660045123a9
Craig Fields <cfields@mit.edu>
1995-11-13 02:48:35 +0000

    Changed to use com_err.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8073 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/xdm/misc/pc.h
+++ athena/etc/xdm/misc/pc.h
@@ -2,6 +2,8 @@
 #define _PC_H_
 #include <sys/types.h>
 
+#include "pc_err.h"
+
 #ifndef _PC_BSD_H_
 typedef struct _pc_port pc_port;
 typedef struct _pc_state pc_state;
@@ -34,19 +36,20 @@ typedef struct {
 #define PC_CHOWN 2
 #define PC_CHMOD 4
 
-extern pc_state	*	pc_init(void);
-extern int 		pc_addport(pc_state *, pc_port *);
-extern int		pc_removeport(pc_state *, pc_port *);
-extern int 		pc_addfd(pc_state *, pc_fd *);
-extern int		pc_removefd(pc_state *, pc_fd *);
-extern pc_message *	pc_wait(pc_state *);
+extern long		pc_init(pc_state **);
+extern long 		pc_addport(pc_state *, pc_port *);
+extern long		pc_removeport(pc_state *, pc_port *);
+extern long 		pc_addfd(pc_state *, pc_fd *);
+extern long		pc_removefd(pc_state *, pc_fd *);
+extern long		pc_wait(pc_message **, pc_state *);
 
-extern pc_port *	pc_makeport(char *, int, uid_t, gid_t, mode_t);
-extern int		pc_chprot(pc_port *, uid_t, gid_t, mode_t);
-extern pc_port *	pc_openport(char *);
-extern int		pc_send(pc_message *);
-extern pc_message *	pc_receive(pc_port *);
-extern int		pc_close(pc_port *);
-extern int		pc_freemessage(pc_message *);
+extern long		pc_makeport(pc_port **, char *, int,
+				    uid_t, gid_t, mode_t);
+extern long		pc_chprot(pc_port *, uid_t, gid_t, mode_t);
+extern long		pc_openport(pc_port **, char *);
+extern long		pc_send(pc_message *);
+extern long		pc_receive(pc_message **, pc_port *);
+extern long		pc_close(pc_port *);
+extern long		pc_freemessage(pc_message *);
 
 #endif /* _PC_H_ */
