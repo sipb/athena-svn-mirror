23f7b05e7348bd8bb544fb128c2a16ee53e2d0f7
Craig Fields <cfields@mit.edu>
1995-11-25 01:09:15 +0000

    rewrote exchangeVars to retry when N_RETRY is true, and cleaned up error handling. Turned some strings into macros ("OK" -> N_OK, etc.) changed loginUser to also return the current tty added logoutUser
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8102 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/xdm/misc/nannylib.c
+++ athena/etc/xdm/misc/nannylib.c
@@ -4,94 +4,79 @@
 #include "var.h"
 #include "cvt.h"
 
+#define C(stuff) if (stuff) goto cleanup
+
 int nanny_exchangeVars(varlist *vin, varlist **vout)
 {
-  pc_state *ps;
-  pc_port *outport;
-  pc_message *message;
-  buffer *buf;
+  pc_state *ps = NULL;
+  pc_port *outport = NULL;
+  pc_message messageIn, *messageOut = NULL;
+  buffer *bufIn = NULL, bufOut;
+  char *value;
   int ret;
 
   if (vin == NULL || vout == NULL)
     return 1;
 
-  message = malloc(sizeof(pc_message));
-  if (message == NULL)
+  C(cvt_vars2buf(&bufIn, vin));
+
+  messageIn.data = bufIn->buf;
+  messageIn.length = bufIn->len;
+
+  C(pc_init(&ps));
+
+  while (1)
+    {
+      C(pc_openport(&outport, NANNYPORT)); /* create outport */
+
+      messageIn.source = outport;
+      C(pc_send(&messageIn));
+
+      pc_addport(ps, outport);
+      C(pc_wait(&messageOut, ps));	/* create messageOut */
+      pc_removeport(ps, outport);
+      pc_close(outport);		outport = NULL;
+
+      bufOut.buf = messageOut->data;
+      bufOut.len = messageOut->length;
+
+      C(var_init(vout));		/* create *vout */
+      C(cvt_buf2vars(*vout, &bufOut));
+
+      pc_freemessage(messageOut);	messageOut = NULL;
+
+      if (var_getString(*vout, N_RETRY, &value) ||
+	  !strcmp(value, N_FALSE))
+	{
+	  pc_destroy(ps);
+	  free(bufIn->buf);
+	  free(bufIn);
+	  return 0;
+	}
+
+      var_destroy(*vout);		*vout = NULL;
+      sleep(1);
+    }
+
+  cleanup:
+    if (messageOut)
+      pc_freemessage(messageOut);
+    if (bufIn)
+      {
+	free(bufIn->buf);
+	free(bufIn);
+      }
+    if (outport)
+      pc_close(outport);
+    if (ps)
+      pc_destroy(ps);
+    if (*vout)
+      {
+	var_destroy(*vout);
+	*vout = NULL;
+      }
+
     return 1;
-
-  if (cvt_vars2buf(&buf, vin))
-    {
-      free(message);
-      return 1;
-    }
-
-  message->data = buf->buf;
-  message->length = buf->len;
-
-  if (pc_openport(&outport, NANNYPORT))
-    {
-      free(buf->buf);
-      free(buf);
-      free(message);
-      return 1;
-    }
-
-  message->source = outport;
-  ret = pc_send(message);
-
-  free(buf->buf);
-  free(message);
-
-  if (ret)
-    {
-      pc_close(outport);
-      free(buf);
-      return 1;
-    }
-
-  if (pc_init(&ps))
-    {
-      pc_close(outport);
-      free(buf);
-      return 1;
-    }
-
-  pc_addport(ps, outport);
-  ret = pc_wait(&message, ps);
-
-  pc_removeport(ps, outport);
-  pc_destroy(ps);
-  pc_close(outport);
-
-  if (ret)
-    {
-      free(buf);
-      return 1;
-    }
-
-  buf->buf = message->data;
-  buf->len = message->length;
-
-  if (var_init(vout))
-    {
-      pc_freemessage(message);
-      free(buf);
-      return 1;
-    }
-
-  if (cvt_buf2vars(*vout, buf))
-    {
-      pc_freemessage(message);
-      free(buf);
-      var_destroy(*vout);
-      *vout = NULL;
-      return 1;
-    }
-
-  pc_freemessage(message);
-  free(buf);
-
-  return 0;
 }
 
 int nanny_setupUser(char *name, int add, char **env, char **args)
@@ -99,8 +84,6 @@ int nanny_setupUser(char *name, int add, char **env, char **args)
   varlist *vsend = NULL, *vget = NULL;
   buffer *buf = NULL;
   char *value;
-
-#define C(stuff) if (stuff) goto cleanup
 
   if (name == NULL || env == NULL || args == NULL)
     return 1;
@@ -124,7 +107,7 @@ int nanny_setupUser(char *name, int add, char **env, char **args)
   var_destroy(vsend);  vsend = NULL;
 
   C(var_getString(vget, N_USER, &value));
-  if (strcmp(value, "OK"))
+  if (strcmp(value, N_OK))
     {
       var_destroy(vget);
       return 1;
@@ -148,14 +131,14 @@ int nanny_setupUser(char *name, int add, char **env, char **args)
   return 1;
 }
 
-int nanny_loginUser(char ***env, char ***args)
+int nanny_loginUser(char ***env, char ***args, char **tty)
 {
   varlist *vsend = NULL, *vget = NULL;
   buffer buf;
   char *value;
   int len;
 
-  if (env == NULL || args == NULL)
+  if (env == NULL || args == NULL || tty == NULL)
     return 1;
 
   *env = NULL;
@@ -164,15 +147,20 @@ int nanny_loginUser(char ***env, char ***args)
   if (var_init(&vsend))
     return 1;
 
-  C(var_setString(vsend, N_LOGGED_IN, "TRUE"));
+  C(var_setString(vsend, N_LOGGED_IN, N_TRUE));
   C(var_setString(vsend, N_ENV, cvt_query));
   C(var_setString(vsend, N_XSESSARGS, cvt_query));
+  C(var_setString(vsend, N_TTY, cvt_query));
 
   C(nanny_exchangeVars(vsend, &vget));
   var_destroy(vsend);  vsend = NULL;
 
   C(cvt_var2strings(vget, N_ENV, env));
   C(cvt_var2strings(vget, N_XSESSARGS, args));
+
+  C(var_getString(vget, N_TTY, &value));
+  C(!(*tty = malloc(strlen(value) + 1)));
+  strcpy(*tty, value);
 
   /* XXX Check "LOGGED_IN=OK" */
 
@@ -186,6 +174,36 @@ int nanny_loginUser(char ***env, char ***args)
     var_destroy(vget);
   if (*env)
     cvt_freeStrings(*env);
+  if (*args)
+    cvt_freeStrings(*args);
+  return 1;
+}
+
+int nanny_logoutUser()
+{
+  varlist *vsend = NULL, *vget = NULL;
+  buffer buf;
+  char *value;
+  int len;
+
+  if (var_init(&vsend))
+    return 1;
+
+  C(var_setString(vsend, N_LOGGED_IN, N_FALSE));
+
+  C(nanny_exchangeVars(vsend, &vget));
+  var_destroy(vsend);  vsend = NULL;
+
+  /* XXX Check "LOGGED_IN=OK"... or something */
+
+  var_destroy(vget);
+  return 0;
+
+ cleanup:
+  if (vsend)
+    var_destroy(vsend);
+  if (vget)
+    var_destroy(vget);
 
   return 1;
 }
