eefedfcbb20ac1f680f9b92bf54b1702c29131b7
Craig Fields <cfields@mit.edu>
1995-11-25 01:07:17 +0000

    remove a column in prototypes add pc_secure prototype
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8100 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/xdm/misc/pc.h
+++ athena/etc/xdm/misc/pc.h
@@ -36,21 +36,21 @@ typedef struct {
 #define PC_CHOWN 2
 #define PC_CHMOD 4
 
-extern long		pc_init(pc_state **);
-extern long		pc_destroy(pc_state *);
-extern long 		pc_addport(pc_state *, pc_port *);
-extern long		pc_removeport(pc_state *, pc_port *);
-extern long 		pc_addfd(pc_state *, pc_fd *);
-extern long		pc_removefd(pc_state *, pc_fd *);
-extern long		pc_wait(pc_message **, pc_state *);
-
-extern long		pc_makeport(pc_port **, char *, int,
-				    uid_t, gid_t, mode_t);
-extern long		pc_chprot(pc_port *, uid_t, gid_t, mode_t);
-extern long		pc_openport(pc_port **, char *);
-extern long		pc_send(pc_message *);
-extern long		pc_receive(pc_message **, pc_port *);
-extern long		pc_close(pc_port *);
-extern long		pc_freemessage(pc_message *);
+extern long	pc_init(pc_state **);
+extern long	pc_destroy(pc_state *);
+extern long 	pc_addport(pc_state *, pc_port *);
+extern long	pc_removeport(pc_state *, pc_port *);
+extern long 	pc_addfd(pc_state *, pc_fd *);
+extern long	pc_removefd(pc_state *, pc_fd *);
+extern long	pc_wait(pc_message **, pc_state *);
+extern long	pc_secure(pc_state *, pc_port *, pc_message *);
+extern long	pc_makeport(pc_port **, char *, int,
+			    uid_t, gid_t, mode_t);
+extern long	pc_chprot(pc_port *, uid_t, gid_t, mode_t);
+extern long	pc_openport(pc_port **, char *);
+extern long	pc_send(pc_message *);
+extern long	pc_receive(pc_message **, pc_port *);
+extern long	pc_close(pc_port *);
+extern long	pc_freemessage(pc_message *);
 
 #endif /* _PC_H_ */
