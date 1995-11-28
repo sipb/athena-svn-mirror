0882b74c788508f7271b143e273f365bcbfc9b33
Craig Fields <cfields@mit.edu>
1995-11-28 23:55:34 +0000

    The code was mistakenly assuming that there was a persistent copy of the attachtab for attachtab_lookup to be searching, in order to avoid trying to detach things we already know are detached. But there wasn't, so attachtab_lookup always returned false, so we had nothing to actually detach. So now we make ourselves a copy of the attachtab to be consulted. While we're at it, we also preserve the value of explicit (in case that's important).
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8147 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/attach/mul.c
+++ athena/bin/attach/mul.c
@@ -1,10 +1,10 @@
 /*
- * $Header: /afs/dev.mit.edu/source/repository/athena/bin/attach/mul.c,v 1.5 1994-03-25 15:58:31 miki Exp $
+ * $Header: /afs/dev.mit.edu/source/repository/athena/bin/attach/mul.c,v 1.6 1995-11-28 23:55:34 cfields Exp $
  *
  * Copyright (c) 1990 by the Massachusetts Institute of Technology.
  */
 
-static char *rcsid_mul_c = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/attach/mul.c,v 1.5 1994-03-25 15:58:31 miki Exp $";
+static char *rcsid_mul_c = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/attach/mul.c,v 1.6 1995-11-28 23:55:34 cfields Exp $";
 
 
 #include "attach.h"
@@ -33,22 +33,36 @@ int mul_detach(atp)
 struct _attachtab *atp;
 {
     int status = SUCCESS;
+    int tempexp;
     char mul_buf[BUFSIZ], *cp;
-    
+    extern struct _attachtab	*attachtab_last, *attachtab_first;
+    struct _attachtab *atptmp;
+
+    lock_attachtab();
+    get_attachtab();
+    unlock_attachtab();
+
+    atptmp = attachtab_first;
+
+    tempexp = explicit;
     strcpy(mul_buf, atp->hostdir);
     cp = &mul_buf[strlen(mul_buf)];
     while (cp-- >= mul_buf) {
 	explicit = 0;
+	attachtab_first = atptmp;
 	if (cp < mul_buf || *cp == ',') {
 	    if (attachtab_lookup(cp+1)
 		&& detach(cp+1) != SUCCESS
 		&& error_status != ERR_DETACHNOTATTACHED)
-		status = FAILURE;
+	      status = FAILURE;
 	    if (cp >= mul_buf)
 		*cp = '\0';
 	}
     }
 
+    attachtab_first = atptmp;
+    free_attachtab();
+    explicit = tempexp;
     error_status = (status == SUCCESS) ? ERR_NONE : ERR_SOMETHING;
     return status;
 }
