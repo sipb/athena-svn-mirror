918a00a74a2886c7f8e5aabf5b3fa21e4bcdba01
Greg Hudson <ghudson@mit.edu>
1996-04-17 05:51:11 +0000

    Use sunsoft cc instead of printing out an error message, as decided by the release team.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8282 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/platform/sun4/cc.sh
+++ packs/glue/platform/sun4/cc.sh
@@ -1,23 +1,5 @@
 #! /bin/sh
-echo 'At the present time Sun Microsystems does not ship a C compiler'
-echo 'with its Solaris operating system.'
-echo
-echo 'There is no command "cc".'
-echo
-echo 'The various C compilers available are so different in the'
-echo 'arguments they take that we felt it would be misleading to'
-echo 'arbitrarily choose a C compiler and argument set and install it'
-echo 'as cc.  So instead you are getting this printout.'
-echo
-echo 'The SunSoft C compiler is now available. You may access it'
-echo 'by typing "add sunsoft" at the athena prompt.  The compiler'
-echo 'is called suncc.'
-echo
-echo 'The Cygnus gcc compiler is available.  You may access it'
-echo 'by typing "add cygnus" at the athena prompt.  The compiler is'
-echo 'called gcc, and may be accessed by running /mit/cygnus/sun4bin/gcc.'
-echo
-echo 'The Athena software was built with /usr/gcc/bin/gcc, which was'
-echo 'built from the Cygnus 95q2 gcc source tree.'
-echo
-exit 1
+if [ ! -x /mit/sunsoft/sun4bin/cc ]; then
+	attach -h -q sunsoft
+fi
+/mit/sunsoft/sun4bin/cc "$@"
