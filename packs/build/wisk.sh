ff7d16a41f7767137d8ee3b0502431009ec0e8b5
Greg Hudson <ghudson@mit.edu>
1996-05-23 20:45:56 +0000

    Add third/supported/mae-2.0 to machthird for sun4; supporting materials for the Mac emulator.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8455 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/wisk.sh
+++ packs/build/wisk.sh
@@ -14,7 +14,7 @@
 #	endpackage	the name of the package in the package list to
 #			stop building at
 
-# $Revision: 1.66 $
+# $Revision: 1.67 $
 
 umask 2
 
@@ -69,7 +69,7 @@ switch ( $machine )
     breaksw
 
   case sun4
-    set machthird="third/unsupported/ditroff third/supported/talk third/unsupported/x3270 third/unsupported/transcript-v4.1 third/unsupported/ansi third/unsupported/jove third/unsupported/learn"
+    set machthird="third/unsupported/ditroff third/supported/talk third/unsupported/x3270 third/unsupported/transcript-v4.1 third/unsupported/ansi third/unsupported/jove third/unsupported/learn third/supported/mae-2.0"
     breaksw
 
   case rsaix
