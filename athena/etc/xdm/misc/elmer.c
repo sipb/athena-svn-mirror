cdb1e2709a01314702e7da334ac667c16bb84ab1
Craig Fields <cfields@mit.edu>
1996-05-28 01:02:07 +0000

    Introduction of hair to elmer. In case xlogin failed to communicate to nanny about who wants to log in, and we're root (usually because the workstation is broken in a way that caused xlogin to fail, so xdm put up a login screen itself), allow root to get logged in so we're not hosed.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8483 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/xdm/misc/elmer.c
+++ athena/etc/xdm/misc/elmer.c
@@ -15,21 +15,28 @@
  */
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 #define DELAY 5
+#define ROOT (uid_t)0
+#define N_ROOT "root"
 
 char *session = "/etc/athena/login/Xsession";
 
-main(int argc, char **argv)
+char *defargs[2] = { "1", "" };
+
+main(int argc, char **argv, char **envp)
 {
   char **env, **args, *tty;
   char **ptr, *name;
   char *prelogin;
-  int c;
+  char athconsole[64];
+  int c, nannyKnows;
+  pid_t uid;
 
   if (argv[0])
     {
@@ -40,7 +47,46 @@ main(int argc, char **argv)
   else
     name = "elmer";
 
-  if (!nanny_loginUser(&env, &args, &tty))
+  uid = getuid();
+  nannyKnows = !nanny_loginUser(&env, &args, &tty);
+
+  /*
+   * If xlogin failed for some reason and we fell back to xdm,
+   * enable root logins to be possible via xdm. In this case,
+   * nanny hasn't yet been informed that anyone is getting logged
+   * in, so we try to do it. But we're not too fancy.
+   *
+   * Of course, this is only one reason the loginUser call may
+   * be failing. If it's failing for other reasons, this code
+   * will still help us get through a root login.
+   */
+  if (!nannyKnows && uid == ROOT)
+    {
+      if (!nanny_setupUser(N_ROOT, 0, envp, defargs))
+	nannyKnows = !nanny_loginUser(&env, &args, &tty);
+
+
+      /* If we're here because xdm took over, it killed the console.
+	 Therefore, we make sure it's running. */
+      nanny_setXConsolePref(1);
+
+      /*
+       * If for some strange reason our efforts failed anyway,
+       * still try to get the tty information properly.
+       */
+      if (!nannyKnows)
+	{
+	  env = NULL;
+	  args = defargs;
+
+	  if (nanny_getTty(athconsole, sizeof(athconsole)))
+	    tty = "/dev/console";
+	  else
+	    tty = athconsole;
+	}
+    }
+
+  if (nannyKnows || uid == ROOT)
     {
       /* Redirect stdout and stderr to the console window. */
       if (NULL == freopen(tty, "w", stdout))
@@ -49,8 +95,9 @@ main(int argc, char **argv)
       if (NULL == freopen(tty, "w", stderr))
 	(void)freopen("/dev/console", "w", stderr);
 
-      for (ptr = env; *ptr != NULL; ptr++)
-	putenv(*ptr);
+      if (env)
+	for (ptr = env; *ptr != NULL; ptr++)
+	  putenv(*ptr);
 
       setpag();
 
@@ -79,7 +126,8 @@ main(int argc, char **argv)
       else
 	waitpid(c, NULL, 0);
 
-      nanny_logoutUser();
+      if (nannyKnows)
+	nanny_logoutUser();
     }
   else
     {
