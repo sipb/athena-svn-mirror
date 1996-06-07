faa223494b08fc0eb7ea579bd50a96589f22ecdb
Greg Hudson <ghudson@mit.edu>
1996-06-07 00:35:30 +0000

    Modernize, and enforce consistency.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8537 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/newpag/newpag.c
+++ athena/bin/newpag/newpag.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-	
-main(argc, argv)
+#include <stdlib.h>
+#include <unistd.h>
+
+return main(argc, argv)
 	int argc;
 	char **argv;
 {
-	extern char *getenv();
 	char *shell = getenv("SHELL");
 	int code = setpag();
+
 	if (code != 0) {
 		perror("setpag");
 		exit(1);
 	}
 	if (argc == 1)
-		execl (shell != NULL ? shell : "/bin/sh", "sh", 0);
+		execl(shell != NULL ? shell : "/bin/sh", "sh", 0);
 	else 
-		execvp (argv[1], argv+1);
+		execvp(argv[1], argv + 1);
 	exit(1);
 }
