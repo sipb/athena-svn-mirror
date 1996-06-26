d64c4075af766baf25d26a8cdcebd3339616ed6f
Greg Hudson <ghudson@mit.edu>
1996-06-26 07:50:11 +0000

    Only declare variables here for telnetd, not for libtelnet.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8609 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/telnet/telnetd/ext.h
+++ athena/bin/telnet/telnetd/ext.h
@@ -195,8 +195,6 @@ extern void
 	writenet P((unsigned char *, int));
 
 #ifdef	ENCRYPTION
-extern void	(*encrypt_output) P((unsigned char *, int));
-extern int	(*decrypt_input) P((int));
 extern char	*nclearto;
 #endif	/* ENCRYPTION */
 
