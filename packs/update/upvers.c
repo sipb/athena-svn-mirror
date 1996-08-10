55c2e01f4a2163cd157d0f2a02519681fdd81889
Craig Fields <cfields@mit.edu>
1996-08-10 21:38:19 +0000

    Cleaned up strings.h bogosity, {r,}index->str{r,}chr, removed direct support.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8742 728af825-273c-0410-89f9-f7d3b574a069

--- packs/update/upvers.c
+++ packs/update/upvers.c
@@ -1,16 +1,12 @@
 /*
- * $Header: /afs/dev.mit.edu/source/repository/packs/update/upvers.c,v 1.11 1994-05-26 17:57:13 cfields Exp $
+ * $Header: /afs/dev.mit.edu/source/repository/packs/update/upvers.c,v 1.12 1996-08-10 21:38:19 cfields Exp $
  * $Source: /afs/dev.mit.edu/source/repository/packs/update/upvers.c,v $
  * $Author: cfields $
  */
  
 #include	<sys/types.h>
-#ifdef SOLARIS
-#include 	<strings.h>
-#include      <dirent.h>
-#else
-#include	<sys/dir.h>
-#endif
+#include 	<string.h>
+#include	<dirent.h>
 #include	<ctype.h>
 
 struct	verfile {
@@ -20,7 +16,7 @@ struct	verfile {
 } vf[1024];
 
 #ifndef lint
-char	rcsid[] = "$Header: /afs/dev.mit.edu/source/repository/packs/update/upvers.c,v 1.11 1994-05-26 17:57:13 cfields Exp $";
+char	rcsid[] = "$Header: /afs/dev.mit.edu/source/repository/packs/update/upvers.c,v 1.12 1996-08-10 21:38:19 cfields Exp $";
 #endif
 
 main(argc, argv)
@@ -34,23 +30,20 @@ char	*argv[];
 	int	olddeg, newdeg, start, end, vcmp();
 	char	file[80];
 	extern	int	errno;
-#ifdef POSIX
 	struct	dirent	*dirp;
-#else
-	struct	direct	*dirp;
-#endif
+
 	if (argc < 4) 
 		puts("Usage: verup <old-vers> <new-vers> <libdir>"), exit(2);
 
 	if (chdir(argv[3]) == -1) 
 		perror(argv[3]), exit(1);
- 	oldmjr = atoi(index(argv[1], '.') - 1);
-	oldmnr = atoi(rindex(argv[1], '.') + 1);
+ 	oldmjr = atoi(strchr(argv[1], '.') - 1);
+	oldmnr = atoi(strrchr(argv[1], '.') + 1);
 	if (isalpha(argv[1][strlen(argv[1]) - 1]))
 		olddeg = argv[1][strlen(argv[1]) - 1];
 		
-	newmjr = atoi(index(argv[2], '.') - 1);
-	newmnr = atoi(rindex(argv[2], '.') + 1);
+	newmjr = atoi(strchr(argv[2], '.') - 1);
+	newmnr = atoi(strrchr(argv[2], '.') + 1);
 	if (isalpha(argv[2][strlen(argv[2]) - 1]))
 		newdeg = argv[2][strlen(argv[2]) - 1];
 		
@@ -59,8 +52,8 @@ char	*argv[];
 		
 	while (dirp = readdir(dp)) {
 		if(isdigit(dirp->d_name[0])) {
-			vf[n].mjr = atoi(index(dirp->d_name, '.') - 1);
-			vf[n].mnr = atoi(rindex(dirp->d_name, '.') + 1);
+			vf[n].mjr = atoi(strchr(dirp->d_name, '.') - 1);
+			vf[n].mnr = atoi(strrchr(dirp->d_name, '.') + 1);
 			if (isalpha(dirp->d_name[strlen(dirp->d_name) - 1]))
 				vf[n].deg = dirp->d_name[strlen(dirp->d_name) - 1];
 			n++;
