62586db7fbfe48eb3de8535ff48582870c12edc5
Greg Hudson <ghudson@mit.edu>
1996-09-17 18:46:19 +0000

    Script to set AFS permissions on either the repository or on a checked-out tree.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8827 728af825-273c-0410-89f9-f7d3b574a069

---
+++ CVSROOT/afs-protections.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+genacl="write:source write read:source read adm:source adm read:staff read"
+genacl="$genacl system:anyuser read"
+fascist="system:anyuser l"
+lessfascist="system:anyuser l system:authuser read"
+
+find . -type d -print | xargs fs sa -acl $genacl -clear -dir
+find athena/bin/lpr -type d -print | xargs fs sa -acl $fascist -dir
+find athena/bin/quota -type d -print | xargs fs sa -acl $fascist -dir
+find athena/bin/login -type d -print | xargs fs sa -acl $fascist -dir
+find athena/bin/write -type d -print | xargs fs sa -acl $fascist -dir
+find athena/bin/voldump -type d -print | xargs fs sa -acl $fascist -dir
+find athena/lib/gdss/lib -type d -print | xargs fs sa -acl $fascist -dir
+find athena/etc/synctree -type d -print | xargs fs sa -acl $lessfascist -dir
+find athena/etc/ftpd -type d -print | xargs fs sa -acl $fascist -dir
