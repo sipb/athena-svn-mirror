2c74b54c79cc0a2aa02de9c6208428dabf027fa4
Greg Hudson <ghudson@mit.edu>
1996-09-19 08:13:43 +0000

    Eliminate the window where some non-public sources became readable. Use a different acl for the repository and /source, so staff can't  touch /source and the wash can't touch the repository. Deprecate write:source in favor of a larger group write:staff.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8839 728af825-273c-0410-89f9-f7d3b574a069

--- CVSROOT/afs-protections.sh
+++ CVSROOT/afs-protections.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 
-genacl="write:source write read:source read adm:source la read:staff read"
-genacl="$genacl system:anyuser read"
-fascist="system:anyuser l"
-lessfascist="system:anyuser l system:authuser read"
+genacl="read:source read adm:source la"
 
-find . -type d -print | xargs fs sa -acl $genacl -clear -dir
-find athena/bin/lpr -type d -print | xargs fs sa -acl $fascist -dir
-find athena/bin/quota -type d -print | xargs fs sa -acl $fascist -dir
-find athena/bin/login -type d -print | xargs fs sa -acl $fascist -dir
-find athena/bin/write -type d -print | xargs fs sa -acl $fascist -dir
-find athena/bin/voldump -type d -print | xargs fs sa -acl $fascist -dir
-find athena/lib/gdss/lib -type d -print | xargs fs sa -acl $fascist -dir
-find athena/etc/synctree -type d -print | xargs fs sa -acl $lessfascist -dir
-find athena/etc/ftpd -type d -print | xargs fs sa -acl $fascist -dir
+case "$1" in
+	repository)
+		genacl="$genacl write:staff write"
+		;;
+	wd)
+		genacl="$genacl read:staff read write:update write"
+		;;
+	*)
+		echo "Usage: $0 {repository|wd}"
+		exit 1
+		;;
+esac
+
+public="$genacl system:anyuser read"
+fascist="$genacl system:anyuser l"
+auth="$genacl system:anyuser l system:authuser read"
+
+attach -h -n -q gnu
+`athdir /mit/gnu bin`/gfind . -noleaf \
+	-path ./athena/bin/lpr -prune -o \
+	-path ./athena/bin/quote -prune -o \
+	-path ./athena/bin/login -prune -o \
+	-path ./athena/bin/write -prune -o \
+	-path ./athena/bin/voldump -prune -o \
+	-path ./athena/lib/gdss/lib -prune -o \
+	-path ./athena/etc/synctree -prune -o \
+	-path ./athena/etc/ftpd -prune -o \
+	-type d -print | xargs fs sa -acl $public -clear -dir
+
+find athena/bin/lpr -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/bin/quota -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/bin/login -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/bin/write -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/bin/voldump -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/lib/gdss/lib -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/etc/synctree -type d -print | xargs fs sa -acl $auth -clear -dir
+find athena/etc/ftpd -type d -print | xargs fs sa -acl $fascist -clear -dir
