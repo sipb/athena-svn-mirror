b0b81fb23b9b5cfa90bb5a52813069b181d810db
Greg Hudson <ghudson@mit.edu>
1996-09-20 04:26:08 +0000

    Regenerate
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@8922 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/AL/ptypes.h
+++ athena/lib/AL/ptypes.h
@@ -11,20 +11,30 @@ void initialize_ale_error_table P_((void));
 /* etalw.c */
 void initialize_alw_error_table P_((void));
 
+/* init.c */
+long ALinit P_((void));
+long ALinitAL P_((ALsession session, ALflag_t initial_flags));
+long ALinitSession P_((ALsession session));
+
 /* setuser.c */
-long ALsetUser P_((ALsession session, char *uname, ALflag_t initial_flags));
+long ALinitUser P_((ALsession session, ALflag_t initial_flags));
+long ALsetUser P_((ALsession session, char *uname));
 
 /* passwd.c */
 long ALincRefCount P_((ALsession session));
+int ALlockPasswdFile P_((ALsession session));
+int ALunlockPasswdFile P_((ALsession session));
 void ALgetErrorContextFromFile P_((ALsession session, char *filename));
 long ALaddPasswdEntry P_((ALsession session));
 long ALremovePasswdEntry P_((ALsession session));
 
 /* modify.c */
-int ALopenLockFile P_((char *filename));
+char *ALlockFile P_((int lockfile));
+int ALopenLockFile P_((ALsession session, int lockfile));
+int ALcloseLockFile P_((ALsession session, int lockfile));
 long ALmodifyRemoveUser P_((ALsession session, char buf[]));
 long ALmodifyAppendPasswd P_((ALsession session, int fd));
-long ALmodifyLinesOfFile P_((ALsession session, char *filename, char *lockfilename, long (*modify )(ALsession, char[]), long (*append )(ALsession, int )));
+long ALmodifyLinesOfFile P_((ALsession session, char *filename, int lockfile, long (*modify )(ALsession, char[]), long (*append )(ALsession, int )));
 
 /* group.c */
 char *ALcopySubstring P_((char *string, char buffer[], int delimiter));
@@ -46,5 +56,11 @@ long ALgetHomedir P_((ALsession session));
 char *krb_get_phost P_((char *alias));
 
 /* getrealm.c */
+
+/* utmp.c */
+long ALinitUtmp P_((ALsession session));
+long ALsetUtmpInfo P_((ALsession session, ALflag_t flags, ALut *ut));
+long ALsetTtyFd P_((ALsession sess, int fd));
+long ALputUtmp P_((ALsession sess));
 
 #undef P_
