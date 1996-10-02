a23129a905e6a909c5d2ac4ad93b5cf3074728a7
Greg Hudson <ghudson@mit.edu>
1996-10-02 06:09:35 +0000

    Import of tcsh 6.03
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@9005 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/tcsh/patchlevel.h
@@ -0,0 +1,14 @@
+/* $Header: /afs/dev.mit.edu/source/repository/third/tcsh/patchlevel.h,v 1.1.1.1 1996-10-02 06:09:30 ghudson Exp $ */
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Cornell"
+#define REV 6
+#define VERS 3
+#define PATCHLEVEL 0
+#define DATE "92/11/20"
+
+#endif /* _h_patchlevel */
