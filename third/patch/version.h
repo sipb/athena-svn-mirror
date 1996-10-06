0c4ef581e26b3505e751b0c54aed472bc2271410
Greg Hudson <ghudson@mit.edu>
1996-10-06 21:29:02 +0000

    Import of patch 2.1
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@9068 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/patch/version.h
@@ -0,0 +1,9 @@
+/* $Header: /afs/dev.mit.edu/source/repository/third/patch/version.h,v 1.1.1.1 1996-10-06 21:29:02 ghudson Exp $
+ *
+ * $Log: not supported by cvs2svn $
+ * Revision 2.0  86/09/17  15:40:14  lwall
+ * Baseline for netwide release.
+ * 
+ */
+
+void version();
