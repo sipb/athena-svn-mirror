a6a8e5892138a1a085addd6d9068c787bc8a400f
Greg Hudson <ghudson@mit.edu>
1996-10-07 07:14:34 +0000

    This commit was generated by cvs2svn to compensate for changes in r9072, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@9073 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/mh/sbr/formatdef.c
@@ -0,0 +1,7 @@
+/* formatdef.c - some defines for sbr/formatsbr.c */
+#ifndef	lint
+static char ident[] = "@(#)$Id: formatdef.c,v 1.1.1.1 1996-10-07 07:13:50 ghudson Exp $";
+#endif	/* lint */
+#include "../h/addrsbr.h"
+
+int	fmt_norm = AD_NAME;
