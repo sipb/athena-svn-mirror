6bd63e9b3d11ef902c8db00ff8a78582cf67baa8
Greg Hudson <ghudson@mit.edu>
1997-01-11 19:21:02 +0000

    Conform to the Athena coding standards. kill() does not return ESRCH; it returns -1 and sets errno to ESRCH.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@9521 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/access/access_off.c
+++ athena/bin/access/access_off.c
@@ -1,48 +1,54 @@
-/*
- *	$Source: /afs/dev.mit.edu/source/repository/athena/bin/access/access_off.c,v $
- *	$Header: /afs/dev.mit.edu/source/repository/athena/bin/access/access_off.c,v 1.4 1995-10-08 22:16:26 cfields Exp $
+/* Copyright 1996 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
  */
 
-#ifndef lint
-static char *rcsid_access_off_c = "$Header: /afs/dev.mit.edu/source/repository/athena/bin/access/access_off.c,v 1.4 1995-10-08 22:16:26 cfields Exp $";
-#endif /* lint */
+static const char rcsid[] = "$Id: access_off.c,v 1.5 1997-01-11 19:21:02 ghudson Exp $";
 
 #include <stdio.h>
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
 
-main(argc,argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
-    FILE *pidfile;
-    int pid;
-    int status;
-    char *pindex;
+  FILE *pidfile;
+  int pid, status, signo;
+  char *progname;
 
-#ifdef ultrix
-    if ((pidfile = fopen("/etc/inetd.pid", "r")) == NULL) {
-#else
-    if ((pidfile = fopen("/etc/athena/inetd.pid", "r")) == NULL) {
-#endif
-	printf("cannot read process id file--daemon probably not running\n");
-	exit(1);
+  pidfile = fopen("/etc/athena/inetd.pid", "r");
+  if (!pidfile)
+    {
+      printf("cannot read process id file--daemon probably not running\n");
+      return 1;
     }
-    if (fscanf(pidfile, "%d", &pid) != 1) {
-	printf("error reading process id file\n");
-	exit(1);
+  if (fscanf(pidfile, "%d", &pid) != 1)
+    {
+      printf("error reading process id file\n");
+      return 1;
     }
-    fclose(pidfile);
-    if ((pindex = strrchr(argv[0], '/')) == (char *) NULL)
-      pindex = argv[0];
-    else pindex++;
+  fclose(pidfile);
 
-    if (status = kill(pid, !strcmp(pindex, "access_off") ?
-		      SIGUSR2 : SIGUSR1)) {
-			  if (status != ESRCH)
-			    perror("error killing daemon");
-			  exit(1);
-		      }
-    exit(0);
+  progname = strrchr(argv[0], '/');
+  progname = (progname) ? progname + 1 : argv[0];
+  signo = (strcmp(progname, "access_off") == 0) ? SIGUSR2 : SIGUSR1;
+
+  if (kill(pid, signo) < 0)
+    {
+      if (errno != ESRCH)
+	  perror("error killing daemon");
+      return 1;
+    }
+
+  return 0;
 }
