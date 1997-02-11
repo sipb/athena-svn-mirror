3367c49c1eebd70d4ee9eadf0c27be77b7b523a7
Greg Hudson <ghudson@mit.edu>
1997-02-11 18:26:48 +0000

    Conform to standard style for scripts. Add a -a option to force sync of all variables. Simplify handling of repeat synchronization. Introduce a function to handle symlinks in rc0 and rc2 (will make  it easier to do the Solaris version). Exit with status 1 if scripts were added to /etc/rc2.d, so we can  reboot if that happens at startup time.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@9592 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/platform/sgi/syncconf.sh
+++ packs/maint/platform/sgi/syncconf.sh
@@ -1,281 +1,328 @@
 #!/bin/sh
+# $Id: syncconf.sh,v 1.2 1997-02-11 18:26:48 ghudson Exp $
 
-ECHO=echo
-ECHO1=echo
-CONFIG=/etc/config
-SETCONFIG="/sbin/chkconfig -f"
-RCCONF=/etc/athena/rc.conf
-RCSYNC=/etc/athena/.rc.conf.sync
-RCSYNCOUT=$RCSYNC
-debug=0
+config=/etc/config
+setconfig="/sbin/chkconfig -f"
+rcconf=/etc/athena/rc.conf
+rcsync=/etc/athena/.rc.conf.sync
+rcsyncout=$rcsync
+handled=
+rc2added=
+all=
+debug=
+startup=
+echo=echo
+maybe=
 
 configbool()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 $SETCONFIG "$1" "$2"
-  else
-    $SETCONFIG "$1" "$2"
-  fi
+	$maybe $setconfig "$1" "$2"
 }
 
 configopt()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 "echo $2 > $CONFIG/$1"
-  else
-    echo "$2" > "$CONFIG/$1"
-  fi
+	if [ -n "$debug" ]; then
+		$echo "echo $2 > $config/$1"
+	else
+		echo "$2" > "$config/$1"
+	fi
 }
 
 syncvar()
 {
-  case "$2" {
-      true|on)  configbool "$1" on ;;
-    false|off)  configbool "$1" off ;;
-            *)  configopt "$1" "$2" ;;
-  }
+	case "$2" in
+	true|on)
+		configbool "$1" on
+		;;
+	false|off)
+		configbool "$1" off
+		;;
+	*)
+		configopt "$1" "$2"
+		;;
+	esac
+}
+
+# Usage: syncrc2 scriptname order value
+# e.g. "syncrc2 mail 50 false" turns off the /etc/rc2.d/S50mail link.
+syncrc2()
+{
+	if [ "$3" = false ]; then
+		prefix=s
+	else
+		prefix=S
+	fi
+	if [ "$3" != false -a ! -h "/etc/rc2.d/S$2$1" ]; then
+		rc2added=1
+	fi
+	$maybe rm -f "/etc/rc2.d/s$2$1" "/etc/rc2.d/S$2$1"
+	$maybe ln -s "../init.d/$2" "/etc/rc2.d/$prefix$2$1"
+}
+
+# Usage: syncrc scriptname order value
+# e.g. "syncrc0 mail 20 true" turns on the /etc/rc2.d/K20mail link.
+syncrc0()
+{
+	if [ "$3" = false ]; then
+		prefix=k
+	else
+		prefix=K
+	fi
+	$maybe rm -f "/etc/rc0.d/k$2$1" "/etc/rc0.d/K$2$1"
+	$maybe ln -s "../init.d/$2" "/etc/rc0.d/$prefix$2$1"
 }
 
 remove()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 rm -f "$1"
-  else
-    rm -f "$1"
-  fi
+	$maybe rm -f "$1"
 }
 
 move()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 mv "$1" "$2"
-  else
-    mv "$1" "$2"
-  fi
+	$maybe mv "$1" "$2"
 }
 
 quiet_move()
 {
-  if [ -f "$1" ]; then
-    if [ $debug = 1 ]; then
-      $ECHO1 mv "$1" "$2"
-    else
-      mv "$1" "$2"
-    fi
-  fi
+	if [ -f "$1" ]; then
+		move "$1" "$2"
+	fi
 }
 
 put()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 echo "$2 > $1"
-  else
-    echo "$2" > "$1"
-  fi
+	if [ -n "$debug" ]; then
+		$echo "echo $2 > $1"
+	else
+		echo "$2" > "$1"
+	fi
 }
 
 append()
 {
-  if [ $debug = 1 ]; then
-    $ECHO1 echo "$2 >> $1"
-  else
-    echo "$2" >> "$1"
-  fi
+	if [ -n "$debug" ]; then
+		$echo "echo $2 >> $1"
+	else
+		echo "$2" >> "$1"
+	fi
 }
 
 handle()
 {
-  case "$1" in
-  TIMECLIENT|timeclient)
-    # gettime and AFS time synchronization
-    syncvar timeclient ${TIMECLIENT}
-    DEPENDENCIES="$DEPENDENCIES AFSCLIENT"
-    ;;
+	# Don't handle anything twice.
+	case "$handled" in
+	*" $1 "*)
+		return
+		;;
+	esac
+	handled="$handled $1 "
 
-  TIMEHUB|timehub)
-    syncvar timeclient.options ${TIMEHUB}
-    ;;
+	case "$1" in
+	TIMECLIENT)
+		# gettime and AFS time synchronization
+		syncvar timeclient "$TIMECLIENT"
+		dependencies="$dependencies AFSCLIENT"
+		;;
 
-  TIMESRV|timesrv)
-    syncvar timed $TIMESRV
-    ;;
+	TIMEHUB)
+		syncvar timeclient.options "$TIMEHUB"
+		;;
 
-  AFSCLIENT|afsclient)
-    if [ "$afsc_done" -ne 1 ]; then
-      if [ "${AFSCLIENT}" != "false" ]; then
+	TIMESRV)
+		syncvar timed "$TIMESRV"
+		;;
 
-        syncvar afsml true
-        syncvar afsclient true
+	AFSCLIENT)
+		if [ "$AFSCLIENT" != false ]; then
 
-        AFSCLIENTNUM=4
-        if [ "${AFSCLIENT}" != "true" ]; then
-          AFSCLIENTNUM=${AFSCLIENT}
-        fi
+			syncvar afsml true
+			syncvar afsclient true
 
-        if [ "${TIMECLIENT}" = "false" ]; then
-          AFSTIME="-nosettime "
-        fi
+			afsclientnum=4
+			if [ "$AFSCLIENT" != "true" ]; then
+				afsclientnum="$AFSCLIENT"
+			fi
 
-        syncvar afsd.options "${AFSTIME}-stat 2000 -dcache 800 -daemons $AFSCLIENTNUM -volumes 70"
-      else
-        syncvar afsml false
-        syncvar afsclient false
-      fi
+			options="-stat 2000 -dcache 800 -volumes 70"
+			options="$options -daemons $afsclientnum"
+			if [ "$TIMECLIENT" = false ]; then
+				options="$options -nosettime"
+			fi
 
-    afsc_done=1
-    fi
-    ;;
+			syncvar afsd.options "$options"
+		else
+			syncvar afsml false
+			syncvar afsclient false
+		fi
+		;;
 
-  NFSSRV|nfssrv|NFSCLIENT|nfsclient)
-    if [ "$nfs_done" -ne 1 ]; then
-      if [ "${NFSSRV}" != "false" -o "${NFSCLIENT}" != "false" ]; then
-        syncvar nfs true
+	NFS)
+		if [ "$NFSSRV" != false -o "$NFSCLIENT" != false ]; then
+			syncvar nfs true
 
-        case "${NFSCLIENT}" {
-          true|false)  remove $CONFIG/biod.options ;;
-                   *)  syncvar biod.options "${NFSCLIENT}" ;;
-        }
+		        case "$NFSCLIENT" in
+			true|false)
+				remove $config/biod.options
+				;;
+			*)
+				syncvar biod.options "$NFSCLIENT"
+				;;
+			esac
 
-        case "${NFSSRV}" {
-                true)  remove $CONFIG/nfsd.options ;;
-               false)  syncvar nfsd.options 0 ;;
-                   *)  syncvar nfsd.options "${NFSSRV}" ;;
-        }
-      else
-        syncvar nfs false
-      fi
+			case "$NFSSRV" in
+	                true)
+				remove $config/nfsd.options
+				;;
+			false)
+				syncvar nfsd.options 0
+				;;
+			*)
+				syncvar nfsd.options "$NFSSRV"
+				;;
+			esac
+		else
+			syncvar nfs false
+		fi
+		;;
 
-      nfs_done=1;
-    fi
-    ;;
+	SENDMAIL)
+		syncrc2 mail 50 "$SENDMAIL"
+		syncrc0 mail 20 "$SENDMAIL"
+		;;
 
-  SENDMAIL|sendmail)
-    if [ "${SENDMAIL}" = "false" ]; then
-      quiet_move /etc/rc2.d/S50mail /etc/rc2.d/s50mail
-      quiet_move /etc/rc0.d/K20mail /etc/rc0.d/k20mail
-    else
-      move /etc/rc2.d/s50mail /etc/rc2.d/S50mail
-      move /etc/rc0.d/k20mail /etc/rc0.d/K20mail
-    fi
-    ;;
+	SNMP)
+		syncvar snmpd "$SNMP"
+		;;
 
-  SNMP|snmp)
-    syncvar snmpd $SNMP
-    ;;
+	SAVECORE)
+		if [ "$SAVECORE" != false ]; then
+			case "$SAVECORE" in
+			true)
+				minfree=30000000
+				;;
+			*)
+				minfree="$SAVECORE"
+				;;
+			esac
+			put /var/adm/crash/minfree $minfree
+		fi
+		syncrc2 savecore 48 "$SAVECORE"
+		;;
 
-  SAVECORE|savecore)
-    if [ "${SAVECORE}" = "false" ]; then
-      quiet_move /etc/rc2.d/S48savecore /etc/rc2.d/s48savecore
-    else
-      case "${SAVECORE}" {
-         true)  minfree=30000000 ;;
-            *)  minfree=${SAVECORE} ;;
-      }
-      put /var/adm/crash/minfree $minfree
-      quiet_move /etc/rc2.d/s48savecore /etc/rc2.d/S48savecore
-    fi
-    ;;
+	ACCOUNT)
+		syncvar acct "$ACCOUNT"
+		;;
 
-  ACCOUNT|account)
-    syncvar acct ${ACCOUNT}
-    ;;
+	QUOTAS)
+		syncvar quotacheck "$QUOTAS"
+		syncvar quotas "$QUOTAS"
+		;;
 
-  QUOTAS|quotas)
-    syncvar quotacheck ${QUOTAS}
-    syncvar quotas ${QUOTAS}
-    ;;
+	HOSTADDR)
+		move /etc/sys_id /etc/sys_id.saved
+		move /etc/hosts /etc/hosts.saved
+		move $config/staticroute.options \
+			$config/staticroute.options.saved
+		move $config/ifconfig-1.options \
+			$config/ifconfig-1.options.saved
 
-  HOST|host|ADDR|addr)
-    if [ "$ip_done" -ne 1 ]; then
-      move /etc/sys_id /etc/sys_id.saved
-      move /etc/hosts /etc/hosts.saved
-      move $CONFIG/staticroute.options $CONFIG/staticroute.options.saved
-      move $CONFIG/ifconfig-1.options $CONFIG/ifconfig-1.options.saved
+		net=`echo $ADDR | awk -F. '{ print $1 "." $2 }'`
+		gateway=$net.0.1
+		broadcast=$net.255.255
 
-      NET=`echo $ADDR | awk -F. '{ print $1 "." $2 }'`
-      GATEWAY=$NET.0.1
-      BROADCAST=$NET.255.255
+		put    /etc/sys_id $HOST
+		put    $config/staticroute.options $gateway
+		put    $config/ifconfig-1.options "netmask 0xffff0000"
+		append $config/ifconfig-1.options "broadcast $broadcast"
+		put    /etc/hosts "#"
+		append /etc/hosts "# Internet host table"
+		append /etc/hosts "#"
+		append /etc/hosts "127.0.0.1  localhost"
+		append /etc/hosts "$ADDR  $HOST.MIT.EDU $HOST"
+		;;
 
-      put    /etc/sys_id $HOST
-      put    $CONFIG/staticroute.options $GATEWAY
-      put    $CONFIG/ifconfig-1.options "netmask 0xffff0000"
-      append $CONFIG/ifconfig-1.options "broadcast $BROADCAST"
-      put    /etc/hosts "#"
-      append /etc/hosts "# Internet host table"
-      append /etc/hosts "#"
-      append /etc/hosts "127.0.0.1  localhost"
-      append /etc/hosts "$ADDR  $HOST.MIT.EDU $HOST"
-
-      ip_done=1;
-    fi
-    ;;
-
-  *)
-    $ECHO ""
-    echo "syncconf: unknown variable $1"
-    ;;
-  esac
+	*)
+		$echo "syncconf: unknown variable $1"
+		;;
+	esac
 }
 
-while [ -n "$1" ]; do
-  case "$1" in
-    -n)  debug=1 ;;
-    -q1) ECHO1=: ;;
-    -q)  ECHO=: ;;
-     *)  echo "Usage: syncconf [-n] [-q]"; exit ;;
-  esac
-  shift
+while getopts anq opt; do
+	case "$opt" in
+	a)
+		all=1
+		;;
+	n)
+		debug=1
+		rcsyncout=/tmp/rc.conf.sync
+		maybe=$echo
+		;;
+	q)
+		echo=:
+		;;
+	\?)
+		echo "Usage: syncconf [-anq]"
+		exit 1
+		;;
+	esac
 done
-
-if [ $debug = 1 ]; then
-  RCSYNCOUT=/tmp/rc.conf.sync
+shift `expr $OPTIND - 1`
+if [ "$#" -ne 0 ]; then
+	echo "Usage: syncconf [-anq]"
+	exit 1
 fi
 
-ALL="timeclient timehub timesrv afsclient nfssrv nfsclient sendmail snmp savecore account quotas host addr"
+$echo "Synchronizing configuration... \c"
 
-$ECHO "Synchronizing configuration... \c"
+. "$rcconf"
 
-. $RCCONF
-
-if test -f $RCSYNC; then
-  . $RCSYNC
+if [ -z "$all" -a -f "$rcsync" ]; then
+	. "$rcsync"
 else
-  CHANGES="$ALL"
+	changes="TIMECLIENT TIMEHUB TIMESRV AFSCLIENT NFS SENDMAIL SNMP"
+	changes="$changes SAVECORE ACCOUNT QUOTAS HOSTADDR"
 fi
 
-if [ "$CHANGES" = "" ]; then
-  $ECHO "No changes to synchronize."
-  exit
+if [ -z "$changes" ]; then
+	$echo "No changes to synchronize."
+	exit
 fi
 
-for i in $CHANGES; do
-  $ECHO "$i \c"
-  if [ $debug = 1 ]; then $ECHO; fi
-  handle "$i"
+for i in $changes; do
+	$echo "$i \c"
+	if [ -n "$debug" ]; then
+		$echo ""
+	fi
+	handle "$i"
 done
 
-if [ "$DEPENDENCIES" != "" ]; then
-  for i in $DEPENDENCIES; do
-    $ECHO "($i) \c"
-    if [ $debug = 1 ]; then $ECHO; fi
-    handle "$i"
-  done
-fi
+for i in $dependencies; do
+	$echo "($i) \c"
+	if [ -n "$debug" ]; then
+		$echo ""
+	fi
+	handle "$i"
+done
 
-$ECHO ""
+$echo ""
 
-cat > $RCSYNCOUT << EOF
-if [ \$TIMECLIENT != $TIMECLIENT ]; then CHANGES="\$CHANGES TIMECLIENT"; fi
-if [ \$TIMEHUB != $TIMEHUB ]; then CHANGES="\$CHANGES TIMEHUB"; fi
-if [ \$TIMESRV != $TIMESRV ]; then CHANGES="\$CHANGES TIMESRV"; fi
-if [ \$AFSCLIENT != $AFSCLIENT ]; then CHANGES="\$CHANGES AFSCLIENT"; fi
-if [ \$NFSSRV != $NFSSRV ]; then CHANGES="\$CHANGES NFSSRV"; fi
-if [ \$NFSCLIENT != $NFSCLIENT ]; then CHANGES="\$CHANGES NFSCLIENT"; fi
-if [ \$SENDMAIL != $SENDMAIL ]; then CHANGES="\$CHANGES SENDMAIL"; fi
-if [ \$SNMP != $SNMP ]; then CHANGES="\$CHANGES SNMP"; fi
-if [ \$SAVECORE != $SAVECORE ]; then CHANGES="\$CHANGES SAVECORE"; fi
-if [ \$ACCOUNT != $ACCOUNT ]; then CHANGES="\$CHANGES ACCOUNT"; fi
-if [ \$QUOTAS != $QUOTAS ]; then CHANGES="\$CHANGES QUOTAS"; fi
-if [ \$HOST != $HOST ]; then CHANGES="\$CHANGES HOST"; fi
-if [ \$ADDR != $ADDR ]; then CHANGES="\$CHANGES ADDR"; fi
+cat > $rcsyncout << EOF
+if [ \$TIMECLIENT != $TIMECLIENT ]; then changes="\$changes TIMECLIENT"; fi
+if [ \$TIMEHUB != $TIMEHUB ]; then changes="\$changes TIMEHUB"; fi
+if [ \$TIMESRV != $TIMESRV ]; then changes="\$changes TIMESRV"; fi
+if [ \$AFSCLIENT != $AFSCLIENT ]; then changes="\$changes AFSCLIENT"; fi
+if [ \$NFSSRV != $NFSSRV ]; then changes="\$changes NFS"; fi
+if [ \$NFSCLIENT != $NFSCLIENT ]; then changes="\$changes NFS"; fi
+if [ \$SENDMAIL != $SENDMAIL ]; then changes="\$changes SENDMAIL"; fi
+if [ \$SNMP != $SNMP ]; then changes="\$changes SNMP"; fi
+if [ \$SAVECORE != $SAVECORE ]; then changes="\$changes SAVECORE"; fi
+if [ \$ACCOUNT != $ACCOUNT ]; then changes="\$changes ACCOUNT"; fi
+if [ \$QUOTAS != $QUOTAS ]; then changes="\$changes QUOTAS"; fi
+if [ \$HOST != $HOST ]; then changes="\$changes HOSTADDR"; fi
+if [ \$ADDR != $ADDR ]; then changes="\$changes HOSTADDR"; fi
 EOF
+
+if [ -n "$rc2added" ]; then
+	# Exit with status 1 to indicate need to reboot if run at startup.
+	exit 1
+fi
