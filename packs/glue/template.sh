36edcbe05a89807cef44bee43ca9cdeabcb652c9
Greg Hudson <ghudson@mit.edu>
1997-06-02 07:37:59 +0000

    Pull up change from rev 1.2 to Athena 8.1 release branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_1@10105 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/template.sh
+++ packs/glue/template.sh
@@ -3,9 +3,11 @@
 if [ ! -r /mit/@LOCKER@ ]; then
 	/bin/athena/attach -q @LOCKER@
 fi
-if [ ! -x @PATH@ ]; then
+if [ "$ATHENA_ATTACHRUN_RECURSION" = $$ -o ! -x @BINDIR@/@PROGRAM@ ]; then
 	echo "Can't find the @PROGRAM@ program in the @LOCKER@ locker."
 	echo "Please try again later."
 	exit 1
 fi
-exec @PATH@ "$@"
+ATHENA_ATTACHRUN_RECURSION=$$ export ATHENA_ATTACHRUN_RECURSION
+PATH="@BINDIR@:$PATH"
+exec @PROGRAM@ "$@"
