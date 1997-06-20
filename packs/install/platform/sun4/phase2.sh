27abd73bd93cacb25122981ab471f0b3efae5a00
Michal N Lusztig <miki@mit.edu>
1997-06-20 19:05:37 +0000

    Added 8.1 as the standard system installed Added the possibility of partitioning the system disk by hand
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@10167 728af825-273c-0410-89f9-f7d3b574a069

--- packs/install/platform/sun4/phase2.sh
+++ packs/install/platform/sun4/phase2.sh
@@ -4,11 +4,11 @@
 ### installation program.  It is called by the first script,
 ### athenainstall.
 
-### $Header: /afs/dev.mit.edu/source/repository/packs/install/platform/sun4/phase2.sh,v 1.22 1997-06-10 03:06:40 jweiss Exp $
+### $Header: /afs/dev.mit.edu/source/repository/packs/install/platform/sun4/phase2.sh,v 1.23 1997-06-20 19:05:37 miki Exp $
 ### $Locker:  $
 
 echo "Set some variables"
-PATH=/os/usr/bin:/srvd/bin/athena:/os/etc:/os/usr/sbin:/bin:/etc:/sbin:/usr/sbin
+PATH=/sbin:/os/usr/bin:/srvd/bin/athena:/os/etc:/os/usr/sbin:/bin:/etc:/sbin:/usr/sbin
 export PATH
 umask 2
 
@@ -45,8 +45,9 @@ echo "Installing on ${drive}."
 
 case $CUSTOM in
 N)
-  echo "standard installation"
-  ln -s /afs/athena/system/sun4m_54/srvd /tmp/srvd
+  echo "standard installation - 8.1"
+  ln -s /afs/athena.mit.edu/system/sun4x_55/srvd-8.1 /tmp/srvd
+  ln -s /afs/athena.mit.edu/system/sun4x_55/os /tmp/os
   ;;
 
 Y)
@@ -58,9 +59,15 @@ Y)
        echo "installing 7.7"
        ln -s /afs/athena.mit.edu/system/sun4m_53/srvd.77 /tmp/srvd
        ;;
-   *)
+   8.0)
        echo "installing 8.0"
-       ln -s /afs/athena.mit.edu/system/sun4m_54/srvd /tmp/srvd
+       ln -s /afs/athena.mit.edu/system/sun4m_54/srvd.80 /tmp/srvd
+       ln -s /afs/athena.mit.edu/system/sun4m_54/os /tmp/os 
+       ;;
+   *)
+       echo "installing 8.1"
+       ln -s /afs/athena.mit.edu/system/sun4x_55/srvd-8.1 /tmp/srvd
+       ln -s /afs/athena.mit.edu/system/sun4x_55/os /tmp/os
        ;;
    esac
    echo "done choosing rev"
@@ -69,42 +76,78 @@ esac
 
 ls -l /srvd
 ls -l /tmp/srvd
+ls -l /os
+ls -l /tmp/os
 
 echo "formatting  "
 DISK=`/sbin/machtype -r`
 export DISK
 echo $DISK
-case $DISK in
-SUN0424)
-	echo "formatting SUN0424"
-	cat /util/format.input.SUN0424 | \
-		/usr/sbin/format ${drive} >/dev/null 2>&1
-	;;
-SUN0535)
-	echo "formatting SUN0535"
-	cat /util/format.input.SUN0535 | \
-		/usr/sbin/format ${drive} >/dev/null 2>&1
-	;;
-SUN1.05)
-	echo "formatting SUN1.05"
-	cat /util/format.input.SUN1.05 | \
-		/usr/sbin/format ${drive} >/dev/null 2>&1
-	;;
-SUN2.1G)
-	echo "formatting SUN2.1G"
-	cat /util/format.input.SUN2.1G | \
-		/usr/sbin/format ${drive} >/dev/null 2>&1
-	;;
-SEAGATE*5660N)
-	echo "formatting SEAGATE-ST5660N"
-	cat /util/format.input.seagate.5660 | \
-		/usr/sbin/format ${drive} >/dev/null 2>&1
-	;;
-*)
-	echo "can't format the disks - type unknown"
-	echo "Call an expert !"
-	exit 1
+
+case $CUSTOM in
+Y)
+   echo "Partitioning the disk yourself? (will default after 60 seconds) [n]"
+   case x`/util/to 60` in
+   xtimeout|xn|xN|x)
+     PARTITION=N; echo "Doing standard partitions"; export PARTITION
+     ;;
+   *)
+     PARTITION=Y; echo "Doing custom partitions"; export PARTITION
+     ;;
+   esac
+   ;;
+*)     
+
+     PARTITION=N; echo "Doing standard partitions"
+     ;;
 esac
+
+case $PARTITION in
+Y)
+     echo "The rest of the installation assumes that
+     partition 0 is / and needs about 30MB;
+     partition 1 is swap;
+     partition 3 is AFS cache;
+     partition 5 is /usr and needs 50MB;
+     partition 6 is /var and needs at least 120MB "
+     sleep 10
+     /usr/sbin/format
+     ;;
+*)
+     case $DISK in
+     SUN0424)
+        echo "formatting SUN0424"
+        cat /util/format.input.SUN0424 | \
+		/usr/sbin/format ${drive} >/dev/null 2>&1
+        ;;
+     SUN0535)
+        echo "formatting SUN0535"
+        cat /util/format.input.SUN0535 | \
+		/usr/sbin/format ${drive} >/dev/null 2>&1
+        ;;
+    SUN1.05)
+       echo "formatting SUN1.05"
+       cat /util/format.input.SUN1.05 | \
+		/usr/sbin/format ${drive} >/dev/null 2>&1
+       ;;
+    SUN2.1G)
+       echo "formatting SUN2.1G"
+       cat /util/format.input.SUN2.1G | \
+		/usr/sbin/format ${drive} >/dev/null 2>&1
+       ;;
+    SEAGATE*5660N)
+       echo "formatting SEAGATE-ST5660N"
+       cat /util/format.input.seagate.5660 | \
+		/usr/sbin/format ${drive} >/dev/null 2>&1
+       ;;
+    *)
+       echo "can't format the disks - type unknown"
+       echo "Call an expert !"
+       exit 1
+       esac
+esac
+
+
 
 echo "Making the filesystems..."
 echo ""
