bdfcf2b963f4927bbb36b8d5c671c12a01e2f27f
Greg Hudson <ghudson@mit.edu>
1997-09-08 20:45:31 +0000

    Set mustreboot if either the hostname or IP address changed, not just if the hostname changed.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@10338 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/platform/sun4/syncconf.sh
+++ packs/maint/platform/sun4/syncconf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: syncconf.sh,v 1.6 1997-07-22 19:34:06 ghudson Exp $
+# $Id: syncconf.sh,v 1.7 1997-09-08 20:45:31 ghudson Exp $
 
 rcconf=/etc/athena/rc.conf
 rcsync=/var/athena/rc.conf.sync
@@ -91,12 +91,14 @@ handle()
 		;;
 
 	HOSTADDR)
+		oldhost=`cat /etc/nodename`
+		oldaddr=`awk '{ a = $1; } END { print a; }' /etc/inet/hosts`
+
 		move /etc/nodename /etc/nodename.saved
 		move /etc/hostname.le0 /etc/hostname.le0.saved
 		move /etc/defaultrouter /etc/defaultrouter.saved
 		move /etc/inet/hosts /etc/inet/hosts.saved
 
-		oldhost=`cat /etc/nodename.saved`
 		net=`echo $ADDR | awk -F. '{ print $1 "." $2 }'`
 		gateway=$net.0.1
 		broadcast=$net.255.255
@@ -112,7 +114,7 @@ handle()
 
 		# Hostname configuration happens prior to rc scripts on
 		# Solaris.
-		if [ "$HOST" != "$oldhost" ]; then
+		if [ "$HOST" != "$oldhost" -o "$ADDR" != "$oldaddr" ]; then
 			mustreboot=1
 		fi
 		;;
