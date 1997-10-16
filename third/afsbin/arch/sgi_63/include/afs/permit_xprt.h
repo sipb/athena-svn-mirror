a5930e0e7bd790d92e42c74ee69b239009f8ca3e
Greg Hudson <ghudson@mit.edu>
1997-10-16 14:41:14 +0000

    Base configuration afs3.4 5.28, the fifth patch release fo AFS 3.4a
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@10526 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/afsbin/arch/sgi_63/include/afs/permit_xprt.h
@@ -0,0 +1,7 @@
+/* Copyright (C) 1990 Transarc Corporation - All rights reserved */
+/* $Header: /afs/dev.mit.edu/source/repository/third/afsbin/arch/sgi_63/include/afs/permit_xprt.h,v 1.1.1.1 1997-10-16 14:40:23 ghudson Exp $ */
+
+#undef HIDE
+#define HIDE
+#define xprt_CoerceLevel
+#define xprt_CryptOK(x) 1
