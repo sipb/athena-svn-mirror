5bc06544e6eef1c21a6f953976e0a9ad18140a78
Greg Hudson <ghudson@mit.edu>
1997-11-13 23:40:26 +0000

    Clean up.  Nuke old platform support, indent code consistently, switch on HOSTTYPE (`/bin/athena/machtype`), do a better job of cleaning temporary areas, clean /var/athena/tmphomedirs, use /var/athena instead of /var/tmp to store the ps_data file on Solaris.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@10741 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/reactivate.sh
+++ packs/maint/reactivate.sh
@@ -1,187 +1,154 @@
 #!/bin/sh
 # Script to bounce the packs on an Athena workstation
 #
-# $Id: reactivate.sh,v 1.31 1997-10-31 00:49:03 jweiss Exp $
+# $Id: reactivate.sh,v 1.32 1997-11-13 23:40:26 ghudson Exp $
 
 trap "" 1 15
 
 PATH=/bin:/bin/athena:/usr/ucb:/usr/bin; export PATH
+HOSTTYPE=`/bin/athena/machtype`; export HOSTTYPE
 
 umask 22
 . /etc/athena/rc.conf
 
-# Default options
-dflags="-clean"
-
-
 # Set various flags (based on environment and command-line)
-if [ "$1" = "-detach" ]; then dflags=""; fi
+if [ "$1" = -detach ]; then
+	dflags=""
+else
+	dflags="-clean"
+fi
 
-if [ "$1" = "-prelogin" ]; then
-	if [ "${PUBLIC}" = "false" ]; then exit 0; fi
+if [ "$1" = -prelogin ]; then
+	if [ "$PUBLIC" = "false" ]; then
+		exit 0;
+	fi
 	echo "Cleaning up..." >> /dev/console
+	full=false
 else
 	full=true
 fi
 
-if [ "${USER}" = "" ]; then
+if [ -z "$USER" ]; then
 	exec 1>/dev/console 2>&1
 	quiet=-q
 else
-	echo Reactivating workstation...
-	quiet=
+	echo "Reactivating workstation..."
+	quiet=""
 fi
-
-case "${SYSTEM}" in
-ULTRIX*)
-	cp=/bin/cp
-	;;
-*)
-	cp="/bin/cp -p"
-esac
 
 # Flush all NFS uid mappings
 /bin/athena/fsid $quiet -p -a
 
-# Sun attach bug workaround. Attach apparently loses on some options.
-# fsid does not though, so we zap them only after executing fsid.
-if [ "${MACHINE}" = "SUN4" ]; then
-	dflags=""
-	quiet=""
-fi
-
 # Tell the Zephyr hostmanager to reset state
-if [ -f /var/athena/zhm.pid -a "${ZCLIENT}" = "true" ] ; then 
+if [ -f /var/athena/zhm.pid -a "$ZCLIENT" = true ] ; then 
 	/bin/kill -HUP `/bin/cat /var/athena/zhm.pid`
 fi
-
-case "${MACHINE}" in
-RSAIX)
-	chmod 666 /dev/hft
-	;;
-*)
-	;;
-esac
 
 # kdestroy from /tmp any ticket files that may have escaped other methods
 # of destruction, before we clear /tmp.
 for i in /tmp/tkt* /tmp/krb5cc*; do
-  KRBTKFILE=$i /usr/athena/bin/kdestroy -f
+	KRBTKFILE=$i /usr/athena/bin/kdestroy -f
 done
 
-if [ $full ]; then		# START tmp clean
-# Clean temporary areas (including temporary home directories)
-case "${MACHINE}" in
-RSAIX)
-	find /tmp -depth \( -type f -o -type l \) -print | xargs /bin/rm -f -
-	find /tmp -depth -type d -print | xargs /bin/rmdir 1>/dev/null 2>&1
-	;;
-
-SUN4)
-	cp -p /tmp/ps_data /usr/tmp/ps_data
-	/bin/rm -rf /tmp/* > /dev/null 2>&1
-	cp -p /usr/tmp/ps_data /tmp/ps_data
-	/bin/rm -f /usr/tmp/ps_data
-	;;
-INDY)
-	/bin/rm -rf /tmp/* > /dev/null 2>&1
-	;;
-*)
-	/bin/mv /tmp/.X11-unix /tmp/../.X11-unix
-	/bin/rm -rf /tmp/ > /dev/null 2>&1
-	/bin/mv /tmp/../.X11-unix /tmp/.X11-unix
-	;;
-esac
-fi				# END tmp clean
+if [ "$full" = true ]; then
+	# Clean temporary areas (including temporary home directories)
+	case "$HOSTTYPE" in
+	sun4)
+		cp -p /tmp/ps_data /var/athena/ps_data
+		rm -rf /tmp/* /tmp/.[^.]* > /dev/null 2>&1
+		cp -p /var/athena/ps_data /tmp/ps_data
+		rm -f /var/athena/ps_data
+		;;
+	*)
+		rm -rf /tmp/* /tmp/.[^.]* > /dev/null 2>&1
+		;;
+	esac
+	rm -rf /var/athena/tmphomedir/* > /dev/null 2>&1
+fi
 
 # Copy in latest password file
-if [ "${PUBLIC}" = "true" ]; then
+if [ "$PUBLIC" = true ]; then
 	if [ -r /srvd/etc/passwd ]; then
-	    ${cp} /srvd/etc/passwd /etc/passwd.local
-	    chmod 644 /etc/passwd.local
-	    chown root /etc/passwd.local
+		cp -p /srvd/etc/passwd /etc/passwd.local
+		chmod 644 /etc/passwd.local
+		chown root /etc/passwd.local
 	fi
 	if [ -r /srvd/etc/shadow ]; then
-	    ${cp} /srvd/etc/shadow /etc/shadow.local
-	    chmod 600 /etc/shadow.local
-	    chown root /etc/shadow.local
+		cp -p /srvd/etc/shadow /etc/shadow.local
+		chmod 600 /etc/shadow.local
+		chown root /etc/shadow.local
 	fi
 fi
 
 # Restore password and group files
-case "${MACHINE}" in
-RSAIX)
-	;;
-*)
-	if [ -f /etc/passwd.local ] ; then
-	    ${cp} /etc/passwd.local /etc/ptmp && /bin/mv -f /etc/ptmp /etc/passwd
-	fi
-	if [ -f /etc/shadow.local ] ; then
-	    ${cp} /etc/shadow.local /etc/stmp && /bin/mv -f /etc/stmp /etc/shadow
-	fi
-	if [ -f /etc/group.local ] ; then
-	    ${cp} /etc/group.local /etc/gtmp && /bin/mv -f /etc/gtmp /etc/group
-	fi
-	;;
-esac
-
-if [ $full ]; then		# START AFS reconfig
-# Reconfigure AFS state
-if [ "${AFSCLIENT}" != "false" ]; then
-    /etc/athena/config_afs > /dev/null 2>&1 &
+if [ -f /etc/passwd.local ] ; then
+	cp -p /etc/passwd.local /etc/ptmp && /bin/mv -f /etc/ptmp /etc/passwd
 fi
-fi				# END AFS reconfig
+if [ -f /etc/shadow.local ] ; then
+	cp -p /etc/shadow.local /etc/stmp && /bin/mv -f /etc/stmp /etc/shadow
+fi
+if [ -f /etc/group.local ] ; then
+	cp -p /etc/group.local /etc/gtmp && /bin/mv -f /etc/gtmp /etc/group
+fi
 
-# punt any processes owned by users not in /etc/passwd
+if [ "$full" = true ]; then
+	# Reconfigure AFS state
+	if [ "$AFSCLIENT" != "false" ]; then
+		/etc/athena/config_afs > /dev/null 2>&1 &
+	fi
+fi
+
+# Punt any processes owned by users not in /etc/passwd.
 /etc/athena/cleanup -passwd
 
-if [ $full ]; then		# START time-consuming stuff
-# Finally, detach all remote filesystems
-/bin/athena/detach -O -h -n $quiet $dflags -a
+if [ "$full" = true ]; then
+	# Detach all remote filesystems
+	/bin/athena/detach -O -h -n $quiet $dflags -a
 
-# Now start activate again
-/etc/athena/save_cluster_info
+	# Now start activate again
+	/etc/athena/save_cluster_info
 
-if [ -f /var/athena/clusterinfo.bsh ] ; then
-	. /var/athena/clusterinfo.bsh
-else
-	if [ "${RVDCLIENT}" = "true" ]; then
-		echo "Can't find library servers."
+	if [ -f /var/athena/clusterinfo.bsh ] ; then
+		. /var/athena/clusterinfo.bsh
+	elif [ "$RVDCLIENT" = true ]; then
+		echo "Can't determine system packs location."
 		exit 1
 	fi
-fi
 
-if [ "${RVDCLIENT}" = "true" ]; then
-	/bin/athena/attach	$quiet -h -n -o hard -O $SYSLIB
-fi
-
-# Perform an update if appropriate
-/srvd/auto_update reactivate
-
-if [ "${PUBLIC}" = "true" -a -f /srvd/.rvdinfo ]; then
-	NEWVERS=`awk '{a=$5} END{print a}' /srvd/.rvdinfo`
-	THISVERS=`awk '{a=$5} END{print a}' /etc/athena/version`
-	if [ "${NEWVERS}" = "${THISVERS}" ]; then
-		/usr/athena/etc/track
-		for i in `cat /srvd/usr/athena/lib/update/configfiles`; do
-			if [ -f /srvd$i ]; then
-				cp -p /srvd$i $i
-			else
-				cp -p /os$i $i
-			fi
-		done
-		kill -HUP `cat /var/athena/inetd.pid`
+	if [ "$RVDCLIENT" = true ]; then
+		/bin/athena/attach	$quiet -h -n -o hard -O $SYSLIB
 	fi
-	rm -f /etc/athena/reactivate.local
-fi
-fi				# END time-consuming stuff
 
-if [ -f /usr/athena/bin/access_off ]; then /usr/athena/bin/access_off; fi
+	# Perform an update if appropriate
+	/srvd/auto_update reactivate
 
-if [ $full ]; then		# START reactivate.local
-if [ -f /etc/athena/reactivate.local ]; then
-	/etc/athena/reactivate.local
+	if [ "$PUBLIC" = true -a -f /srvd/.rvdinfo ]; then
+		NEWVERS=`awk '{a=$5} END{print a}' /srvd/.rvdinfo`
+		THISVERS=`awk '{a=$5} END{print a}' /etc/athena/version`
+		if [ "$NEWVERS" = "$THISVERS" ]; then
+			/usr/athena/etc/track
+			cf=`cat /srvd/usr/athena/lib/update/configfiles`
+			for i in $cf; do
+				if [ -f /srvd$i ]; then
+					cp -p /srvd$i $i
+				else
+					cp -p /os$i $i
+				fi
+			done
+			kill -HUP `cat /var/athena/inetd.pid`
+		fi
+		rm -f /etc/athena/reactivate.local
+	fi
 fi
-fi				# END reactivate.local
+
+if [ -f /usr/athena/bin/access_off ]; then
+	/usr/athena/bin/access_off
+fi
+
+if [ "$full" = true ]; then
+	if [ -f /etc/athena/reactivate.local ]; then
+		/etc/athena/reactivate.local
+	fi
+fi
 
 exit 0
