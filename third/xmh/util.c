4627861a1a72c34d0093c1e1abd8ac3cbb5d6326
Greg Hudson <ghudson@mit.edu>
1997-12-13 16:03:19 +0000

    Use strerror.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@10892 728af825-273c-0410-89f9-f7d3b574a069

--- third/xmh/util.c
+++ third/xmh/util.c
@@ -40,11 +40,7 @@
 static char *SysErrorMsg (n)
     int n;
 {
-    extern char *sys_errlist[];
-    extern int sys_nerr;
-    char *s = ((n >= 0 && n < sys_nerr) ? sys_errlist[n] : "unknown error");
-
-    return (s ? s : "no such error");
+    return strerror(n);
 }
 
 /* Something went wrong; panic and quit. */
