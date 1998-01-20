13a7dfe5669a64df6bf5429ee8a5741e3aa083e0
Greg Hudson <ghudson@mit.edu>
1998-01-20 23:17:08 +0000

    From svalente: only define LOCK_SH et al if they aren't already defiend.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@11056 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/lpr/posix.h
+++ athena/bin/lpr/posix.h
@@ -2,10 +2,12 @@
  * BSD functions in POSIX
  */
 
+#ifndef LOCK_SH
 #define   LOCK_SH   1    /* shared lock */
 #define   LOCK_EX   2    /* exclusive lock */
 #define   LOCK_NB   4    /* don't block when locking */
 #define   LOCK_UN   8    /* unlock */
+#endif
 
 int
 flock(int fd, int operation);
