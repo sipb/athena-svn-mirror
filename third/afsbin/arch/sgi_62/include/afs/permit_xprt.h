a3593a85f12a79e4a74832ba2cb14a6de2180a66
Greg Hudson <ghudson@mit.edu>
1998-02-20 21:46:42 +0000

    This commit was generated by cvs2svn to compensate for changes in r11148, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@11149 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/afsbin/arch/sgi_62/include/afs/permit_xprt.h
@@ -0,0 +1,7 @@
+/* Copyright (C) 1990 Transarc Corporation - All rights reserved */
+/* $Header: /afs/dev.mit.edu/source/repository/third/afsbin/arch/sgi_62/include/afs/permit_xprt.h,v 1.1.1.1 1998-02-20 21:46:41 ghudson Exp $ */
+
+#undef HIDE
+#define HIDE
+#define xprt_CoerceLevel
+#define xprt_CryptOK(x) 1
