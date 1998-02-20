e7b564c109d62fd0eb4460c8aceb3add8d2ed1ff
Greg Hudson <ghudson@mit.edu>
1998-02-20 21:39:25 +0000

    Base configuration ports 1.67.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@11144 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/afsbin/arch/sun4x_56/include/afs/bumon.h
@@ -0,0 +1,55 @@
+/* Machine generated file -- Do NOT edit */
+
+#ifndef	_RXGEN_BUMON_
+#define	_RXGEN_BUMON_
+
+#ifdef	KERNEL
+/* The following 'ifndefs' are not a good solution to the vendor's omission of surrounding all system includes with 'ifndef's since it requires that this file is included after the system includes...*/
+#include "../afs/param.h"
+#include "../h/types.h"
+#ifndef	SOCK_DGRAM  /* XXXXX */
+#include "../h/socket.h"
+#endif
+#ifndef	DTYPE_SOCKET  /* XXXXX */
+#ifdef AFS_DEC_ENV
+#include "../h/smp_lock.h"
+#endif
+#include "../h/file.h"
+#endif
+#ifndef	S_IFMT  /* XXXXX */
+#include "../h/stat.h"
+#endif
+#ifndef	IPPROTO_UDP /* XXXXX */
+#include "../netinet/in.h"
+#endif
+#ifndef	DST_USA  /* XXXXX */
+#include "../h/time.h"
+#endif
+#include "../rpc/types.h"
+#ifndef	XDR_GETLONG /* XXXXX */
+#include "../rpc/xdr.h"
+#endif
+#include "../afsint/rxgen_consts.h"
+#include "../afs/osi.h"
+#include "../rx/rx.h"
+#else	/* KERNEL */
+#include <sys/types.h>
+#include <rx/xdr.h>
+#include <rx/rx.h>
+#include <afs/rxgen_consts.h>
+#endif	/* KERNEL */
+
+
+struct bc_junk {
+	int32 a;
+};
+typedef struct bc_junk bc_junk;
+bool_t xdr_bc_junk();
+
+
+/* Opcode-related useful stats for package: BC_ */
+#define BC_LOWEST_OPCODE   100
+#define BC_HIGHEST_OPCODE	100
+#define BC_NUMBER_OPCODES	1
+
+#endif	/* _RXGEN_BUMON_ */
---
+++ third/afsbin/arch/sun4x_56/include/afs/permit_xprt.h
@@ -0,0 +1,7 @@
+/* Copyright (C) 1990 Transarc Corporation - All rights reserved */
+/* $Header: /afs/dev.mit.edu/source/repository/third/afsbin/arch/sun4x_56/include/afs/permit_xprt.h,v 1.1.1.1 1998-02-20 21:35:28 ghudson Exp $ */
+
+#undef HIDE
+#define HIDE
+#define xprt_CoerceLevel
+#define xprt_CryptOK(x) 1
