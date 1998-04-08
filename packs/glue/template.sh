b912a70233eb2973510b9daa32536f411c8aeaa7
Craig Fields <cfields@mit.edu>
1998-04-08 22:10:38 +0000

    Use attachandrun.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@11367 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/template.sh
+++ packs/glue/template.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-
-if [ ! -r /mit/@LOCKER@ ]; then
-	/bin/athena/attach -q @LOCKER@
-fi
-if [ "$ATHENA_ATTACHRUN_RECURSION" = $$ -o ! -x @BINDIR@/@PROGRAM@ ]; then
-	echo "Can't find the @PROGRAM@ program in the @LOCKER@ locker."
-	echo "Please try again later."
-	exit 1
-fi
-ATHENA_ATTACHRUN_RECURSION=$$ export ATHENA_ATTACHRUN_RECURSION
-PATH="@BINDIR@:$PATH"
-exec @PROGRAM@ "$@"
+exec /bin/athena/attachandrun @LOCKER@ @PROGRAM@ "$0" "$@"
