855dc0df07aeea156f03c67ea2d24064b956ea67
Robert A Basch <rbasch@mit.edu>
1998-04-10 02:20:10 +0000

    Add support for 6.2 miniroot: copy in additional tar sets for different kernels and CPU architecture dependent files; read OSVERSION file in miniroot directory if present to determine the OS version of the miniroot; use nawk instead of awk in deriving the OSLoadPartition nvram variable.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@11376 728af825-273c-0410-89f9-f7d3b574a069

--- packs/update/platform/sgi/setup-swap-boot.sh
+++ packs/update/platform/sgi/setup-swap-boot.sh
@@ -49,10 +49,84 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-/srvd/install/netconf --swap --root $swapmount --hostname $HOST --ipaddr $ADDR
+# Indy's have different kernels to choose from, based on the
+# CPU/graphics combination.  There's also different versions
+# of binaries such as /lib32/libc.so.1 between the R4000 and
+# R5000 architectures.  These are split into different tar sets.
+if [ `uname -m` = "IP22" ]; then
+	eval `hinv | awk '\
+	    /CPU: MIPS/			{ p = $3 }			\
+	    /Graphics board:/		{ g = $3 }			\
+	    /graphics installed/	{ g = $1 }			\
+	    END		{ printf("cpu=%s; graphics=%s", p, g); }'`
 
-# XXX Copy in appropriate kernel, lib32(?)...
-# XXX Not an issue for 6.3, but 6.2 needs it.
+	# Copy in files specific to the hardware combination (kernel)
+	case ${cpu} in
+	    R4600)
+		hdwr=46
+		;;
+	    R5000)
+		hdwr=50
+		;;
+	    *)
+		echo "Unrecognized CPU type: ${cpu}"
+		exit 1
+		;;
+	esac
+
+	case ${graphics} in
+	    GR3-XZ)
+		hdwr=${hdwr}xz
+		;;
+	    Indy)
+		hdwr=${hdwr}xl
+		;;
+	    *)
+		echo "Unrecognized graphics subsystem: ${graphics}"
+		exit 1
+		;;
+	esac
+
+	echo "Untarring miniroot.${hdwr}..."
+	(cd $swapmount; \
+	    zcat /install/miniroot/miniroot.${hdwr}.tar.Z | tar xf -)
+	if [ $? -ne 0 ]; then
+	    echo "Failed untarring miniroot.${hdwr}."
+	    exit 1
+	fi
+
+	# Copy in files specific to the CPU architecture
+	case ${cpu} in
+	    R4?00)
+		cpuarch=R4000
+		;;
+	    R5?00)
+		cpuarch=R5000
+		;;
+	    *)
+		echo "$prog: Unrecognized CPU architecture: ${cpu}"
+		exit 1
+		;;
+	esac
+
+	echo "Untarring miniroot.${cpuarch}..."
+	(cd $swapmount; \
+	    zcat /install/miniroot/miniroot.${cpuarch}.tar.Z | tar xf -)
+	if [ $? -ne 0 ]; then
+	    echo "Failed untarring miniroot.${cpuarch}."
+	    exit 1
+	fi
+
+fi
+
+# The miniroot may be a different OS version from
+# the currently running system.
+if [ -r /install/miniroot/OSVERSION ]; then
+	AIS_OSVERSION=`cat /install/miniroot/OSVERSION`
+	export AIS_OSVERSION
+fi
+
+/srvd/install/netconf --swap --root $swapmount --hostname $HOST --ipaddr $ADDR
 
 # XXX Kernel needs to be tweaked to boot with the appropriate root
 # XXX partition. This should be the only thing that currently has a
@@ -86,7 +160,7 @@ template="scsi(%d)disk(%d)rdisk(0)partition(%d)"
 if [ `uname -m` = "IP32" ]; then
 	template="pci(0)$template"
 fi
-nvram OSLoadPartition `devnm $swapmount/var | awk -F/ -v template=$template \
+nvram OSLoadPartition `devnm $swapmount/var | nawk -F/ -v template=$template \
 	'{ printf(template, substr($4,4,1), substr($4,6,1), substr($4,8,1)) }'`
 
 # Clean up.
