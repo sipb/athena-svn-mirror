16ed4a4f97ff56782472cb9dee3858eae4c50057
Jonathon Weiss <jweiss@mit.edu>
1998-06-30 02:56:21 +0000

    SMSUPDATE became MRUPDATE years ago
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@11681 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/snmp/server/src/conf_grp.c
+++ athena/etc/snmp/server/src/conf_grp.c
@@ -12,9 +12,12 @@
  * 15 April 1990
  *
  *    $Source: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/conf_grp.c,v $
- *    $Author: ghudson $
+ *    $Author: jweiss $
  *    $Locker:  $
  *    $Log: not supported by cvs2svn $
+ *    Revision 2.1  1997/02/27 06:47:21  ghudson
+ *    BSD -> ANSI memory functions
+ *
  *    Revision 2.0  1992/04/22 02:04:04  tom
  *    release 7.4
  *
@@ -35,7 +38,7 @@
  */
 
 #ifndef lint
-static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/conf_grp.c,v 2.1 1997-02-27 06:47:21 ghudson Exp $";
+static char *rcsid = "$Header: /afs/dev.mit.edu/source/repository/athena/etc/snmp/server/src/conf_grp.c,v 2.2 1998-06-30 02:56:21 jweiss Exp $";
 #endif
 
 
@@ -141,7 +144,7 @@ lu_rcvar(varnode, repl, instptr, reqflg)
     case N_RCZSERVER:
       return(get_rc_variable("ZSERVER",   &(repl->val.value.str)));
     case N_RCSMSUPDATE:
-      return(get_rc_variable("SMSUPDATE", &(repl->val.value.str)));
+      return(get_rc_variable("MRUPDATE", &(repl->val.value.str)));
     case N_RCINETD:
       return(get_rc_variable("INETD",     &(repl->val.value.str)));
     case N_RCNOCREATE:
