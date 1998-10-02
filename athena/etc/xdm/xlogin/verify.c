630da1532e8dcf648ee4fde8c8d02dec520d237d
Greg Hudson <ghudson@mit.edu>
1998-10-02 17:55:45 +0000

    Pull up 1.96 to Athena 8.2 branch: move larvnet notifications to xlogin.c.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_2@12035 728af825-273c-0410-89f9-f7d3b574a069

--- athena/etc/xdm/xlogin/verify.c
+++ athena/etc/xdm/xlogin/verify.c
@@ -1,4 +1,4 @@
-/* $Header: /afs/dev.mit.edu/source/repository/athena/etc/xdm/xlogin/verify.c,v 1.93.2.2 1998-09-24 14:14:00 ghudson Exp $ */
+/* $Header: /afs/dev.mit.edu/source/repository/athena/etc/xdm/xlogin/verify.c,v 1.93.2.3 1998-10-02 17:55:45 ghudson Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -604,14 +604,8 @@ char *dologin(user, passwd, option, script, tty, session, display)
   if (nanny_setupUser(pwd->pw_name, environment, newargv))
     return lose("failed to setup for login");
 
-#ifndef XDM
-  larv_set_busy(1);
-#endif
   exit(0);
 #else
-#ifndef XDM
-  larv_set_busy(1);
-#endif
   execle(session, "sh", errbuf, script, NULL, environment);
 #endif /* sgi */
 
