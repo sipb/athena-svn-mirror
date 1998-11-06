2ba1c234a6f0c80a507c12653dba8ce1a70f8a7b
Greg Hudson <ghudson@mit.edu>
1998-11-06 18:52:40 +0000

    Pull up 1.11 to Athena 8.2 branch: read boot environment.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_2@12126 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/access/access_off.c
+++ athena/bin/access/access_off.c
@@ -13,20 +13,32 @@
  * without express or implied warranty.
  */
 
-static const char rcsid[] = "$Id: access_off.c,v 1.9.2.1 1998-07-30 17:20:49 ghudson Exp $";
+static const char rcsid[] = "$Id: access_off.c,v 1.9.2.2 1998-11-06 18:52:40 ghudson Exp $";
 
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define PATH_INETD_PID		"/var/athena/inetd.pid"
+#define PATH_SSHD_PID		"/var/athena/sshd.pid"
+#define PATH_SSHD		"/etc/athena/sshd"
+#define PATH_RC_CONF		"/etc/athena/rc.conf"
+#define PATH_BOOT_ENVIRON	"/var/athena/env.boot"
+#define ENV_FALLBACK		{ "PATH=/etc/athena", NULL }
+
+static char **get_boot_environment(void);
 
 int main(int argc, char **argv)
 {
   FILE *pidfile, *rcfile;
-  int pid, status, on, running;
-  char *progname, buf[1024], *envp[1] = { NULL };
+  int pid, on, running;
+  char *progname, buf[1024], **env, *env_fallback[] = ENV_FALLBACK;
 
   progname = strrchr(argv[0], '/');
   progname = (progname) ? progname + 1 : argv[0];
@@ -36,16 +48,16 @@ int main(int argc, char **argv)
   on = (strcmp(progname, "access_off") != 0);
 
   /* Send SIGUSR1 or SIGUSR2 to the Athena inetd. */
-  pidfile = fopen("/var/athena/inetd.pid", "r");
+  pidfile = fopen(PATH_INETD_PID, "r");
   if (!pidfile)
     {
-      fprintf(stderr, "Cannot read /var/athena/inetd.pid."
-	      "  Daemon probably not running\n");
+      fprintf(stderr, "Cannot read %s.  Daemon probably not running\n",
+	      PATH_INETD_PID);
       return 1;
     }
   if (fscanf(pidfile, "%d", &pid) != 1)
     {
-      fprintf(stderr, "Error reading /var/athena/inetd.pid.\n");
+      fprintf(stderr, "Error reading %s.\n", PATH_INETD_PID);
       return 1;
     }
   fclose(pidfile);
@@ -58,10 +70,10 @@ int main(int argc, char **argv)
 
   /* If SSHD is set to "switched" in /etc/athena/rc.conf, then start or stop
    * sshd if it's not already in the state we want. */
-  rcfile = fopen("/etc/athena/rc.conf", "r");
+  rcfile = fopen(PATH_RC_CONF, "r");
   if (!rcfile)
     {
-      fprintf(stderr, "Cannot read /etc/athena/rc.conf: %s", strerror(errno));
+      fprintf(stderr, "Cannot read %s: %s", PATH_RC_CONF, strerror(errno));
       return 1;
     }
   while (fgets(buf, sizeof(buf), rcfile) != NULL)
@@ -76,7 +88,7 @@ int main(int argc, char **argv)
 
       /* Read the sshd pidfile and send a signal 0 to the contained pid
        * to determine if sshd is running. */
-      pidfile = fopen("/var/athena/sshd.pid", "r");
+      pidfile = fopen(PATH_SSHD_PID, "r");
       if (pidfile && fscanf(pidfile, "%d", &pid) == 1 && kill(pid, 0) == 0)
 	running = 1;
       else
@@ -97,10 +109,14 @@ int main(int argc, char **argv)
 	    {
 	      fclose(rcfile);
 	      /* Make ruid and saved uid equal to effective uid before exec,
-	       * or sshd might notice and get confused. Clear the environment
-	       * to prevent evil ldd tricks. */
+	       * or sshd might notice and get confused.  Set the environment
+	       * to the boot-time environment.
+	       */
 	      setuid(geteuid());
-	      execle("/etc/athena/sshd", "sshd", (char *) NULL, envp);
+	      env = get_boot_environment();
+	      if (!env)
+		env = env_fallback;
+	      execle(PATH_SSHD, PATH_SSHD, (char *) NULL, env);
 	      perror("Error running sshd");
 	    }
 	}
@@ -116,4 +132,69 @@ int main(int argc, char **argv)
   fclose(rcfile);
 
   return 0;
+}
+
+static char **get_boot_environment(void)
+{
+  int fd, len, count;
+  struct stat st;
+  char *buf, *p, *q, **env, **ep;
+
+  /* Open and read the stored boot environment. */
+  fd = open(PATH_BOOT_ENVIRON, O_RDONLY);
+  if (fd == -1)
+    return NULL;
+  if (fstat(fd, &st) == -1)
+    {
+      close(fd);
+      return NULL;
+    }
+  buf = malloc(st.st_size);
+  if (!buf)
+    {
+      close(fd);
+      return NULL;
+    }
+  len = 0;
+  while (len < st.st_size)
+    {
+      count = read(fd, buf + len, st.st_size - len);
+      if (count < 0 && errno != EINTR)
+	{
+	  close(fd);
+	  free(buf);
+	  return NULL;
+	}
+      else if (count > 0)
+	len += count;
+    }
+  close(fd);
+
+  /* Allocate space for the environment pointers. */
+  count = 0;
+  for (p = buf; *p; p++)
+    {
+      if (*p == '\n')
+	count++;
+    }
+  env = malloc((count + 1) * sizeof(char *));
+  if (!env)
+    {
+      free(buf);
+      return NULL;
+    }
+
+  /* Set the environment pointers. */
+  ep = env;
+  p = buf;
+  while ((q = strchr(p, '\n')) != NULL)
+    {
+      *ep++ = p;
+      *q = 0;
+      p = q + 1;
+    }
+
+  /* Terminate the environment list and return it. */
+  *ep = NULL;
+  return env;
 }
