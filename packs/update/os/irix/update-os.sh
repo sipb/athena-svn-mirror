f0440429c28cfd3ce5ac8474906c3b497483eb08
Robert A Basch <rbasch@mit.edu>
1999-03-02 15:51:04 +0000

    Changes for IRIX 6.5 update: - Run inst in multiple passes, for both local and link packages, split   by distribution sets. - Add hack to delay building of filetype database until the end of all   symlink mode passes, to save wasted time in inst. - Add invocations of tweakdev and docinst. - Add /srvd/install to PATH. - New inst command line options. - Nuke 5.3-only code to track /install.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@12615 728af825-273c-0410-89f9-f7d3b574a069

--- packs/update/os/irix/update-os.sh
+++ packs/update/os/irix/update-os.sh
@@ -1,5 +1,8 @@
 #!/sbin/sh
 
+PATH=/srvd/install:$PATH
+export PATH
+
 if [ -f $CONFVARS ]; then
 	. $CONFVARS
 fi
@@ -12,27 +15,64 @@ if [ -s "$DEADFILES" ]; then
 	done
 fi
 
-# This block of code is applicable only to Irix 6.x.
-if [ -s $LOCALPACKAGES -o -s $LINKPACKAGES ]; then
+seldir=/install/selections
+dists=`cat ${seldir}/dist_order 2>/dev/null`
+
+# See if we need to run inst to update OS packages.
+runinst=0
+for dist in $dists ; do
+	if [ -s $LOCALPACKAGES.$dist -o -s $LINKPACKAGES.$dist ]; then
+		runinst=1
+		break
+	fi
+done
+
+if [ "$runinst" -ne 0 ]; then
 	echo "Running inst to make operating system changes..."
 
 	opts="-a -Vinstmode:normal -Vstartup_script:ignore -N"
+	opts="$opts -Vdelay_idb_read:on -Voverlay_mode:silent"
 	opts="$opts -r $UPDATE_ROOT/"
 
-	if [ -s $LOCALPACKAGES ]; then
-		echo "Updating local operating system packages"
-		cat /install/selections/header $LOCALPACKAGES > \
-		    /tmp/selections.local
+	# Install local packages.
+	for dist in $dists ; do
+		if [ -s $LOCALPACKAGES.$dist ]; then
+			echo "Updating local $dist packages"
+			cat $seldir/header.$dist $LOCALPACKAGES.$dist > \
+				/tmp/selections.local
 
-		inst $opts -F /tmp/selections.local
+			inst $opts -F /tmp/selections.local
+		fi
+	done
+
+	# Skip having inst rebuild the file type database by moving
+	# the Makefile out of the way to install symlinks.  This is a
+	# big win during a full install, when the make would normally
+	# be invoked many times.  We'll restore the Makefile and do
+	# one make after the symlink mode passes.
+	if [ "$DELAYFTMAKE" = true ]; then
+		ftmakefile=${UPDATE_ROOT}/usr/lib/filetype/Makefile
+		mv ${ftmakefile} ${ftmakefile}.hold
 	fi
 
-	if [ -s $LINKPACKAGES ]; then
-		echo "Updating linked operating system packages"
-		cat /install/selections/header $LINKPACKAGES > \
-		    /tmp/selections.link
+	# Install symlink packages.
+	for dist in $dists ; do
+		if [ -s $LINKPACKAGES.$dist ]; then
+			echo "Updating linked $dist packages"
+			cat $seldir/header.$dist $LINKPACKAGES.$dist > \
+				/tmp/selections.link
 
-		inst $opts -T/os -F /tmp/selections.link -Vskip_rqs:true
+			inst $opts -T/os -F /tmp/selections.link \
+				-Vskip_rqs:true
+		fi
+	done
+
+	# Now put the file type database Makefile back, and do the make.
+	if [ "$DELAYFTMAKE" = true ]; then
+		mv ${ftmakefile}.hold ${ftmakefile}
+		echo "Doing 'make' in /usr/lib/filetype..."
+		chroot ${UPDATE_ROOT} /bin/sh -c \
+		    "cd /usr/lib/filetype; make > /dev/null"
 	fi
 
 	# Restore any new config file that inst has replaced.
@@ -55,16 +95,21 @@ if [ -s $LOCALPACKAGES -o -s $LINKPACKAGES ]; then
 		done
 	fi
 
+	# An update from the miniroot can leave the wrong disklinks
+	# in /dev (e.g., because there is no swap partition).  Fix them.
+	if [ "$MINIROOT" = true ]; then
+		sh /srvd/install/tweakdev --root $UPDATE_ROOT/
+	fi
+
 	sh /srvd/install/oschanges --root $UPDATE_ROOT/
 fi
 
-echo "Tracking changes"
-
-# Irix 5.3 only.
-if [ "$TRACKOS" = true ]; then
-	/install/install/track -v -F /install -T / -d \
-		-W /install/install/lib
+# See if we need to update the documentation trees.
+if [ "$UPDATEDOC" = true ]; then
+	sh /srvd/install/docinst --root $UPDATE_ROOT/
 fi
+
+echo "Tracking changes"
 
 # Track from the srvd.
 track -v -F /srvd -T $UPDATE_ROOT/ -d -W /srvd/usr/athena/lib
