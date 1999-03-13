b33dfa90bbdae920158988aabe073d309d9a7432
Robert A Basch <rbasch@mit.edu>
1999-03-13 21:23:45 +0000

    Base configuration ports 1.73, afs 3.4a
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@12673 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/afsbin/arch/sgi_65/include/afs/permit_xprt.h
@@ -0,0 +1,7 @@
+/* Copyright (C) 1990 Transarc Corporation - All rights reserved */
+/* $Header: /afs/dev.mit.edu/source/repository/third/afsbin/arch/sgi_65/include/afs/permit_xprt.h,v 1.1.1.1 1999-03-13 21:23:44 rbasch Exp $ */
+
+#undef HIDE
+#define HIDE
+#define xprt_CoerceLevel
+#define xprt_CryptOK(x) 1
