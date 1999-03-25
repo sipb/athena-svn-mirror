34a304ae453b4b8c466017d806a95db604aad710
Dan Winship <danw@mit.edu>
1999-03-25 04:33:55 +0000

    This commit was generated by cvs2svn to compensate for changes in r12763, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@12764 728af825-273c-0410-89f9-f7d3b574a069

--- third/krb5/src/lib/krb5/krb/brand.c
+++ third/krb5/src/lib/krb5/krb/brand.c
@@ -12,4 +12,4 @@
 
 /* Format: "KRB5_BRAND: <cvs tag> <date>" */
 
-static char krb5_brand[] = "KRB5_BRAND: V1_0_5_FREEZE4 1.0.5 19980206";
+static char krb5_brand[] = "KRB5_BRAND: V1_0_BRANCH 1.0.6 19990324";
