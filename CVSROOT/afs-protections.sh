b2456b1ef04c4a9692778d8a66a144e1932b0661
Greg Hudson <ghudson@mit.edu>
1999-04-01 18:00:28 +0000

    De-restrict quota.  Leave rpcsvc restricted. It'd be nice if we could avoid error messages for directories which  are gone (quota/rpcsvc, login, gdss, ftpd, and some day lpr and mwm).
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@12813 728af825-273c-0410-89f9-f7d3b574a069

--- CVSROOT/afs-protections.sh
+++ CVSROOT/afs-protections.sh
@@ -2,17 +2,17 @@
 
 genacl="read:source read read:staff read adm:source la"
 
-case "$1" in
-	repository)
-		genacl="$genacl write:staff write"
-		;;
-	wd)
-		genacl="$genacl write:update write"
-		;;
-	*)
-		echo "Usage: $0 {repository|wd}"
-		exit 1
-		;;
+case $1 in
+repository)
+	genacl="$genacl write:staff write"
+	;;
+wd)
+	genacl="$genacl write:update write"
+	;;
+*)
+	echo "Usage: $0 {repository|wd}"
+	exit 1
+	;;
 esac
 
 public="$genacl system:anyuser read"
@@ -22,7 +22,7 @@ auth="$genacl system:anyuser l system:authuser read"
 attach -h -n -q gnu
 `athdir /mit/gnu bin`/gfind . -noleaf \
 	-path ./athena/bin/lpr -prune -o \
-	-path ./athena/bin/quota -prune -o \
+	-path ./athena/bin/quota/rpcsvc -prune -o \
 	-path ./athena/bin/login -prune -o \
 	-path ./athena/bin/write -prune -o \
 	-path ./athena/bin/voldump -prune -o \
@@ -36,7 +36,8 @@ attach -h -n -q gnu
 	-type d -print | xargs fs sa -acl $public -clear -dir
 
 find athena/bin/lpr -type d -print | xargs fs sa -acl $fascist -clear -dir
-find athena/bin/quota -type d -print | xargs fs sa -acl $fascist -clear -dir
+find athena/bin/quota/rpcsvc -type d -print \
+	| xargs fs sa -acl $fascist -clear -dir
 find athena/bin/login -type d -print | xargs fs sa -acl $fascist -clear -dir
 find athena/bin/write -type d -print | xargs fs sa -acl $fascist -clear -dir
 find athena/bin/voldump -type d -print | xargs fs sa -acl $fascist -clear -dir
