70844065e68e3edda46b36b557e5752aab270764
Greg Hudson <ghudson@mit.edu>
1999-07-12 20:42:28 +0000

    Pull up 1.2 to Athena 8.3 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_3@13366 728af825-273c-0410-89f9-f7d3b574a069

--- third/lprng/lprng/src/include/printjob.h
+++ third/lprng/lprng/src/include/printjob.h
@@ -4,7 +4,7 @@
  * Copyright 1988-1999, Patrick Powell, San Diego, CA
  *     papowell@astart.com
  * See LICENSE for conditions of use.
- * $Id: printjob.h,v 1.1.1.2 1999-05-04 18:07:11 danw Exp $
+ * $Id: printjob.h,v 1.1.1.2.2.1 1999-07-12 20:42:28 ghudson Exp $
  ***************************************************************************/
 
 
@@ -13,7 +13,8 @@
 #define _PRINTJOB_H_ 1
 
 /* PROTOTYPES */
-int Wait_for_pid( int of_pid, char *name, int suspend, int timeout );
+int Wait_for_pid( int of_pid, char *name, int suspend, int timeout,
+	plp_status_t *status);
 void Print_job( int output, struct job *job, int timeout );
 char *Fix_str( char *str );
 void Print_banner( char *name, char *pgm, struct job *job );
@@ -21,6 +22,7 @@ int Write_outbuf_to_OF( struct job *job, char *title,
 	int of_pid, int of_fd, int of_error,
 	char *buffer, int outlen,
 	char *msg, int msgmax,
-	int timeout, int suspend, int max_wait );
+	int timeout, int suspend, int max_wait, int *exit_status,
+	plp_status_t *ps_status);
 
 #endif
