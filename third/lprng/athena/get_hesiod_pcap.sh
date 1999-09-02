e6b8bf19d43163dafc5a4ac9000f08f75a0e8af1
Greg Hudson <ghudson@mit.edu>
1999-09-02 14:33:29 +0000

    Put /bin in PATH.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@13548 728af825-273c-0410-89f9-f7d3b574a069

--- third/lprng/athena/get_hesiod_pcap.sh
+++ third/lprng/athena/get_hesiod_pcap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# $Id: get_hesiod_pcap.sh,v 1.2 1999-04-08 01:26:11 mwhitson Exp $
+# $Id: get_hesiod_pcap.sh,v 1.3 1999-09-02 14:33:29 ghudson Exp $
 
 # Support script used by LPRng to fetch Hesiod printcap entries
 
-PATH=/bin/athena:/usr/bin:/usr/bsd
+PATH=/bin/athena:/usr/bin:/bin:/usr/bsd
 
 read printer
 
