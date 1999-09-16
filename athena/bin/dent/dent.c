d08a9273451f449757d592b40b6fafcfadae3a65
Dan Winship <danw@mit.edu>
1999-09-16 00:02:55 +0000

    redo in Athena coding style, add read_line so it works on files with lines > 1024 characters long, fix warnings, and build with warnings enabled
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@13591 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/dent/dent.c
+++ athena/bin/dent/dent.c
@@ -1,66 +1,132 @@
-/*
- *	$Id: dent.c,v 1.3 1999-01-22 23:09:17 ghudson Exp $
+/* Copyright 1985-1999 by the Massachusetts Institute of Technology.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in
+ * advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ * M.I.T. makes no representations about the suitability of
+ * this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
  */
 
-#ifndef lint
-static char *rcsid_dent_c = "$Id: dent.c,v 1.3 1999-01-22 23:09:17 ghudson Exp $";
-#endif /* lint */
+/* This is dent, a program to indent the standard input. */
+
+static const char rcsid[] = "$Id: dent.c,v 1.4 1999-09-16 00:02:55 danw Exp $";
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAXCHARS 1024		/* maximum length of a single line */
+void indent(FILE *fptr, int nspaces);
+void spaceout(int spaces);
+static int read_line(FILE *fp, char **buf, int *bufsize);
 
-/*
-	This routine indents the standard input.
-*/
-
-main(argc,argv)
-int argc;
-char *argv[];
+int main(int argc, char **argv)
 {
-	FILE *fptr = stdin;	/* initially read standard input file */
-	register int i,j;
-	int nspaces = 8;
-	for(i = 1; i < argc; i++) {
-		if(argv[i][0] == '-') {
-			if(sscanf(&argv[i][1],"%d",&nspaces) == 0) {
-				fprintf(stderr,
-					"%s: argument must be integer.\n",
-					argv[0]);
-				exit(1);
-			}
-			continue;
-		}
-		if((fptr = fopen(argv[i],"r")) == NULL) {
-			fprintf(stderr,"%s: no such file %s.\n",
-				argv[0],argv[i]);
-			exit(1);
-		}
-		indent(fptr,nspaces);
-	fclose(fptr);
+  FILE *fptr = stdin;	/* initially read standard input file */
+  int i;
+  int nspaces = 8;
+
+  for (i = 1; i < argc; i++)
+    {
+      if (argv[i][0] == '-')
+	{
+	  if (sscanf(&argv[i][1], "%d", &nspaces) == 0)
+	    {
+	      fprintf(stderr, "%s: argument must be integer.\n", argv[0]);
+	      exit(1);
+	    }
+	  continue;
 	}
-	if(fptr == stdin) indent(fptr,nspaces);
-	exit(0);
+
+      fptr = fopen(argv[i],"r");
+      if (!fptr)
+	{
+	  fprintf(stderr,"%s: no such file %s.\n", argv[0], argv[i]);
+	  exit(1);
+	}
+      indent(fptr, nspaces);
+      fclose(fptr);
+    }
+
+  if (fptr == stdin)
+    indent(fptr, nspaces);
+
+  return 0;
 }
 
-int indent(fptr,nspaces)
-FILE *fptr;
-int nspaces;
+void indent(FILE *fptr, int nspaces)
 {
-	register int i,j;
-	char line[MAXCHARS];
-	while(fgets(line,MAXCHARS,fptr) != NULL) {
-		if(strlen(line) >= 1) {
-			spaceout(nspaces);
-			for (j = 0; j < strlen(line); j++) {
-				putchar(line[j]);
-				if(line[j] == '\r') spaceout(nspaces);
-			}
-		}
+  int j, linesize;
+  char *line = NULL;
+
+  while (read_line(fptr, &line, &linesize) == 0)
+    {
+      if (strlen(line) >= 1)
+	{
+	  spaceout(nspaces);
+	  for (j = 0; j < strlen(line); j++)
+	    {
+	      putchar(line[j]);
+	      if (line[j] == '\r')
+		spaceout(nspaces);
+	    }
+	  putchar('\n');
 	}
+    }
 }
-spaceout(spaces)
-register int spaces;
+
+void spaceout(int spaces)
 {
-	while(spaces-- > 0) putchar(' ');
+  while (spaces-- > 0)
+    putchar(' ');
+}
+
+/* This is an internal function.  Its contract is to read a line from a
+ * file into a dynamically allocated buffer, zeroing the trailing newline
+ * if there is one.  The calling routine may call read_line multiple
+ * times with the same buf and bufsize pointers; *buf will be reallocated
+ * and *bufsize adjusted as appropriate.  The initial value of *buf
+ * should be NULL.  After the calling routine is done reading lines, it
+ * should free *buf.  This function returns 0 if a line was successfully
+ * read, 1 if the file ended, and -1 if there was an I/O error or if it
+ * ran out of memory.
+ */
+
+static int read_line(FILE *fp, char **buf, int *bufsize)
+{
+  char *newbuf;
+  int offset = 0, len;
+
+  if (*buf == NULL)
+    {
+      *buf = malloc(128);
+      if (!*buf)
+	return -1;
+      *bufsize = 128;
+    }
+
+  while (1)
+    {
+      if (!fgets(*buf + offset, *bufsize - offset, fp))
+	return (offset != 0) ? 0 : (ferror(fp)) ? -1 : 1;
+      len = offset + strlen(*buf + offset);
+      if ((*buf)[len - 1] == '\n')
+	{
+	  (*buf)[len - 1] = 0;
+	  return 0;
+	}
+      offset = len;
+
+      /* Allocate more space. */
+      newbuf = realloc(*buf, *bufsize * 2);
+      if (!newbuf)
+	return -1;
+      *buf = newbuf;
+      *bufsize *= 2;
+    }
 }
