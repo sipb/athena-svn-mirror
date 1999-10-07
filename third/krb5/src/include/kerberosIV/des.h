79c92d3a08c0dcf1257625838f0a4a7fc388a212
Greg Hudson <ghudson@mit.edu>
1999-10-07 00:59:49 +0000

    Revert to vendor branch state; krb5 1.1 fixed our problem in a different way.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@13670 728af825-273c-0410-89f9-f7d3b574a069

--- third/krb5/src/include/kerberosIV/des.h
+++ third/krb5/src/include/kerberosIV/des.h
@@ -16,7 +16,10 @@
  * this permission notice appear in supporting documentation, and that
  * the name of M.I.T. not be used in advertising or publicity pertaining
  * to distribution of the software without specific, written prior
- * permission.  M.I.T. makes no representations about the suitability of
+ * permission.  Furthermore if you modify this software you must label
+ * your software as modified software and not distribute it in such a
+ * fashion that it might be confused with the original M.I.T. software.
+ * M.I.T. makes no representations about the suitability of
  * this software for any purpose.  It is provided "as is" without express
  * or implied warranty.
  * 
@@ -27,14 +30,69 @@
 #ifndef DES_DEFS
 #define DES_DEFS
 
+#if defined(_WIN32) && !defined(_WINDOWS)
+#define _WINDOWS
+#endif
+
+#if defined(_WINDOWS)
+#ifndef KRB4
+#define KRB4 1
+#endif
+#include <win-mac.h>
+#endif
+
+/* Windows declarations */
+#ifndef KRB5_CALLCONV
+#define KRB5_CALLCONV
+#define KRB5_CALLCONV_C
+#define KRB5_DLLIMP
+#define GSS_DLLIMP
+#define KRB5_EXPORTVAR
+#endif
+#ifndef FAR
+#define FAR
+#define NEAR
+#endif
+
+#ifndef __alpha
+#define KRB4_32	long
+#else
+#define KRB4_32	int
+#endif
+
+
+#ifndef PROTOTYPE
+#if (defined(__STDC__) || defined(_WINDOWS)) && !defined(KRB5_NO_PROTOTYPES)
+#define PROTOTYPE(x) x
+#else
+#define PROTOTYPE(x) ()
+#endif
+#endif
+
+
+
 typedef unsigned char des_cblock[8];	/* crypto-block size */
+
 /* Key schedule */
-typedef struct des_ks_struct {
-    union {
-	long pad;
-	des_cblock _;
-    } __;
-} des_key_schedule[16];
+/* Ick.  We need this in here unfortunately... */
+#ifndef DES_INT32
+#ifdef SIZEOF_INT
+#if SIZEOF_INT >= 4
+#define DES_INT32 int
+#else
+#define DES_INT32 long
+#endif
+#else /* !defined(SIZEOF_INT) */
+#include <limits.h>
+#if (UINT_MAX >= 0xffffffff)
+#define DES_INT32 int
+#else
+#define DES_INT32 long
+#endif
+#endif /* !defined(SIZEOF_INT) */
+#endif /* !defined(DES_INT32) */
+
+typedef struct des_ks_struct {  DES_INT32 _[2]; } des_key_schedule[16];
 
 #define DES_KEY_SZ 	(sizeof(des_cblock))
 #define DES_ENCRYPT	1
@@ -60,4 +118,26 @@ typedef struct des_ks_struct bit_64;
 
 #define des_cblock_print(x) des_cblock_print_file(x, stdout)
 
+
+/*
+ * Function Prototypes
+ */
+
+KRB5_DLLIMP int KRB5_CALLCONV
+des_key_sched
+	PROTOTYPE((C_Block, Key_schedule));
+
+KRB5_DLLIMP int KRB5_CALLCONV
+des_pcbc_encrypt
+	PROTOTYPE((C_Block FAR *in, C_Block FAR *out, long length,
+		   Key_schedule schedule, C_Block FAR *ivec, int encrypt));
+
+KRB5_DLLIMP unsigned long KRB5_CALLCONV
+des_quad_cksum
+	PROTOTYPE((unsigned char FAR *in, unsigned KRB4_32 FAR *out,
+		   long length, int out_count, C_Block FAR *seed));
+
+KRB5_DLLIMP int KRB5_CALLCONV
+des_string_to_key
+	PROTOTYPE((char FAR *, C_Block));
 #endif	/* DES_DEFS */
