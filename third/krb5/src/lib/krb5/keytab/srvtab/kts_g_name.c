8c8acd5bce9abb19c1d32473a422db07d88dfbff
Greg Hudson <ghudson@mit.edu>
1999-10-08 14:18:21 +0000

    krb5 1.1 took this feature; go to vendor branch state.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@13701 728af825-273c-0410-89f9-f7d3b574a069

--- third/krb5/src/lib/krb5/keytab/srvtab/kts_g_name.c
+++ third/krb5/src/lib/krb5/keytab/srvtab/kts_g_name.c
@@ -16,7 +16,10 @@
  * this permission notice appear in supporting documentation, and that
  * the name of M.I.T. not be used in advertising or publicity pertaining
  * to distribution of the software without specific, written prior
- * permission.  M.I.T. makes no representations about the suitability of
+ * permission.  Furthermore if you modify this software you must label
+ * your software as modified software and not distribute it in such a
+ * fashion that it might be confused with the original M.I.T. software.
+ * M.I.T. makes no representations about the suitability of
  * this software for any purpose.  It is provided "as is" without express
  * or implied warranty.
  * 
@@ -27,7 +30,7 @@
 #include "k5-int.h"
 #include "ktsrvtab.h"
 
-krb5_error_code
+krb5_error_code KRB5_CALLCONV
 krb5_ktsrvtab_get_name(context, id, name, len)
     krb5_context context;
     krb5_keytab id;
