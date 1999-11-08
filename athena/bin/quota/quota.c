f8d003e4c71e590360f7169736b88eb493736261
Greg Hudson <ghudson@mit.edu>
1999-11-08 23:03:28 +0000

    Pull up 1.25-1.26 to athena-8_3 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_3@13897 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/quota/quota.c
+++ athena/bin/quota/quota.c
@@ -17,10 +17,11 @@
  * various types of lockers.
  */
 
-static const char rcsid[] = "$Id: quota.c,v 1.24 1999-03-29 19:14:20 danw Exp $";
+static const char rcsid[] = "$Id: quota.c,v 1.24.2.1 1999-11-08 23:03:28 ghudson Exp $";
 
 #include <ctype.h>
 #include <pwd.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -48,6 +49,7 @@ int main(int argc, char **argv)
   int fsind = 0, fssize = 0, heading_printed = 0;
   int verbose = 0;
   struct quota_fs *fslist = NULL;
+  sigset_t mask;
 
   myuid = getuid();
   if (locker_init(&context, myuid, NULL, NULL) != LOCKER_SUCCESS)
@@ -55,6 +57,13 @@ int main(int argc, char **argv)
       fprintf(stderr, "quota: Could not initialize locker library.\n");
       exit(1);
     }
+
+  /* Block ^Z to prevent holding locks on the attachtab. */
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGTSTP);
+  sigaddset(&mask, SIGTTOU);
+  sigaddset(&mask, SIGTTIN);
+  sigprocmask(SIG_BLOCK, &mask, NULL);
 
   while ((opt = getopt(argc, argv, "af:guv")) != -1)
     {
@@ -108,7 +117,7 @@ int main(int argc, char **argv)
 	  break;
 
 	default:
-	  fprintf(stderr, "quota: %s: unknown option\n", argv[optind]);
+	  fprintf(stderr, "quota: %s: unknown option\n", argv[optind - 1]);
 	  usage();
 	  exit(1);
 	}
