6b6b0f1cbac6ab5a065a253c2d5cc849a944686d
Greg Hudson <ghudson@mit.edu>
2000-01-07 23:03:20 +0000

    Pull up 1.2 to athena-8_3 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-8_3@14186 728af825-273c-0410-89f9-f7d3b574a069

--- third/traceroute/ifaddrlist.h
+++ third/traceroute/ifaddrlist.h
@@ -18,7 +18,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * @(#) $Header: /afs/dev.mit.edu/source/repository/third/traceroute/ifaddrlist.h,v 1.1.1.1 1997-09-24 06:24:33 ghudson Exp $ (LBL)
+ * @(#) $Header: /afs/dev.mit.edu/source/repository/third/traceroute/ifaddrlist.h,v 1.1.1.1.4.1 2000-01-07 23:03:20 ghudson Exp $ (LBL)
  */
 
 struct ifaddrlist {
@@ -26,4 +26,4 @@ struct ifaddrlist {
 	char *device;
 };
 
-int	ifaddrlist(struct ifaddrlist **, char *);
+int	ifaddrlist(struct ifaddrlist **, char *, int);
