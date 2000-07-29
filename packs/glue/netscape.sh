fd03bf6c22ad6064dc25e6ea02fe210f5a7043a4
Greg Hudson <ghudson@mit.edu>
2000-07-29 14:18:23 +0000

    Make sure infoagents is attached before we run the local netscape.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@14968 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/netscape.sh
+++ packs/glue/netscape.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# $Id: netscape.sh,v 1.1 2000-06-08 18:49:33 ghudson Exp $
+# $Id: netscape.sh,v 1.2 2000-07-29 14:18:23 ghudson Exp $
 
 localscript=/var/athena/infoagents/arch/share/bin/netscape.adjusted
 if [ -x $localscript ]; then
+  /bin/athena/attach -nq infoagents
   exec $localscript "$@"
 else
   exec /bin/athena/attachandrun infoagents netscape "$0" "$@"
