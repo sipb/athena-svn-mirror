4f7873344d5a633a5799b773195dc4c6aa49e76a
Greg Hudson <ghudson@mit.edu>
2000-09-21 19:14:15 +0000

    Pull in remaining bugfixes from nmh 1.0.6.  Fixes several core dump problems.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15105 728af825-273c-0410-89f9-f7d3b574a069

--- third/nmh/sbr/m_getfld.c
+++ third/nmh/sbr/m_getfld.c
@@ -2,7 +2,7 @@
 /*
  * m_getfld.c -- read/parse a message
  *
- * $Id: m_getfld.c,v 1.2 2000-08-23 19:08:53 ghudson Exp $
+ * $Id: m_getfld.c,v 1.3 2000-09-21 19:14:15 ghudson Exp $
  */
 
 #include <h/mh.h>
@@ -390,7 +390,8 @@ m_getfld (int state, unsigned char *name, unsigned char *buf,
 		*cp++ = j = *(iob->_ptr + c);
 		c = _filbuf(iob);
 #endif
-		if ((j == '\0' || j == '\n') && c != ' ' && c != '\t') {
+                if (c == EOF || 
+		  ((j == '\0' || j == '\n') && c != ' ' && c != '\t')) {
 		    if (c != EOF) {
 #ifdef LINUX_STDIO
 			--iob->_IO_read_ptr;
@@ -538,7 +539,7 @@ m_unknown(FILE *iob)
 	    ;
 #else /* RPATHS */
 	cp = unixbuf;
-	while ((c = getc (iob)) != '\n')
+	while ((c = getc (iob)) != '\n' && cp - unixbuf < BUFSIZ - 1)
 	    *cp++ = c;
 	*cp = 0;
 #endif /* RPATHS */
@@ -571,7 +572,7 @@ m_unknown(FILE *iob)
     pat_map = (unsigned char **) calloc (256, sizeof(unsigned char *));
 
     for (cp = (char *) fdelim + 1; cp < (char *) delimend; cp++ )
-	pat_map[*cp] = (unsigned char *) cp;
+	pat_map[(unsigned char)*cp] = (unsigned char *) cp;
 
     if (msg_style == MS_MMDF) {
 	/* flush extra msg hdrs */
@@ -639,7 +640,7 @@ m_Eom (int c, FILE *iob)
 		break;
 #else /* RPATHS */
 	cp = unixbuf;
-	while ((c = getc (iob)) != '\n' && c >= 0)
+	while ((c = getc (iob)) != '\n' && c >= 0 && cp - unixbuf < BUFSIZ - 1)
 	    *cp++ = c;
 	*cp = 0;
 #endif /* RPATHS */
@@ -723,7 +724,8 @@ matchc(int patln, char *pat, int strln, char *str)
 		while (pc != *str++)
 			if (str > es)
 				return 0;
-
+		if (str > es+1)
+			return 0;
 		sp = str; pp = pat;
 		while (pp < ep && *sp++ == *pp)
 			pp++;
