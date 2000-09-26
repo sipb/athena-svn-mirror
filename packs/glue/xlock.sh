b0b6d7c49e4a40578000f417fe12de8bed8b3491
Greg Hudson <ghudson@mit.edu>
2000-09-26 14:46:42 +0000

    Just run xss.  You'll feel better.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15153 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/xlock.sh
+++ packs/glue/xlock.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
-# $Id: xlock.sh,v 1.4 1998-07-31 20:48:17 ghudson Exp $
+# $Id: xlock.sh,v 1.5 2000-09-26 14:46:42 ghudson Exp $
 
-if [ -r /etc/athena/rc.conf ]; then
-	. /etc/athena/rc.conf
-	if [ "$PUBLIC" != true ]; then
-		exec xlock.real "$@"
-	fi
+if /usr/athena/bin/xss-command -version 2>/dev/null; then
+  exec /usr/athena/bin/xss-command -lock
+else
+  exec /usr/athena/bin/xss -start-locked
 fi
-
-echo "xlock is not appropriate for cluster workstations because"
-echo "it does not display an elapsed time or put up a button to"
-echo "allow others to log you out after a set time."
-echo ""
-echo "If you really wish to run xlock, run it as 'xlock.real' instead."
-echo "But if you do so, others may legitimately reboot the machine at"
-echo "any time to log you out."
-echo ""
-echo "Press return to read the On-Line Consulting stock answer about"
-echo "screensavers, or press Control-C to go back to the prompt."
-read dummy
-/bin/athena/attachandrun infoagents htmlview htmlview \
-	http://web.mit.edu/answers/workstations/other_xscreensaver.html
