c3ce35686b678d7e560a77f91e27583f425cbe00
Greg Hudson <ghudson@mit.edu>
2000-10-04 04:59:08 +0000

    Cope with a modes line containing only a 1600x1200 mode.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15157 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/os/linux/fix-xconfig.sh
+++ packs/maint/os/linux/fix-xconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: fix-xconfig.sh,v 1.1 2000-09-04 16:49:10 ghudson Exp $
+# $Id: fix-xconfig.sh,v 1.2 2000-10-04 04:59:08 ghudson Exp $
 
 # fix-xconfig: Modify the X configuration to match timings retrieved
 # from read-edid and to have the desired maximum resolution.  Run from
@@ -12,26 +12,30 @@ if [ ! -f $cfg ]; then
   exit
 fi
 
+# Some helper stuff for running sed.
 runsed() {
   sed -e "$1" $cfg > $cfg.new
   if [ -s $cfg.new ]; then
     mv -f $cfg.new $cfg
   fi
 }
+optwsp=`printf "[ \t]*"`
+wsp=`printf "[ \t][ \t]*"`
 
 # First, fix up the horizontal and vertical sync rates.
 
-edid=`get-edid | parse-edid 2>/dev/null`
+edid=`get-edid 2>/dev/null | parse-edid 2>/dev/null`
 
 horiz=`echo "$edid" | awk '/HorizSync/ { print $2; }'`
 if [ -n "$horiz" ]; then
-  runsed "s/\(HorizSync[ 	][ 	]*\)[0-9][0-9,.-]*/\1$horiz/"
+  runsed "s/\(HorizSync$wsp\)[0-9][0-9,.-]*/\1$horiz/"
 fi
 
 vert=`echo "$edid" | awk '/VertRefresh/ { print $2; }'`
 if [ -n "$vert" ]; then
-  runsed "s/\(VertRefresh[ 	][ 	]*\)[0-9][0-9,.-]*/\1$vert/"
+  runsed "s/\(VertRefresh$wsp\)[0-9][0-9,.-]*/\1$vert/"
 fi
 
 # Second, get rid of any 1600x1200 mode which might be present.
-runsed '/^[ 	]*Modes/s/"1600x1200" *//'
+runsed "/^${optwsp}Modes$wsp\"1600x1200\"$optwsp\$/s/1600x1200/1280x1024/"
+runsed "/^[ 	]*Modes/s/\"1600x1200\"$optwsp//"
