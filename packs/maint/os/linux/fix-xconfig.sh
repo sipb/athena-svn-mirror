7c87b4e7f3c438aac93202ae585023eb3712f1c8
Greg Hudson <ghudson@mit.edu>
2000-10-14 17:31:34 +0000

    Special-case HP 90 monitors. Eliminate the whole value of HorizSync or VertRefresh, even if it  contains spaces.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15173 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/os/linux/fix-xconfig.sh
+++ packs/maint/os/linux/fix-xconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: fix-xconfig.sh,v 1.2 2000-10-04 04:59:08 ghudson Exp $
+# $Id: fix-xconfig.sh,v 1.3 2000-10-14 17:31:34 ghudson Exp $
 
 # fix-xconfig: Modify the X configuration to match timings retrieved
 # from read-edid and to have the desired maximum resolution.  Run from
@@ -23,17 +23,23 @@ optwsp=`printf "[ \t]*"`
 wsp=`printf "[ \t][ \t]*"`
 
 # First, fix up the horizontal and vertical sync rates.
-
 edid=`get-edid 2>/dev/null | parse-edid 2>/dev/null`
 
 horiz=`echo "$edid" | awk '/HorizSync/ { print $2; }'`
-if [ -n "$horiz" ]; then
-  runsed "s/\(HorizSync$wsp\)[0-9][0-9,.-]*/\1$horiz/"
+vert=`echo "$edid" | awk '/VertRefresh/ { print $2; }'`
+
+if echo "$edid" | grep -q 'Identifier "HP 90 Monitor"'; then
+  # Special-case; doesn't seem to actually live up to its promises.
+  horiz=31.5-64.3
+  vert=50-95
 fi
 
-vert=`echo "$edid" | awk '/VertRefresh/ { print $2; }'`
+if [ -n "$horiz" ]; then
+  runsed "s/\(HorizSync$wsp\).*/\1$horiz/"
+fi
+
 if [ -n "$vert" ]; then
-  runsed "s/\(VertRefresh$wsp\)[0-9][0-9,.-]*/\1$vert/"
+  runsed "s/\(VertRefresh$wsp\).*/\1$vert/"
 fi
 
 # Second, get rid of any 1600x1200 mode which might be present.
