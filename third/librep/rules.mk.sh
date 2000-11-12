e13ec5a5df6c462809d235a859b1e6c413ae82da
Greg Hudson <ghudson@mit.edu>
2000-11-12 06:12:05 +0000

    This commit was generated by cvs2svn to compensate for changes in r15282, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15283 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/librep/rules.mk.sh
@@ -0,0 +1,33 @@
+# rules.mk.sh -- Build dynamically-loadable objects for librep
+# $Id: rules.mk.sh,v 1.1.1.1 2000-11-12 06:10:21 ghudson Exp $
+
+repdir=$1
+repexecdir=$2
+repdocfile=$3
+
+cat <<EOF
+# rules.mk
+
+repdir=$repdir
+repexecdir=$repexecdir
+rpath_repexecdir=$repexecdir
+repdocfile=$repdocfile
+
+rep_LIBTOOL=\$(repexecdir)/libtool
+rep_INSTALL_ALIASES=\$(repexecdir)/install-aliases
+
+# use this like:
+# foo.la : foo.lo bar.lo
+#	\$(rep_DL_LD) link-opts...
+
+rep_DL_LD=\$(rep_LIBTOOL) --mode=link \$(CC) -avoid-version -module \
+	  -rpath \$(rpath_repexecdir)
+
+rep_DL_INSTALL=\$(rep_LIBTOOL) --mode=install \$(INSTALL)
+rep_DL_UNINSTALL=\$(rep_LIBTOOL) --mode=uninstall rm
+
+# Rule for libtool controlled C objects
+%.lo : %.c
+	\$(rep_LIBTOOL) --mode=compile \$(CC) -c \$(CPPFLAGS) \$(CFLAGS) \$<
+
+EOF
