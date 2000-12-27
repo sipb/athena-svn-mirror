684b62bfb2bfdd575f8e46eb7694d7b0f80c8971
Greg Hudson <ghudson@mit.edu>
2000-12-27 22:08:44 +0000

    Add ATHTOOLROOT support.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15378 728af825-273c-0410-89f9-f7d3b574a069

--- third/libglade/libgladeConf.sh
+++ third/libglade/libgladeConf.sh
@@ -2,11 +2,11 @@ prefix=/usr
 exec_prefix=${prefix}
 
 if test "${prefix}/include" != /usr/include ; then
-  LIBGLADE_INCLUDEDIR="-I${prefix}/include -I/usr/include/gnome-xml"
+  LIBGLADE_INCLUDEDIR="-I$ATHTOOLROOT${prefix}/include -I$ATHTOOLROOT/usr/include/gnome-xml"
 else
-  LIBGLADE_INCLUDEDIR="-I/usr/include/gnome-xml"
+  LIBGLADE_INCLUDEDIR="-I$ATHTOOLROOT/usr/include/gnome-xml"
 fi
-LIBGLADE_LIBDIR="-L${exec_prefix}/lib -rdynamic -L/usr/lib -L/usr/X11R6/lib"
+LIBGLADE_LIBDIR="-L$ATHTOOLROOT${exec_prefix}/lib -rdynamic -L/usr/lib -L/usr/X11R6/lib"
 LIBGLADE_LIBS="-lglade-gnome -lglade -L/usr/lib -lxml -lz -rdynamic -lgnomeui -lart_lgpl -lgdk_imlib -lSM -lICE -lgtk -lgdk -lgmodule -lXext -lX11 -lgnome -lgnomesupport -lesd -laudiofile -lm -ldb -lglib -ldl"
 MODULE_VERSION="libglade-0.15"
 
