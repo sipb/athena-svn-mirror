cb0c9341c5caf28d999ae5cb0c39b208b1105c35
Greg Hudson <ghudson@mit.edu>
2001-03-17 16:28:35 +0000

    Must declare g_log_domain_gmodule before including <glib.h> because logging stuff is written out as static functions using it (via a #define).  (Only on systems where the compiler doesn't support variable-argument preprocessing macros.)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15798 728af825-273c-0410-89f9-f7d3b574a069

--- third/glib/gmodule/gmodule.h
+++ third/glib/gmodule/gmodule.h
@@ -27,13 +27,13 @@
 #ifndef __GMODULE_H__
 #define __GMODULE_H__
 
+extern const char      *g_log_domain_gmodule;
 #include <glib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-extern const char      *g_log_domain_gmodule;
 
 
 /* exporting and importing functions, this is special cased
