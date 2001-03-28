798009bcbdc2983d49e783d8f5ed850fc21482de
Greg Hudson <ghudson@mit.edu>
2001-03-28 01:38:59 +0000

    Reindent.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@15889 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/build.sh
+++ packs/build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: build.sh,v 1.33 2001-03-26 20:19:12 ghudson Exp $
+# $Id: build.sh,v 1.34 2001-03-28 01:38:57 ghudson Exp $
 
 # This is the script for building the Athena source tree, or pieces of
 # it.  It is less flexible than the do.sh script in this directory.
@@ -17,33 +17,33 @@ usage="build [-s srcdir] [-b builddir] [-d destdir] [-t target] [-k] [-n] [-l]"
 usage="$usage [package [endpackage]]"
 
 while getopts s:b:d:t:knl opt; do
-	case "$opt" in
-	s)
-		source="$OPTARG"
-		;;
-	b)
-		build="$OPTARG"
-		;;
-	d)
-		srvd="$OPTARG"
-		;;
-	t)
-		target="$OPTARG"
-		;;
-	k)
-		ignore=true
-		;;
-	n)
-		nobuild=true
-		;;
-	l)
-		log=true
-		;;
-	\?)
-		echo "$usage"
-		exit 1
-		;;
-	esac
+  case "$opt" in
+  s)
+    source="$OPTARG"
+    ;;
+  b)
+    build="$OPTARG"
+    ;;
+  d)
+    srvd="$OPTARG"
+    ;;
+  t)
+    target="$OPTARG"
+    ;;
+  k)
+    ignore=true
+    ;;
+  n)
+    nobuild=true
+    ;;
+  l)
+    log=true
+    ;;
+  \?)
+    echo "$usage"
+    exit 1
+    ;;
+  esac
 done
 shift `expr $OPTIND - 1`
 start="$1"
@@ -52,43 +52,43 @@ end="${2-$1}"
 # Determine the operating system.
 case `uname -s` in
 SunOS)
-	os=solaris
-	awk=nawk
-	;;
+  os=solaris
+  awk=nawk
+  ;;
 IRIX)
-	os=irix
-	awk=awk
-	;;
+  os=irix
+  awk=awk
+  ;;
 Linux) 
-	os=linux
-	awk=awk
-	;;
+  os=linux
+  awk=awk
+  ;;
 *)
-	echo "Unrecognized system type, aborting." 1>&2
-	exit 1
-	;;
+  echo "Unrecognized system type, aborting." 1>&2
+  exit 1
+  ;;
 esac
 
 # Send all output from this point on to the build log file.
 if [ true = "$log" ]; then
-	mkdir -p "$build/logs" 2>/dev/null
-	now=`date '+%y.%m.%d.%H'`
-	logfile=$build/logs/washlog.$now
-	rm -f "$build/logs/current"
-	ln -s "washlog.$now" "$build/logs/current"
-	exec >> "$logfile" 2>&1
+  mkdir -p "$build/logs" 2>/dev/null
+  now=`date '+%y.%m.%d.%H'`
+  logfile=$build/logs/washlog.$now
+  rm -f "$build/logs/current"
+  ln -s "washlog.$now" "$build/logs/current"
+  exec >> "$logfile" 2>&1
 fi
 
 # Read in the list of packages, filtering for operating system.
 packages=`$awk -f $source/packs/build/getpackages.awk \
-	os="$os" start="$start" end="$end" target="$target" \
-	$source/packs/build/packages` || exit 1
+  os="$os" start="$start" end="$end" target="$target" \
+  $source/packs/build/packages` || exit 1
 
 case $nobuild in
 true)
-	echo $packages
-	exit
-	;;
+  echo $packages
+  exit
+  ;;
 esac
 
 echo ========
@@ -98,37 +98,36 @@ echo Starting at `date` on $os
 # because the Solaris 8 mkdir -p errors out if $build is a symlink to
 # another directory.
 if [ ! -d $build ]; then
-	mkdir -p $build || exit 1
+  mkdir -p $build || exit 1
 fi
 
 # Build the packages.
 for package in $packages; do
-	cd $build || exit 1
-	if [ third/krb5/src/appl != "$package" ]; then
-		rm -rf $package
-		mkdir -p $package
-		cd $package || exit 1
-		synctree -q -s $source/$package -d . -a \
-			$source/packs/build/rconf || exit 1
-	else
-		cd $package || exit 1
-	fi
-	echo "**********************"
-	for op in dist prepare clean all check install; do
-		echo "***** ${package}: $op"
-		sh $source/packs/build/do.sh -c -s "$source" -d "$srvd" "$op"
-		if [ $? -ne 0 ]; then
-			echo "We bombed in $package"
-			if [ false = "$ignore" ]; then
-				exit 1
-			fi
-		fi
+  cd $build || exit 1
+  if [ third/krb5/src/appl != "$package" ]; then
+    rm -rf $package
+    mkdir -p $package
+    cd $package || exit 1
+    synctree -q -s $source/$package -d . -a $source/packs/build/rconf || exit 1
+  else
+    cd $package || exit 1
+  fi
+  echo "**********************"
+  for op in dist prepare clean all check install; do
+    echo "***** ${package}: $op"
+    sh $source/packs/build/do.sh -c -s "$source" -d "$srvd" "$op"
+    if [ $? -ne 0 ]; then
+      echo "We bombed in $package"
+      if [ false = "$ignore" ]; then
+	exit 1
+      fi
+    fi
 
-		# Redo the output redirection command to flush the log file.
-		if [ true = "$log" ]; then
-			exec >> "$logfile" 2>&1
-		fi
-	done
+    # Redo the output redirection command to flush the log file.
+    if [ true = "$log" ]; then
+      exec >> "$logfile" 2>&1
+    fi
+  done
 done
 
 echo "Ending at `date`"
--- packs/build/do.sh
+++ packs/build/do.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: do.sh,v 1.62 2001-03-26 20:31:43 ghudson Exp $
+# $Id: do.sh,v 1.63 2001-03-28 01:38:59 ghudson Exp $
 
 source=/mit/source
 srvd=/afs/dev.mit.edu/system/$ATHENA_SYS/srvd-current
@@ -10,57 +10,57 @@ n=""
 maybe=""
 
 usage() {
-	echo "Usage: do [-cnp] [-s srcdir] [-d destdir] [-t toolroot]" 1>&2
-	echo "	[dist|prepare|clean|all|check|install]" 1>&2
-	exit 1
+  echo "Usage: do [-cnp] [-s srcdir] [-d destdir] [-t toolroot]" 1>&2
+  echo "	[dist|prepare|clean|all|check|install]" 1>&2
+  exit 1
 }
 
 while getopts pcd:ns:t: opt; do
-	case "$opt" in
-	c)
-		contained=true
-		;;
-	p)
-		mungepath=false
-		;;
-	d)
-		srvd=$OPTARG
-		;;
-	n)
-		n=-n
-		maybe=echo
-		;;
-	s)
-		source=$OPTARG
-		;;
-	t)
-		athtoolroot=$OPTARG
-		;;
-	\?)
-		usage
-		;;
-	esac
+  case "$opt" in
+  c)
+    contained=true
+    ;;
+  p)
+    mungepath=false
+    ;;
+  d)
+    srvd=$OPTARG
+    ;;
+  n)
+    n=-n
+    maybe=echo
+    ;;
+  s)
+    source=$OPTARG
+    ;;
+  t)
+    athtoolroot=$OPTARG
+    ;;
+  \?)
+    usage
+    ;;
+  esac
 done
 shift `expr "$OPTIND" - 1`
 operation=${1-all}
 
 case "$operation" in
 dist|prepare|clean|all|check|install)
-	;;
+  ;;
 *)
-	echo Unknown operation \"$operation\" 1>&2
-	usage
-	;;
+  echo Unknown operation \"$operation\" 1>&2
+  usage
+  ;;
 esac
 
 case $contained,$athtoolroot in
 true,)
-	athtoolroot=$srvd
-	;;
+  athtoolroot=$srvd
+  ;;
 true,?*)
-	echo "The -t option and -c flag are mutually exclusive." 1>&2
-	exit 1
-	;;
+  echo "The -t option and -c flag are mutually exclusive." 1>&2
+  exit 1
+  ;;
 esac
 
 # Set up the build environment.
@@ -71,63 +71,62 @@ M4=$athtoolroot/usr/athena/bin/m4
 # Determine proper ATHENA_SYS and ATHENA_SYS_COMPAT value.
 case `uname -srm` in
 "SunOS 5.8 sun4"*)
-	ATHENA_SYS=sun4x_58
-	ATHENA_SYS_COMPAT=sun4x_57:sun4x_56:sun4x_55:sun4m_54:sun4m_53
-	ATHENA_SYS_COMPAT=${ATHENA_SYS_COMPAT}:sun4m_412
-	;;
+  ATHENA_SYS=sun4x_58
+  ATHENA_SYS_COMPAT=sun4x_57:sun4x_56:sun4x_55:sun4m_54:sun4m_53:sun4m_412
+  ;;
 "SunOS 5.7 sun4"*)
-	ATHENA_SYS=sun4x_57
-	ATHENA_SYS_COMPAT=sun4x_56:sun4x_55:sun4m_54:sun4m_53:sun4m_412
-	;;
+  ATHENA_SYS=sun4x_57
+  ATHENA_SYS_COMPAT=sun4x_56:sun4x_55:sun4m_54:sun4m_53:sun4m_412
+  ;;
 "SunOS 5.6 sun4"*)
-	ATHENA_SYS=sun4x_56
-	ATHENA_SYS_COMPAT=sun4x_55:sun4m_54:sun4m_53:sun4m_412
-	;;
+  ATHENA_SYS=sun4x_56
+  ATHENA_SYS_COMPAT=sun4x_55:sun4m_54:sun4m_53:sun4m_412
+  ;;
 "SunOS 5.5 sun4"*)
-	ATHENA_SYS=sun4x_55
-	ATHENA_SYS_COMPAT=sun4m_54:sun4m_53:sun4m_412
-	;;
+  ATHENA_SYS=sun4x_55
+  ATHENA_SYS_COMPAT=sun4m_54:sun4m_53:sun4m_412
+  ;;
 "SunOS 5.4 sun4"*)
-	ATHENA_SYS=sun4m_54
-	ATHENA_SYS_COMPAT=sun4m_53:sun4m_412
-	;;
+  ATHENA_SYS=sun4m_54
+  ATHENA_SYS_COMPAT=sun4m_53:sun4m_412
+  ;;
 "IRIX 5.3 "*)
-	ATHENA_SYS=sgi_53
-	ATHENA_SYS_COMPAT=sgi_52
-	;;
+  ATHENA_SYS=sgi_53
+  ATHENA_SYS_COMPAT=sgi_52
+  ;;
 "IRIX 6.2 "*)
-	ATHENA_SYS=sgi_62
-	ATHENA_SYS_COMPAT=sgi_53:sgi_52
-	;;
+  ATHENA_SYS=sgi_62
+  ATHENA_SYS_COMPAT=sgi_53:sgi_52
+  ;;
 "IRIX 6.3 "*)
-	ATHENA_SYS=sgi_63
-	ATHENA_SYS_COMPAT=sgi_62:sgi_53:sgi_52
-	;;
+  ATHENA_SYS=sgi_63
+  ATHENA_SYS_COMPAT=sgi_62:sgi_53:sgi_52
+  ;;
 "IRIX 6.5 "*)
-	ATHENA_SYS=sgi_65
-	ATHENA_SYS_COMPAT=sgi_63:sgi_62:sgi_53:sgi_52
-	;;
+  ATHENA_SYS=sgi_65
+  ATHENA_SYS_COMPAT=sgi_63:sgi_62:sgi_53:sgi_52
+  ;;
 Linux\ 2.2.*\ i?86)
-	ATHENA_SYS=i386_linux22
-	ATHENA_SYS_COMPAT=i386_linux3:i386_linux2:i386_linux1
-	;;
+  ATHENA_SYS=i386_linux22
+  ATHENA_SYS_COMPAT=i386_linux3:i386_linux2:i386_linux1
+  ;;
 *)
-	echo "Unrecognized system type, aborting." 1>&2
-	exit 1
-	;;
+  echo "Unrecognized system type, aborting." 1>&2
+  exit 1
+  ;;
 esac
 
 # Determine platform name.
 case `uname -sm` in
 "SunOS sun4"*)
-	HOSTTYPE=sun4
-	;;
+  HOSTTYPE=sun4
+  ;;
 "IRIX "*)
-	HOSTTYPE=sgi
-	;;
+  HOSTTYPE=sgi
+  ;;
 "Linux "i?86)
-	HOSTTYPE=linux
-	;;
+  HOSTTYPE=linux
+  ;;
 esac
 
 savepath=$PATH
@@ -136,49 +135,48 @@ savepath=$PATH
 # with plain Makefiles and some third-party packages.
 case `uname -s` in
 SunOS)
-	OS=solaris
-	LD_LIBRARY_PATH=/usr/openwin/lib export LD_LIBRARY_PATH
-	PATH=/usr/ccs/bin:/usr/bin:/usr/ucb:/usr/openwin/bin:/usr/gcc/bin
-	CC=gcc
-	CXX=g++
-	WARN_CFLAGS="-Wall -Wstrict-prototypes -Wmissing-prototypes"
-	ERROR_CFLAGS=-Werror
-	;;
+  OS=solaris
+  LD_LIBRARY_PATH=/usr/openwin/lib export LD_LIBRARY_PATH
+  PATH=/usr/ccs/bin:/usr/bin:/usr/ucb:/usr/openwin/bin:/usr/gcc/bin
+  CC=gcc
+  CXX=g++
+  WARN_CFLAGS="-Wall -Wstrict-prototypes -Wmissing-prototypes"
+  ERROR_CFLAGS=-Werror
+  ;;
 IRIX)
-	OS=irix
-	PATH=/usr/bsd:/usr/bin:/usr/bin/X11
-	CC=cc
-	CXX=CC
-	WARN_CFLAGS=-fullwarn
-	ERROR_CFLAGS=-w2
-	;;
+  OS=irix
+  PATH=/usr/bsd:/usr/bin:/usr/bin/X11
+  CC=cc
+  CXX=CC
+  WARN_CFLAGS=-fullwarn
+  ERROR_CFLAGS=-w2
+  ;;
 Linux)
-	OS=linux
-	PATH=/usr/bin:/bin:/usr/X11R6/bin
-	CC=cc
-	CXX=g++
-	WARN_CFLAGS="-Wall -Wstrict-prototypes -Wmissing-prototypes"
-	ERROR_CFLAGS=-Werror
-	;;
+  OS=linux
+  PATH=/usr/bin:/bin:/usr/X11R6/bin
+  CC=cc
+  CXX=g++
+  WARN_CFLAGS="-Wall -Wstrict-prototypes -Wmissing-prototypes"
+  ERROR_CFLAGS=-Werror
+  ;;
 esac
 PATH=$athtoolroot/usr/athena/bin:$PATH
 
 if [ false = "$mungepath" ]; then
-	PATH=$savepath
+  PATH=$savepath
 fi
 
 if [ -n "$athtoolroot" ]; then
-	ext=${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}
-	LD_LIBRARY_PATH=$athtoolroot/usr/athena/lib$ext
-	REPLISPDIR=$athtoolroot/usr/athena/share/rep/lisp
-	REPEXECDIR=$athtoolroot/usr/athena/libexec/rep
-	export LD_LIBRARY_PATH REPLISPDIR REPEXECDIR
-	if [ irix = "$OS" ]; then
-		# libtool likes to use this variable, which overrides
-		# LD_LIBRARY_PATH.  Work around that.
-		LD_LIBRARYN32_PATH=$LD_LIBRARY_PATH
-		export LD_LIBRARYN32_PATH
-	fi
+  ext=${LD_LIBRARY_PATH+:$LD_LIBRARY_PATH}
+  LD_LIBRARY_PATH=$athtoolroot/usr/athena/lib$ext
+  REPLISPDIR=$athtoolroot/usr/athena/share/rep/lisp
+  REPEXECDIR=$athtoolroot/usr/athena/libexec/rep
+  export LD_LIBRARY_PATH REPLISPDIR REPEXECDIR
+  if [ irix = "$OS" ]; then
+    # libtool likes to use this variable, which overrides LD_LIBRARY_PATH.
+    LD_LIBRARYN32_PATH=$LD_LIBRARY_PATH
+    export LD_LIBRARYN32_PATH
+  fi
 fi
 
 # Determine the Athena version
@@ -191,121 +189,116 @@ export ATHENA_MAJOR_VERSION ATHENA_MINOR_VERSION ATHENA_PATCH_VERSION
 # Determine if gmake is available. (It should be, unless this is a
 # full build and we haven't built it yet.)
 if [ -x $athtoolroot/usr/athena/bin/gmake ]; then
-	MAKE=gmake
+  MAKE=gmake
 else
-	MAKE=make
+  MAKE=make
 fi
 
 export WARN_CFLAGS ERROR_CFLAGS CC CXX MAKE
 
 if [ -r Makefile.athena ]; then
-	export SRVD SOURCE COMPILER ATHTOOLROOT
-	SRVD=$srvd
-	SOURCE=$source
-	COMPILER=$CC
-	ATHTOOLROOT=$athtoolroot
-	if [ dist = "$operation" ]; then
-		export CONFIG_SITE XCONFIGDIR
-		CONFIG_SITE=$source/packs/build/config.site
-		XCONFIGDIR=$source/packs/build/xconfig
-	fi
-	$MAKE $n -f Makefile.athena "$operation"
+  export SRVD SOURCE COMPILER ATHTOOLROOT
+  SRVD=$srvd
+  SOURCE=$source
+  COMPILER=$CC
+  ATHTOOLROOT=$athtoolroot
+  if [ dist = "$operation" ]; then
+    export CONFIG_SITE XCONFIGDIR
+    CONFIG_SITE=$source/packs/build/config.site
+    XCONFIGDIR=$source/packs/build/xconfig
+  fi
+  $MAKE $n -f Makefile.athena "$operation"
 elif [ -f configure.in ]; then
-	export ATHTOOLROOT
-	ATHTOOLROOT=$athtoolroot
-	if [ -x configure.athena ]; then
-		configure=configure.athena
-	else
-		configure=configure
-	fi
-	case $operation in
-	dist)
-		# Copy in support files and run autoconf if this is a
-		# directory using the Athena build system.
-		if [ -f config.do -o ! -f configure ]; then
-			$maybe touch config.do
-			$maybe rm -f mkinstalldirs install-sh config.guess
-			$maybe rm -f config.sub aclocal.m4
-			$maybe cp \
-				"$source/packs/build/autoconf/mkinstalldirs" .
-			$maybe cp "$source/packs/build/autoconf/install-sh" .
-			$maybe cp "$source/packs/build/autoconf/config.guess" .
-			$maybe cp "$source/packs/build/autoconf/config.sub" .
-			$maybe cp "$source/packs/build/aclocal.m4" .
-			$maybe autoconf \
-				-m ${ATHTOOLROOT}/usr/athena/share/autoconf \
-				|| exit 1
-		fi
-		$maybe cp "$source/packs/build/config.site" config.site.athena
-		;;
-	prepare)
-		$maybe rm -f config.cache
-		CONFIG_SITE=`pwd`/config.site.athena $maybe "./$configure"
-		;;
-	clean)
-		$MAKE $n clean
-		;;
-	all)
-		$MAKE $n all
-		;;
-	check)
-		$MAKE -n check >/dev/null 2>&1 && $MAKE $n check || true
-		;;
-	install)
-		$MAKE $n install "DESTDIR=$srvd"
-		;;
-	esac
+  export ATHTOOLROOT
+  ATHTOOLROOT=$athtoolroot
+  if [ -x configure.athena ]; then
+    configure=configure.athena
+  else
+    configure=configure
+  fi
+  case $operation in
+  dist)
+    # Copy in support files and run autoconf if this is a directory using the
+    # Athena build system.
+    if [ -f config.do -o ! -f configure ]; then
+      $maybe touch config.do
+      $maybe rm -f mkinstalldirs install-sh config.guess
+      $maybe rm -f config.sub aclocal.m4
+      $maybe cp "$source/packs/build/autoconf/mkinstalldirs" .
+      $maybe cp "$source/packs/build/autoconf/install-sh" .
+      $maybe cp "$source/packs/build/autoconf/config.guess" .
+      $maybe cp "$source/packs/build/autoconf/config.sub" .
+      $maybe cp "$source/packs/build/aclocal.m4" .
+      $maybe autoconf -m ${ATHTOOLROOT}/usr/athena/share/autoconf || exit 1
+    fi
+    $maybe cp "$source/packs/build/config.site" config.site.athena
+    ;;
+  prepare)
+    $maybe rm -f config.cache
+    CONFIG_SITE=`pwd`/config.site.athena $maybe "./$configure"
+  ;;
+  clean)
+    $MAKE $n clean
+    ;;
+  all)
+    $MAKE $n all
+    ;;
+  check)
+    $MAKE -n check >/dev/null 2>&1 && $MAKE $n check || true
+    ;;
+  install)
+    $MAKE $n install "DESTDIR=$srvd"
+    ;;
+  esac
 elif [ -r Imakefile ]; then
-	case $operation in
-	dist)
-		$maybe mkdir -p config
-		$maybe cp $source/packs/build/xconfig/README config
-		$maybe cp $source/packs/build/xconfig/mkdirhier.sh config
-		$maybe cp $source/packs/build/xconfig/rtcchack.bac config
-		$maybe cp $source/packs/build/xconfig/site.def config
-		$maybe cp $source/packs/build/xconfig/*.cf config
-		$maybe cp $source/packs/build/xconfig/*.rules config
-		$maybe cp $source/packs/build/xconfig/*.tmpl config
-		;;
-	prepare)
-		$maybe imake "-Iconfig" -DUseInstalled \
-			"-DTOPDIR=`pwd`" \
-			"-DTOOLROOT=$athtoolroot" \
-			"-DXCONFIGDIR=`pwd`/config"
-		$maybe $MAKE Makefiles
-		;;
-	clean)
-		$MAKE $n clean
-		;;
-	all)
-		$MAKE $n includes depend  all \
-		    TOP=`pwd` MAKE="$MAKE TOP=`pwd`" ATHTOOLROOT=$ATHTOOLROOT
-		;;
-	check)
-		;;
-	install)
-		$MAKE $n install install.man "DESTDIR=$srvd" TOP=`pwd` \
-			MAKE="$MAKE TOP=`pwd`" ATHTOOLROOT=$ATHTOOLROOT
-		;;
-	esac
+  case $operation in
+  dist)
+    $maybe mkdir -p config
+    $maybe cp $source/packs/build/xconfig/README config
+    $maybe cp $source/packs/build/xconfig/mkdirhier.sh config
+    $maybe cp $source/packs/build/xconfig/rtcchack.bac config
+    $maybe cp $source/packs/build/xconfig/site.def config
+    $maybe cp $source/packs/build/xconfig/*.cf config
+    $maybe cp $source/packs/build/xconfig/*.rules config
+    $maybe cp $source/packs/build/xconfig/*.tmpl config
+    ;;
+  prepare)
+    $maybe imake "-Iconfig" -DUseInstalled "-DTOPDIR=`pwd`" \
+      "-DTOOLROOT=$athtoolroot" "-DXCONFIGDIR=`pwd`/config"
+    $maybe $MAKE Makefiles
+    ;;
+  clean)
+    $MAKE $n clean
+    ;;
+  all)
+    $MAKE $n includes depend all TOP=`pwd` MAKE="$MAKE TOP=`pwd`" \
+      ATHTOOLROOT=$ATHTOOLROOT
+    ;;
+  check)
+    ;;
+  install)
+    $MAKE $n install install.man "DESTDIR=$srvd" TOP=`pwd` \
+      MAKE="$MAKE TOP=`pwd`" ATHTOOLROOT=$ATHTOOLROOT
+    ;;
+  esac
 elif [ -r Makefile ]; then
-	case $operation in
-	dist|prepare)
-		;;
-	clean)
-		$MAKE $n clean "ATHTOOLROOT=$athtoolroot"
-		;;
-	all)
-		$MAKE $n all CC="$CC" "ATHTOOLROOT=$athtoolroot"
-		;;
-	check)
-		$MAKE $n check
-		;;
-	install)
-		$MAKE $n install "DESTDIR=$srvd" "ATHTOOLROOT=$athtoolroot"
-		;;
-	esac
+  case $operation in
+  dist|prepare)
+    ;;
+  clean)
+    $MAKE $n clean "ATHTOOLROOT=$athtoolroot"
+    ;;
+  all)
+    $MAKE $n all CC="$CC" "ATHTOOLROOT=$athtoolroot"
+    ;;
+  check)
+    $MAKE $n check
+    ;;
+  install)
+    $MAKE $n install "DESTDIR=$srvd" "ATHTOOLROOT=$athtoolroot"
+    ;;
+  esac
 else
-	echo Nothing to do in `pwd` 1>&2
-	exit 1
+  echo Nothing to do in `pwd` 1>&2
+  exit 1
 fi
