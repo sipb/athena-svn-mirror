7093af60c5931e143b5146fd3ca1758dbbbd1fba
Greg Hudson <ghudson@mit.edu>
2001-04-10 23:28:42 +0000

    Reintroduce checksum enforcement changes.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@16049 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/server/dispatch.c
+++ athena/lib/zephyr/server/dispatch.c
@@ -18,7 +18,7 @@
 #ifndef lint
 #ifndef SABER
 static const char rcsid_dispatch_c[] =
-"$Id: dispatch.c,v 1.62 2001-03-05 22:22:03 ghudson Exp $";
+"$Id: dispatch.c,v 1.63 2001-04-10 23:28:19 ghudson Exp $";
 #endif
 #endif
 
@@ -257,20 +257,7 @@ dispatch(notice, auth, who, from_server)
     char dbg_buf[BUFSIZ];
 #endif
 
-    /* Set "authflag" to 1 or 0 for handler functions.  Treat
-     * ZAUTH_CKSUM_FAILED as authentic except for sendit(), which is
-     * handled below. */
-    switch (auth) {
-      case ZAUTH_YES:
-      case ZAUTH_CKSUM_FAILED:
-	authflag = 1;
-	break;
-      case ZAUTH_FAILED:
-      case ZAUTH_NO:
-      default:
-	authflag = 0;
-	break;
-    }
+    authflag = (auth == ZAUTH_YES);
 
     if ((int) notice->z_kind < (int) UNSAFE ||
 	(int) notice->z_kind > (int) CLIENTACK) {
@@ -324,8 +311,6 @@ dispatch(notice, auth, who, from_server)
 	admin_notices.val++;
 	status = server_adispatch(notice, authflag, who, me_server);
     } else {
-	if (auth == ZAUTH_CKSUM_FAILED)
-	    authflag = 0;
 	if (!realm_bound_for_realm(ZGetRealm(), notice->z_recipient)) {
 	    cp = strchr(notice->z_recipient, '@');
 	    if (!cp ||
