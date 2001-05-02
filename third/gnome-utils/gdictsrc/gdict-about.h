8545562128fc206b716ff22e6be7dd5e7885f3a9
Greg Hudson <ghudson@mit.edu>
2001-05-02 20:48:23 +0000

    This commit was generated by cvs2svn to compensate for changes in r16151, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@16152 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/gnome-utils/gdictsrc/gdict-about.h
@@ -0,0 +1,20 @@
+#ifndef __GDICT_ABOUT_H_
+#define __GDICT_ABOUT_H_
+
+/* $Id: gdict-about.h,v 1.1.1.1 2001-05-02 20:42:09 ghudson Exp $ */
+
+/*
+ *  Papadimitriou Spiros <spapadim@cs.cmu.edu>
+ *  Mike Hughes <mfh@psilord.com>
+ *
+ *  This code released under the GNU GPL.
+ *  Read the file COPYING for more information.
+ *
+ *  GDict About box
+ *
+ */
+
+extern void gdict_about (void);
+
+#endif /* __GDICT_ABOUT_H_ */
+
