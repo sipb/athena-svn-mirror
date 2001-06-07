5afdd8b2742812063bf59f39c270010a52c5364b
Garry Zacheiss <zacheiss@mit.edu>
2001-06-07 17:14:02 +0000

    Need quotes around find invocation in case it has no output.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@16292 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/local-menus.sh
+++ packs/maint/local-menus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: local-menus.sh,v 1.1 2001-06-05 19:08:47 ghudson Exp $
+# $Id: local-menus.sh,v 1.2 2001-06-07 17:14:02 zacheiss Exp $
 
 # Tar file containing menus (a symlink into the AFS system config area).
 menutar=/usr/athena/share/gnome/athena/menus.tar
@@ -31,7 +31,7 @@ if [ ! -s $menutar ]; then
 fi
 
 # Nothing to do if $localtar is up to date.
-if [ -s $localtar ] && [ `find $menutar -newer $localtar` != $menutar ]; then
+if [ -s $localtar ] && [ "`find $menutar -newer $localtar`" != $menutar ]; then
   exit 0
 fi
 
