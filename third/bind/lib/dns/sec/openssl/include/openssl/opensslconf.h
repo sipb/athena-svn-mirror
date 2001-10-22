b33d58901d2754c33dee025d435376d104e6e9bc
Greg Hudson <ghudson@mit.edu>
2001-10-22 13:09:53 +0000

    This commit was generated by cvs2svn to compensate for changes in r16677, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@16678 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/bind/lib/dns/sec/openssl/include/openssl/opensslconf.h
@@ -0,0 +1,7 @@
+#ifndef OPENSSL_OPENSSLCONF_H
+#define OPENSSL_OPENSSLCONF_H 1
+
+/* This would be filled in by the openssl configure script if it was run. */
+
+#endif /* OPENSSL_OPENSSLCONF_H */
+
