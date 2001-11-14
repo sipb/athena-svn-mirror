78d352b084c4a42248298c80d270637d81183d1a
Greg Hudson <ghudson@mit.edu>
2001-11-14 10:07:30 +0000

    Pull up 1.9 to athena-9_0 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-9_0@16796 728af825-273c-0410-89f9-f7d3b574a069

--- packs/update/os/solaris/setup-swap-boot.sh
+++ packs/update/os/solaris/setup-swap-boot.sh
@@ -52,6 +52,8 @@ cp -p /etc/netmasks "$swapmount/etc" || die
 cp -p /etc/defaultrouter "$swapmount/etc" || die
 cp -p /etc/networks "$swapmount/etc" || die
 cp -p "/etc/hostname.$NETDEV" "$swapmount/etc" || die
+cp -p /etc/driver_aliases "$swapmount/etc" || die
+cp -p /etc/driver_classes "$swapmount/etc" || die
 
 # Great big kludge.  Some IDE based Ultras have a "dad" device of 134
 # while others have 136.  We need the name_to_major file in the miniroot
@@ -60,7 +62,15 @@ cp -p "/etc/hostname.$NETDEV" "$swapmount/etc" || die
 #
 # We should find a better way to do this.
 dad=`awk '$1 == "dad" {print $2}' /etc/name_to_major`
-cp -p "$swapmount/etc/name_to_major.$dad" "$swapmount/etc/name_to_major" || die
+cputype=`machtype -c`
+case $cputype in
+ UltraAX-i2|Sun-Blade*)
+    cp -p "$swapmount/etc/name_to_major.$dad.$cputype" "$swapmount/etc/name_to_major" || die
+    ;;
+ *)
+    cp -p "$swapmount/etc/name_to_major.$dad" "$swapmount/etc/name_to_major" || die
+    ;;
+esac
 
 echo "Making devices..."
 (cd $swapmount && drvconfig -r devices "-p$swapmount/etc/path_to_inst") || die
@@ -113,3 +123,7 @@ cd /
 umount "$swapmount"
 fsck -y -F ufs "$rswapdev"
 rmdir $swapmount
+echo "Update partially completed, system will reboot in 15 seconds."
+sync
+sleep 15
+exec reboot -- "$newboot"
