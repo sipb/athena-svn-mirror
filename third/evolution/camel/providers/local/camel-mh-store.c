f5ac1e08a06623aaca338618d1e567ec16162352
Greg Hudson <ghudson@mit.edu>
2001-11-28 20:43:45 +0000

    Finish up MH support somewhat so that users can look at their old MH mail.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@16837 728af825-273c-0410-89f9-f7d3b574a069

--- third/evolution/camel/providers/local/camel-mh-store.c
+++ third/evolution/camel/providers/local/camel-mh-store.c
@@ -27,6 +27,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include "camel-mh-store.h"
 #include "camel-mh-folder.h"
@@ -41,7 +42,9 @@ static CamelLocalStoreClass *parent_class = NULL;
 #define CMHF_CLASS(so) CAMEL_MH_FOLDER_CLASS (CAMEL_OBJECT_GET_CLASS(so))
 
 static CamelFolder *get_folder(CamelStore * store, const char *folder_name, guint32 flags, CamelException * ex);
+static CamelFolder *get_inbox (CamelStore *store, CamelException *ex);
 static void delete_folder(CamelStore * store, const char *folder_name, CamelException * ex);
+static CamelFolderInfo * get_folder_info (CamelStore *store, const char *top, guint32 flags, CamelException *ex);
 
 static void camel_mh_store_class_init(CamelObjectClass * camel_mh_store_class)
 {
@@ -52,7 +55,9 @@ static void camel_mh_store_class_init(CamelObjectClass * camel_mh_store_class)
 
 	/* virtual method overload, use defaults for most */
 	camel_store_class->get_folder = get_folder;
+	camel_store_class->get_inbox = get_inbox;
 	camel_store_class->delete_folder = delete_folder;
+	camel_store_class->get_folder_info = get_folder_info;
 }
 
 CamelType camel_mh_store_get_type(void)
@@ -115,6 +120,12 @@ static CamelFolder *get_folder(CamelStore * store, const char *folder_name, guin
 	return camel_mh_folder_new(store, folder_name, flags, ex);
 }
 
+static CamelFolder *
+get_inbox (CamelStore *store, CamelException *ex)
+{
+	return get_folder (store, "inbox", 0, ex);
+}
+
 static void delete_folder(CamelStore * store, const char *folder_name, CamelException * ex)
 {
 	char *name;
@@ -132,4 +143,100 @@ static void delete_folder(CamelStore * store, const char *folder_name, CamelExce
 
 	/* and remove metadata */
 	((CamelStoreClass *)parent_class)->delete_folder(store, folder_name, ex);
+}
+
+/* Add a folder if we haven't already seen it and if it exists as a
+   directory. */
+static void check_and_add(GPtrArray *folders, GHashTable *seen,
+			  const char *root, const char *path)
+{
+	char *fullpath, *pathcopy;
+	struct stat st;
+	CamelFolderInfo *fi;
+	const char *base;
+
+	if (g_hash_table_lookup(seen, path) != NULL)
+		return;
+
+	fullpath = g_strdup_printf("%s%s", root, path);
+	if (stat(fullpath, &st) == 0 && S_ISDIR(st.st_mode)) {
+		/* Make sure we don't add this folder twice. */
+		pathcopy = g_strdup(path);
+		g_hash_table_insert(seen, pathcopy, pathcopy);
+
+		/* Get the last path component for the folder name. */
+		base = strrchr(path, '/');
+		base = (base == NULL) ? path : base + 1;
+
+		/* Build the folder info structure. */
+		fi = g_malloc(sizeof(*fi));
+		fi->parent = NULL;
+		fi->sibling = NULL;
+		fi->child = NULL;
+		fi->url = g_strdup_printf("mh:%s#%s", root, path);
+		fi->full_name = g_strdup(path);
+		fi->name = g_strdup(base);
+		fi->unread_message_count = 0;
+		camel_folder_info_build_path(fi, '/');
+
+		g_ptr_array_add(folders, fi);
+	}
+	g_free(fullpath);
+}
+
+static void free_str(gpointer key, gpointer value, gpointer user_data)
+{
+	g_free(key);
+}
+
+static CamelFolderInfo *
+get_folder_info (CamelStore *store, const char *top, guint32 flags, CamelException *ex)
+{
+	GPtrArray *folders;
+	GHashTable *seen;
+	CamelFolderInfo *tree;
+	char *root, *name, buf[1024];
+	FILE *fp;
+	DIR *dp;
+	struct dirent *d;
+	int len;
+
+	root = CAMEL_LOCAL_STORE(store)->toplevel_dir;
+	folders = g_ptr_array_new();
+	seen = g_hash_table_new(g_str_hash, g_str_equal);
+
+	/* Read the MH folders file if present. */
+	name = g_strdup_printf("%s.folders", root);
+	fp = fopen(name, "r");
+	if (fp != NULL) {
+		while (fgets(buf, sizeof(buf), fp) != NULL) {
+			len = strlen(buf);
+			/* Stop if we see a ridiculously long line. */
+			if (buf[len - 1] != '\n')
+				break;
+			buf[len - 1] = 0;
+			check_and_add(folders, seen, root, buf);
+		}
+		fclose(fp);
+	}
+	free(name);
+
+	/* Also scan the top level directory. */
+	dp = opendir(root);
+	if (dp) {
+		while ((d = readdir(dp)) != NULL) {
+			if (strcmp(d->d_name, ".") == 0
+			    || strcmp(d->d_name, "..") == 0)
+				continue;
+			check_and_add(folders, seen, root, d->d_name);
+		}
+	}
+	closedir(dp);
+
+	g_hash_table_foreach(seen, free_str, NULL);
+	g_hash_table_destroy(seen);
+
+	tree = camel_folder_info_build(folders, NULL, '/', TRUE);
+	g_ptr_array_free(folders, TRUE);
+	return tree;
 }
