78d0cd13b6442595091e094a13c93963195adb9d
Greg Hudson <ghudson@mit.edu>
2002-02-28 19:48:12 +0000

    Declare ospeed in a more portable fashion.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@17258 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/zwgc/tty_filter.c
+++ athena/lib/zephyr/zwgc/tty_filter.c
@@ -4,7 +4,7 @@
  *
  *      Created by:     Marc Horowitz <marc@athena.mit.edu>
  *
- *      $Id: tty_filter.c,v 1.19 2001-10-23 17:57:57 ghudson Exp $
+ *      $Id: tty_filter.c,v 1.20 2002-02-28 19:48:12 ghudson Exp $
  *
  *      Copyright (c) 1989 by the Massachusetts Institute of Technology.
  *      For copying and distribution information, see the file
@@ -14,7 +14,7 @@
 #include <sysdep.h>
 
 #if (!defined(lint) && !defined(SABER))
-static const char rcsid_tty_filter_c[] = "$Id: tty_filter.c,v 1.19 2001-10-23 17:57:57 ghudson Exp $";
+static const char rcsid_tty_filter_c[] = "$Id: tty_filter.c,v 1.20 2002-02-28 19:48:12 ghudson Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
@@ -36,7 +36,7 @@ static const char rcsid_tty_filter_c[] = "$Id: tty_filter.c,v 1.19 2001-10-23 17
 
 extern int tgetent();
 extern char *tgetstr(),*getenv();
-short ospeed;
+extern speed_t ospeed;
 char PC;
 
 /* Dictionary naming convention:
