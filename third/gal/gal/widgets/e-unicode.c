e5a44b32dd13b203b2d8ac5086ba67654d1463ad
Greg Hudson <ghudson@mit.edu>
2002-05-13 20:38:46 +0000

    Merge with gal 0.19.2.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@17572 728af825-273c-0410-89f9-f7d3b574a069

--- third/gal/gal/widgets/e-unicode.c
+++ third/gal/gal/widgets/e-unicode.c
@@ -42,7 +42,7 @@
 #include <gdk/gdkkeysyms.h>
 #include <gtk/gtkmenuitem.h>
 #include "e-font.h"
-#include <gnome-xml/xmlmemory.h>
+#include <xmlmemory.h>
 #include <stdlib.h>
 #include "gal/util/e-iconv.h"
 
