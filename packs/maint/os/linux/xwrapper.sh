b6c77e14c0d3a59d687b63d86937decff24ea7ba
Greg Hudson <ghudson@mit.edu>
2002-05-14 22:07:53 +0000

    Continue supporting XFree86 3 even if Red Hat 7.3 is iffy about it.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@17584 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/os/linux/xwrapper.sh
+++ packs/maint/os/linux/xwrapper.sh
@@ -2,13 +2,20 @@
 
 # Give people a way to disable the wrapper if it's annoying.
 if [ -f /etc/athena/x-no-wrapper ]; then
-	exec /usr/X11R6/bin/X "$@"
+  exec /usr/X11R6/bin/X "$@"
 fi
 
 options=
 if [ -f /var/athena/x-8-bit ]; then
-	options="-bpp 8"
+  options="-bpp 8"
 fi
 rm -f /var/athena/x-8-bit
 
-exec /usr/X11R6/bin/X $options "$@"
+# Red Hat 7.3 punts their X wrapper (/usr/X11R6/bin/X is just a
+# symlink to XFree86, the XFree86 4 server), so we have to use the
+# symlink ourselves if it's there to maintain XFree86 3 compatibility.
+if [ -x /etc/X11/X ]; then
+  exec /etc/X11/X $options "$@"
+else
+  exec /usr/X11R6/bin/X $options "$@"
+fi
