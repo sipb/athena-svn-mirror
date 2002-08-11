32b5657fbfac3f48932ed3e3d0446a4986b9e6b9
Garry Zacheiss <zacheiss@mit.edu>
2002-08-11 05:39:12 +0000

    Files with negatives ctimes/mtimes/atimes get those numbers turned into large 64 bit positive numbers due Solaris kernel stupidity, causing some 32 bit syscalls to blow out with EOVERFLOW.  Cast to int to work around the problem.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@17840 728af825-273c-0410-89f9-f7d3b574a069

--- third/openafs/src/afs/VNOPS/afs_vnop_attrs.c
+++ third/openafs/src/afs/VNOPS/afs_vnop_attrs.c
@@ -21,7 +21,7 @@
 #include <afsconfig.h>
 #include "../afs/param.h"
 
-RCSID("$Header: /afs/dev.mit.edu/source/repository/third/openafs/src/afs/VNOPS/afs_vnop_attrs.c,v 1.2 2002-07-20 19:33:21 zacheiss Exp $");
+RCSID("$Header: /afs/dev.mit.edu/source/repository/third/openafs/src/afs/VNOPS/afs_vnop_attrs.c,v 1.3 2002-08-11 05:39:12 zacheiss Exp $");
 
 #include "../afs/sysincludes.h"	/* Standard vendor system headers */
 #include "../afs/afsincludes.h"	/* Afs-based standard headers */
@@ -97,7 +97,7 @@ afs_CopyOutAttrs(avc, attrs)
     attrs->va_nlink = fakedir ? 100 : avc->m.LinkCount;
     attrs->va_size = fakedir ? 4096 : avc->m.Length;
     attrs->va_atime.tv_sec = attrs->va_mtime.tv_sec = attrs->va_ctime.tv_sec =
-	fakedir ? 0 : avc->m.Date;
+	fakedir ? 0 : (int)avc->m.Date;
     /* set microseconds to be dataversion # so that we approximate NFS-style
      * use of mtime as a dataversion #.  We take it mod 512K because
      * microseconds *must* be less than a million, and 512K is the biggest
