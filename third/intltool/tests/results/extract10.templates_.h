e217a073b5839c31d67ea219fe6c63c6bb3e409a
Greg Hudson <ghudson@mit.edu>
2002-12-26 16:42:14 +0000

    Import intltool 0.24.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@18238 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/intltool/tests/results/extract10.templates_.h
@@ -0,0 +1,7 @@
+char *s = N_("Current month?");
+char *s = N_("January, February, March, April, May, June, July, August, September, October, November, December");
+char *s = N_("Please select the time zone that is appropriate for your location.");
+char *s = N_("Please select your geographical area from the choices provided.  If you are in the United States, you can just use the US option.  You can also find UTC and GMT-offset zone settings in the Etc directory.");
+char *s = N_("Select Area");
+char *s = N_("Time zone");
+char *s = N_("US");
