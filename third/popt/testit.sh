1886cc993c603878043b457a87852c8ad52d8f8c
Greg Hudson <ghudson@mit.edu>
2002-12-26 03:10:52 +0000

    Import popt 1.7.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@18179 728af825-273c-0410-89f9-f7d3b574a069

--- third/popt/testit.sh
+++ third/popt/testit.sh
@@ -14,6 +14,30 @@ run() {
     fi
 }
 
+run_diff() {
+    prog=$1; shift
+    name=$1; shift
+    in_file=$1; shift
+    answer_file=$1; shift
+
+    out=$builddir/tmp.out
+    diff_file=$builddir/tmp.diff
+
+    echo Running test $name.
+
+    $builddir/$prog $in_file > $out
+    ret=$?
+    diff $out $answer_file > $diff_file
+    diff_ret=$?
+
+    if [ "$diff_ret" != "0" ]; then
+       echo "Test \"$name\" failed output is in $out, diff is:"
+       cat $diff_file
+       exit 2
+    fi
+    rm $out $diff_file
+}
+
 builddir=`pwd`
 srcdir=$builddir
 cd ${srcdir}
@@ -36,21 +60,21 @@ run test1 "test1 - 11" "arg1: 0 arg2: bar" -T bar
 run test1 "test1 - 12" "arg1: 1 arg2: (none)" -O 
 run test1 "test1 - 13" "arg1: 1 arg2: foo" -OT foo
 run test1 "test1 - 14" "arg1: 0 arg2: (none) inc: 1" --inc
-run test1 "test1 - 15" "arg1: 0 arg2: foo inc: 1" -i --arg2 foo
-export POSIX_ME_HARDER=1
+run test1 "test1 - 15" "arg1: 0 arg2: foo inc: 1" -I --arg2 foo
+POSIX_ME_HARDER=1 ; export POSIX_ME_HARDER
 run test1 "test1 - 16" "arg1: 1 arg2: (none) rest: foo --arg2 something" --arg1 foo --arg2 something
 unset POSIX_ME_HARDER
-export POSIXLY_CORRECT=1
+POSIXLY_CORRECT=1 ; export POSIXLY_CORRECT
 run test1 "test1 - 17" "arg1: 1 arg2: (none) rest: foo --arg2 something" --arg1 foo --arg2 something
 unset POSIXLY_CORRECT
 run test1 "test1 - 18" "callback: c sampledata bar arg1: 1 arg2: (none)" --arg1 --cb bar
 run test1 "test1 - 19" "" --echo-args
 run test1 "test1 - 20" "--arg1" --echo-args --arg1
 run test1 "test1 - 21" "--arg2 something" -T something -e
-run test1 "test1 - 22" "--arg2 something -- more args" -T something -a more args
+run test1 "test1 - 22" "--arg2 something more args" -T something -a more args
 run test1 "test1 - 23" "--echo-args -a" --echo-args -e -a
-run test1 "test1 - 24" "arg1: 0 arg2: (none) short: 1" -shortoption
-run test1 "test1 - 25" "arg1: 0 arg2: (none) short: 1" --shortoption
+run test1 "test1 - 24" "arg1: 0 arg2: (none) short: 1" -onedash
+run test1 "test1 - 25" "arg1: 0 arg2: (none) short: 1" --onedash
 run test1 "test1 - 26" "callback: c arg for cb2 foo arg1: 0 arg2: (none)" --cb2 foo
 run test1 "test1 - 27" "arg1: 0 arg2: (none) -" -
 run test1 "test1 - 28" "arg1: 0 arg2: foo -" - -2 foo
@@ -62,6 +86,15 @@ run test1 "test1 - 32" "arg1: 0 arg2: (none) aFloat: 10.1" -f 10.1
 run test1 "test1 - 33" "arg1: 0 arg2: (none) aFloat: 10.1" --float 10.1
 run test1 "test1 - 34" "arg1: 0 arg2: (none) aDouble: 10.1" -d 10.1
 run test1 "test1 - 35" "arg1: 0 arg2: (none) aDouble: 10.1" --double 10.1
+run test1 "test1 - 36" "arg1: 0 arg2: (none) oStr: (none)" --optional
+run test1 "test1 - 37" "arg1: 0 arg2: (none) oStr: yadda" --optional=yadda
+run test1 "test1 - 38" "arg1: 0 arg2: (none) oStr: yadda" --optional yadda
+run test1 "test1 - 39" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional=ping pong
+run test1 "test1 - 40" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional ping pong
+
+run_diff test3 "test3 - 41" test3-data/01.input test3-data/01.answer
+run_diff test3 "test3 - 42" test3-data/02.input test3-data/02.answer
+run_diff test3 "test3 - 43" test3-data/03.input test3-data/03.answer
 
 echo ""
 echo "Passed."
