86bac4e659b2b66d8789454208910f1b53986656
Greg Hudson <ghudson@mit.edu>
2003-01-29 20:25:47 +0000

    Import librsvg 2.2.1.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@18608 728af825-273c-0410-89f9-f7d3b574a069

--- third/librsvg/rsvg-css.h
+++ third/librsvg/rsvg-css.h
@@ -1,7 +1,42 @@
+/* vim: set sw=4: -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+   rsvg-css.h : CSS utility functions
+
+   Copyright (C) 2000 Eazel, Inc.
+   Copyright (C) 2002 Dom Lachowicz <cinamod@hotmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Raph Levien <raph@artofcode.com>
+*/
+#ifndef RSVG_CSS_H
+#define RSVG_CSS_H
+
 #include <glib/gtypes.h>
+#include <pango/pangoft2.h>
+
+G_BEGIN_DECLS
 
 double
-rsvg_css_parse_length (const char *str, int *fixed);
+rsvg_css_parse_length (const char *str, gdouble pixels_per_inch, 
+		       gint *percent, gint *em, gint *ex);
+
+double
+rsvg_css_parse_normalized_length(const char *str, gdouble pixels_per_inch,
+								 gdouble width_or_height, gdouble font_size);
 
 gboolean
 rsvg_css_param_match (const char *str, const char *param_name);
@@ -16,4 +51,33 @@ guint
 rsvg_css_parse_opacity (const char *str);
 
 double
-rsvg_css_parse_fontsize (const char *str);
+rsvg_css_parse_angle (const char * str);
+
+double
+rsvg_css_parse_frequency (const char * str);
+
+double
+rsvg_css_parse_time (const char * str);
+
+PangoStyle
+rsvg_css_parse_font_style (const char * str, PangoStyle inherit);
+
+PangoVariant
+rsvg_css_parse_font_variant (const char * str, PangoVariant inherit);
+
+PangoWeight
+rsvg_css_parse_font_weight (const char * str, PangoWeight inherit);
+
+PangoStretch
+rsvg_css_parse_font_stretch (const char * str, PangoStretch inherit);
+
+const char *
+rsvg_css_parse_font_family (const char * str, const char * inherit);
+
+gboolean
+rsvg_css_parse_vbox (const char * vbox, double * x, double * y,
+					 double * w, double * h);
+
+G_END_DECLS
+
+#endif /* RSVG_CSS_H */
--- third/librsvg/rsvg-paint-server.h
+++ third/librsvg/rsvg-paint-server.h
@@ -1,6 +1,36 @@
+/* vim: set sw=4: -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+   rsvg-paint-server.h : RSVG colors
+
+   Copyright (C) 2000 Eazel, Inc.
+   Copyright (C) 2002 Dom Lachowicz <cinamod@hotmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Raph Levien <raph@artofcode.com>
+*/
+
+#ifndef RSVG_PAINT_SERVER_H
+#define RSVG_PAINT_SERVER_H
+
 #include <glib/gtypes.h>
 #include <libart_lgpl/art_render_gradient.h>
 #include "rsvg-defs.h"
+
+G_BEGIN_DECLS
 
 typedef struct _RsvgGradientStop RsvgGradientStop;
 typedef struct _RsvgGradientStops RsvgGradientStops;
@@ -40,19 +70,20 @@ struct _RsvgGradientStops {
 struct _RsvgLinearGradient {
   RsvgDefVal super;
   double affine[6]; /* user space to actual at time of gradient def */
+  RsvgGradientStops *stops;
+  ArtGradientSpread spread;
   double x1, y1;
   double x2, y2;
-  ArtGradientSpread spread;
-  RsvgGradientStops *stops;
 };
 
 struct _RsvgRadialGradient {
   RsvgDefVal super;
   double affine[6]; /* user space to actual at time of gradient def */
+  RsvgGradientStops *stops;
+  ArtGradientSpread spread;
   double cx, cy;
   double r;
   double fx, fy;
-  RsvgGradientStops *stops;
 };
 
 /* Create a new paint server based on a specification string. */
@@ -69,3 +100,12 @@ rsvg_paint_server_ref (RsvgPaintServer *ps);
 void
 rsvg_paint_server_unref (RsvgPaintServer *ps);
 
+RsvgRadialGradient *
+rsvg_clone_radial_gradient (const RsvgRadialGradient *grad, gboolean * shallow_cloned);
+
+RsvgLinearGradient *
+rsvg_clone_linear_gradient (const RsvgLinearGradient *grad, gboolean * shallow_cloned);
+
+G_END_DECLS
+
+#endif
