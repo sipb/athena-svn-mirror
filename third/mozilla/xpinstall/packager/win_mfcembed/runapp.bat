723b8c5fb13a96765e962c4e50220e0e1ceba8c7
Robert A Basch <rbasch@mit.edu>
2003-02-14 21:44:35 +0000

    Import mozilla 1.2.1.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@18859 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/mozilla/xpinstall/packager/win_mfcembed/runapp.bat
@@ -0,0 +1,10 @@
+@echo OFF
+REM --------------------------------------------------------
+REM Modify GRE_DIR below to match your GRE install location
+REM --------------------------------------------------------
+set GRE_VERSION=1.2
+set GRE_DIR=c:\Program Files\mozilla.org\GRE\%GRE_VERSION%
+
+set PATH=%PATH%;%GRE_DIR%;%GRE_DIR%\components
+mfcembed.exe -console
+
