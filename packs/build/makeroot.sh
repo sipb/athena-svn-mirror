8640167e41e0e28dbb119d659d636f57b252e7a7
Greg Hudson <ghudson@mit.edu>
2003-07-03 15:45:37 +0000

    Pull up 1.18 to athena-9_2 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-9_2@19492 728af825-273c-0410-89f9-f7d3b574a069

--- packs/build/makeroot.sh
+++ packs/build/makeroot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: makeroot.sh,v 1.17 2003-04-04 17:27:16 ghudson Exp $
+# $Id: makeroot.sh,v 1.17.2.1 2003-07-03 15:45:37 ghudson Exp $
 
 if [ $# -lt 1 ]; then
   echo "Usage: $0 rootdir [fullversion]" >&2
@@ -20,7 +20,7 @@ linux)
 
   cd "$sysprefix"
   list=`awk '{ x = $2; } END { print x; }' "$syscontrol"`
-  rpms=`awk '/^athena/ { next; } { print $1; }' $list`
+  rpms=`awk '/\/athena-/ { next; } { print $1; }' $list`
 
   mkdir -p "$root/var/lib/rpm" "$root/etc"
   touch "$root/etc/fstab"
