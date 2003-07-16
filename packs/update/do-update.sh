bff81e1a198efc0158d0327a0579a03a0f57358e
Greg Hudson <ghudson@mit.edu>
2003-07-16 17:11:04 +0000

    Pull up 1.43 to athena-9_2 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-9_2@19605 728af825-273c-0410-89f9-f7d3b574a069

--- packs/update/do-update.sh
+++ packs/update/do-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: do-update.sh,v 1.42 2003-03-05 22:24:59 miki Exp $
+# $Id: do-update.sh,v 1.42.2.1 2003-07-16 17:11:04 ghudson Exp $
 
 # Copyright 1996 by the Massachusetts Institute of Technology.
 #
@@ -81,9 +81,6 @@ sleep 2
 echo "Shutting down running services"
 if [ -f /var/athena/inetd.pid ]; then
 	kill `cat /var/athena/inetd.pid` > /dev/null 2>&1
-fi
-if [ -f /var/athena/sshd.pid ]; then
-	kill `cat /var/athena/sshd.pid` > /dev/null 2>&1
 fi
 if [ -f /var/athena/named.pid ]; then
 	kill `cat /var/athena/named.pid` > /dev/null 2>&1
