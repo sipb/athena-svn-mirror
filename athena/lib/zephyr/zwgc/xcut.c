63456c6da2229dc173c59cefd5a76479e01a4ba3
Greg Hudson <ghudson@mit.edu>
2003-07-17 21:07:39 +0000

    Pull up 1.12 to athena-9_2 branch.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/athena-9_2@19617 728af825-273c-0410-89f9-f7d3b574a069

--- athena/lib/zephyr/zwgc/xcut.c
+++ athena/lib/zephyr/zwgc/xcut.c
@@ -4,7 +4,7 @@
  *
  *      Created by:     Marc Horowitz <marc@athena.mit.edu>
  *
- *      $Id: xcut.c,v 1.11 1999-01-22 23:20:43 ghudson Exp $
+ *      $Id: xcut.c,v 1.11.12.1 2003-07-17 21:07:39 ghudson Exp $
  *
  *      Copyright (c) 1989 by the Massachusetts Institute of Technology.
  *      For copying and distribution information, see the file
@@ -14,7 +14,7 @@
 #include <sysdep.h>
 
 #if (!defined(lint) && !defined(SABER))
-static const char rcsid_xcut_c[] = "$Id: xcut.c,v 1.11 1999-01-22 23:20:43 ghudson Exp $";
+static const char rcsid_xcut_c[] = "$Id: xcut.c,v 1.11.12.1 2003-07-17 21:07:39 ghudson Exp $";
 #endif
 
 #include <zephyr/mit-copyright.h>
@@ -242,6 +242,10 @@ void xcut(dpy,event,desc_context)
       case ButtonPress:
 	if (current_pressop != PRESSOP_NONE) {
 	   current_pressop = PRESSOP_STOP;
+	} else if (event->xbutton.button==Button4 ||
+		   event->xbutton.button==Button5) {
+	  /* Ignore scroll wheel movement. */
+	  break;
 	} else if ( (event->xbutton.state)&ShiftMask ) {
 	   if (event->xbutton.button==Button1) {
 	      if (selecting_in)
