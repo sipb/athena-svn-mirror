12b85c55a4e79ea9fba058036420dd4b4e60b1e1
Robert A Basch <rbasch@mit.edu>
2004-01-21 23:17:19 +0000

    Add mitmailexp, a command line tool to expunge deleted messages from an IMAP folder.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20055 728af825-273c-0410-89f9-f7d3b574a069

---
+++ athena/bin/mitmailexp/mitmailexp.pl
@@ -0,0 +1,96 @@
+#!/usr/athena/bin/perl -w
+
+# $Id: mitmailexp.pl,v 1.1 2004-01-21 23:17:19 rbasch Exp $
+
+# Expunge an IMAP folder.
+
+use strict;
+use warnings FATAL => 'all';
+use Cyrus::IMAP;
+use Getopt::Long;
+
+sub usage(;$);
+sub send_command($);
+sub fetch_callback(@);
+sub number_callback(@);
+sub make_msgspecs(@);
+sub close_connection();
+sub errorout($);
+
+my $prog = $0;
+
+sub usage(;$) {
+    print STDERR "$prog: $_[0]\n" if ($_[0] && $_[0] ne "help");
+    print STDERR <<EOF;
+Usage: $prog [<options>]
+  Options:
+    --debug                turn on debugging
+    --help                 print this usage information
+    --host=<name>          query host <name> instead of default POBOX server
+    --mailbox=<name>       examine mailbox <name> instead of INBOX
+EOF
+    exit 1;
+}
+
+# Parse the command line arguments.
+use vars qw($opt_debug $opt_host $opt_mailbox);
+
+GetOptions("debug",
+	   "help" => \&usage,
+	   "host=s",
+	   "mailbox=s") || usage;
+
+usage "Too many arguments" if @ARGV > 0;
+
+$opt_mailbox = 'INBOX' unless $opt_mailbox;
+
+my $username = $ENV{'USER'} || getlogin || (getpwuid($<))[0] ||
+    errorout "Cannot determine user name";
+
+unless ($opt_host) {
+    $opt_host = (split(" ", `hesinfo $username pobox`))[1] ||
+        errorout "Cannot find Post Office server for $username";
+}
+
+# Connect to the IMAP server, and authenticate.
+my $client = Cyrus::IMAP->new($opt_host) ||
+    errorout "Cannot connect to IMAP server on $opt_host";
+unless ($client->authenticate(-authz => $username)) {
+    close_connection();
+    errorout "Cannot authenticate to $opt_host";
+}
+
+# Access the mailbox.  Make sure we have read-write access.
+send_command "SELECT $opt_mailbox";
+
+# Close the mailbox, thereby expunging deleted messages.
+send_command "CLOSE";
+
+# We are done talking to the IMAP server, close down the connection.
+close_connection();
+
+# Subroutine to send a command to the IMAP server, and wait for the
+# response; any defined callbacks for the response are invoked.
+# If the server response indicates failure, we error out.
+sub send_command($) {
+    print "Sending: $_[0]\n" if $opt_debug;
+    my ($status, $text) = $client->send('', '', $_[0]);
+    print "Response: status $status, text $text\n" if $opt_debug;
+    errorout "Premature end-of-file on IMAP connection to $opt_host"
+	if $status eq 'EOF';
+    if ($status ne 'OK') {
+	close_connection();
+	errorout "IMAP error for $opt_mailbox on $opt_host: $text" 
+    }
+}
+
+# Logout from the IMAP server, and close the connection.
+sub close_connection() {
+    $client->send('', '', "LOGOUT");
+    $client->DESTROY();
+}
+
+sub errorout($) {
+    print STDERR "$prog: $_[0]\n";
+    exit 1;
+}
