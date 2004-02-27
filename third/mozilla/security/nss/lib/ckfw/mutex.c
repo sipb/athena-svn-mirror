f5df00767bdf80273337d347a8852775ff31d476
Robert A Basch <rbasch@mit.edu>
2004-02-27 14:02:27 +0000

    Import mozilla 1.6.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20181 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/mutex.c
+++ third/mozilla/security/nss/lib/ckfw/mutex.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: mutex.c,v $ $Revision: 1.1.1.3 $ $Date: 2003-12-24 13:11:02 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: mutex.c,v $ $Revision: 1.1.1.4 $ $Date: 2004-02-27 14:02:27 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
