0307eebbc1217c3bbbc9c63ae80966e2782d3468
Robert A Basch <rbasch@mit.edu>
2004-02-27 17:52:01 +0000

    Import mozilla 1.6.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20197 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/base/config.mk
+++ third/mozilla/security/nss/lib/base/config.mk
@@ -30,7 +30,7 @@
 # may use your version of this file under either the MPL or the
 # GPL.
 #
-CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.1.1.3 $ $Date: 2003-12-24 17:05:41 $ $Name: not supported by cvs2svn $"
+CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.1.1.4 $ $Date: 2004-02-27 17:51:58 $ $Name: not supported by cvs2svn $"
 
 ifdef BUILD_IDG
 DEFINES += -DNSSDEBUG
