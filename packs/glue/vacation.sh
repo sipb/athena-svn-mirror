77f44430712ee6fd740b4fd9537e91c90e0fcd6e
Robert A Basch <rbasch@mit.edu>
2004-03-16 04:16:57 +0000

    Remove the reference to vacation.real, which has been removed.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20282 728af825-273c-0410-89f9-f7d3b574a069

--- packs/glue/vacation.sh
+++ packs/glue/vacation.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# $Id: vacation.sh,v 1.2 2003-05-13 03:42:18 jweiss Exp $
+# $Id: vacation.sh,v 1.3 2004-03-16 04:16:57 rbasch Exp $
 
 echo 1>&2 "For most Athena users, incoming mail goes to the MIT mail hubs and"
 echo 1>&2 "from there onto a post-office server machine.  Your home directory"
 echo 1>&2 "is not consulted until you incorporate the mail.  The system"
 echo 1>&2 "'vacation' program assumes more traditional Unix mail routing"
 echo 1>&2 "controllable by .forward files, and thus will not work."
-echo 1>&2 "If you want to run the system vacation program anyway, you can"
-echo 1>&2 "run it as 'vacation.real'."
 echo 1>&2 
 case "$DISPLAY" in
   "")
