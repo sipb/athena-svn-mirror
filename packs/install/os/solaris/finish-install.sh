38f4436cff509723826c313be740cd7ac0b4be1c
Robert A Basch <rbasch@mit.edu>
2004-05-25 16:50:36 +0000

    Rework finish-install for 9.3: * Add finish-install-wrapper.sh, to handle the tasks formerly done in   the finish-update boot script (since the latter script may be overwritten   during finish-install). * Install MIT non-core packages in finish-install. * Set rc.conf variables according to the install.vars file passed from   the earlier stage of the install. * Copy the passwd/shadow/group files from AFS. * Perform other end-of-install tasks.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20498 728af825-273c-0410-89f9-f7d3b574a069

--- packs/install/os/solaris/finish-install.sh
+++ packs/install/os/solaris/finish-install.sh
@@ -1,18 +1,82 @@
 #!/bin/sh
-# $Id: finish-install.sh,v 1.1 2003-09-12 01:52:03 ghudson Exp $
+# $Id: finish-install.sh,v 1.2 2004-05-25 16:50:36 rbasch Exp $
 
 echo "Starting the second stage of the install at `date`."
 
 # We get one argument, the workstation version we're installing at.
 vers="$1"
 
-echo "Creating config files for Athena software"
-sh /srvd/install/athchanges
+. /var/athena/install.vars
+
+echo "Setting root filesystem fast."
+/srvd/install/fastfs / fast
+
+# Install the Athena passwd/shadow/group files.  We need to do this before
+# the discuss package is installed, so that pkgadd does not complain about
+# files owned by the discuss user.
+sys=`/bin/athena/machtype -S`
+pwconfig=/afs/athena.mit.edu/system/config/passwd/$sys
+if [ -r $pwconfig/passwd ]; then
+  cp -p $pwconfig/passwd /etc/passwd.local
+  chmod 644 /etc/passwd.local
+  chown root /etc/passwd.local
+else
+  cp -p /srvd/etc/passwd.fallback /etc/passwd.local
+fi
+if [ -r $pwconfig/shadow ]; then
+  cp -p $pwconfig/shadow /etc/shadow.local
+  chown root /etc/shadow.local
+else
+  cp -p /srvd/etc/shadow.fallback /etc/shadow.local
+fi
+cp -p /etc/passwd.local /etc/passwd
+cp -p /etc/shadow.local /etc/shadow
+chmod 600 /etc/shadow
+if [ -r $pwconfig/group ]; then
+  cp -p $pwconfig/group /etc/group
+fi
+
+pkgdir=/srvd/pkg/$vers
+libdir=/srvd/usr/athena/lib/update
+echo "Installing Athena non-core packages."
+for pkg in `awk '{ print $1; }' $pkgdir/.order-version` ; do
+  # Skip core packages, which have already been installed.
+  if pkginfo -q $pkg ; then
+    :
+  else
+    echo "$pkg"
+    pkgadd -a $libdir/admin-update -n -d $pkgdir $pkg
+  fi
+done
+echo "Finished installing Athena non-core packages."
+echo ""
+
+# Set the PUBLIC, AUTOUPDATE, and RVDCLIENT variables in rc.conf.
+# Machine-dependent variables such as HOST, ADDR, etc., should be
+# properly set by package postinstall scripts.
+echo "Setting rc.conf variables."
+sed -e "s|^PUBLIC=[^;]*|PUBLIC=$PUBLIC|" \
+    -e "s|^AUTOUPDATE=[^;]*|AUTOUPDATE=$AUTOUPDATE|" \
+    -e "s|^RVDCLIENT=[^;]*|RVDCLIENT=$RVDCLIENT|" /etc/athena/rc.conf \
+  > /etc/athena/rc.conf.new
+mv /etc/athena/rc.conf.new /etc/athena/rc.conf
+
+# Run catman to format Athena man pages, and create the windex
+# database.  Suppress all output, since catman will complain about
+# pages in lockers which cannot be read without tokens (e.g. psutils).
+echo "Formatting Athena man pages."
+/usr/bin/catman -M /usr/athena/man > /dev/null 2>&1
+
+echo "Creating windex databases."
+/usr/bin/catman -w -M /usr/openwin/share/man:/usr/dt/share/man:/usr/share/man
 
 echo "Updating version"
 hosttype=`/bin/athena/machtype`
 echo "Athena Workstation ($hosttype) Version $vers `date`" \
   >> /etc/athena/version
+
+echo "Setting root filesystem slow."
+/srvd/install/fastfs / slow
 
 echo "Cleaning up initial system packs symlinks"
 rm -f /srvd /os
