e6e38a1517b9bbc9bdfb5ea138165e6b6cb55c2f
Robert A Basch <rbasch@mit.edu>
2004-05-25 16:24:39 +0000

    Rework the install for 9.3, including using pkgadd to install MIT packages, instead of running track.  As in the update, packages are split into core and non-core, with only the former getting installed here; finish-install will install the non-core packages, and take care of most of the end-of-install tasks.  Other changes include: * Run fastfs to enable delayed I/O on the target root filesystem. * Use the new filesystem layout, i.e. mount /usr/vice/cache instead   of /var/usr/vice. * Don't redirect pkgadd error output when installing OS packages;   install1.sh will now redirect both descriptors 1 and 2 to the log   file. * Use /install/cdrom and /install/patches instead of /cdrom and   /patches (install1.sh now sets these up correctly). * Use pkgadd -M (ignore the root's vfstab file). * Use /root/var/athena/install.vars to pass along needed parameters   to finish-install. * Make sure we put the fully qualified and unqualified host names   into /etc/inet/hosts, allowing for non-mit.edu names. * Run netparams, and use the masks file, from /srvd. * Cosmetic change: make spacing consistent.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20496 728af825-273c-0410-89f9-f7d3b574a069

--- packs/install/os/solaris/install2.sh
+++ packs/install/os/solaris/install2.sh
@@ -11,7 +11,10 @@ cputype=`/sbin/machtype -c`
 
 UPDATE_ROOT=/root; export UPDATE_ROOT
 echo "Mounting hard disk's root partition..."
-/etc/mount  $rootdrive $UPDATE_ROOT
+mount $rootdrive /root
+
+echo "turn fs fast"
+/util/fastfs /root fast
 
 cd /
 echo "Making dirs on root"
@@ -20,59 +23,59 @@ mkdir /root/usr
 mkdir /root/proc
 ln -s var/rtmp /root/tmp
 
-echo "Mount var, usr , var/usr/vice..."
-case $partitioning in
-many)
-    /sbin/mount  $vardrive /root/var
-    /sbin/mount  $usrdrive /root/usr
-    ;;
-*)
-    ;;
-esac
-mkdir /root/var/usr
-mkdir /root/var/usr/vice
-ln -s /var/usr/vice /root/usr/vice
+mkdir -p /root/usr/vice
 mkdir /root/var/tmp
 mkdir /root/var/rtmp
 chmod 1777 /root/var/tmp
 chmod 1777 /root/var/rtmp
 
-
 echo "installing the os packages"
+libdir=/srvd/usr/athena/lib/update
 case `uname -m` in
 sun4u)
-    for i in `cat /cdrom/.order.install`; do 
-        echo $i; cat /util/yes-file | pkgadd -R /root -d /cdrom $i
-    done 2>/root/var/athena/install.pkgerr
-	;;
+    for i in `cat /install/cdrom/.order.install` ; do 
+	echo $i
+	pkgadd -M -a $libdir/admin-update -n -R /root -d /install/cdrom $i
+    done
+    ;;
 *)
     echo "unsupported architecture - contact Athena administration"
-      exit 1
-	;;
+    exit 1
+    ;;
 esac
 
-
 echo "correction to pkg installation"
-cp /cdrom/INST_RELEASE /root/var/sadm/system/admin/
+cp /install/cdrom/INST_RELEASE /root/var/sadm/system/admin/
 
-echo "make it appear as  configured"
 rm /root/etc/.UNC*
-cp /cdrom/.sysIDtool.state /root/etc/default/
+cp /install/cdrom/.sysIDtool.state /root/etc/default/
 
 echo "Installing Requested and Security patches for OS "
-    cat /util/yes-file | /util/patchadd -d -R /root -u -M \
-	/patches/patches.link `cat /patches/current-patches` 
-
+patches=`cat /install/patches/current-patches` 
+yes | patchadd -d -R /root -u -M /install/patches/patches.link $patches
 
 echo "add/remove osfiles as needed\n"
 sh /srvd/install/oschanges 2>/dev/null
 echo "the os part is installed"
 
-echo "tracking the srvd"
-/srvd/usr/athena/lib/update/track-srvd
-echo "copying kernel modules from /srvd/kernel"
-cp -p -r /srvd/kernel/fs/* /root/kernel/fs/
-cp -p -r /srvd/kernel/fs/sparcv9/afs /root/kernel/fs/sparcv9/
+# Install the "core" Athena packages; these are the packages needed by
+# finish-install to do its work.
+echo "Installing Athena core packages"
+# Get the package directory for the current version.
+vers=`awk '{ print $5; }' /srvd/.rvdinfo`
+pkgdir=/srvd/pkg/$vers
+core_pkgs=`pkginfo -c MITcore -d $pkgdir | awk '{ print $2; }'`
+for pkg in `$libdir/pkg-order -d $pkgdir $core_pkgs` ; do
+  echo $pkg
+  pkgadd -M -a $libdir/admin-update -n -R /root -d $pkgdir $pkg
+done
+echo "Finished installing Athena core packages."
+
+# Install the finish-update script; it will invoke finish-install.
+cp -p /srvd/etc/init.d/finish-update /root/etc/init.d
+rm -f /root/etc/rc2.d/S70finish-update
+ln -s ../init.d/finish-update /root/etc/rc2.d/S70finish-update
+
 echo "Create devices and dev"
 cd /root
 /usr/sbin/devfsadm -r /root -t /root/etc/devlink.tab -p /root/etc/path_to_inst
@@ -82,62 +85,46 @@ chmod 755 /root/devices
 cp /dev/null /root/reconfigure
 
 cd /root
-echo "Creating other files/directories on the pack's root..."
-mkdir afs mit 
+echo "Creating other files/directories..."
+mkdir -p afs mit 
 chmod 1777 /root/tmp 
 
-echo "Finishing etc"
 cp /dev/null etc/mnttab
 cp /dev/null etc/.mnttab.lock
 cp /dev/null etc/dumpdates
-for i in `cat /srvd/usr/athena/lib/update/configfiles`; do
-    cp -p /srvd$i /root$i
-done
 
 if=`ifconfig -au | awk -F: '/^[a-z]/ { if ($1 != "lo0") { print $1; exit; } }'`
 if [ -z "$if" ]; then if=le0; fi
+
+# Set the hostname to the fully-qualified name, forced to lower case.
+# Also put the unqualified name in the hosts file.
+fqdn=`host $netaddr | sed -n -e 's|^.*domain name pointer \(.*\)$|\1|p' | \
+    sed -e 's|\.*$||'`
+if [ -n "$fqdn" ]; then
+    hostname=$fqdn
+fi
 hostname=`echo $hostname | /usr/bin/tr "[A-Z]" "[a-z]"`
+case $hostname in
+*.*)
+    shortname=`echo $hostname | awk -F. '{ print $1; }'`
+    ;;
+*)
+    # We do not know the fully-qualified name.  Assume the mit.edu domain.
+    shortname=$hostname
+    hostname=$hostname.mit.edu
+    ;;
+esac
 echo "Host name is $hostname"
 echo "Address is $netaddr"
 echo $hostname >etc/nodename
 echo $hostname >etc/hostname.$if
-set -- `/etc/athena/netparams $netaddr`
+set -- `/srvd/etc/athena/netparams -f /srvd/etc/athena/masks $netaddr`
 echo "$2  $1" > /root/etc/inet/netmasks
-echo "$4" >etc/defaultrouter
-echo "$netaddr	${hostname}.MIT.EDU $hostname" >>etc/inet/hosts
+echo "$4" > etc/defaultrouter
+echo "$netaddr	$hostname $shortname" >> etc/inet/hosts
 
-cd /root
-if [ -r /srvd/etc/passwd ]; then
-    cp -p /srvd/etc/passwd etc/passwd.local
-    chmod 644 etc/passwd.local
-    chown root etc/passwd.local
-else
-    cp -p /srvd/etc/passwd.fallback etc/passwd.local
-fi
-if [ -r /srvd/etc/shadow ]; then
-    cp -p /srvd/etc/shadow etc/shadow.local
-    chown root etc/shadow.local
-else
-    cp -p /srvd/etc/shadow.fallback etc/shadow.local
-fi
-cp -p etc/passwd.local etc/passwd
-cp -p etc/shadow.local etc/shadow
-chmod 600 etc/shadow
-cp -p /srvd/etc/group etc/group
-cp -p /srvd/etc/athena/athinfo.access etc/athena/athinfo.access
-cp -p /srvd/etc/athena/local-lockers.conf etc/athena/local-lockers.conf
-cp -p /srvd/etc/athena/*.conf etc/athena/
-echo "Updating dm config"
-cp -p /srvd/etc/athena/login/config etc/athena/login/config
-echo "Editing rc.conf and version"
-/srvd/etc/athena/gettime -s time.mit.edu
-sed -e 	"s#^HOST=[^;]*#HOST=$hostname#
-	s#^ADDR=[^;]*#ADDR=$netaddr#
-	s#^NETDEV=[^;]*#NETDEV=$if#
-	s#^MACHINE=[^;]*#MACHINE=$cputype#
-	s#^SYSTEM=[^;]*#SYSTEM=Solaris#" \
-	< /srvd/etc/athena/rc.conf > /root/etc/athena/rc.conf
 rm -f /root/.rvdinfo
+/srvd/etc/athena/gettime -s time.mit.edu
 echo installed on `date` from `df -k / | tail -1 | awk '{print $1}'` \
 	> /root/etc/athena/version
 if [ $CUSTOM = Y ]; then
@@ -153,34 +140,33 @@ sed -e 's/RVD/Workstation/' -e 's/Version/Version Install/' \
 
 echo "Updating vfstab"
 rm -f etc/vfstab
-case $partitioning in
-many)
-    sed "s/@DISK@/$drive/g" /srvd/etc/vfstab.std > etc/vfstab
-    ;;
-*)
-    sed "s/@DISK@/$drive/g;/d0s5/d;/d0s6/d" /srvd/etc/vfstab.std > etc/vfstab
-    ;;
-esac
+sed "s/@DISK@/$drive/g;/d0s5/d;/d0s6/d" /srvd/etc/vfstab.std > etc/vfstab
 chmod 644 etc/vfstab
 cp /dev/null etc/named.local
 
+# Set up the /usr/vice/etc directory in the target root.
+mkdir -p /root/usr/vice/etc
 
-cd /var/usr/vice
-for i in  CellAlias CellServDB SuidCells 
-	do
-	cp -p /afs/athena/service/$i etc/
-	chown root:root etc/$i
-	chmod a+r etc/$i
+# Copy ThisCell and cacheinfo from the miniroot.
+for i in ThisCell cacheinfo ; do
+    cp /usr/vice/etc/$i /root/usr/vice/etc/$i
+    chown root:root /root/usr/vice/etc/$i
+    chmod a+r /root/usr/vice/etc/$i
 done
-echo "Copied afs service files into var/usr/vice/etc"
+
+# Get current copies of CellAlias, CellServDB, and SuidCells from AFS.
+for i in CellAlias CellServDB SuidCells ; do
+    cp -p /afs/athena/service/$i /root/usr/vice/etc/
+    chown root:root /root/usr/vice/etc/$i
+    chmod a+r /root/usr/vice/etc/$i
+done
+echo "Copied afs service files into /usr/vice/etc"
 
 echo "Setting up initial system packs symlinks"
 (cd /tmp && tar cf - srvd os) | (cd /root && tar xf -)
 
 echo "turn fs slow"
 /util/fastfs /root slow
-/util/fastfs /root/usr slow
-/util/fastfs /root/var slow
 
 echo "Initializing var/adm and spool files "
 cp /dev/null /root/var/adm/lastlog
@@ -198,34 +184,24 @@ installboot "/os/usr/platform/$platform/lib/fs/ufs/bootblk" "$rrootdrive"
 echo "setting the boot device"
 luxadm set_boot_dev -y $rootdrive
 
-cd /root
+mkdir -p /root/var/athena
 cp /tmp/install.log /root/var/athena/install.log
-case $REBOOT in
-N)
-    echo "after doing your customizations, type exit "
-    echo "and the machine will reboot itself"
-    /sbin/sh
-    ;;
-default)
-    ;;
-esac
+
+# Pass along needed variables to the next stage of the install.
+echo "REBOOT=$REBOOT" >> /root/var/athena/install.vars
+echo "PUBLIC=$PUBLIC" >> /root/var/athena/install.vars
+echo "AUTOUPDATE=$AUTOUPDATE" >> /root/var/athena/install.vars
+echo "RVDCLIENT=$RVDCLIENT" >> /root/var/athena/install.vars
+
 echo "Unmounting filesystems and checking them"
 cd /
 
-umount /var/usr/vice > /dev/null 2>&1
+umount /usr/vice/cache > /dev/null 2>&1
 fsck -y -F ufs $rcachedrive
-case $partitioning in
-many)
-    umount /root/var > /dev/null 2>&1
-    fsck -y -F ufs $rvardrive
-    umount /root/usr
-    fsck -y -F ufs $rusrdrive
-    ;;
-esac
 /sbin/umount /root > /dev/null 2>&1
 /usr/sbin/fsck -y -F ufs $rrootdrive
 sleep 5
 
-echo "Rebooting now"
+echo "Rebooting into the target root to complete the installation."
 reboot
 
