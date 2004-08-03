374dcf6183852b17648c1e68435000eea20fb74b
Robert A Basch <rbasch@mit.edu>
2004-08-03 17:39:13 +0000

    Cosmetic change only: reformat using our standard spacing.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20604 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/os/solaris/xwrapper.sh
+++ packs/maint/os/solaris/xwrapper.sh
@@ -2,7 +2,7 @@
 
 # Give people a way to disable the wrapper if it's annoying.
 if [ -f /etc/athena/x-no-wrapper ]; then
-	exec /usr/openwin/bin/Xsun "$@"
+  exec /usr/openwin/bin/Xsun "$@"
 fi
 
 framebuf=`/bin/athena/machtype -d`
@@ -10,9 +10,9 @@ options=
 
 # Determine if user has selected "restart in 8-bit mode".
 if [ -f /var/athena/x-8-bit ]; then
-	mode=8
+  mode=8
 else
-	mode=24
+  mode=24
 fi
 rm -f /var/athena/x-8-bit
 
@@ -20,27 +20,27 @@ rm -f /var/athena/x-8-bit
 # options.
 case $framebuf,$mode in
 afb,24|ffb,24|ifb,24|jfb,24)
-	# With no options, the default visual would be 8-bit.
-	options="-dev /dev/fb defdepth 24"
-	;;
+  # With no options, the default visual would be 8-bit.
+  options="-dev /dev/fb defdepth 24"
+  ;;
 m,24)
-	# Reset the card to defaults (in case there's a new monitor).
-	# Then try to configure the card to 1152x900x76 at depth 24.
-	# Redirect stdin so if m64config asks for a confirmation
-	# (because it can't be sure the monitor can do this) the
-	# answer is "no."  Make all failures silent; if we end up in
-	# 8-bit mode, that's not a tragedy.
-	m64config -defaults
-	m64config -res 1152x900x76 -depth 24 < /dev/null > /dev/null 2>&1
-	;;
+  # Reset the card to defaults (in case there's a new monitor).
+  # Then try to configure the card to 1152x900x76 at depth 24.
+  # Redirect stdin so if m64config asks for a confirmation
+  # (because it can't be sure the monitor can do this) the
+  # answer is "no."  Make all failures silent; if we end up in
+  # 8-bit mode, that's not a tragedy.
+  m64config -defaults
+  m64config -res 1152x900x76 -depth 24 < /dev/null > /dev/null 2>&1
+  ;;
 m,8)
-	# Reset the card to defaults (in case there's a new monitor).
-	# Then try to configure the card to 1152x900x76 at depth 8.
-	# This appears to be necessary to get an 8 bit visual on 
-	# Suns that have updated to Solaris 8.
-	m64config -defaults
-	m64config -res 1152x900x76 -depth 8 < /dev/null > /dev/null 2>&1
-	;;
+  # Reset the card to defaults (in case there's a new monitor).
+  # Then try to configure the card to 1152x900x76 at depth 8.
+  # This appears to be necessary to get an 8 bit visual on 
+  # Suns that have updated to Solaris 8.
+  m64config -defaults
+  m64config -res 1152x900x76 -depth 8 < /dev/null > /dev/null 2>&1
+  ;;
 esac
 
 exec /usr/openwin/bin/Xsun $options "$@"
