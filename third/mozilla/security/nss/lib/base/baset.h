39c43ffbbe5008252198ea9b5e0b1e8aba4ff76e
Robert A Basch <rbasch@mit.edu>
2004-08-09 11:52:09 +0000

    Import mozilla 1.7.2.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20609 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/base/baset.h
+++ third/mozilla/security/nss/lib/base/baset.h
@@ -35,7 +35,7 @@
 #define BASET_H
 
 #ifdef DEBUG
-static const char BASET_CVS_ID[] = "@(#) $RCSfile: baset.h,v $ $Revision: 1.1.1.5 $ $Date: 2004-06-30 14:43:02 $ $Name: not supported by cvs2svn $";
+static const char BASET_CVS_ID[] = "@(#) $RCSfile: baset.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 11:44:03 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/pki1/oid.c
+++ third/mozilla/security/nss/lib/pki1/oid.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: oid.c,v $ $Revision: 1.1.1.5 $ $Date: 2004-06-30 14:50:05 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: oid.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 11:51:06 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
