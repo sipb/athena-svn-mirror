141a931ccde75dae3c820ff27fe5909d9d3b99a8
Robert A Basch <rbasch@mit.edu>
2004-08-09 15:39:04 +0000

    This commit was generated by cvs2svn to compensate for changes in r20637, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20638 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki1/pki1.h
+++ third/mozilla/security/nss/lib/pki1/pki1.h
@@ -35,7 +35,7 @@
 #define PKI1_H
 
 #ifdef DEBUG
-static const char PKI1_CVS_ID[] = "@(#) $RCSfile: pki1.h,v $ $Revision: 1.1.1.5 $ $Date: 2004-06-30 18:42:17 $ $Name: not supported by cvs2svn $";
+static const char PKI1_CVS_ID[] = "@(#) $RCSfile: pki1.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 15:39:04 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
