87b3f95087bcc813f70e4f1c96ea0f8c8eadae43
Robert A Basch <rbasch@mit.edu>
2004-08-09 13:27:08 +0000

    This commit was generated by cvs2svn to compensate for changes in r20619, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20620 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/dev/ckhelper.c
+++ third/mozilla/security/nss/lib/dev/ckhelper.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: ckhelper.c,v $ $Revision: 1.1.1.5 $ $Date: 2004-06-30 16:28:06 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: ckhelper.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:27:08 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
