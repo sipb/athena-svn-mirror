10e3c8e3ab951036d46ceeaf453259768e5ef05e
Robert A Basch <rbasch@mit.edu>
2004-09-17 13:05:20 +0000

    Import mozilla 1.7.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20684 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/base/nssbaset.h
+++ third/mozilla/security/nss/lib/base/nssbaset.h
@@ -35,7 +35,7 @@
 #define NSSBASET_H
 
 #ifdef DEBUG
-static const char NSSBASET_CVS_ID[] = "@(#) $RCSfile: nssbaset.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 11:47:29 $ $Name: not supported by cvs2svn $";
+static const char NSSBASET_CVS_ID[] = "@(#) $RCSfile: nssbaset.h,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 13:00:45 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/pki/nsspki.h
+++ third/mozilla/security/nss/lib/pki/nsspki.h
@@ -35,7 +35,7 @@
 #define NSSPKI_H
 
 #ifdef DEBUG
-static const char NSSPKI_CVS_ID[] = "@(#) $RCSfile: nsspki.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 11:52:09 $ $Name: not supported by cvs2svn $";
+static const char NSSPKI_CVS_ID[] = "@(#) $RCSfile: nsspki.h,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 13:05:20 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
