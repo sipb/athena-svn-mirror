3d657265e927110a5d038ad16f695cfbd11fffda
Robert A Basch <rbasch@mit.edu>
2004-09-17 15:38:55 +0000

    Import mozilla 1.7.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20706 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/mailnews/local/src/nsPop3Protocol.h
+++ third/mozilla/mailnews/local/src/nsPop3Protocol.h
@@ -191,6 +191,7 @@ typedef struct Pop3UidlHost {
 } Pop3UidlHost;
 
 typedef struct Pop3MsgInfo {
+    PRInt32 msgnum;
     PRInt32 size;
     char* uidl;
 } Pop3MsgInfo;
@@ -350,6 +351,7 @@ private:
   void BackupAuthFlags();
   void RestoreAuthFlags();
   PRInt32 m_origAuthFlags;
+  PRInt32 m_listpos;
 
   //////////////////////////////////////////////////////////////////////////////////////////
       // Begin Pop3 protocol state handlers
