2cb077a3f9320a697c63b3023b3b3765833698d1
Robert A Basch <rbasch@mit.edu>
2004-09-17 14:32:07 +0000

    Import mozilla 1.7.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20692 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/builtins/certdata.c
+++ third/mozilla/security/nss/lib/ckfw/builtins/certdata.c
@@ -32,7 +32,7 @@
  * GPL.
  */
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:21:01 $ $Name: not supported by cvs2svn $""; @(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:21:01 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 14:32:06 $ $Name: not supported by cvs2svn $""; @(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 14:32:06 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef BUILTINS_H
@@ -448,7 +448,7 @@ static const NSSItem nss_builtins_items_0 [] = {
   { (void *)&ck_false, (PRUint32)sizeof(CK_BBOOL) },
   { (void *)"CVS ID", (PRUint32)7 },
   { (void *)"NSS", (PRUint32)4 },
-  { (void *)"@(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:21:01 $ $Name: not supported by cvs2svn $""; @(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:21:01 $ $Name: not supported by cvs2svn $", (PRUint32)179 }
+  { (void *)"@(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 14:32:06 $ $Name: not supported by cvs2svn $""; @(#) $RCSfile: certdata.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 14:32:06 $ $Name: not supported by cvs2svn $", (PRUint32)179 }
 };
 #endif /* DEBUG */
 static const NSSItem nss_builtins_items_1 [] = {
--- third/mozilla/security/nss/lib/dev/dev.h
+++ third/mozilla/security/nss/lib/dev/dev.h
@@ -41,7 +41,7 @@
  */
 
 #ifdef DEBUG
-static const char DEV_CVS_ID[] = "@(#) $RCSfile: dev.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 13:12:59 $ $Name: not supported by cvs2svn $";
+static const char DEV_CVS_ID[] = "@(#) $RCSfile: dev.h,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 14:24:23 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKT_H
