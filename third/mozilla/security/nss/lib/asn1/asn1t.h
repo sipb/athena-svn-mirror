b62b6d4eba2f2273369c8dc14915ef30b30ba5c6
Robert A Basch <rbasch@mit.edu>
2004-09-17 16:25:58 +0000

    Import mozilla 1.7.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@20712 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/asn1/asn1t.h
+++ third/mozilla/security/nss/lib/asn1/asn1t.h
@@ -35,7 +35,7 @@
 #define ASN1T_H
 
 #ifdef DEBUG
-static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.6 $ $Date: 2004-08-09 15:25:01 $ $Name: not supported by cvs2svn $";
+static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 16:24:49 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
