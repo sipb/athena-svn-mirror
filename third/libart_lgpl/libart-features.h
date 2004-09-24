141b2c0e4e81d16c5716fd99b07af11ff1039ca5
Greg Hudson <ghudson@mit.edu>
2004-09-24 20:19:34 +0000

    This commit was generated by cvs2svn to compensate for changes in r20812, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@20813 728af825-273c-0410-89f9-f7d3b574a069

--- third/libart_lgpl/libart-features.h
+++ third/libart_lgpl/libart-features.h
@@ -3,11 +3,21 @@
 
 #define LIBART_MAJOR_VERSION (2)
 #define LIBART_MINOR_VERSION (3)
-#define LIBART_MICRO_VERSION (11)
-#define LIBART_VERSION "2.3.11"
+#define LIBART_MICRO_VERSION (16)
+#define LIBART_VERSION "2.3.16"
 
-extern const unsigned int libart_major_version, libart_minor_version, libart_micro_version;
-extern const char *libart_version;
+#ifdef _WIN32
+#  ifdef LIBART_COMPILATION
+#    define LIBART_VAR __declspec(dllexport)
+#  else
+#    define LIBART_VAR extern __declspec(dllimport)
+#  endif
+#else
+#  define LIBART_VAR extern
+#endif
+
+LIBART_VAR const unsigned int libart_major_version, libart_minor_version, libart_micro_version;
+LIBART_VAR const char *libart_version;
 
 void libart_preinit(void *app, void *modinfo);
 void libart_postinit(void *app, void *modinfo);
