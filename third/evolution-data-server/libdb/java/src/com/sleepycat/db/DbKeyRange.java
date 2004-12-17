b1030ca17cdaf90545412ce2444114b3cc15794d
Greg Hudson <ghudson@mit.edu>
2004-12-17 17:31:46 +0000

    Import evolution-data-server 1.0.0.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21188 728af825-273c-0410-89f9-f7d3b574a069

---
+++ third/evolution-data-server/libdb/java/src/com/sleepycat/db/DbKeyRange.java
@@ -0,0 +1,23 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 1997-2002
+ *      Sleepycat Software.  All rights reserved.
+ *
+ * $Id: DbKeyRange.java,v 1.1.1.1 2004-12-17 17:27:10 ghudson Exp $
+ */
+
+package com.sleepycat.db;
+
+/**
+ *
+ * @author Donald D. Anderson
+ */
+public class DbKeyRange
+{
+    public double less;
+    public double equal;
+    public double greater;
+}
+
+// end of DbKeyRange.java
