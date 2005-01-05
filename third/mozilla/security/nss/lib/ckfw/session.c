6dcd89e1e2505605146808663232c4fc679aacef
Robert A Basch <rbasch@mit.edu>
2005-01-05 13:43:18 +0000

    Import mozilla 1.7.5.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21231 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/session.c
+++ third/mozilla/security/nss/lib/ckfw/session.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 12:48:00 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 13:43:18 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
