dffa4c292d00559fa3a55a8e38613cb043eb344f
Robert A Basch <rbasch@mit.edu>
2005-01-05 17:20:20 +0000

    Import mozilla 1.7.5.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21256 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/asn1/asn1t.h
+++ third/mozilla/security/nss/lib/asn1/asn1t.h
@@ -35,7 +35,7 @@
 #define ASN1T_H
 
 #ifdef DEBUG
-static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 16:24:49 $ $Name: not supported by cvs2svn $";
+static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 17:19:11 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/tests/pkcs11/netscape/trivial/trivial.c
+++ third/mozilla/security/nss/tests/pkcs11/netscape/trivial/trivial.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: trivial.c,v $ $Revision: 1.1.1.7 $ $Date: 2004-09-17 16:18:09 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: trivial.c,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 17:12:32 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
