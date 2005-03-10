7cb154af16de6aa7dd000afed60197466c3d9fe3
Greg Hudson <ghudson@mit.edu>
2005-03-10 21:51:52 +0000

    This commit was generated by cvs2svn to compensate for changes in r21468, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21469 728af825-273c-0410-89f9-f7d3b574a069

--- third/intltool/intltool.spec
+++ third/intltool/intltool.spec
@@ -1,6 +1,6 @@
 # Note this is NOT a relocatable thing :)
 %define name		intltool
-%define ver		0.31.2
+%define ver		0.33
 %define RELEASE		1
 %define rel		%{?CUSTOM_RELEASE} %{!?CUSTOM_RELEASE:%RELEASE}
 
--- third/intltool/tests/results/extract8.glade.h
+++ third/intltool/tests/results/extract8.glade.h
@@ -1,3 +1,4 @@
+/* Comment on <property> tag */
 char *s = N_("A label");
 char *s = N_("Cancel (and exit) the test.");
 char *s = N_("Click the button below");
@@ -7,4 +8,6 @@ char *s = N_("This action\n"
              "does nothing interesting.");
 char *s = N_("This button\n"
              "dares you to invoke this action.");
+/* Multiline comments
+should be supported as well */
 char *s = N_("_Push me");
