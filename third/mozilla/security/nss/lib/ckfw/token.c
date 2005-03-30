d84a9e257805c3bdf05d533144ce83f3878292af
Robert A Basch <rbasch@mit.edu>
2005-03-30 18:43:40 +0000

    Import mozilla 1.7.6.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21636 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/token.c
+++ third/mozilla/security/nss/lib/ckfw/token.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: token.c,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 14:48:17 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: token.c,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 18:29:04 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/pki/pkistore.h
+++ third/mozilla/security/nss/lib/pki/pkistore.h
@@ -35,7 +35,7 @@
 #define PKISTORE_H
 
 #ifdef DEBUG
-static const char PKISTORE_CVS_ID[] = "@(#) $RCSfile: pkistore.h,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 14:58:40 $ $Name: not supported by cvs2svn $";
+static const char PKISTORE_CVS_ID[] = "@(#) $RCSfile: pkistore.h,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 18:39:24 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSPKIT_H
