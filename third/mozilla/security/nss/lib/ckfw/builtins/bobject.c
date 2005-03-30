96462ddc0ac113b8ee82dfda1d32818fc09cae6c
Robert A Basch <rbasch@mit.edu>
2005-03-30 18:08:45 +0000

    This commit was generated by cvs2svn to compensate for changes in r21634, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21635 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/builtins/bobject.c
+++ third/mozilla/security/nss/lib/ckfw/builtins/bobject.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: bobject.c,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 14:21:21 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: bobject.c,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 18:02:10 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #include "builtins.h"
--- third/mozilla/security/nss/lib/dev/nssdev.h
+++ third/mozilla/security/nss/lib/dev/nssdev.h
@@ -35,7 +35,7 @@
 #define NSSDEV_H
 
 #ifdef DEBUG
-static const char NSSDEV_CVS_ID[] = "@(#) $RCSfile: nssdev.h,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 14:22:26 $ $Name: not supported by cvs2svn $";
+static const char NSSDEV_CVS_ID[] = "@(#) $RCSfile: nssdev.h,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 18:03:15 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 /*
  * nssdev.h
