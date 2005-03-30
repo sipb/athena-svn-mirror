b404465bad049a677972af5c153bc1408e46be1a
Robert A Basch <rbasch@mit.edu>
2005-03-30 17:24:10 +0000

    This commit was generated by cvs2svn to compensate for changes in r21628, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21629 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki/nsspkit.h
+++ third/mozilla/security/nss/lib/pki/nsspkit.h
@@ -35,7 +35,7 @@
 #define NSSPKIT_H
 
 #ifdef DEBUG
-static const char NSSPKIT_CVS_ID[] = "@(#) $RCSfile: nsspkit.h,v $ $Revision: 1.1.1.8 $ $Date: 2005-01-05 13:42:48 $ $Name: not supported by cvs2svn $";
+static const char NSSPKIT_CVS_ID[] = "@(#) $RCSfile: nsspkit.h,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 17:23:40 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
