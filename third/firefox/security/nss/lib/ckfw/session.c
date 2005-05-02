772326c293e869c8b1d0a7e9b3d887159285b331
Robert A Basch <rbasch@mit.edu>
2005-05-02 16:17:35 +0000

    This commit was generated by cvs2svn to compensate for changes in r21838, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21839 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/ckfw/session.c
+++ third/firefox/security/nss/lib/ckfw/session.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.1 $ $Date: 2005-04-01 12:06:35 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 16:17:35 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
