75af82136f1d1cd3d4829a0131f38da5609f9bc4
Robert A Basch <rbasch@mit.edu>
2005-05-02 16:34:37 +0000

    Import firefox 1.0.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21842 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/dev/nssdevt.h
+++ third/firefox/security/nss/lib/dev/nssdevt.h
@@ -35,7 +35,7 @@
 #define NSSDEVT_H
 
 #ifdef DEBUG
-static const char NSSDEVT_CVS_ID[] = "@(#) $RCSfile: nssdevt.h,v $ $Revision: 1.1.1.1 $ $Date: 2005-04-01 12:21:47 $ $Name: not supported by cvs2svn $";
+static const char NSSDEVT_CVS_ID[] = "@(#) $RCSfile: nssdevt.h,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 16:32:47 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/firefox/security/nss/lib/pki/nsspki.h
+++ third/firefox/security/nss/lib/pki/nsspki.h
@@ -35,7 +35,7 @@
 #define NSSPKI_H
 
 #ifdef DEBUG
-static const char NSSPKI_CVS_ID[] = "@(#) $RCSfile: nsspki.h,v $ $Revision: 1.1.1.1 $ $Date: 2005-04-01 12:23:37 $ $Name: not supported by cvs2svn $";
+static const char NSSPKI_CVS_ID[] = "@(#) $RCSfile: nsspki.h,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 16:34:37 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
