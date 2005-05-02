a814f4aba34b64be16e38e05bc22355a5d20e34b
Robert A Basch <rbasch@mit.edu>
2005-05-02 18:17:55 +0000

    Import firefox 1.0.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21869 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/dev/devtoken.c
+++ third/firefox/security/nss/lib/dev/devtoken.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.1 $ $Date: 2005-04-01 14:07:01 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 18:17:55 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
