f65945c22384ea3f0e131bff56419897e6922149
Robert A Basch <rbasch@mit.edu>
2005-05-02 14:16:05 +0000

    This commit was generated by cvs2svn to compensate for changes in r21820, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21821 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/base/whatnspr.c
+++ third/mozilla/security/nss/lib/base/whatnspr.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: whatnspr.c,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 17:51:06 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: whatnspr.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 14:12:16 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef BASE_H
--- third/mozilla/security/nss/lib/pki1/atav.c
+++ third/mozilla/security/nss/lib/pki1/atav.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: atav.c,v $ $Revision: 1.1.1.9 $ $Date: 2005-03-30 17:53:13 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: atav.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 14:14:23 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
