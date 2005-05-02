399d1ddd2d94c5b3151cfa12002fdfd0416e9681
Robert A Basch <rbasch@mit.edu>
2005-05-02 20:05:50 +0000

    Import firefox 1.0.3.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21901 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/ckfw/config.mk
+++ third/firefox/security/nss/lib/ckfw/config.mk
@@ -30,7 +30,7 @@
 # may use your version of this file under either the MPL or the
 # GPL.
 #
-CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.1.1.1 $ $Date: 2005-04-01 15:54:35 $ $Name: not supported by cvs2svn $"
+CONFIG_CVS_ID = "@(#) $RCSfile: config.mk,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 20:05:50 $ $Name: not supported by cvs2svn $"
 
 ifdef BUILD_IDG
 DEFINES += -DNSSDEBUG
