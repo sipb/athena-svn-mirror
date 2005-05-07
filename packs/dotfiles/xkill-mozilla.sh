2285b1f6674f1a083a946c7eee24e0b838e1e8cb
Robert A Basch <rbasch@mit.edu>
2005-05-07 16:12:55 +0000

    Kill all mozilla applications (mozilla, firefox, thunderbird) found running on the display.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21926 728af825-273c-0410-89f9-f7d3b574a069

--- packs/dotfiles/xkill-mozilla.sh
+++ packs/dotfiles/xkill-mozilla.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# $Id: xkill-mozilla.sh,v 1.1 2004-06-07 19:10:49 rbasch Exp $
+# $Id: xkill-mozilla.sh,v 1.2 2005-05-07 16:12:55 rbasch Exp $
 
-# Kill all of the Mozilla instances running on this X display.
+# Kill all mozilla.org applications running on this X display.
 
 moz_libdir=/usr/athena/lib/mozilla
 
+# See if any mozilla program is running.
 ping_mozilla () {
   LD_LIBRARY_PATH=$moz_libdir${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"} \
     $moz_libdir/mozilla-xremote-client "ping()" > /dev/null 2>&1
@@ -16,25 +17,28 @@ killed=
 timeout=3
 i=0
 while ping_mozilla ; do
-  # Find all windows which plausibly belong to Mozilla.
-  for win in `xwininfo -root -tree | awk '/Mozilla/ { print $1; }'` ; do
+  # Find all windows which plausibly belong to a mozilla application.
+  for win in `xwininfo -root -tree | \
+    awk '/Mozilla|Firefox|Thunderbird/ { print $1; }'` ; do
     # Skip any window we have already tried to kill.
     case $killed in
     *" $win "*)
       continue
       ;;
     esac
-    # See if the window has the _MOZILLA_VERSION property set.
-    moz_version="`xprop -id $win _MOZILLA_VERSION 2>/dev/null | \
+    # See if the window has the _MOZILLA_PROGRAM property set.
+    moz_program="`xprop -id $win _MOZILLA_PROGRAM 2>/dev/null | \
       sed -n -e 's/.*"\(.*\)"/\1/p'`"
-    if [ -n "$moz_version" ]; then 
+    case $moz_program in
+    mozilla|firefox|thunderbird)
       # Found one.  Kill it, add it to the list of killed windows, and
       # go back to the top of the outer loop to check against the new
       # set of windows.
       xkill -id $win > /dev/null 2>&1
       killed=" $killed $win "
       continue 2
-    fi
+      ;;
+    esac
   done
   # If we get here, the ping found a running mozilla, but we were unable
   # to find a window which had not already been killed.  Wait one second
