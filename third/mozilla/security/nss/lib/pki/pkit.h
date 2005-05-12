7214133a5339cca41c07c60815457c37b630ec12
Robert A Basch <rbasch@mit.edu>
2005-05-12 21:50:18 +0000

    Import mozilla 1.7.8.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21982 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki/pkit.h
+++ third/mozilla/security/nss/lib/pki/pkit.h
@@ -35,7 +35,7 @@
 #define PKIT_H
 
 #ifdef DEBUG
-static const char PKIT_CVS_ID[] = "@(#) $RCSfile: pkit.h,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 16:31:11 $ $Name: not supported by cvs2svn $";
+static const char PKIT_CVS_ID[] = "@(#) $RCSfile: pkit.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 21:46:01 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/pki/trustdomain.c
+++ third/mozilla/security/nss/lib/pki/trustdomain.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: trustdomain.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 16:31:11 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: trustdomain.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 21:46:01 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef DEV_H
