a098ba1a134c27871beaadce055129ad20b283c8
Robert A Basch <rbasch@mit.edu>
2005-05-12 18:53:26 +0000

    This commit was generated by cvs2svn to compensate for changes in r21945, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21946 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/pki/tdcache.c
+++ third/firefox/security/nss/lib/pki/tdcache.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 18:30:05 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.3 $ $Date: 2005-05-12 18:53:26 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef PKIM_H
