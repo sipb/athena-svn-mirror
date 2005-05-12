4750c7bb3184922a441cfb987a3544500864f06b
Robert A Basch <rbasch@mit.edu>
2005-05-12 21:02:08 +0000

    This commit was generated by cvs2svn to compensate for changes in r21978, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21979 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/dev/devtoken.c
+++ third/mozilla/security/nss/lib/dev/devtoken.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 15:47:33 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 21:02:08 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
