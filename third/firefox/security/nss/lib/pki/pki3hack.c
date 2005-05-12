1846f9d1c34d04d3abcd616c7d3fb9cef75366a0
Robert A Basch <rbasch@mit.edu>
2005-05-12 19:35:26 +0000

    Import firefox 1.0.4.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21955 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/pki/pki3hack.c
+++ third/firefox/security/nss/lib/pki/pki3hack.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: pki3hack.c,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 19:12:06 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: pki3hack.c,v $ $Revision: 1.1.1.3 $ $Date: 2005-05-12 19:35:26 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
