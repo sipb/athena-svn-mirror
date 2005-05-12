9213780d98c06876ace779a75de9e929b21bab8e
Robert A Basch <rbasch@mit.edu>
2005-05-12 22:37:14 +0000

    This commit was generated by cvs2svn to compensate for changes in r21988, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21989 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/asn1/asn1t.h
+++ third/mozilla/security/nss/lib/asn1/asn1t.h
@@ -35,7 +35,7 @@
 #define ASN1T_H
 
 #ifdef DEBUG
-static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 17:21:04 $ $Name: not supported by cvs2svn $";
+static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:36:05 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/ckfw/mechanism.c
+++ third/mozilla/security/nss/lib/ckfw/mechanism.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: mechanism.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 17:16:14 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: mechanism.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:31:15 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
