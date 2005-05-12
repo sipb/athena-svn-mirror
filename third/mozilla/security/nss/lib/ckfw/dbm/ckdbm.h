086cecbc77349884be4786af99b129237b709913
Robert A Basch <rbasch@mit.edu>
2005-05-12 22:20:28 +0000

    Import mozilla 1.7.8.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21986 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/dbm/ckdbm.h
+++ third/mozilla/security/nss/lib/ckfw/dbm/ckdbm.h
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CKDBM_CVS_ID[] = "@(#) $RCSfile: ckdbm.h,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 17:03:46 $ $Name: not supported by cvs2svn $";
+static const char CKDBM_CVS_ID[] = "@(#) $RCSfile: ckdbm.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:18:47 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef CKDBM_H
