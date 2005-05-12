156983dadb97d736de77897abce07334bb69a403
Robert A Basch <rbasch@mit.edu>
2005-05-12 22:37:14 +0000

    Import mozilla 1.7.8.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21988 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/tests/pkcs11/netscape/trivial/trivial.c
+++ third/mozilla/security/nss/tests/pkcs11/netscape/trivial/trivial.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: trivial.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 17:14:25 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: trivial.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:29:26 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
