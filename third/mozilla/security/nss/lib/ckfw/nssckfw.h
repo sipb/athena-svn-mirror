d7104c8ddd33e184707353c4c082062329f0e3da
Robert A Basch <rbasch@mit.edu>
2005-05-12 19:30:41 +0000

    Import mozilla 1.7.8.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21953 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/nssckfw.h
+++ third/mozilla/security/nss/lib/ckfw/nssckfw.h
@@ -35,7 +35,7 @@
 #define NSSCKFW_H
 
 #ifdef DEBUG
-static const char NSSCKFW_CVS_ID[] = "@(#) $RCSfile: nssckfw.h,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 14:16:05 $ $Name: not supported by cvs2svn $";
+static const char NSSCKFW_CVS_ID[] = "@(#) $RCSfile: nssckfw.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 19:30:41 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
