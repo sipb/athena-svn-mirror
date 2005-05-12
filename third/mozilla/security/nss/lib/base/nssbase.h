add18ec07a934293bec7c120edacebb97b2c1554
Robert A Basch <rbasch@mit.edu>
2005-05-12 22:13:10 +0000

    This commit was generated by cvs2svn to compensate for changes in r21984, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@21985 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/base/nssbase.h
+++ third/mozilla/security/nss/lib/base/nssbase.h
@@ -35,7 +35,7 @@
 #define NSSBASE_H
 
 #ifdef DEBUG
-static const char NSSBASE_CVS_ID[] = "@(#) $RCSfile: nssbase.h,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 16:58:06 $ $Name: not supported by cvs2svn $";
+static const char NSSBASE_CVS_ID[] = "@(#) $RCSfile: nssbase.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:13:07 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/ckfw/hash.c
+++ third/mozilla/security/nss/lib/ckfw/hash.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: hash.c,v $ $Revision: 1.1.1.10 $ $Date: 2005-05-02 16:57:48 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: hash.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:12:49 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
