d3caf590d1b5ce2a32b54408c7b1bfec4e0170b3
Robert A Basch <rbasch@mit.edu>
2005-05-12 20:29:19 +0000

    Import firefox 1.0.4.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@21967 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/asn1/asn1t.h
+++ third/firefox/security/nss/lib/asn1/asn1t.h
@@ -35,7 +35,7 @@
 #define ASN1T_H
 
 #ifdef DEBUG
-static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.2 $ $Date: 2005-05-02 20:04:41 $ $Name: not supported by cvs2svn $";
+static const char ASN1T_CVS_ID[] = "@(#) $RCSfile: asn1t.h,v $ $Revision: 1.1.1.3 $ $Date: 2005-05-12 20:28:10 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
