d9966d051b97d83db8e3c729bc95377e06b84efc
Robert A Basch <rbasch@mit.edu>
2005-05-19 19:44:54 +0000

    Suggested by jhawk: Add "duplex" and "2upduplex" printer names, with appropriate corresponding print_command settings.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22002 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/modules/libpref/src/init/all.js
+++ third/firefox/modules/libpref/src/init/all.js
@@ -1544,7 +1544,9 @@ pref("applications.rlogin_with_user", "xterm -e rlogin %h -l %u");
 pref("applications.tmp_dir", "/tmp");
 // On Solaris/IRIX, this should be "lp"
 pref("print.print_command", "lpr ${MOZ_PRINTER_NAME:+'-P'}${MOZ_PRINTER_NAME}");
-pref("print.printer_list", ""); // list of printers, separated by spaces
+pref("print.printer_PostScript/duplex.print_command", "lpr -Zduplex ");
+pref("print.printer_PostScript/2upduplex.print_command", "psnup -q -n 2 | lpr -Zduplex ");
+pref("print.printer_list", "default duplex 2upduplex"); // list of printers, separated by spaces
 pref("print.print_reversed", false);
 pref("print.print_color", true);
 pref("print.print_landscape", false);
