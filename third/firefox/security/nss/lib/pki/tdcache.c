3487be6eacadc5e720081f9390ac31b7c66a6956
Robert A Basch <rbasch@mit.edu>
2005-07-26 18:41:43 +0000

    Import firefox 1.0.6.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22111 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/pki/tdcache.c
+++ third/firefox/security/nss/lib/pki/tdcache.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.3 $ $Date: 2005-05-12 18:53:26 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.4 $ $Date: 2005-07-26 18:41:43 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef PKIM_H
