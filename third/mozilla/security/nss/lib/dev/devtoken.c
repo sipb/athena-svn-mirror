c5ae500a925691ea78a299adaa91c0d18c1a45e5
Robert A Basch <rbasch@mit.edu>
2005-07-27 13:27:12 +0000

    Import mozilla 1.7.10.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22145 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/dev/devtoken.c
+++ third/mozilla/security/nss/lib/dev/devtoken.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 21:02:08 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: devtoken.c,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 13:27:12 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
