2ad746011aebd661c344ea17d1b8d10b86df0e29
Robert A Basch <rbasch@mit.edu>
2005-07-27 14:45:27 +0000

    This commit was generated by cvs2svn to compensate for changes in r22155, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22156 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/dbm/anchor.c
+++ third/mozilla/security/nss/lib/ckfw/dbm/anchor.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: anchor.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:18:47 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: anchor.c,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 14:43:46 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
--- third/mozilla/security/nss/lib/ckfw/dbm/instance.c
+++ third/mozilla/security/nss/lib/ckfw/dbm/instance.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: instance.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 22:18:48 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: instance.c,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 14:43:47 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #include "ckdbm.h"
