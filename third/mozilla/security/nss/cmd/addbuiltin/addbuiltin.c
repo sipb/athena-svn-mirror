573e0e70895e20d5747e2ee6aa76b3be4809ac02
Robert A Basch <rbasch@mit.edu>
2005-07-27 12:44:34 +0000

    This commit was generated by cvs2svn to compensate for changes in r22137, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22138 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/cmd/addbuiltin/addbuiltin.c
+++ third/mozilla/security/nss/cmd/addbuiltin/addbuiltin.c
@@ -34,7 +34,7 @@
 /*
  * Tool for converting builtin CA certs.
  *
- * $Id: addbuiltin.c,v 1.1.1.11 2005-05-12 20:13:41 rbasch Exp $
+ * $Id: addbuiltin.c,v 1.1.1.12 2005-07-27 12:38:52 rbasch Exp $
  */
 
 #include "nss.h"
@@ -194,7 +194,7 @@ printheader() {
      "# may use your version of this file under either the MPL or the\n"
      "# GPL.\n"
      "#\n"
-     "CVS_ID \"@(#) $RCSfile: addbuiltin.c,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 20:13:41 $ $Name: not supported by cvs2svn $\"\n"
+     "CVS_ID \"@(#) $RCSfile: addbuiltin.c,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 12:38:52 $ $Name: not supported by cvs2svn $\"\n"
      "\n"
      "#\n"
      "# certdata.txt\n"
--- third/mozilla/security/nss/lib/dev/ckhelper.h
+++ third/mozilla/security/nss/lib/dev/ckhelper.h
@@ -41,7 +41,7 @@
 #define CKHELPER_H
 
 #ifdef DEBUG
-static const char CKHELPER_CVS_ID[] = "@(#) $RCSfile: ckhelper.h,v $ $Revision: 1.1.1.11 $ $Date: 2005-05-12 19:55:31 $ $Name: not supported by cvs2svn $";
+static const char CKHELPER_CVS_ID[] = "@(#) $RCSfile: ckhelper.h,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 12:20:43 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKT_H
