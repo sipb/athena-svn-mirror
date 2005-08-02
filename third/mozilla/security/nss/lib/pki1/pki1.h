231ba7c43ee9bdacf0e93b92f253315b6f781c00
Robert A Basch <rbasch@mit.edu>
2005-08-02 15:22:04 +0000

    Import mozilla 1.7.11.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22199 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki1/pki1.h
+++ third/mozilla/security/nss/lib/pki1/pki1.h
@@ -35,7 +35,7 @@
 #define PKI1_H
 
 #ifdef DEBUG
-static const char PKI1_CVS_ID[] = "@(#) $RCSfile: pki1.h,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 15:15:05 $ $Name: not supported by cvs2svn $";
+static const char PKI1_CVS_ID[] = "@(#) $RCSfile: pki1.h,v $ $Revision: 1.1.1.13 $ $Date: 2005-08-02 15:22:04 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
