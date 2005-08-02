ba508467c6b8b2bdbdeb9cdaf277e0ca7e56da8b
Robert A Basch <rbasch@mit.edu>
2005-08-02 13:46:04 +0000

    Import mozilla 1.7.11.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22187 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki/tdcache.c
+++ third/mozilla/security/nss/lib/pki/tdcache.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.12 $ $Date: 2005-07-27 13:38:48 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: tdcache.c,v $ $Revision: 1.1.1.13 $ $Date: 2005-08-02 13:46:04 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef PKIM_H
