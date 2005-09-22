55f6e444d3924858045f3f9346a9cead2d2487ae
Robert A Basch <rbasch@mit.edu>
2005-09-22 15:53:16 +0000

    Import firefox 1.0.7.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22271 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/base/nssbaset.h
+++ third/firefox/security/nss/lib/base/nssbaset.h
@@ -35,7 +35,7 @@
 #define NSSBASET_H
 
 #ifdef DEBUG
-static const char NSSBASET_CVS_ID[] = "@(#) $RCSfile: nssbaset.h,v $ $Revision: 1.1.1.4 $ $Date: 2005-07-26 16:41:57 $ $Name: not supported by cvs2svn $";
+static const char NSSBASET_CVS_ID[] = "@(#) $RCSfile: nssbaset.h,v $ $Revision: 1.1.1.5 $ $Date: 2005-09-22 15:48:46 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 /*
