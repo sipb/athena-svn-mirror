c22b7843bd411d303ee2280d37097b74620dcfc9
Robert A Basch <rbasch@mit.edu>
2005-09-22 18:36:16 +0000

    This commit was generated by cvs2svn to compensate for changes in r22301, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22302 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/pki/cryptocontext.c
+++ third/mozilla/security/nss/lib/pki/cryptocontext.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: cryptocontext.c,v $ $Revision: 1.1.1.13 $ $Date: 2005-08-02 12:47:44 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: cryptocontext.c,v $ $Revision: 1.1.1.14 $ $Date: 2005-09-22 18:32:10 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef DEV_H
