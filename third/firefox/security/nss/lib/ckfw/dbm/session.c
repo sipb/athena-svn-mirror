677e9ee00fea51900dae3566f28ef663cb954395
Robert A Basch <rbasch@mit.edu>
2005-09-22 19:07:22 +0000

    Import firefox 1.0.7.
    git-svn-id: svn+ssh://svn.mit.edu/athena/branches/vendor@22311 728af825-273c-0410-89f9-f7d3b574a069

--- third/firefox/security/nss/lib/ckfw/dbm/session.c
+++ third/firefox/security/nss/lib/ckfw/dbm/session.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.4 $ $Date: 2005-07-26 19:58:52 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: session.c,v $ $Revision: 1.1.1.5 $ $Date: 2005-09-22 19:05:43 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #include "ckdbm.h"
