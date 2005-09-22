a8dbffb2d8efff30ffd470547b571df48c9fb63d
Robert A Basch <rbasch@mit.edu>
2005-09-22 19:11:16 +0000

    This commit was generated by cvs2svn to compensate for changes in r22313, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22314 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/dev/devslot.c
+++ third/mozilla/security/nss/lib/dev/devslot.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: devslot.c,v $ $Revision: 1.1.1.13 $ $Date: 2005-08-02 13:26:52 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: devslot.c,v $ $Revision: 1.1.1.14 $ $Date: 2005-09-22 19:11:16 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #ifndef NSSCKEPV_H
