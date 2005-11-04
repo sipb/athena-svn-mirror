fb5e7fb0057276d4b1e226204fec550d0741fbca
Greg Hudson <ghudson@mit.edu>
2005-11-04 17:27:10 +0000

    Prevent menu cache coherency problems when updating to a new full release.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22351 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/local-menus.sh
+++ packs/maint/local-menus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: local-menus.sh,v 1.4 2003-10-23 23:11:49 ghudson Exp $
+# $Id: local-menus.sh,v 1.5 2005-11-04 17:27:10 ghudson Exp $
 
 # Tar file containing menus (a symlink into the AFS system config area).
 menutar=/usr/athena/share/gnome/athena/menus.tar
@@ -10,6 +10,9 @@ menudir=/usr/athena/share/gnome/athena/menus
 
 # Local copy of the menu tar file.
 localtar=/var/athena/menus.tar
+
+# Record of the release version of the last menu update
+versionfile=/var/athena/menus.version
 
 # Local copy of the menu directory (made from the tar file, not from
 # $menudir).
@@ -30,11 +33,18 @@ if [ ! -s $menutar ]; then
   fail
 fi
 
+oldversion=none
+newversion=`/bin/athena/machtype -A`
+if [ -s $versionfile ]; then
+  oldversion=`cat $versionfile`
+fi
+
 # Nothing to do if $localtar is up to date.  (Check the $menutar
 # symlink as well as what it points to; after a full release, the
 # target of the symlink might not be newer than the last time we
 # updated, but the symlink itself will be newer.)
-if [ -s $localtar ] && \
+if [ "x$oldversion" = "x$newversion" ] && \
+   [ -s $localtar ] && \
    [ "`find $menutar -newer $localtar`" != $menutar ] && \
    [ "`find $menutar -follow -newer $localtar`" != $menutar ]; then
   exit 0
@@ -48,3 +58,4 @@ mkdir $localdir || fail
 chmod -R a+rX $localdir || fail
 rm -rf $menus
 ln -s $localdir $menus
+echo "$newversion" > $versionfile
