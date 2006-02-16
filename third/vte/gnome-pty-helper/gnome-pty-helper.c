968ad5ed452fbce1879c063f28f7b3d7156a0705
Robert A Basch <rbasch@mit.edu>
2006-02-16 20:25:54 +0000

    Apply patches from upstream to shut down cleanly (including updating utmp) upon receiving SIGTERM or SIGHUP.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22386 728af825-273c-0410-89f9-f7d3b574a069

--- third/vte/gnome-pty-helper/gnome-pty-helper.c
+++ third/vte/gnome-pty-helper/gnome-pty-helper.c
@@ -98,7 +98,7 @@ static pty_info *pty_list;
 static struct cmsghdr *cmptr;
 
 static int
-init_msg_pass ()
+init_msg_pass (void)
 {
 	cmptr = malloc (CONTROLLEN);
 
@@ -654,6 +654,15 @@ sanity_checks (void)
 	}
 }
 
+static gboolean done;
+
+static void
+exit_handler (int signum)
+{
+	done = TRUE;
+}
+
+
 int
 main (int argc, char *argv [])
 {
@@ -675,16 +684,21 @@ main (int argc, char *argv [])
 	if (!display_name)
 		display_name = "localhost";
 
+	done = FALSE;
+
+	/* Make sure we clean up utmp/wtmp even under vncserver */
+	signal (SIGHUP, exit_handler);
+	signal (SIGTERM, exit_handler);
 
 	if (init_msg_pass () == -1)
 		exit (1);
 
-	for (;;) {
+	while (!done) {
 		res = n_read (STDIN_FILENO, &op, sizeof (op));
 
 		if (res != sizeof (op)) {
-			shutdown_helper ();
-			return 1;
+			done = TRUE;
+			continue;
 		}
 
 		switch (op) {
@@ -727,7 +741,7 @@ main (int argc, char *argv [])
 			n = n_read (STDIN_FILENO, &tag, sizeof (tag));
 			if (n != sizeof (tag)) {
 				shutdown_helper ();
-				return 1;
+				exit (1);
 			}
 			close_pty_pair (tag);
 			break;
@@ -735,6 +749,7 @@ main (int argc, char *argv [])
 
 	}
 
+	shutdown_helper ();
 	return 0;
 }
 
