3701e675e3e4a71e0918736e3815dbf7a4e2d95d
Robert A Basch <rbasch@mit.edu>
2006-05-30 19:30:30 +0000

    This commit was generated by cvs2svn to compensate for changes in r22485, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22486 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/ckfw/dbm/slot.c
+++ third/mozilla/security/nss/lib/ckfw/dbm/slot.c
@@ -32,7 +32,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: slot.c,v $ $Revision: 1.1.1.14 $ $Date: 2005-09-22 20:35:38 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: slot.c,v $ $Revision: 1.1.1.15 $ $Date: 2006-05-30 19:28:51 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #include "ckdbm.h"
