3e9ce34364eaae6ec47aecff3cbdb6a2ae273541
Robert A Basch <rbasch@mit.edu>
2006-05-30 18:30:15 +0000

    This commit was generated by cvs2svn to compensate for changes in r22479, which included commits to RCS files with non-trunk default branches.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22480 728af825-273c-0410-89f9-f7d3b574a069

--- third/mozilla/security/nss/lib/util/utf8.c
+++ third/mozilla/security/nss/lib/util/utf8.c
@@ -33,7 +33,7 @@
  */
 
 #ifdef DEBUG
-static const char CVS_ID[] = "@(#) $RCSfile: utf8.c,v $ $Revision: 1.1.1.14 $ $Date: 2005-09-22 19:37:26 $ $Name: not supported by cvs2svn $";
+static const char CVS_ID[] = "@(#) $RCSfile: utf8.c,v $ $Revision: 1.1.1.15 $ $Date: 2006-05-30 18:30:15 $ $Name: not supported by cvs2svn $";
 #endif /* DEBUG */
 
 #include "seccomon.h"
