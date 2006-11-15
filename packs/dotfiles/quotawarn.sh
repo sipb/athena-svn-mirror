d002ab7b99282128c387154db0d7790fc73ece7a
Greg Hudson <ghudson@mit.edu>
2006-11-15 21:28:42 +0000

    Use info dialogs instead of warnings, so that the buttons make sense.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22565 728af825-273c-0410-89f9-f7d3b574a069

--- packs/dotfiles/quotawarn.sh
+++ packs/dotfiles/quotawarn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: quotawarn.sh,v 1.2 2006-08-21 14:16:02 jweiss Exp $
+# $Id: quotawarn.sh,v 1.3 2006-11-15 21:28:42 ghudson Exp $
 
 # Determine the user's home directory usage and quota.
 qline=`quota -v -f "$USER" | awk '/^\// {print}'`
@@ -18,7 +18,7 @@ useful to identify unneeded files:
 
   athrun consult helpquota"
 elif [ -n "$usage" -a -n "$quota90" -a "$usage" -ge "$quota90" ]; then
-  zenity --warning --text="
+  zenity --info --text="
 Your home directory usage is near your quota (${usage}KB
 used out of ${quota}KB).  Consider removing unneeded
 files to free up space.  You may find the following
@@ -34,7 +34,7 @@ quota=`echo "$qline" | awk '{print $3}'`
 quota90=`expr "$quota" \* 9 / 10`
 
 if [ -n "$usage" -a -n "$quota90" -a "$usage" -ge "$quota90" ]; then
-  zenity --warning --text="
+  zenity --info --text="
 Your MIT mail usage is close to or exceeding your mail
 quota (${usage}KB used out of ${quota}KB).  Consider
 removing unneeded mail messages to free up space.  You
