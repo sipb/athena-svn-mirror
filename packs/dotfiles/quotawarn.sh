339e36fa2b3a897ba7b0451d81da26718b920e67
Greg Hudson <ghudson@mit.edu>
2007-04-09 17:13:19 +0000

    Prevent some warnings which can crop up if a server is down.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22631 728af825-273c-0410-89f9-f7d3b574a069

--- packs/dotfiles/quotawarn.sh
+++ packs/dotfiles/quotawarn.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# $Id: quotawarn.sh,v 1.3 2006-11-15 21:28:42 ghudson Exp $
+# $Id: quotawarn.sh,v 1.4 2007-04-09 17:13:19 ghudson Exp $
 
 # Determine the user's home directory usage and quota.
 qline=`quota -v -f "$USER" | awk '/^\// {print}'`
 usage=`echo "$qline" | awk '{print $2}'`
 quota=`echo "$qline" | awk '{print $3}'`
-quota90=`expr "$quota" \* 9 / 10`
+quota90=`expr "${quota:-0}" \* 9 / 10`
 
-if [ -n "$usage" -a -n "$quota" -a "$usage" -ge "$quota" ]; then
+if [ -n "$usage" -a -n "$quota" ] && [ "$usage" -ge "$quota" ]; then
   zenity --error --text="
 Your home directory usage exceeds your quota (${usage}KB
 used out of ${quota}KB).  You will be unable to use
@@ -17,7 +17,7 @@ unneeded files.  You may find the following command
 useful to identify unneeded files:
 
   athrun consult helpquota"
-elif [ -n "$usage" -a -n "$quota90" -a "$usage" -ge "$quota90" ]; then
+elif [ -n "$usage" -a -n "$quota90" ] && [ "$usage" -ge "$quota90" ]; then
   zenity --info --text="
 Your home directory usage is near your quota (${usage}KB
 used out of ${quota}KB).  Consider removing unneeded
@@ -31,9 +31,9 @@ fi
 qline=`mailquota | tail +2`
 usage=`echo "$qline" | awk '{print $2}'`
 quota=`echo "$qline" | awk '{print $3}'`
-quota90=`expr "$quota" \* 9 / 10`
+quota90=`expr "${quota:-0}" \* 9 / 10`
 
-if [ -n "$usage" -a -n "$quota90" -a "$usage" -ge "$quota90" ]; then
+if [ -n "$usage" -a -n "$quota90" ] && [ "$usage" -ge "$quota90" ]; then
   zenity --info --text="
 Your MIT mail usage is close to or exceeding your mail
 quota (${usage}KB used out of ${quota}KB).  Consider
