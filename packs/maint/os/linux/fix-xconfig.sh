2a94a3de030ab6426432dca318e55bf09bedf901
andrew m. boardman <amb@mit.edu>
2007-08-09 19:47:19 +0000

    Properly turn on DontZap if a ServerFlags section already exists. Remove permissions entry for DRI.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22655 728af825-273c-0410-89f9-f7d3b574a069

--- packs/maint/os/linux/fix-xconfig.sh
+++ packs/maint/os/linux/fix-xconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: fix-xconfig.sh,v 1.5 2006-05-15 20:09:22 amb Exp $
+# $Id: fix-xconfig.sh,v 1.6 2007-08-09 19:47:19 amb Exp $
 
 # fix-xconfig: Modify the X configuration to match timings retrieved
 # from read-edid and to have the desired maximum resolution.  Run from
@@ -61,4 +61,23 @@ Section "ServerFlags"
 	Option "DontZap"  "on"
 EndSection
 EOF
+else if ! grep -qi "^${optwsp}Option${wsp}\"DontZap\"${optwsp}\"off\"$" $cfg
+  then awk 'BEGIN {sflags=0;} 
+	/Section.*"ServerFlags"/ { sflags=1; dz=0; }
+	/Option.*DontZap/ { gsub("off","on"); dz=1; }
+	/EndSection/ {if (sflags==1 && dz==0)
+	    print "\tOption\t\"DontZap\"\t\"on\""; sflags=0;}
+	{ print $0; }
+	' < $cfg > $cfg.new
+  mv -f $cfg.new $cfg
+fi
+
+# Comment out "Group 0" entry for DRI permissions if present.
+if grep -qi "^${optwsp}Group${wsp}0${optwsp}$" $cfg ; then
+  awk 'BEGIN {dri=0;} 
+	/Section.*"DRI"/ { dri=1; }
+	/Group/ { if (dri==1) gsub("^","#");}
+	/EndSection/ { dri=0; }
+	{ print $0; } ' < $cfg > $cfg.new
+  if cmp $cfg $cfg.new ; then rm $cfg.new ; else mv -f $cfg.new $cfg; fi
 fi
