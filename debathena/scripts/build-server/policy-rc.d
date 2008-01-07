b89227377f4827b308fd4a76320cc9d8bee75902
Greg Hudson <ghudson@mit.edu>
2008-01-07 18:12:33 +0000

    Document build server scripts.
    * debathena/scripts/build-server/policy-rc.d,
      debathena/scripts/build-server/make-chroot: Comment.
    
    
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22692 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/scripts/build-server/policy-rc.d
+++ debathena/scripts/build-server/policy-rc.d
@@ -1,2 +1,6 @@
 #!/bin/sh
+# This script gets installed in /usr/sbin/policy-rc.d inside template
+# chroots.  By exiting 101 ("action not allowed") we suppress any init
+# script from starting any daemon from within the chroot.  This is
+# standard practice in Debian chroot environments.
 exit 101
