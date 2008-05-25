71503c2bc82a9fc89942a2c3977a7a1234349e87
Timothy G Abbott <tabbott@mit.edu>
2008-05-25 07:03:27 +0000

    Use the new --apt-update feature of schroot 1.20 to simplify sbuildhack.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@22978 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/scripts/SbuildHack.pm
+++ debathena/scripts/SbuildHack.pm
@@ -1,7 +1,6 @@
 package SbuildHack;
 
 use Sbuild qw(binNMU_version);
-use Sbuild::Chroot qw(begin_session);
 use Fcntl qw(:flock);
 
 sub new_binNMU_version {
@@ -12,31 +11,9 @@ sub new_binNMU_version {
     return $v . $ENV{"NMUTAG"};
 };
 
-*old_begin_session = \&Sbuild::Chroot::begin_session;
-
-sub new_begin_session {
-    open(APTLOCK, ">/tmp/debathena-repository-lock");
-    flock(APTLOCK, LOCK_SH);
-    my $r = old_begin_session(@_);
-    if (!open(PIPE, Sbuild::Chroot::get_apt_command("$conf::apt_get", "-q update", "root", 1) . " 2>&1 |")) {
-	print PLOG "Can't open pipe to apt-get: $!\n";
-	return 0;
-    }
-    while(<PIPE>) {
-	print PLOG;
-	print STDERR;
-    }
-    close(PIPE);
-    print PLOG "apt-get update failed\n" if $?;
-    flock(APTLOCK, LOCK_UN);
-    close(APTLOCK);
-    return $r;
-}
-
 {
     no warnings 'redefine';
     *Sbuild::binNMU_version = \&new_binNMU_version;
-    *Sbuild::Chroot::begin_session = \&new_begin_session;
 }
 
 1;
