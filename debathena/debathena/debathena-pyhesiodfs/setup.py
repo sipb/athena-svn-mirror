b3ca49fb51e2cc76b6d00d69b368c4db5a52c82e
Evan Broder <broder@mit.edu>
2008-08-20 05:31:19 +0000

    Update pyhesiodfs to new version using python-hesiod
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@23151 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/debathena-pyhesiodfs/setup.py
+++ debathena/debathena/debathena-pyhesiodfs/setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
+import sys
+
+extra_options = {}
+
+if sys.platform == 'darwin':
+    extra_options['data_files'] = [('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))]
+
 setup(name='pyHesiodFS',
       version='1.0',
       author='Quentin Smith',
       author_email='pyhesiodfs@mit.edu',
-      py_modules=['hesiod'],
       scripts=['pyHesiodFS.py'],
-      data_files=[('/Library/LaunchDaemons', ('edu.mit.sipb.mit-automounter.plist',))])
+      requires=['PyHesiod (>=0.2.0)'],
+      **extra_options)
