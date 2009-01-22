113afad48f34b0b3cfe6af1b080a48c2947541a0
Evan Broder <broder@mit.edu>
2009-01-22 01:41:22 +0000

    In auto-update:   * Run the auto-update cron job over 2 hours, instead of 30 minutes, and     desyncronize it over the full 2 hours.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@23284 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -2,4 +2,4 @@ SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 #min  hour  mday  mon  wday  user  command
-0,30  *     *     *    *     root  desync 1800; athena-auto-update cron
+0     */2   *     *    *     root  desync 7200; athena-auto-update cron
