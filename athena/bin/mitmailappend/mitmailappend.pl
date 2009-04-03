300794bea188eb35b0b931f2db10ed3de230ddfb
Evan Broder <broder@mit.edu>
2009-04-03 01:10:19 +0000

    In mitmailappend:   * Try $ATHENA_USER before using $USER.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@23695 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/mitmailappend/mitmailappend.pl
+++ athena/bin/mitmailappend/mitmailappend.pl
@@ -50,7 +50,7 @@ $opt_mailbox = "INBOX" unless $opt_mailbox;
 # By default we read the message from standard input.
 $opt_file = "-" unless $opt_file;
 
-my $username = $ENV{'USER'} || getlogin || (getpwuid($<))[0] ||
+my $username = $ENV{'ATHENA_USER'} || $ENV{'USER'} || getlogin || (getpwuid($<))[0] ||
     errorout "Cannot determine user name";
 
 unless ($opt_host) {
