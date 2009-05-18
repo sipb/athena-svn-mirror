286cce890d2c7794df4024826d10b832c19d295b
Evan Broder <broder@mit.edu>
2009-05-18 18:33:40 +0000

    Add workstation to the installer as a valid flavor.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@23797 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/scripts/installer/install-debathena.sh
+++ debathena/scripts/installer/install-debathena.sh
@@ -43,6 +43,8 @@ echo ""
 echo "  standard:        Athena client software (e.g. discuss) and customizations"
 echo "  login:           Allow Athena users to log into your machine"
 echo "  login-graphical: Athena graphical login customizations"
+echo "  workstation:     Managed graphical workstation with minimal need for"
+echo "                   manual maintenance"
 echo ""
 
 # Hack to deal with the older PXE installer (which used a simple flag file to
@@ -60,7 +62,8 @@ if test -f /root/pxe-install-flag ; then
   fi
 fi
 while [ standard != "$category" -a login != "$category" -a \
-        login-graphical != "$category" -a cluster != "$category" ]; do
+        login-graphical != "$category" -a workstation != "$category" -a \
+        cluster != "$category" ]; do
   output -n "Please choose a category or press control-C to abort: "
   read category
 done
