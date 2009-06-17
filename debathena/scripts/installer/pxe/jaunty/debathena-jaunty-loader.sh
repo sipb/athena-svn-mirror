236c0bdd92ad6e310eccd8efa1bf0c9b28e5ddbe
Geoffrey Thomas <geofft@mit.edu>
2009-06-17 07:52:10 +0000

    Missed one s/intrepid/jaunty/
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@23860 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/scripts/installer/pxe/jaunty/debathena-jaunty-loader.sh
+++ debathena/scripts/installer/pxe/jaunty/debathena-jaunty-loader.sh
@@ -4,7 +4,7 @@
 # 18.9.60.73 = athena10.mit.edu (formerly 18.92.2.195)
 
 cd /
-wget http://18.9.60.73/installer/intrepid/debathena-jaunty.tar.gz > /dev/tty5 2>&1
+wget http://18.9.60.73/installer/jaunty/debathena-jaunty.tar.gz > /dev/tty5 2>&1
 tar xzf debathena-jaunty.tar.gz
 chvt 5
 sh debathena-jaunty/installer.sh < /dev/tty5 > /dev/tty5 2>&1
