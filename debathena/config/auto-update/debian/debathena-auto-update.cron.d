783fb00b3fc5d0219a3c5395538c08fd20725eeb
Geoffrey Thomas <geofft@mit.edu>
2009-11-09 02:48:56 +0000

    In auto-update:   * Slow updates to a 6-hour (desync'd) schedule during waking hours.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24152 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -1,5 +1,6 @@
 SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-#min  hour  mday  mon  wday  user  command
-0     */2   *     *    *     root  desync 7200; athena-auto-update cron
+#min  hour      mday  mon  wday  user  command
+0     2,4,6     *     *    *     root  desync 7200;  athena-auto-update cron
+0     8,14,20   *     *    *     root  desync 21600; athena-auto-update cron
