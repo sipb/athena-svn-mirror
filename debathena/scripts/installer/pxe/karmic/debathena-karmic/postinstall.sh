eddf1228a932af62b2a8ba97cd0a8a62e13d56c9
andrew m. boardman <amb@mit.edu>
2010-01-04 01:43:35 +0000

    A PXE installer tree for karmic, starting from the jaunty version.
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24248 728af825-273c-0410-89f9-f7d3b574a069

---
+++ debathena/scripts/installer/pxe/karmic/debathena-karmic/postinstall.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This is only invoked when the relevant preseed entry is passed in
+# during the preinstall questioning.  (Thus, not for vanilla installs.)
+
+cp /debathena-jaunty/preseed /target/root/debathena.preseed
+cp /debathena-jaunty/install-debathena.sh /target/root
+if test -f /debathena-jaunty/pxe-install-flag ; then
+  cp /debathena-jaunty/pxe-install-flag /target/root/pxe-install-flag
+fi
+
+. /lib/chroot-setup.sh
+
+chroot /target dpkg-divert --rename --add /usr/sbin/policy-rc.d
+if ! chroot_setup; then
+    logger -t postinstall.sh -- "Target system not usable. Can't install Debathena."
+    exit 1
+fi
+
+chvt 5
+chroot /target sh /root/install-debathena.sh < /dev/tty5 > /dev/tty5 2>&1
+# This approach fails due to lingering processes keeping the
+# pipeline open and the script hung.
+# chroot /target sh /root/install-debathena.sh < /dev/tty5 2>&1 \
+#     | chroot /target tee /var/log/athena-install.log > /dev/tty5
+sleep 5
+
+chroot_cleanup
+chroot /target dpkg-divert --rename --remove /usr/sbin/policy-rc.d
+
+chvt 1
