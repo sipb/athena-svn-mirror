1382e09e3c9222911ee7a54cde154e6ebc273879
Jonathan D Reed <jdreed@mit.edu>
2010-04-21 20:58:23 +0000

    In auto-update:   * Use at(1) instead of desync (Trac #309)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24500 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -2,5 +2,5 @@ SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 #min  hour      mday  mon  wday  user  command
-0     2,4,6     *     *    *     root  desync -t /var/lib/desync-athena-update 7200 && athena-auto-update cron || exit 0
-0     8,14,20   *     *    *     root  desync -t /var/lib/desync-athena-update 21600 && athena-auto-update cron || exit 0
+0     2,4,6     *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 120) minutes"
+0     8,14,20   *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 360) minutes"
