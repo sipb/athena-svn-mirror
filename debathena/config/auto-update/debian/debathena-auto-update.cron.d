77d41716fc658e99c2dffcf2e504b2a663e8de7e
Jonathan D Reed <jdreed@mit.edu>
2010-04-21 20:17:32 +0000

    In auto-update:   * Use desync -t instead of sleeping (Trac #309)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24499 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -2,5 +2,5 @@ SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 #min  hour      mday  mon  wday  user  command
-0     2,4,6     *     *    *     root  desync 7200;  athena-auto-update cron
-0     8,14,20   *     *    *     root  desync 21600; athena-auto-update cron
+0     2,4,6     *     *    *     root  desync -t /var/lib/desync-athena-update 7200 && athena-auto-update cron || exit 0
+0     8,14,20   *     *    *     root  desync -t /var/lib/desync-athena-update 21600 && athena-auto-update cron || exit 0
