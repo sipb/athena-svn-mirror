ca17017145039cf63722fe376c3025ddf3d50f82
Jonathan D Reed <jdreed@mit.edu>
2010-08-04 15:22:21 +0000

    First draft of upgrade script
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24797 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -4,3 +4,4 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 #min  hour      mday  mon  wday  user  command
 0     2,4,6     *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 120) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
 0     8,14,20   *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 360) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
+
