e6630358eb7700b794a2eaa97a400be69847a8a2
Jonathan D Reed <jdreed@mit.edu>
2011-01-25 17:43:54 +0000

    In auto-update:   * Don't depend on at, because bad things happen (Trac #782)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24926 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -1,8 +1,7 @@
 SHELL=/bin/sh
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-#min  hour      mday  mon  wday  user  command
-0     2,4,6     *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 120) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
-0     8,14,20   *     *    *     root  echo "/usr/sbin/athena-auto-update cron" | at "now + $(desync -n 360) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
-0     2		*     *	   *	 root  echo "/usr/sbin/athena-auto-upgrade" | at "now + $(desync -n 360) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
+#min           hour  mday  mon  wday  user  command
+0,15,30,45     *     *     *    *     root  /usr/sbin/athena-auto-update cron
+0              2     *     *    *     root  /usr/sbin/athena-auto-upgrade
 
