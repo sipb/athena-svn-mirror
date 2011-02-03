f49b31ffadb9032c0a3909d49b7986916313e50a
Jonathan D Reed <jdreed@mit.edu>
2011-02-03 17:51:47 +0000

    Don't inadvertantly disable auto-upgrades
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24933 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++ debathena/config/auto-update/debian/debathena-auto-update.cron.d
@@ -3,5 +3,6 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 #min           hour  mday  mon  wday  user  command
 0,15,30,45     *     *     *    *     root  /usr/sbin/athena-auto-update cron
-0              2     *     *    *     root  /usr/sbin/athena-auto-upgrade
+0     	       2     *     *    *     root  echo "/usr/sbin/athena-auto-upgrade" | at "now + $(desync -n 360) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
+
 
