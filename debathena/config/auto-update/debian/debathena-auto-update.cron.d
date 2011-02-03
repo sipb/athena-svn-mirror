94e4c75caaeeaa26daec0bb90b4d14c70fe03bd7
Jonathan D Reed <jdreed@mit.edu>
2011-02-03 19:42:43 +0000

    In auto-update:   * Generate crontabs at configure time   * Run an update at boot time if it's been too long since the last one
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24935 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/auto-update/debian/debathena-auto-update.cron.d
+++
@@ -1,8 +0,0 @@
-SHELL=/bin/sh
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-
-#min           hour  mday  mon  wday  user  command
-0,15,30,45     *     *     *    *     root  /usr/sbin/athena-auto-update cron
-0     	       2     *     *    *     root  echo "/usr/sbin/athena-auto-upgrade" | at "now + $(desync -n 360) minutes" 2>&1 | egrep -v "(job [0-9]+ at)|(^warning: commands will be executed using /bin/sh)"
-
-
