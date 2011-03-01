0a73e9675f9644827b4891a0b52da3710bb50264
Jonathan D Reed <jdreed@mit.edu>
2011-03-01 19:04:14 +0000

    In xsession:   * Inform the user that they will lose if there's no AFS
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@24990 728af825-273c-0410-89f9-f7d3b574a069

---
+++ debathena/debathena/xsession/debian/debathena-check-afs.sh
@@ -0,0 +1,6 @@
+if [ "$DEBATHENA_HOME_TYPE" = "afs" ]; then
+    if [ ! -d $HOME ]; then
+	zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation, or a different cluster.\n\nIf the problem appears to only be on this workstation, please notify hotline@mit.edu and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
+	exit 0
+    fi
+fi
