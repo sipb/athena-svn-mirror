17f4999860d130836af20334bd34e349f5b6bdb4
Jonathan D Reed <jdreed@mit.edu>
2011-03-09 00:27:29 +0000

    In xsession:   * Fix broken line continuation in gdm-sucks and check-afs Xsession.d     scripts   * Make check_afs more robust to libpam_debathena_home_type suckage
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25007 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-check-afs.sh
+++ debathena/debathena/xsession/debian/debathena-check-afs.sh
@@ -1,6 +1,16 @@
+youlose() {
+    zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation, or a different cluster.\n\nIf the problem appears to only be on this workstation, please notify hotline@mit.edu and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
+    exit 0
+}
 if [ "$DEBATHENA_HOME_TYPE" = "afs" ]; then
-    if ! [ -d "$HOME" ]; then
-	zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation, or a different cluster.\n\nIf the problem appears to only be on this workstation, please notify hotline@mit.edu and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
-	exit 0
+    [ -d "$HOME" ] || youlose
+elif [ "$DEBATHENA_HOME_TYPE" = "local" ] && echo "$(id -Gn | sed -e 's/ /\n/g')" | grep -q ^nss-nonlocal-users$; then
+    # DEBATHENA_HOME_TYPE=local if AFS is not running, awesome
+    # This code can get removed when Trac #838 is fixed
+    REALHOME=$HOME
+    [ -h $HOME ] && REALHOME=$(readlink $HOME)
+    if echo $REALHOME | grep -q ^/afs; then
+	fs whichcell $REALHOME || youlose
     fi
 fi
+	
