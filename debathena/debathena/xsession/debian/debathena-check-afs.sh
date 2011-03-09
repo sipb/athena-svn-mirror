ff5ede3a18e3ad191a3303a13bad1b7efbc8aa53
Jonathan D Reed <jdreed@mit.edu>
2011-03-09 20:40:02 +0000

    In xsession:   * Incoprorate new DEBATHENA_HOME_TYPE values into check-afs.sh and     remove hack
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25009 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-check-afs.sh
+++ debathena/debathena/xsession/debian/debathena-check-afs.sh
@@ -2,15 +2,20 @@ youlose() {
     zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation, or a different cluster.\n\nIf the problem appears to only be on this workstation, please notify hotline@mit.edu and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
     exit 0
 }
-if [ "$DEBATHENA_HOME_TYPE" = "afs" ]; then
-    [ -d "$HOME" ] || youlose
-elif [ "$DEBATHENA_HOME_TYPE" = "local" ] && echo "$(id -Gn | sed -e 's/ /\n/g')" | grep -q ^nss-nonlocal-users$; then
-    # DEBATHENA_HOME_TYPE=local if AFS is not running, awesome
-    # This code can get removed when Trac #838 is fixed
-    REALHOME=$HOME
-    [ -h $HOME ] && REALHOME=$(readlink $HOME)
-    if echo $REALHOME | grep -q ^/afs; then
-	fs whichcell $REALHOME || youlose
+youmightlose() {
+    if ! zenity --question --text="Your home directory type could not be determined.  This might indicate problems with the AFS servers, or you might have an error in your AFS permissions.\n\nIf you know what you're doing, it's possible that this error is benign and you can continue.  However, it's more than likely that this login session will fail and you'll need to reboot the workstation to log out.  With that in mind, would you like to continue?"; then
+	exit 0
     fi
-fi
+}
+case "$DEBATHENA_HOME_TYPE" in
+    missing)
+	youlose
+	;;
+    unknown)
+	youmightlose
+	;;
+    afs)
+	[ -d "$HOME" ] || youlose
+	;;
+esac
 	
