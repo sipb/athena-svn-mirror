f892999a2e6d62768efb570b1a6f22ad4fd0c65b
Jonathan D Reed <jdreed@mit.edu>
2011-03-10 23:30:20 +0000

    In xsession:   * debathena-homedir-mode now offers to run the command for you (Trac     #387)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25016 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-homedir-mode.sh
+++ debathena/debathena/xsession/debian/debathena-homedir-mode.sh
@@ -3,9 +3,14 @@
 if [ afs = "$DEBATHENA_HOME_TYPE" ] && \
   stat -L --format='%A' "$HOME" | egrep -q '^(.....w|........w)'; then
   hm=$(stat -L --format='%a' "$HOME")
-  message "Your home directory is mode $hm, probably because it was created" \
-    "a long time ago.  You likely received a spurious error dialog about" \
-    ".dmrc as a result of the mode bits on your home directory.  To fix this" \
-    "condition, you can run: chmod 755 \$HOME"
+  if zenity --question --text="Your home directory is mode $hm, probably \
+because it was created a long time ago.  You likely received a spurious \
+error dialog about .dmrc as a result of the mode bits on your home \
+directory.  To fix this condition, you can run: 
+        chmod 755 \$HOME 
+
+Would you like to run this command now?"; then
+      chmod 755 $HOME
+  fi
   unset hm
 fi
