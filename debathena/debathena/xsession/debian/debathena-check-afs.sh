70cc8ceae2ea48ae5a78bc11b38ccf77448fbb39
Jonathan D Reed <jdreed@mit.edu>
2011-08-10 22:54:05 +0000

    In xsession:   * Don't tell private workstation owners to contact hotline (Trac: #998)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25370 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-check-afs.sh
+++ debathena/debathena/xsession/debian/debathena-check-afs.sh
@@ -1,5 +1,7 @@
 youlose() {
-    zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation, or a different cluster.\n\nIf the problem appears to only be on this workstation, please notify hotline@mit.edu and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
+    notify="hotline@mit.edu"
+    [ "$(machtype -L)" != "debathena-cluster" ] && notify="your system administrator"
+    zenity --warning --text="Your AFS home directory appears to be unavailable, and this login session cannot continue.\n\nThis could be due to an AFS outage, a network problem, or an issue with this specific workstation.  We recommend you try another workstation.\n\nIf the problem appears to only be on this workstation, please notify $notify and mention this machine's hostname ($(hostname -f)).  If the problem persists on multiple workstations, please contact the IS&amp;T Helpdesk (helpdesk@mit.edu or x3-1101)."
     exit 0
 }
 youmightlose() {
