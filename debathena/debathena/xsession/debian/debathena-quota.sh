3409320130c14aff2835f91e0146f23e40701799
Jonathan D Reed <jdreed@mit.edu>
2011-08-18 00:41:50 +0000

    In xsession:   * Be clearer about what the first quota warning means (Trac: #748)
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25384 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-quota.sh
+++ debathena/debathena/xsession/debian/debathena-quota.sh
@@ -1,6 +1,6 @@
 if [ "$DEBATHENA_HOME_TYPE" = "afs" ]; then
     QUOTAPCT=$(fs lq $HOME | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')
     if [ $QUOTAPCT -ge 100 ]; then
-	message "You have exceeded your AFS quota.  This login session will likely fail.\n\nTry using the 'Athena TTY Session' login session to get a tty-mode login session, and then remove or compress some files to get below your quota.\n\nYou may also request a quota increase from User Accounts by calling x3-1325."
+	message "You have exceeded (or are within 0.5% of) your AFS quota.  This login session will likely fail.\n\nIf this login session does fail, try using the 'Athena TTY Session' login session to get a tty-mode login session, and then remove or compress some files to get below your quota.\n\nYou may also request a quota increase from User Accounts by calling x3-1325."
     fi
 fi
