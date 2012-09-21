9b86ccb97e0a3e42a7f965f1a939744d8978ee75
Jonathan D Reed <jdreed@mit.edu>
2012-09-21 18:37:52 +0000

    In shell-config:   * Don't source the now non-existent original undiverted profile
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25770 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/config/shell-config/debian/profile.debathena.sh
+++ debathena/config/shell-config/debian/profile.debathena.sh
@@ -1,21 +1,21 @@
-# This is the Debathena /etc/profile configuration, installed by
+# This is the Debathena /etc/profile.d configuration, installed by
 # debathena-bash-config.
 
 DEBATHENA_PROFILE_DIR=/usr/share/debathena-bash-config/profile.d
 DEBATHENA_BASHRC_DIR=/usr/share/debathena-bash-config/bashrc.d
 
-# First source the normal profile. Sometimes this sources bash.bashrc
-# (e.g., on Ubuntu); sometimes it doesn't. Let's find out!
-debathena_test_if_bashrc_runs=0
-. /etc/profile.debathena-orig
+# /etc/profile has been sourced at this point (in fact, we're called
+# as the last step in it) 
+# On Ubuntu, this will run /etc/bash.bashrc.  Our bash.bashrc sets a
+# variable, so we'll know if that happened.
 
 # If it didn't, source all bash scripts in our bashrc.d directory.
 if [ -n "$PS1" ] && [ -n "$BASH" ]; then
-    if [ "$debathena_test_if_bashrc_runs" = 0 ]; then
+    if [ "${debathena_bashrc_sourced:-x}" != 1 ]; then
         for i in `run-parts --list "$DEBATHENA_BASHRC_DIR"`; do . "$i"; done
     fi
 fi
-unset debathena_test_if_bashrc_runs
+unset debathena_bashrc_sourced
 
 # Finally, source all bash scripts in our profile.d directory.
 for i in `run-parts --list "$DEBATHENA_PROFILE_DIR"`; do . "$i"; done
