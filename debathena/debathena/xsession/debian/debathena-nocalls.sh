89c398fdcf9086f96e8a866d34b4e46ce799d29d
Jonathan D Reed <jdreed@mit.edu>
2013-04-11 17:16:46 +0000

    In xsession:   * Don't attempt to remove the lightdm dmrc cache, because we can't.   * Explain why we don't care about the lightdm dmrc cache anyway
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25914 728af825-273c-0410-89f9-f7d3b574a069

--- debathena/debathena/xsession/debian/debathena-nocalls.sh
+++ debathena/debathena/xsession/debian/debathena-nocalls.sh
@@ -7,9 +7,12 @@ if [ "$GDMSESSION" = "000debathena-nocalls" ]; then
 fi
 
 # You don't actually want nocalls as your default session
+# We don't remove it from the lightdm cache because we can't actually
+# read the lightdm cache.  But with our lightdm-greeter, an incorrect
+# value will never get cached anyway (because we always pass a session
+# name, so the cache is not consulted)
 DMRCFILES="$HOME/.dmrc \
-           /var/cache/gdm/$USER/dmrc \
-           /var/cache/lightdm/dmrc/$USER.dmrc"
+           /var/cache/gdm/$USER/dmrc"
 
 for f in $DMRCFILES; do
     [ -e "$f" ] && \
