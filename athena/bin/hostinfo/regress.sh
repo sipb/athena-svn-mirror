bb7bcdccf4798d38fbf3516f683bdf0b3923c73f
Victor Vasiliev <vasilvv@mit.edu>
2013-04-27 20:03:52 +0000

    Fix the regression test script
    git-svn-id: svn+ssh://svn.mit.edu/athena/trunk@25957 728af825-273c-0410-89f9-f7d3b574a069

--- athena/bin/hostinfo/regress.sh
+++ athena/bin/hostinfo/regress.sh
@@ -14,13 +14,13 @@ gather () {
     $1 | sort | tr '\012' ' ' | sed -e 's/ $//'
 }
 
-check "bitsy's host address" 18.72.0.3 "`$SRCDIR/hostinfo.pl -a bitsy 2>&1`"
-check "18.71.0.151's name" STRAWB.MIT.EDU. "`$SRCDIR/hostinfo.pl -h 18.71.0.151 2>&1`"
-check "ls's hostinfo" "\"DEC/VAXSTATION-3100\"/\"UNIX\"" "`$SRCDIR/hostinfo.pl -i ls 2>&1`"
-check "alum's MX record" alum.mit.edu. "`$SRCDIR/hostinfo.pl -m alum.mit.edu 2>&1`"
+check "bitsy's host address" 18.72.0.3 "`$SRCDIR/hostinfo -a bitsy 2>&1`"
+check "18.71.0.151's name" STRAWB.MIT.EDU. "`$SRCDIR/hostinfo -h 18.71.0.151 2>&1`"
+check "ls's hostinfo" "\"DEC/VAXSTATION-3100\"/\"UNIX\"" "`$SRCDIR/hostinfo -i ls 2>&1`"
+check "alum's MX record" alum.mit.edu. "`$SRCDIR/hostinfo -m alum.mit.edu 2>&1`"
 
-athmx=`gather "$SRCDIR/hostinfo.pl -m athena"`
-outmx=`gather "$SRCDIR/hostinfo.pl -m outgoing"`
+athmx=`gather "$SRCDIR/hostinfo -m athena"`
+outmx=`gather "$SRCDIR/hostinfo -m outgoing"`
 check "athena's MX records match outgoing's MX records" "$athmx" "$outmx"
 
 echo "Regression tests passed."
